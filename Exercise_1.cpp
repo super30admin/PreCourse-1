289228f4f3fb24e6a9d3b8abfec91b2c3c57bd8a
GAURAV JOSHI <gsj38@msstate.edu>
2019-06-27 17:26:52 -0500

    Completed Exercise_1 for Precourse 1

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -10,33 +10,50 @@ class Stack {
 public: 
     int a[MAX]; // Maximum size of Stack 
   
-    Stack() { //Constructor here } 
+    Stack() { top = -1;} 
     bool push(int x); 
     int pop(); 
     int peek(); 
     bool isEmpty(); 
+    
 }; 
   
 bool Stack::push(int x) 
 { 
     //Your code here
     //Check Stack overflow as well
+
+    if(top >= (MAX - 1))
+        throw exception("Stack overflow error");
+    else
+    {
+        a[++top] = x;
+        return true;
+    }
+    
 } 
   
 int Stack::pop() 
 { 
-    //Your code here
+    //Your code here 
     //Check Stack Underflow as well 
+
+    if(isEmpty())
+        throw exception("Stack underflow");
+    return a[top-1];
+
 } 
+
 int Stack::peek() 
 { 
-    //Your code here
-    //Check empty condition too
+    if(isEmpty())
+        throw exception("Stack is empty");
+    return a[top];
 } 
   
 bool Stack::isEmpty() 
 { 
-    //Your code here 
+    return (top < 0);
 } 
   
 // Driver program to test above functions 
