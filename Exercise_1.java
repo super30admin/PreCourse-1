b521ee8a685dd0f3e7157791d5314bfc5801bda3
Arivoli AE <aehari2010@gmail.com>
2019-06-28 20:52:18 -0700

    PreCourse_1 Exercise_1

--- Exercise_1.java
+++ Exercise_1.java
@@ -6,28 +6,55 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        return top < 0;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top+1 == MAX){
+            System.out.println(" Stack Overflow");
+          return false;
+        }
+        else {
+          a[top+1] = x;
+          top++;
+          return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(isEmpty()){
+          System.out.print(" Stack Underflow "); 
+          return 0;
+        }
+        else {
+          top--;
+          return a[top+1];
+        }
+
     } 
   
     int peek() 
     { 
-        //Write your code here
+        //Write your code here      
+        if(!isEmpty())
+            return a[top];
+        else{
+            System.out.println(" Stack is Empty ");
+            return 0;
+        }
+        
     } 
 } 
   
@@ -40,5 +67,18 @@ class Main {
         s.push(20); 
         s.push(30); 
         System.out.println(s.pop() + " Popped from stack"); 
+     /*    System.out.println(s.isEmpty()+" isEmpty"); 
+        System.out.println(s.peek()+" Peeked from stack ");
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.peek()+" Peeked from stack "); 
+        System.out.println(s.pop() + " Popped from stack");
+        
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.peek() + " Popped from stack");
+
+        for(int i=0;i<1010;i++){
+            s.push(100);
+        } */
+        
     } 
 }
