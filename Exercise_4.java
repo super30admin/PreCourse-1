762fad645eae66c63d44c98d267b9bbb3ac065bf
Mahima Gupta <mahimagupta1195@gmail.com>
2019-06-29 18:54:58 -0700

    Handling printing  Empty List  case in Singly Linked List

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,59 +1,102 @@
-import java.util.LinkedList; 
-import java.util.Queue; 
-public class GFG { 
-       
-    /* A binary tree node has key, pointer to  
-    left child and a pointer to right child */
+import java.io.*; 
+  
+// Java program to implement 
+// a Singly Linked List 
+ class LinkedList { 
+  
+    Node head; // head of list 
+  
+    // Linked list Node. 
+    // This inner class is made static 
+    // so that main() can access it 
     static class Node { 
-        int key; 
-        Node left, right; 
-          
-        // constructor 
-        Node(int key){ 
-            this.key = key; 
-            left = null; 
-            right = null; 
+  
+        int data; 
+        Node next; 
+  
+        // Constructor 
+        Node(int d) 
+        { 
+            //Write your code here 
+        	this.data = d;
         } 
     } 
-    static Node root; 
-    static Node temp = root; 
-      
-    /* Inorder traversal of a binary tree*/
-    static void inorder(Node temp) 
+  
+    // Method to insert a new node 
+    public static LinkedList insert(LinkedList list, int data) 
     { 
-        if (temp == null) 
-            return; 
-       
-        inorder(temp.left); 
-        System.out.print(temp.key+" "); 
-        inorder(temp.right); 
-    } 
-       
-    /*function to insert element in binary tree */
-    static void insert(Node temp, int key) 
-    { 
+        // Create a new node with given data 
+   
+        // If the Linked List is empty, 
+        // then make the new node as head 
+        
+            // Else traverse till the last node 
+            // and insert the new_node there 
 
-        // Do level order traversal until we find 
-        // an empty place and add the node.  
+            // Insert the new_node at last node 
+        // Return the list by head 
+    	if(list.head==null)
+    	{
+    		list.head = new Node(data);
+    	}
+    	else
+    	{
+    		Node temp = list.head;
+    		
+    		while(temp.next!=null)
+    		{
+    			temp = temp.next;
+    		}
+    		
+    		temp.next = new Node(data);	
+    		
+    	}
+    	return list;
+        
     } 
+  
+    // Method to print the LinkedList. 
+    public static void printList(LinkedList list) 
+    {  
+        // Traverse through the LinkedList 
+   
+            // Print the data at current node 
        
+            // Go to next node 
+    	
+    	int count = 0;
+    	Node temp = list.head;
+    	
+    	if(temp==null)
+    	{
+    		System.out.println("No elements in the list");
+    		
+    	}
+    	while(temp!=null)
+    	{ 
+    		System.out.println("List data at loaction "+ ++count +" "+temp.data);
+    		temp = temp.next;
+    	}
+    } 
+   
     // Driver code 
-    public static void main(String args[]) 
+    public static void main(String[] args) 
     { 
-        root = new Node(10); 
-        root.left = new Node(11); 
-        root.left.left = new Node(7); 
-        root.right = new Node(9); 
-        root.right.left = new Node(15); 
-        root.right.right = new Node(8); 
-       
-        System.out.print( "Inorder traversal before insertion:"); 
-        inorder(root); 
-       
-        int key = 12; 
-        insert(root, key); 
-       
-        System.out.print("\nInorder traversal after insertion:"); 
-        inorder(root); 
+        /* Start with the empty list. */
+        LinkedList list = new LinkedList(); 
+  
+        // 
+        // ******INSERTION****** 
+        // 
+        printList(list); 
+        // Insert the values 
+        list = insert(list, 1); 
+        list = insert(list, 2); 
+        list = insert(list, 3); 
+        list = insert(list, 33); 
+        list = insert(list, 5); 
+  
+        // Print the LinkedList 
+        printList(list); 
     } 
 }
