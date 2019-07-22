dcc91d174f726f2b6c22f0334139d20e84b760da
Sanjay Nag <sanjaynag.bangaloreravishankar@sjsu.edu>
2019-07-22 06:36:46 -0700

    Pre Course 1 - Excercise 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,29 +5,50 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        if(top < 0) {
+            return true;
+        }
+        return false;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top >= MAX) {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        a[++top] = x;
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top < 0) {
+            System.out.println("Stack Underflow");
+            return -1;
+        }
+        return a[top--];
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(this.isEmpty()) {
+            System.out.println("Stack empty");
+            return -1;
+        }
+        return a[top];
     } 
 } 
   
