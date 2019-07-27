4408bfc63ad143c72a7b73b5b48a4fda7b1af6d2
Manogna Mujje <manogna.mujje@gmail.com>
2019-07-27 16:15:23 -0700

    Completed Exercise_1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,38 +1,64 @@
-class Stack { 
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
+package Stack;
+
+public class Stack { 
+	static final int MAX = 1000; 
+    int top; //Index of the top element
+ // Declaring an array of size MAX. An int array declaration in Java initializes all the elements of the array to 0.
+    int a[] = new int[MAX]; 
   
     boolean isEmpty() 
     { 
         //Write your code here 
+    		if (top == -1) {
+    			return true;
+    		}
+    		return false;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+    		// Since all the elements of an array are initialized to 0, top is set to -1 at the beginning.
+    		top = -1; 
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+    		if(top >= MAX-1) {
+    			System.out.println("Stack Overflow");
+    			return false;
+    		} 
+    		top++;
+    		a[top] = x;
+    		System.out.println(x+ " pushed to stack");
+		return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+    		if(top < 0) {
+    			System.out.println("Stack Underflow");
+    			return 0;
+    		}
+    		int x = a[top];
+    		a[top] = 0;
+    		top--;
+    		return x;
     } 
   
     int peek() 
     { 
-        //Write your code here
+        if(top < 0) {
+        		System.out.println("Stack Underflow"); 
+            return 0;
+        } 
+        return a[top];
+        
     } 
-} 
-  
-// Driver code 
-class Main { 
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
@@ -41,4 +67,4 @@ class Main {
         s.push(30); 
         System.out.println(s.pop() + " Popped from stack"); 
     } 
-}
+} 
