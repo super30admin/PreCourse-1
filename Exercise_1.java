8de99be8418ff73e039bdc5c8bae0d6ed90fe801
Ankita Patil <ankita10patil@gmail.com>
2019-07-29 15:55:45 -0700

    Completed Precourse1 Exercise1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 class Stack { 
     static final int MAX = 1000; 
     int top; 
@@ -6,28 +9,54 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+    		if (top < 0){
+    			return true;
+    		}
+    		return false;	
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+    		top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+    		if(top >= MAX -1) {
+    			System.out.println("The Stack is FULL.");
+    			return false;
+    		}else {	
+    			a[++top] = x;	
+    			return true;
+    		}
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+    		if(top < 0){
+    			System.out.println("The Stack is empty");
+    			return 0;
+    		}else {
+    			int x = a[top--];
+    			return x;
+    		}
+    		
     } 
   
     int peek() 
     { 
         //Write your code here
+    		if(top < 0) {
+    			System.out.println("The Stack is empty");
+    			return 0;
+    		}else {
+    			return top;
+    		}
     } 
 } 
   
