871780df213a1a1a267e3eb0dff934b193a5d734
Rakshitha94 <lalitharr@gmail.com>
2019-07-28 22:42:15 -0700

    Stack using linkedlist

--- Exercise_2.py
+++ Exercise_2.py
@@ -5,26 +5,40 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.head=None
+
     def push(self, data):
+        if self.head is None:
+            self.head=Node(data)
+        else:
+            newnode=Node(data)
+            newnode.next=self.head
+            self.head=newnode
+            
         
     def pop(self):
-        
-a_stack = Stack()
-while True:
-    print('push <value>')
-    print('pop')
-    print('quit')
-    do = input('What would you like to do? ').split()
+        if self.head is None:
+            return None
+        temp=self.head.data
+        self.head= self.head.next
+        return temp
 
-    operation = do[0].strip().lower()
-    if operation == 'push':
-        a_stack.push(int(do[1]))
-    elif operation == 'pop':
-        popped = a_stack.pop()
-        if popped is None:
-            print('Stack is empty.')
-        else:
-            print('Popped value: ', int(popped))
-    elif operation == 'quit':
-        break
+if __name__ == '__main__':
+    a_stack = Stack()
+    while True:
+        print('push <value>')
+        print('pop')
+        print('quit')
+        do = input('What would you like to do? ').split()
+
+        operation = do[0].strip().lower()
+        if operation == 'push':
+            a_stack.push(int(do[1]))
+        elif operation == 'pop':
+            popped = a_stack.pop()
+            if popped is None:
+                print('Stack is empty.')
+            else:
+                print('Popped value: ', int(popped))
+        elif operation == 'quit':
+            break
