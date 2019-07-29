a16f98b63e0254493301ad1b5160004addaa30ed
Rakshitha94 <lalitharr@gmail.com>
2019-07-28 22:09:17 -0700

    completed stack using arrays

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,40 @@
-class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-          print("HELLO")
 
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+def createStack():
+    stack=[]
+    return stack
 
-s = myStack()
-s.push('1')
-s.push('2')
-print(s.pop())
-print(s.show())
+def isEmpty(stack):
+    return len(stack)==0
+
+def push(stack,item):
+    stack.append(item)
+    print(item + " pushed")
+
+def pop(stack):
+    if(isEmpty(stack)):
+        return -1
+    return stack.pop()
+
+def peek(stack):
+    if isEmpty(stack):
+        return - 1
+    return stack[len(stack)-1]
+
+
+if __name__== "__main__":
+
+    stack1 = createStack()
+    print(isEmpty(stack1))
+    push(stack1, str(10))
+    push(stack1, str(20))
+    print(isEmpty(stack1))
+    print(peek(stack1)+"item at top")
+    push(stack1, str(30))
+    push(stack1, str(200))
+    print(peek(stack1))
+    print(pop(stack1) + " popped from stack")
+    print(pop(stack1) + " popped from stack")
+    print(pop(stack1) + " popped from stack")
+    print(pop(stack1) + " popped from stack")
+    print(isEmpty(stack1))
+
