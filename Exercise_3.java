8fb42277f518fe011b6c23e0bb0f89646e911071
pradhanyash <yash_pradhan@hotmail.com>
2019-08-19 00:52:03 -0500

    implemented singly linked list

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,70 +1,73 @@
-import java.io.*; 
-  
 // Java program to implement 
 // a Singly Linked List 
-class LinkedList { 
-  
-    Node head; // head of list 
-  
-    // Linked list Node. 
-    // This inner class is made static 
-    // so that main() can access it 
-    static class Node { 
-  
-        int data; 
-        Node next; 
-  
-        // Constructor 
-        Node(int d) 
-        { 
-            //Write your code here 
-        } 
-    } 
-  
-    // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
-    { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
+class LinkedList {
 
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
-    } 
-  
-    // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
-    {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
-    } 
-   
-    // Driver code 
-    public static void main(String[] args) 
-    { 
+    Node head; // head of list
+
+    // Linked list Node.
+    // This inner class is made static
+    // so that main() can access it
+    static class Node {
+        int data;
+        Node next;
+
+        // Constructor
+        Node(int d) {
+            // Write your code here
+            this.data = d;
+        }
+    }
+
+    // Method to insert a new node
+    public static LinkedList insert(LinkedList list, int data) {
+        // Create a new node with given data
+        Node node = new Node(data);
+        // If the Linked List is empty,
+        // then make the new node as head
+        if (list.head == null) {
+            list.head = node;
+        } else {
+            // Else traverse till the last node
+            // and insert the new_node there
+            Node cursor = list.head;
+            while (cursor.next != null) {
+                cursor = cursor.next;
+            }
+            // Insert the new_node at last node
+            cursor.next = node;
+        }
+        // Return the list by head
+        return list;
+    }
+
+    // Method to print the LinkedList.
+    public static void printList(LinkedList list) {
+        Node cursor = list.head;
+        // Traverse through the LinkedList
+        while (cursor != null) {
+            // Print the data at current node
+            System.out.print(cursor.data + " ");
+            // Go to next node
+            cursor = cursor.next;
+        }
+    }
+
+    // Driver code
+    public static void main(String[] args) {
         /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
-  
-        // 
-        // ******INSERTION****** 
-        // 
-  
-        // Insert the values 
-        list = insert(list, 1); 
-        list = insert(list, 2); 
-        list = insert(list, 3); 
-        list = insert(list, 4); 
-        list = insert(list, 5); 
-  
-        // Print the LinkedList 
-        printList(list); 
-    } 
+        LinkedList list = new LinkedList();
+        //
+        // ******INSERTION******
+        //
+
+        // Insert the values
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+
+        // Print the LinkedList
+        printList(list);
+    }
 }
