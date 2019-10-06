206fadeffe26c4ee913b64719d3fb85b88b1bd27
Shilpa Catherine <shilpa.j.catherine@gmail.com>
2019-10-06 17:09:33 -0500

    Completed exercise 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,29 +5,59 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        if(top==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top=0;
+       
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top==MAX)
+        {
+            return false;
+        }
+        else
+        {
+            a[++top]=x;
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        int popvalue=0;
+        if(top<=0)
+        {
+            System.out.println("Stack Underflow");
+            return popvalue;
+        }else
+        {
+            popvalue=a[top];
+            top=top-1;
+            return popvalue;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        return a[top];
     } 
 } 
   
