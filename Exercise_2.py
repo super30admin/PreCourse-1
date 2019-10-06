95d8c487c24926e099d2288f8891f4f7a9b7ba9c
sindu819 <sindu819@gmail.com>
2019-10-06 13:07:07 -0700

    Completed Precourse_1 module

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,14 +1,27 @@
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
+        self.data = data
+        self.next = None
  
-class Stack:
+ class Stack:
     def __init__(self):
-        
+        self.head=None
+    
     def push(self, data):
+        if self.head is None:
+            self.head=Node(data)
+        else:
+            temp=Node(data)
+            temp.next=self.head
+            self.head=temp
         
     def pop(self):
+        if self.head is None:
+            return None
+        else:
+            popped=self.head.data
+            self.head=self.head.next
+            return popped
         
 a_stack = Stack()
 while True:
