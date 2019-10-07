31710b3a9ab201cea9bf60ce9b5a24921f47bb90
08vishal <vishal205014@gmail.com>
2019-10-07 09:11:49 -0500

    Add files via upload

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,24 @@
 class myStack:
      def __init__(self):
-         
+          self.items=[]
      def isEmpty(self):
-         
+         return self.items==[]
      def push(self, item):
-         
+         self.items.append(item)
      def pop(self):
-        
-        
+          return self.items.pop()
      def peek(self):
-        
+          return self.items[len(self.items)-1]
      def size(self):
-         
+          return len(self.items)    
      def show(self):
-         
+          for i in self.items:
+               print(i)
 
 s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
+print(s.peek())
 print(s.show())
+
--- Exercise_2.py
+++ Exercise_2.py
@@ -5,26 +5,40 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head=None
         
     def push(self, data):
-        
+        if self.head==None:
+            self.head=Node(data)
+        else:
+            newnode=Node(data)
+            newnode.next=self.head
+            self.head=newnode
+
     def pop(self):
+        if self.head==None:
+            return None
+        else:
+            poped=self.head
+            self.head=self.head.next
+            return poped.data
         
-a_stack = Stack()
+o = Stack()
+o.push(3)
 while True:
     print('push <value>')
     print('pop')
-    print('quit')
-    do = input('What would you like to do? ').split()
- 
-    operation = do[0].strip().lower()
+    print('exit')
+    choice = str(raw_input('enter choice ').split())
+    operation = choice[0].lower()
     if operation == 'push':
-        a_stack.push(int(do[1]))
+        o.push(int(choice[1]))
+        print("pushed success")
     elif operation == 'pop':
-        popped = a_stack.pop()
-        if popped is None:
+        poped = o.pop()
+        if poped is None:
             print('Stack is empty.')
         else:
-            print('Popped value: ', int(popped))
-    elif operation == 'quit':
+            print('Poped value->', int(poped))
+    elif operation == 'exit':
         break
