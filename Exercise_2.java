4916171a6b60e512983fb58979de724d98ee96b7
Neha Prasad <nehasprasad@usc-guestwireless-upc-newsc17277.usc.edu>
2019-10-09 10:26:43 -0700

    Completed Sprint Precourse1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,52 +1,68 @@
-public class StackAsLinkedList { 
-  
-    StackNode root; 
-  
-    static class StackNode { 
-        int data; 
-        StackNode next; 
-  
-        StackNode(int data) 
-        { 
-            //Constructor here 
-        } 
-    } 
-    
-	
-    public boolean isEmpty() 
-    { 
-        //Write your code here for the condition if stack is empty. 
-    } 
-  
-    public void push(int data) 
-    { 
-        //Write code to push data to the stack. 
-    } 
-  
-    public int pop() 
-    { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
-    } 
-  
-    public int peek() 
-    { 
-        //Write code to just return the topmost element without removing it.
-    } 
-  
-	//Driver code
-    public static void main(String[] args) 
-    { 
-  
-        StackAsLinkedList sll = new StackAsLinkedList(); 
-  
-        sll.push(10); 
-        sll.push(20); 
-        sll.push(30); 
-  
-        System.out.println(sll.pop() + " popped from stack"); 
-  
-        System.out.println("Top element is " + sll.peek()); 
-    } 
-} 
+//Time complexity of all the operations push(), pop(), peek() and isEmpty() is O(1)
+// Space complexity is O(n) -- n is the number of elements in the stack
+
+public class StackAsLinkedList {
+
+    StackNode root;
+
+    static class StackNode{
+        int data;
+        StackNode next;
+
+        StackNode(int data){
+            this.data = data;
+            next = null;
+        }
+    }
+
+    boolean isEmpty(){
+        return root==null;
+    }
+
+    void push(int data){
+        StackNode node = new StackNode(data);
+
+        if(root == null)
+            root = node;
+        else{
+            StackNode temp = root;
+            root = node;
+            node.next = temp;
+        }
+    }
+
+    int pop(){
+        if(root==null){
+            System.out.println("Stack Underfloe");
+            return Integer.MIN_VALUE;
+        }
+        else{
+            int x = root.data;
+            root = root.next;
+            return x;
+        }
+    }
+
+    int peek(){
+        if(root==null)
+            return Integer.MIN_VALUE;
+        else{
+            return root.data;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        StackAsLinkedList sll = new StackAsLinkedList();
+
+        sll.push(10);
+        sll.push(20);
+        sll.push(30);
+
+        System.out.println(sll.pop() + " popped from stack");
+
+        System.out.println("Top element is " + sll.peek());
+
+
+    }
+}
