24e67f18b75f4a97885a4d2003523d079e450798
rutulusc <43349933+rutulusc@users.noreply.github.com>
2019-11-15 18:35:33 -0800

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,27 @@
 class myStack:
      def __init__(self):
+          self.container=[]
          
      def isEmpty(self):
+          return self.size() == 0
          
      def push(self, item):
+          self.container.append(item)
          
      def pop(self):
-        
+        return self.container.pop()
         
      def peek(self):
+          if self.isEmpty():
+               raise Exception("Stack Empty !")
+          return self.container[-1]                    #-1 represents last element of the index
+          
         
      def size(self):
+          return len(self.container)
          
      def show(self):
+          return self.container
          
 
 s = myStack()
