7eac8db416542debcffb2907e46334f84ee3d5ef
Akhil Raveendran <itsrakhil163@gmail.com>
2019-11-17 01:06:43 -0800

    Update Exercise_3.java

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,70 +1,68 @@
-import java.io.*; 
-  
-// Java program to implement 
-// a Singly Linked List 
-public class LinkedList { 
-  
-    Node head; // head of list 
-  
-    // Linked list Node. 
-    // This inner class is made static 
-    // so that main() can access it 
-    static class Node { 
-  
-        int data; 
-        Node next; 
-  
-        // Constructor 
-        Node(int d) 
-        { 
-            //Write your code here 
-        } 
-    } 
-  
-    // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
-    { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
+Time Complexity-Insertion-O(n)
+Space Complexity-O(n)
 
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
-    } 
-  
-    // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
-    {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
-    } 
-   
-    // Driver code 
-    public static void main(String[] args) 
-    { 
-        /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
-  
-        // 
-        // ******INSERTION****** 
-        // 
-  
-        // Insert the values 
-        list = insert(list, 1); 
-        list = insert(list, 2); 
-        list = insert(list, 3); 
-        list = insert(list, 4); 
-        list = insert(list, 5); 
-  
-        // Print the LinkedList 
-        printList(list); 
-    } 
-}
+package com.company;
+
+public class LinkedList {
+    Node head;
+    static class Node {
+        int key;
+        Node next;
+
+        Node(int d) {
+            this.key = d;
+            this.next = null;
+
+        }
+    }
+
+        LinkedList(){
+            this.head=null;
+        }
+        public static LinkedList insert(LinkedList list, int data)
+        {
+            Node temp=new Node(data);
+            if(list.head==null)
+                list.head=temp;
+            else {
+                Node curr = list.head;
+                while (curr.next != null) {
+                    curr = curr.next;
+                }
+                curr.next = temp;
+            }
+
+            return list;
+        }
+
+
+        public static void printList(LinkedList list)
+        {
+           Node curr= list.head;
+           while(curr!=null)
+           {
+               System.out.println(curr.key);
+               curr=curr.next;
+           }
+        }
+
+
+        public static void main(String[] args)
+        {
+            LinkedList list = new LinkedList();
+
+            // Insert the values
+            list = insert(list, 331);
+            list = insert(list, 212);
+            list = insert(list, 312);
+            list = insert(list, 423);
+            list = insert(list, 53);
+            list = insert(list, 3142);
+            list = insert(list, 4253);
+            list = insert(list, 532);
+
+            // Print the LinkedList
+            printList(list);
+        }
+
+    }
