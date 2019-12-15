2d49fc2c7592451f862cc71d763204fb981a1bc7
Kamalesh <kamalesh.anantha@gmail.com>
2019-12-15 13:22:06 -0600

    Added solution for Exercise 1
    Added implementation for myStack class

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,39 @@
 class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def show(self):
+        return self.items
+
 
 s = myStack()
 s.push('1')
+print(s.show())
 s.push('2')
+print(s.show())
 print(s.pop())
 print(s.show())
+s.push('3')
+print(s.show())
+print(s.peek())
+print(s.show())
+print(s.pop())
+print(s.show())
+print(s.isEmpty())
+print(s.pop())
+print(s.isEmpty())
