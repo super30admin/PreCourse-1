f756becc80c4f74029038292aba5b7a3d93dc94d
Vaishnavi Ramesh <vramesh@pulsesecure.net>
2019-12-23 16:12:11 -0800

    Implemented Stack using Array

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,32 +1,59 @@
 class Stack { 
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
+
+    static final int MAX = 1000;
+
+    int top;
+    int a[]; // Maximum size of Stack
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+
+        if(top==-1)
+            return true;
+
+        return false;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        a = new int[MAX];
+
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top+1 >= MAX)
+            return false;
+
+        a[++top] = x;
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+
+        if(top==-1){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return a[top--];
     } 
   
     int peek() 
-    { 
+    {
+        if(top > -1){
+            return a[top];
+        }
+        else{
+            System.out.println("Stack is Empty.Nothing to peek");
+            return 0;
+        }
         //Write your code here
     } 
 } 
@@ -35,10 +62,27 @@ class Stack {
 class Main { 
     public static void main(String args[]) 
     { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        Stack s = new Stack();
+
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+
+        System.out.println("Is stack empty ?"+s.isEmpty());
+
+        System.out.println(s.peek() + " Peeked from stack");
+        System.out.println(s.pop() + " Popped from stack");
+
+        System.out.println(s.peek() + " Peeked from stack");
+        System.out.println(s.pop() + " Popped from stack");
+
+        System.out.println(s.peek() + " Peeked from stack");
+        System.out.println(s.pop() + " Popped from stack");
+
+        System.out.println(s.peek() + " Peeked from stack");
+        System.out.println(s.pop() + " Popped from stack");
+
+        System.out.println("Is stack empty ?"+s.isEmpty());
     } 
 }
