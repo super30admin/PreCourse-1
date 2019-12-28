87636160b7320a9421588f7019deab05b6853a46
Venkata09578 <50789204+Venkata09578@users.noreply.github.com>
2019-12-27 22:43:33 -0800

    Update Exercise_4.py

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,3 +1,11 @@
+// Time Complexity : inorder() - O(n), insert() - O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : None
+
+
+// Your code here along with comments explaining your approach
+
 # Python program to insert element in binary tree  
 class newNode():  
   
@@ -7,16 +15,35 @@ class newNode():
         self.right = None
           
 """ Inorder traversal of a binary tree"""
-def inorder(temp): 
+def inorder(temp):
+    if (not temp):
+        return
+    inorder(temp.left)
+    print(temp.key)
+    inorder(temp.right)
   
     
   
   
 """function to insert element in binary tree """
 def insert(temp,key): 
-  
+    que = []
+    que.append(temp)
+    while(len(que)):
+        temp = que[0]
+        que.pop(0)
     
+        if (not temp.left):
+            temp.left = newNode(key)  
+            break
+        else:
+            que.append(temp.left)  
   
+        if (not temp.right):
+            temp.right = newNode(key)  
+            break
+        else:
+            que.append(temp.right)  
        
 # Driver code  
 if __name__ == '__main__': 
