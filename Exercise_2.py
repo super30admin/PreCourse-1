81889ab845325378662fb3908e166eee4e646ef3
Venkata09578 <50789204+Venkata09578@users.noreply.github.com>
2019-12-27 18:48:18 -0800

    Update Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,11 @@
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
 class Node:
     def __init__(self, data):
        self.data = data
