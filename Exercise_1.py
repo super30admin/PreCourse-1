70e6a03006573d25b974fe7c251364d61ff1d3ee
Sayan Guha <sayan@Sayans-MacBook-Pro.local>
2020-01-28 20:30:07 -0600

    Complete precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,56 @@
 class myStack:
      def __init__(self):
-         
+          self.l = []
+
      def isEmpty(self):
-         
+          '''
+          Check if length is 0 and return True or False accordingly
+          '''
+          return len(self.l) == 0
+
      def push(self, item):
-         
+          '''
+          Add item to stack
+          '''
+
+          self.l.append(item)
+
      def pop(self):
-        
+          '''
+           Check if stack is empty and print Underflow else pop the last item
+          '''
+
+          if self.isEmpty():
+               print("Stack Underflow")
+               return 0
+          else:
+               return self.l.pop()
         
      def peek(self):
-        
+          '''
+          Check if Stack is empty and print Stack Empty else
+          return the last item without removing it from stack
+          '''
+
+          if self.isEmpty():
+               print("Stack Empty")
+               return -1
+          else:
+               return self.l[-1]
+
      def size(self):
-         
+          '''
+          Return the size of the array
+          '''
+
+          return len(self.l)
+
      def show(self):
+          '''
+          Return the complete array
+          '''
+
+          return self.l
          
 
 s = myStack()
--- Exercise_4.py
+++ Exercise_4.py
@@ -7,16 +7,57 @@ class newNode():
         self.right = None
           
 """ Inorder traversal of a binary tree"""
-def inorder(temp): 
-  
-    
-  
+def inorder(temp):
+    '''
+    Iterate to the left of the root
+    print value and then iterate to
+    the right.
+    Complexity: O(n)
+    '''
+    if temp:
+        inorder(temp.left)
+        print(temp.key)
+        inorder(temp.right)
+    else:
+        print("No Nodes to traverse")
+
   
 """function to insert element in binary tree """
-def insert(temp,key): 
-  
-    
-  
+def insert(temp,key):
+    '''
+    Check if temp is None, then make this the root
+    else create a Queue, append the root into it.
+    We then remove the first element in the
+    queue; if the root.left is empty we create a
+    new node and assign our key to this else
+    we traverse further to the root.left.
+    Similarly we check if root.right is empty,
+    if it is we create a node and assign it
+    here else we append the root.right into the
+    queue and iteratively do this until we either
+    find a left or right which is empty.
+    Complexity: O(n)
+    '''
+    if temp is None:
+        temp = newNode(key)
+    queue = []
+    queue.append(temp)
+
+    while len(queue):
+        temp = queue[0]
+        queue.pop(0)
+        if not temp.left:
+            temp.left = newNode(key)
+            break
+        else:
+            queue.append(temp.left)
+
+        if not temp.right:
+            temp.right = newNode(key)
+            break
+        else:
+            queue.append(temp.right)
+
        
 # Driver code  
 if __name__ == '__main__': 
@@ -27,12 +68,12 @@ if __name__ == '__main__':
     root.right.left = newNode(15)  
     root.right.right = newNode(8)  
   
-    print("Inorder traversal before insertion:", end = " ") 
+    print("Inorder traversal before insertion:")
     inorder(root)  
   
     key = 12
     insert(root, key)  
   
     print()  
-    print("Inorder traversal after insertion:", end = " ") 
+    print("Inorder traversal after insertion:")
     inorder(root) 
