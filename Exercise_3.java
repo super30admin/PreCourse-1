07a4794317b0f50d8ab00625ff21bfb00563c3d6
Vinay Potla <vinaypotla777@gmail.com>
2020-01-29 23:09:13 -0600

    Exercise_3 done

--- Exercise_3.java
+++ Exercise_3.java
@@ -2,7 +2,7 @@ import java.io.*;
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList {
   
     Node head; // head of list 
   
@@ -17,34 +17,54 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            //Write your code here
+            data = d;
+            next = null;
         } 
     } 
   
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
     { 
-        // Create a new node with given data 
+        // Create a new node with given data
+        Node newNode = new Node(data);
+        newNode.next = null;
    
         // If the Linked List is empty, 
-        // then make the new node as head 
+        // then make the new node as head
+        if(list.head == null)
+            list.head = newNode;
         
             // Else traverse till the last node 
-            // and insert the new_node there 
+            // and insert the new_node there
+        else{
+            Node temp = list.head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
 
             // Insert the new_node at last node 
-        // Return the list by head 
+        // Return the list by head
+        return list;
         
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
-    {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+    {
+        Node temp = list.head;
+        if(temp == null)
+            return;
+        // Traverse through the LinkedList
+        while(temp != null){
+            // Print the data at current node
+            System.out.println(temp.data + " ");
+            // Go to next node
+            temp = temp.next;
+
+        }
     } 
    
     // Driver code 
