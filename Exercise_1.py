869e77785124332d27de1a68c1ea36e5b8471c24
Arnav Shah <arnav1996@gmail.com>
2020-01-29 22:40:04 -0800

    Completed exercises 1 and 2

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,26 @@
+# Time complexity:O(1)
+# Space complexity: 0(n)
+# Did code run successfully on leetcode: yes
+# Any problem you faced: No
+
+
+
 class myStack:
      def __init__(self):
-         
+         self.ini = []
      def isEmpty(self):
-         
+         return self.ini == []
      def push(self, item):
-         
+         self.ini.append(item)
      def pop(self):
-        
-        
+         self.ini.pop()
+
      def peek(self):
-        
+         return self.ini[len(self.ini) - 1]
      def size(self):
-         
+         return len(self.ini)
      def show(self):
-         
+         return self.ini
 
 s = myStack()
 s.push('1')
