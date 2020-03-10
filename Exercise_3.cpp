3b9bfbf860577fe3322cd9d5c6eb65b4fdfc8f7b
Ayesha Siddiqua <asiddiqua004@gmail.com>
2020-03-10 15:05:20 -0700

    precourse 1 MR from ayesha

--- Exercise_3.cpp
+++ Exercise_3.cpp
@@ -1,80 +1,151 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-  
-// A linked list node (changes) 
-class Node  
-{  
-    public: 
-    int data;  
-    Node *next;  
-};  
-  
-/* Given a reference (pointer to pointer) 
-to the head of a list and an int, inserts 
-a new node on the front of the list. */
-void push(Node** head_ref, int new_data)  
-{  
-    /* 1. allocate node */ 
-  
-    /* 2. put in the data */  
-  
-    /* 3. Make next of new node as head */ 
-  
-    /* 4. move the head to point to the new node */
-}  
-  
-/* Given a node prev_node, insert a new node after the given  
-prev_node */
-void insertAfter(Node* prev_node, int new_data)  
-{  
-    /*1. check if the given prev_node is NULL */ 
-  
-    /* 2. allocate new node */ 
-  
-    /* 3. put in the data */ 
-  
-    /* 4. Make next of new node as next of prev_node */
-  
-    /* 5. move the next of prev_node as new_node */ 
-}  
-  
-/* Given a reference (pointer to pointer) to the head  
-of a list and an int, appends a new node at the end */
-void append(Node** head_ref, int new_data)  
-{  
-    /* 1. allocate node */ 
-  
-    /* 2. put in the data */ 
-  
-    /* 3. This new node is going to be  
-    the last node, so make next of  
-    it as NULL*/  
-  
-    /* 4. If the Linked List is empty, 
-    then make the new node as head */
-  
-    /* 5. Else traverse till the last node */
-  
-    /* 6. Change the next of last node */ 
-}  
-  
-// This function prints contents of 
-// linked list starting from head  
-void printList(Node *node)  
-{  
-    //Your code here
-}  
-  
-/* Driver code*/
-int main()  
-{  
-    Node* head = NULL;   
-    append(&head, 6);  
-    push(&head, 7);  
-    push(&head, 1);    
-    append(&head, 4);    
-    insertAfter(head->next, 8);  
-    cout<<"Created Linked list is: ";  
-    printList(head);  
-    return 0;  
-}  
+// Time Complexity :
+//Addition -: O(n)
+//Deletion -: O(n)
+//Insetion-O(n)
+// Space Complexity :
+//O(n)
+// Did this code successfully run on Leetcode : N/A
+// Any problem you faced while coding this : Deferencing the pointer to a pointer.
+
+
+// Your code here along with comments explaining your approach
+
+//Singly linked list is implemented using linked list
+//Operation : add_at_end, insertion, deletion, displaying, count_of_numbers_variables
+
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+struct node_s {
+    T data;
+    node_s * link;
+};
+
+template <typename T>
+void list_add_at_end(node_s<T> **monitored_stack_head, T value){
+    //create new node_s
+    node_s<T> * new_node = new(node_s<T>);
+    new_node->data = value;
+    new_node->link =NULL;
+
+    //edge case
+    if (*monitored_stack_head==NULL){
+        *monitored_stack_head = new_node;
+        return;
+    }
+    node_s<T> * temp = *monitored_stack_head;
+    while(temp->link!=NULL){
+        temp=temp->link;
+    }
+    //Element is added
+    temp->link = new_node;
+}
+
+
+template <typename T>
+void list_insert(node_s<T> **monitored_stack_head, T value, int index){
+    //create new node_s
+    node_s<T> * new_node = new(node_s<T>);
+    new_node->data = value;
+    new_node->link =NULL;
+   
+    //edge case
+    if (index==1 ){
+        if (*monitored_stack_head==NULL){
+           *monitored_stack_head = new_node;
+        }
+        else{
+            new_node->link = *monitored_stack_head;
+            *monitored_stack_head  = new_node;
+        }
+        return;
+    }
+    
+    
+    node_s<T> * temp = *monitored_stack_head;
+    while(temp->link!=NULL && index>2){
+        temp=temp->link;
+        index--;
+    }
+    //Element is added
+    new_node->link = temp->link;
+    temp->link = new_node;
+}
+
+
+template <typename T>
+void list_delete(node_s<T> **monitored_stack_head,  int index){
+     node_s<T> * temp = *monitored_stack_head;
+    //edge case
+    if (index==1 ){
+        if (*monitored_stack_head==NULL);
+        else{
+            *monitored_stack_head  = temp->link;
+            delete(temp);
+        }
+        return;
+    }
+   
+    while(temp->link!=NULL && index>2){
+        temp=temp->link;
+        index--;
+    }
+    
+    node_s<T> *prev= temp;
+    
+    //Delete is added
+    if (temp->link!=NULL)
+        temp = temp->link;
+    else
+        return;
+    
+     if (temp->link!=NULL)
+        prev->link = temp->link;
+     delete(temp);
+     return;
+}
+
+template <typename T>
+void list_display(node_s<T> **head){
+    node_s<T> * temp = *head;
+    while(temp!=NULL){
+        cout<<temp->data<<"\t";
+        temp=temp->link;
+    }
+}
+
+
+
+int main(){
+    
+
+    
+    node_s<int> * head = NULL;
+    
+    bool is_func_implemented =0;
+    
+    list_insert(&head, 61, 1);
+    list_add_at_end(&head, 88);
+    list_add_at_end(&head, 71);
+    list_insert(&head, 60, 1);
+    list_insert(&head, 64, 4);
+    list_display(&head);
+    cout<<"\n";
+    list_delete(&head,  6);
+    list_display(&head);
+    cout<<"\n";
+    list_delete(&head,  1);
+    list_display(&head);
+    cout<<"\n";
+    list_insert(&head, 63, 3);
+    list_display(&head);
+    cout<<"\n";
+
+
+    cout <<is_func_implemented<<endl;
+    
+
+    return 0;
+}
--- Exercise_4.cpp
+++ Exercise_4.cpp
@@ -1,62 +1,78 @@
-#include <iostream> 
-#include <queue> 
-using namespace std; 
-//new one
-  
-/* A binary tree node has key, pointer to left child 
-and a pointer to right child */
-struct Node { 
-    int key; 
-    struct Node* left, *right; 
-}; 
-  
-/* function to create a new node of tree and r 
-   eturns pointer */
-struct Node* newNode(int key) 
-{ 
-    //Your code here
-}; 
-  
-/* Inorder traversal of a binary tree*/
-void inorder(struct Node* temp) 
-{ 
-    if (!temp) 
-        return; 
-  
-    inorder(temp->left); 
-    cout << temp->key << " "; 
-    inorder(temp->right); 
-} 
-  
-/*function to insert element in binary tree */
-void insert(struct Node* temp, int key) 
-{ 
-  
-    // Do level order traversal until we find 
-    // an empty place.  
+ Time Complexity :
+//Insetion -: Average : O(logn), Worst : O(n)
+
+// Space Complexity :
+//O(1)
+// Did this code successfully run on Leetcode : N/A
+// Any problem you faced while coding this : Deferencing the pointer to a pointer.
+
+
+// Your code here along with comments explaining your approach
+
+//Insertion of an element in Binary Search Tree
+
+
+#include <iostream>
+
+using namespace std;
+
+
+template <typename T>
+struct BST_node{
+    T data;
+    BST_node * left;
+    BST_node * right;
+};
+
+template <typename T>
+BST_node<T>* create_new_node(BST_node<T>* root, T data){
+    root = new BST_node<T>;
+    root->data = data;
+    root->left =NULL;
+    root->right =NULL;
+    return root;
+}
+
+template <typename T>
+BST_node<T>* insert_new_node(BST_node<T>* root, T data){
+    if (root ==NULL){
+        root = create_new_node(root, data);
+    }
+    else if(data <= root->data){
+        root->left = insert_new_node(root->left, data);
+    }
+    else{
+         root->right = insert_new_node(root->right, data);
+    }
+     return root;
+}
+
+template <typename T>
+void display_BST(BST_node<T>* root){
     
-    //Your code here
-} 
-  
-// Driver code 
-int main() 
-{ 
-    struct Node* root = newNode(10); 
-    root->left = newNode(11); 
-    root->left->left = newNode(7); 
-    root->right = newNode(9); 
-    root->right->left = newNode(15); 
-    root->right->right = newNode(8); 
-  
-    cout << "Inorder traversal before insertion:"; 
-    inorder(root); 
-  
-    int key = 12; 
-    insert(root, key); 
-  
-    cout << endl; 
-    cout << "Inorder traversal after insertion:"; 
-    inorder(root); 
-  
-    return 0; 
-} 
+    if (root==NULL){
+        return;
+    }
+    if (root->left !=NULL){
+        display_BST(root->left);
+    }
+    if (root->right !=NULL){
+        display_BST(root->right);
+    }
+    
+    cout<<root->data<<"\t";
+    return;
+}
+
+int main(void){
+    BST_node<int> *root =NULL;
+    root = insert_new_node(root, 5);
+    root = insert_new_node(root, 4);
+    root = insert_new_node(root, 3);
+    root = insert_new_node(root, 6);
+    root = insert_new_node(root, 7);
+    display_BST(root);
+    cout<<"\n";
+    return 0;
+}
+
