e3d23e8968c892be831e10a8cecebb32f8901229
Megha Rajam Rao <megha.rrao90@gmail.com>
2020-03-10 01:41:56 -0700

    Create Exercise2.py
    Please review

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,7 @@
+Time and space complexity: O(1)
+Executed in Leetcode: No Link. Executed in Jupyter notebook.
+Challenges: Few errors when i din't code for self.root is none in pop() function.
+    
 class Node:
     def __init__(self, data):
        self.data = data
@@ -5,10 +9,22 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.root = None
         
     def push(self, data):
+        NewNode = Node(data)
+        NewNode.next = self.root
+        self.root = NewNode   
+        #return str(data)  + "successfully added to stack using Linked List"
         
     def pop(self):
+        if self.root == None: 
+          print("Empty string")
+          return
+        popItem = self.root.data
+        self.root = self.root.next
+        return popItem
+        # return str(popItem) + "is the item popped"
         
 a_stack = Stack()
 while True:
