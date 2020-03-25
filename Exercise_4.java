abda162ea0a94c9f4b3aa986851c2fe8c7ad0917
navnitss <43095390+navnitss@users.noreply.github.com>
2020-03-25 16:43:26 -0500

    Update Exercise_4.java

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,59 +1,73 @@
-import java.util.LinkedList; 
-import java.util.Queue; 
-public class GFG { 
-       
-    /* A binary tree node has key, pointer to  
-    left child and a pointer to right child */
-    static class Node { 
-        int key; 
-        Node left, right; 
-          
-        // constructor 
-        Node(int key){ 
-            this.key = key; 
-            left = null; 
-            right = null; 
-        } 
-    } 
-    static Node root; 
-    static Node temp = root; 
-      
-    /* Inorder traversal of a binary tree*/
-    static void inorder(Node temp) 
-    { 
-        if (temp == null) 
-            return; 
-       
-        inorder(temp.left); 
-        System.out.print(temp.key+" "); 
-        inorder(temp.right); 
-    } 
-       
-    /*function to insert element in binary tree */
-    static void insert(Node temp, int key) 
-    { 
+import java.util.LinkedList;
+import java.util.Queue;
+ class GFG {
 
-        // Do level order traversal until we find 
-        // an empty place and add the node.  
-    } 
-       
+    /* A binary tree node has key, pointer to
+    left child and a pointer to right child */
+    static class Node {
+        int key;
+        Node left, right;
+
+        // constructor
+        Node(int key){
+            this.key = key;
+            left = null;
+            right = null;
+        }
+    }
+    static Node root;
+    static Node temp = root;
+    static int flag=0;
+    /* Inorder traversal of a binary tree*/
+    static void inorder(Node temp)
+    {
+        if (temp == null)
+            return;
+
+        inorder(temp.left);
+        System.out.print(temp.key+" ");
+        inorder(temp.right);
+    }
+
+    /*function to insert element in binary tree */
+    static void insert(Node temp, int key)
+    {
+
+        // Do level order traversal until we find
+        // an empty place and add the node.
+        if(temp.left==null)
+        {
+            temp.left=new Node(key);
+            flag=1;//Tells the program that an empty space was found and further traversal is not necessary
+            return;
+        }
+        if(temp.right==null) {
+            temp.right = new Node(key);
+            flag=1;//Tells the program that an empty space was found and further traversal is not necessary
+            return;
+        }
+        insert(temp.left,key);
+        if(flag==0)
+        insert(temp.right,key);
+    }
+
     // Driver code 
-    public static void main(String args[]) 
-    { 
-        root = new Node(10); 
-        root.left = new Node(11); 
-        root.left.left = new Node(7); 
-        root.right = new Node(9); 
-        root.right.left = new Node(15); 
-        root.right.right = new Node(8); 
-       
-        System.out.print( "Inorder traversal before insertion:"); 
-        inorder(root); 
-       
-        int key = 12; 
-        insert(root, key); 
-       
-        System.out.print("\nInorder traversal after insertion:"); 
-        inorder(root); 
-    } 
+    public static void main(String args[])
+    {
+        root = new Node(10);
+        root.left = new Node(11);
+        root.left.left = new Node(7);
+        root.right = new Node(9);
+        root.right.left = new Node(15);
+        root.right.right = new Node(8);
+
+        System.out.print( "Inorder traversal before insertion:");
+        inorder(root);
+
+        int key = 12;
+        insert(root, key);
+
+        System.out.print("\nInorder traversal after insertion:");
+        inorder(root);
+    }
 }
