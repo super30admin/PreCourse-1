3d8c616830103e0c6ff2ce99b6ffc646ae7086c2
VarshaKatariya <varshakatariya0604@gmail.com>
2020-04-15 20:28:17 -0700

    Completed Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -3,31 +3,58 @@ class Stack {
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
+    // Time Complexity = O(1)
     boolean isEmpty() 
     { 
         //Write your code here 
+        return top == -1;
     } 
 
     Stack() 
     { 
+        top = -1;
         //Initialize your constructor 
     } 
   
+    // Time Complexity = O(1)
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top<MAX-1){
+            top++;
+            a[top] = x;
+            return true;
+        }else{
+            return false;
+        }
+        
     } 
-  
+    
+    // Time Complexity = O(1)
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top == -1){
+            System.out.println(" Stack Underflow");
+            return 0;
+        }else{
+            int temp = a[top];
+            top--;
+            return temp;
+        }
+        
     } 
   
+    // Time Complexity = O(1)
     int peek() 
     { 
         //Write your code here
+        if(top!=-1)
+            return a[top];
+        else
+            return 0;
     } 
 } 
   
