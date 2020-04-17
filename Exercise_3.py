7712ac7b6da5c7bc68c7f156ea8300c4348da239
Sonali Sreedhar <sonalisreedhar@Sonalis-MBP.lan1>
2020-04-16 19:00:53 -0700

    Completed Sprint Precourse1

--- Exercise_3.py
+++ Exercise_3.py
@@ -17,6 +17,10 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        newNode=Node(data)
+        newnode.next=self.head
+        self.head=newNode
+        
         
     def find(self, key):
         """
@@ -24,9 +28,31 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        current=self.head
+        found=False
+        while current and found is False:
+            if current.data==key:
+                found=Trueloo
+            else:
+                current=current.next
+        if current is None:
+            return ("Data is not in list")
+            
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        current=self.head
+        previousNode=None
+        while current is not None:
+            if current== key:
+                if previousNode is not None:
+                    previousNode.next=current.next
+                else:
+                    self.head=current.next
+                    
+            previousNode=current
+            current=current.next
+            
