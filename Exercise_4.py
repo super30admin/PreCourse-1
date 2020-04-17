b2638776d5e54faab95ed6654f0d9f46ec4670be
Sonali Sreedhar <sonalisreedhar@Sonalis-MacBook-Pro.local>
2020-04-16 18:41:02 -0700

    Completed Sprint Precourse1

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,38 +1,58 @@
-# Python program to insert element in binary tree  
-class newNode():  
+# Python program to insert element in binary tree
+class newNode():
   
-    def __init__(self, data):  
-        self.key = data 
+    def __init__(self, data):
+        
         self.left = None
         self.right = None
-          
+        self.key = data
+       
+#Time complexities would be depend on the number of nodes ie. O(n) where n is the number of nodes.
 """ Inorder traversal of a binary tree"""
-def inorder(temp): 
+def inorder(temp):
   
-    
+    if temp:
+        inorder(temp.left)
+        print(temp.key)
+        inorder(temp.right)
   
   
 """function to insert element in binary tree """
-def insert(temp,key): 
-  
+def insert(temp,key):
     
+    tempNode = newNode(key)
   
+    if temp is None:
+        temp=tempNode
+    else:
+        if temp.key<key:
+            if temp.right is None:
+                temp.right=tempNode
+            else:
+                insert(temp.right,key)
+        else:
+            if temp.left is None:
+                temp.left=tempNode
+            else:
+                insert(temp.left,key)
+                
        
-# Driver code  
-if __name__ == '__main__': 
-    root = newNode(10)  
-    root.left = newNode(11)  
-    root.left.left = newNode(7)  
-    root.right = newNode(9)  
-    root.right.left = newNode(15)  
-    root.right.right = newNode(8)  
+# Driver code
+if __name__ == '__main__':
+    
+    root = newNode(10)
+    root.left = newNode(11)
+    root.left.left = newNode(7)
+    root.right = newNode(9)
+    root.right.left = newNode(15)
+    root.right.right = newNode(8)
   
-    print("Inorder traversal before insertion:", end = " ") 
-    inorder(root)  
+    print("Inorder traversal before insertion:", end = " ")
+    inorder(root)
   
     key = 12
-    insert(root, key)  
+    insert(root, key)
   
-    print()  
-    print("Inorder traversal after insertion:", end = " ") 
+    print()
+    print("Inorder traversal after insertion:", end = " ")
     inorder(root) 
