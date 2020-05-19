74bbdd618d66d13b0adeb927c470055e96a769b5
Akshata Sawhney <akshatasawhney@gmail.com>
2020-05-19 17:12:09 -0500

    Completed Precourse1 May2020

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,10 @@
+"""
+// Time Complexity :  O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : problem not there on Leetcode
+// Any problem you faced while coding this : No
+"""
+
 class Node:
     def __init__(self, data):
        self.data = data
@@ -5,10 +12,19 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.my_stack = None #initializing my_stack as None
         
     def push(self, data):
+        node = Node(data) #create new node 
+        node.next = self.my_stack # attaching the node at the beginning by adding the pre-existing LL in the next variable of the new node
+        self.my_stack = node #setting head of my_stack to point at the new node
         
     def pop(self):
+        #if self.my_stack==None:
+        #   return None
+        rem = self.my_stack.data #storing the value about to be deleted in a variable
+        self.my_stack = self.my_stack.next #setting the head of my_stack to node next in sequence
+        return rem
         
 a_stack = Stack()
 while True:
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,4 +1,14 @@
+"""
+// Time Complexity :  O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes, checked for inorder function
+// Any problem you faced while coding this : slight difficulty while building up logic for traversals
+"""
+
+
+
 # Python program to insert element in binary tree  
+from collections import deque
 class newNode():  
   
     def __init__(self, data):  
@@ -7,19 +17,53 @@ class newNode():
         self.right = None
           
 """ Inorder traversal of a binary tree"""
-def inorder(temp): 
-  
-    
-  
-  
+def inorder(temp): #(left,root,right)
+    stack=[]
+    output=[]
+        
+    cur=temp #start from root
+    while True:
+        while cur:
+            stack.append(cur) #for adding all the left children along the way to the stack
+            cur=cur.left
+        if stack: 
+            cur=stack.pop()
+            output.append(cur.key)#adding the node to the result variable
+            cur=cur.right #setting the next node to be the right child
+        else:
+            break
+    print(output)
+         
 """function to insert element in binary tree """
 def insert(temp,key): 
-  
+    q=deque() #using queue for level-order traversal
+    node = newNode(key) #creating new tree node 
+    
+    if not temp: #if no tree exists, new node is the root node
+      temp=node
+ 
+    else: #else we traverse levelwise
+      q.append(temp)
+      
+      while q:
+          cur = q.popleft()
+          
+          if not cur.left: #if current node has no left child, we insert the new node there
+              cur.left=node 
+              break
+          else: #else the left node is added to the queue
+              q.append(cur.left)
+        
+          if not cur.right:#if current node has no right child, we insert the new node there
+              cur.right=node 
+              break
+          else:
+              q.append(cur.right)#else the right node is added to the queue
     
   
        
 # Driver code  
-if __name__ == '__main__': 
+if __name__ == '__main__':
     root = newNode(10)  
     root.left = newNode(11)  
     root.left.left = newNode(7)  
@@ -29,10 +73,11 @@ if __name__ == '__main__':
   
     print("Inorder traversal before insertion:", end = " ") 
     inorder(root)  
-  
+    
     key = 12
     insert(root, key)  
   
     print()  
     print("Inorder traversal after insertion:", end = " ") 
     inorder(root) 
+    
