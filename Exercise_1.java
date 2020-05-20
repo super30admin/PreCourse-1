fd0985d8882c4e5eedaed90044cb36ff7c70dd51
cr21 <cr.tagadiya@gmail.com>
2020-05-19 20:14:16 -0400

    Exercise_2 complete

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,38 +1,73 @@
 class Stack { 
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
+    static final int MAX = 2; 
+     // Maximum size of Stack 
+    int top;
+    int a[] = new int[MAX];
+
     boolean isEmpty() 
     { 
         //Write your code here 
+        if (top < 0){
+            return true;
+        }else{
+            return false;
+        }
+        // return true if top < 0 else false ;
     } 
 
     Stack() 
     { 
+        top = -1;
         //Initialize your constructor 
+        // System.out.print("Stack initialized");
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        // System.out.println("top"+top);
+        if (top >=(MAX-1)){
+            System.out.println("Stack is Full, Pop Some Items to add");
+            return false;
+        }
+        else{
+            a[++top]=x;
+            // System.out.println(x +" is Pushed in Stack");
+            // System.out.println("top After"+top);
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if (isEmpty()){
+            System.out.println("Stack Overflow");
+            return 0;
+        }
+        else{
+            // int current_top = peek()
+            int poped = a[top--];
+            System.out.println("Popped Item "+poped );
+            return poped;
+        }
     } 
   
     int peek() 
-    { 
-        //Write your code here
+    {   
+        if (isEmpty()){
+            return 0;
+        }else{
+            return a[top];
+        }
+        
     } 
 } 
   
 // Driver code 
-class Main { 
+public class Exercise_1 { 
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,4 @@
-public class StackAsLinkedList { 
+class StackAsLinkedList { 
   
     StackNode root; 
   
@@ -9,6 +9,7 @@ public class StackAsLinkedList {
         StackNode(int data) 
         { 
             //Constructor here 
+            this.data = data;
         } 
     } 
     
@@ -16,26 +17,71 @@ public class StackAsLinkedList {
     public boolean isEmpty() 
     { 
         //Write your code here for the condition if stack is empty. 
+        if (root == null){
+            return true;
+        }else{
+            return false;
+        }
     } 
   
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
+        StackNode new_node = new StackNode(data);
+        // if stack is Empty then new node will be set as root
+        if (isEmpty()){
+            root = new StackNode(data);
+        }else{
+            // temp node is root
+            // then set new node as root and its next as temp node
+            StackNode tempNode = root;
+            root = new_node;
+            new_node.next = tempNode;
+        }
     } 
   
     public int pop() 
     { 	
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+    //Also return the popped element 
+        if(isEmpty()){
+            return 0;
+        }else{
+            // get next of the current root, that will be new root after poping the element
+            StackNode  node = root.next;
+            int data = root.data;
+            root= node;
+            return data;
+        }
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+        if(isEmpty()){
+            System.out.println("STACK IS EMPTY");
+            return 0;
+        }
+        else{
+            return root.data;
+        }
     } 
   
 	//Driver code
+    
+    public void print_linkedList(){
+        StackNode temp = root;
+        System.out.print("Root -> ");
+        while ( temp != null){
+            System.out.print(temp.data+" -> ");
+            temp = temp.next;
+        }
+        System.out.print("null");
+        System.out.println();
+    }
+} 
+
+public class Exercise_2{
     public static void main(String[] args) 
     { 
   
@@ -44,9 +90,11 @@ public class StackAsLinkedList {
         sll.push(10); 
         sll.push(20); 
         sll.push(30); 
+        System.out.println("current Elements in stack");
+        sll.print_linkedList();
   
         System.out.println(sll.pop() + " popped from stack"); 
   
         System.out.println("Top element is " + sll.peek()); 
     } 
-} 
+}
