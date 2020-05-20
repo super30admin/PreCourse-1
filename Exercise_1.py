68a0929bc183aea6f4063ebb5faaf67cbbe24794
Shantanu609 <shantanu_shinde_@mail.fresnostate.edu>
2020-05-19 18:36:46 -0700

    Completed Sprint PreCourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,27 @@
+# O(N) time | O(1) Space
 class myStack:
      def __init__(self):
+         self.stack = []
          
      def isEmpty(self):
-         
+          if not self.stack:
+               return True 
+          return False
      def push(self, item):
+          self.stack.append(item)
          
      def pop(self):
-        
+          return self.stack.pop()
         
      def peek(self):
-        
+          return self.stack[-1]
+   
      def size(self):
-         
+          
+          return len(self.stack)
+     
      def show(self):
-         
+          return self.stack
 
 s = myStack()
 s.push('1')
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,22 +1,39 @@
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
+# O(N) time | O(1) Space
 class Stack:
     def __init__(self):
-        
+        self.stack = Node(None)
+
     def push(self, data):
-        
+        node = self.stack
+        while node.next is not None:
+            node = node.next
+        node.next = Node(data)
+
     def pop(self):
-        
+        node = self.stack.next
+        if node is None:
+            return None
+        prev = self.stack
+        while node.next is not None:
+            node = node.next
+            prev = prev.next
+        res = node.data
+        prev.next = None
+        return res
+
+
 a_stack = Stack()
 while True:
     print('push <value>')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
- 
+
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
