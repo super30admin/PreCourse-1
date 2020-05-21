434f120873b9496b09617288bbba944334016efc
Ashwini R <ashwinirudrawar14@gmail.com>
2020-05-20 18:33:08 -0700

    Updated java files with assignments

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,29 +5,51 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        return top < 0;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
-    } 
+        this.top = -1;
+    }
   
     boolean push(int x) 
-    { 
+    {
         //Check for stack Overflow
+        if(top >= MAX-1) {
+            return false;
+        }
+
         //Write your code here
+        top++;
+        a[top] = x;
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
+        if(isEmpty()) {
+            System.out.println(" Stack Underflow");
+            return 0;
+        }
+
         //Write your code here
+        int result = a[top];
+        a[top] = 0;
+        top--;
+        return result;
     } 
   
     int peek() 
-    { 
+    {
+        if(isEmpty()) {
+            System.out.println(" Stack Underflow");
+            return 0;
+        }
         //Write your code here
+        return a[top];
     } 
 } 
   
@@ -39,6 +61,9 @@ class Main {
         s.push(10); 
         s.push(20); 
         s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+        System.out.println(s.pop() + " Popped from stack");
+    }
+
+    //Time Complexity for each operation: O(1)
+    //Space Complexity: Size of Array + 1 (for TOP)
 }
