7f9c5c4e96cba322425fa98966e4513e36b796b7
Manasi Burange <burangemanasi@gmail.com>
2020-05-20 20:34:00 -0700

    Completed Precourse_1 - Please Review

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,33 +1,59 @@
+//Time Complexity: O(1) -> all operations execute in O(1) complexity
+                        //push, pop, peek, isEmpty
+//Space Complexity: O(n) -> worst case all elements are stored in the stack
+                        //occupying linear space
+
 class Stack { 
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
-  
+    
     boolean isEmpty() 
-    { 
-        //Write your code here 
+    {   //returning true if top is < 0
+        //if not; return false
+        return (top < 0);
     } 
-
+    //constructor
     Stack() 
-    { 
-        //Initialize your constructor 
+    {   //top is initialized to empty top
+        top = -1;
     } 
   
     boolean push(int x) 
-    { 
-        //Check for stack Overflow
-        //Write your code here
+    {   //if stack size if full; cannot insert new element
+        //if not; increment the top pointer -> push
+        if(top >= MAX-1){
+            System.out.println("Can't push element. Stack Overflow!");
+            return false;
+        } else {
+            a[++top] = x;
+            System.out.println("Element " + x + " is pushed to stack successfully!");
+            return true;
+        }
     } 
   
     int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+    {   //if no elements to pop; returning 0
+        //else return the last inserted element
+        if(top < 0){
+            System.out.println("No elements to pop. Empty stack!");
+            return 0; 
+        } else {
+            int poppedElement = a[top--];
+            return poppedElement;
+        }
     } 
   
     int peek() 
-    { 
-        //Write your code here
+    {   //if no elements; return 0
+        //else return the top most element
+        if(top < 0){
+            System.out.println("No elements to peek. Empty stack!");
+            return 0; 
+        } else {
+            int peekedElement = a[top--];
+            return peekedElement;
+        }
     } 
 } 
   
