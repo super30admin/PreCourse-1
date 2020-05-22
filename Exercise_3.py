e1e83f6224ac3ed302bd47ac5e51baeb55384595
Abhishek Prusty <Abhishek.lan@gmail.com>
2020-05-21 23:47:40 -0500

    Added code for exercises

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,64 +1,64 @@
 class ListNode:
-    """
-    A node in a singly-linked list.
-    """
-    def __init__(self, data=None, next=None):
-        self.data = data
-        self.next = next
-    
+	"""
+	A node in a singly-linked list.
+	"""
+	def __init__(self, data=None, next=None):
+		self.data = data
+		self.next = next
+	
 class SinglyLinkedList:
-    def __init__(self):
-        """
-        Create a new singly-linked list.
-        Takes O(1) time.
-        """
-        self.head = None
+	def __init__(self):
+		"""
+		Create a new singly-linked list.
+		Takes O(1) time.
+		"""
+		self.head = None
 
-    def append(self, data):
-        """
-        Insert a new element at the end of the list.
-        Takes O(n) time.
-        """
-        searchnode = self.head
-        if not searchnode:
-            self.head = ListNode(data = data)
-            return
-        while searchnode.next:
-            searchnode = searchnode.next
+	def append(self, data):
+		"""
+		Insert a new element at the end of the list.
+		Takes O(n) time.
+		"""
+		searchnode = self.head
+		if not searchnode:
+			self.head = ListNode(data = data)
+			return
+		while searchnode.next:
+			searchnode = searchnode.next
 
-        searchnode.next = ListNode(data = data)
+		searchnode.next = ListNode(data = data)
 
-        
-    def find(self, key):
-        """
-        Search for the first element with `data` matching
-        `key`. Return the element or `None` if not found.
-        Takes O(n) time.
-        """
-        findnode = self.head
-        while(findnode):
-            if findnode.data == key:
-                return findnode.data
-            findnode = findnode.next    
-        return None
-        
-    def remove(self, key):
-        """
-        Remove the first occurrence of `key` in the list.
-        Takes O(n) time.
-        """
-        removenode = self.head
-        while(removenode):
-            if removenode.data == key:
-                self.head = removenode.next
-                return
+		
+	def find(self, key):
+		"""
+		Search for the first element with `data` matching
+		`key`. Return the element or `None` if not found.
+		Takes O(n) time.
+		"""
+		findnode = self.head
+		while(findnode):
+			if findnode.data == key:
+				return findnode.data
+			findnode = findnode.next    
+		return None
+		
+	def remove(self, key):
+		"""
+		Remove the first occurrence of `key` in the list.
+		Takes O(n) time.
+		"""
+		removenode = self.head
+		while(removenode):
+			if removenode.data == key:
+				self.head = removenode.next
+				return
 
-            elif removenode.next.data == key:
-                if removenode.next.next:
-                    removenode.next = removenode.next.next
-                    return
-                else:
-                    removenode.next = None
-                    return
-            removenode = removenode.next
-        return
+			elif removenode.next.data == key:
+				if removenode.next.next:
+					removenode.next = removenode.next.next
+					return
+				else:
+					removenode.next = None
+					return
+			removenode = removenode.next
+		return
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,57 +1,57 @@
 # Python program to insert element in binary tree  
 class newNode():  
   
-    def __init__(self, data):  
-        self.key = data 
-        self.left = None
-        self.right = None
-          
+	def __init__(self, data):  
+		self.key = data 
+		self.left = None
+		self.right = None
+		  
 """ Inorder traversal of a binary tree"""
 def inorder(temp): 
-    if temp:
-        inorder(temp.left)
-        print(temp.key)
-        inorder(temp.right)
+	if temp:
+		inorder(temp.left)
+		print(temp.key)
+		inorder(temp.right)
   
   
 """function to insert element in binary tree """
 def insert(temp,key):
-    if not temp:
-        temp = newNode(key)
-    else:
-        if not temp.left:
-            temp.left = newNode(key)
-            return
-        elif temp.left:
-            insert(temp.left, key)
-            return
-        if not temp.right:
-            temp.right = newNode(key)
-            return
-        elif temp.right:
-            insert(temp.right, key)
-            return
+	if not temp:
+		temp = newNode(key)
+	else:
+		if not temp.left:
+			temp.left = newNode(key)
+			return
+		elif temp.left:
+			insert(temp.left, key)
+			return
+		if not temp.right:
+			temp.right = newNode(key)
+			return
+		elif temp.right:
+			insert(temp.right, key)
+			return
 
 #I failed to understand the condition to insert a node in the binary tree since there is no ordering as such. Therefore it is inserted as the left of 7
   
-    
+	
   
-       
+	   
 # Driver code  
 if __name__ == '__main__': 
-    root = newNode(10)  
-    root.left = newNode(11)  
-    root.left.left = newNode(7)  
-    root.right = newNode(9)  
-    root.right.left = newNode(15)  
-    root.right.right = newNode(8)  
+	root = newNode(10)  
+	root.left = newNode(11)  
+	root.left.left = newNode(7)  
+	root.right = newNode(9)  
+	root.right.left = newNode(15)  
+	root.right.right = newNode(8)  
   
-    print("Inorder traversal before insertion:", end = " ") 
-    inorder(root)  
+	print("Inorder traversal before insertion:", end = " ") 
+	inorder(root)  
   
-    key = 12
-    insert(root, key)  
+	key = 12
+	insert(root, key)  
   
-    print()  
-    print("Inorder traversal after insertion:", end = " ") 
-    inorder(root) 
+	print()  
+	print("Inorder traversal after insertion:", end = " ") 
+	inorder(root) 
