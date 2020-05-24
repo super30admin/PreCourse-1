2d4037345ec58c181b48dabbd85311fedefe8dcb
Ashwini Mangesh Jaitapkar <ashwini@Ashwinis-MacBook-Pro.local>
2020-05-24 16:03:56 -0400

    Precourse_1

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,34 +5,53 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        if(top< 0){
+            return true;
+        }else{
+            return false;
+        }
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top =-1;
+
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        //Checking for overflow condition 
+       if(top == MAX-1){
+           return false;
+       }else{
+           top++;
+           a[top]=x;
+           return true;
+       }
     } 
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if(top < 0){
+            System.out.println("Stack Underflow");
+            return 0;
+        }else{
+            int poppedElement = a[top]; //reassign while pushing
+             top--;
+             return poppedElement;
+        }
     } 
   
     int peek() 
     { 
-        //Write your code here
+        int topElement = a[top];
+        return topElement;
     } 
 } 
   
 // Driver code 
-class Main { 
+class Exercise_1 { 
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,70 +1,89 @@
-import java.io.*; 
-  
-// Java program to implement 
-// a Singly Linked List 
-public class LinkedList { 
-  
-    Node head; // head of list 
-  
-    // Linked list Node. 
-    // This inner class is made static 
-    // so that main() can access it 
-    static class Node { 
-  
-        int data; 
-        Node next; 
-  
-        // Constructor 
-        Node(int d) 
-        { 
-            //Write your code here 
-        } 
-    } 
-  
-    // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
-    { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
+import java.io.*;
 
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
-    } 
-  
-    // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
-    {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
-    } 
-   
-    // Driver code 
-    public static void main(String[] args) 
-    { 
+// Java program to implement
+// a Singly Linked List
+public class Exercise_3 {
+
+    Node head; // head of list
+
+    // Linked list Node.
+    // This inner class is made static
+    // so that main() can access it
+    static class Node {
+
+        int data;
+        Node next;
+
+        // Constructor
+        Node(int d)
+        {
+            this.data = d;
+            next=null;
+
+        }
+    }
+
+    // Method to insert a new node
+    public static Exercise_3 insert(Exercise_3 list, int data)
+
+    {
+        // Create a new node with given data
+        Node new_node = new Node(data);
+        if(list.head == null){
+            // If the Linked List is empty,
+            // then make the new node as head
+            list.head = new_node;
+        }
+        else{
+            // Else traverse till the lastElement node
+            // and insert the new_node there
+
+            Node lastElement = list.head;
+            while(lastElement.next!=null){
+                lastElement = lastElement.next;
+            }
+            // Insert the new_node at last node
+            lastElement.next=new_node;
+
+        }
+        // Return the list by head
+        return list;
+
+    }
+
+    // Method to print the LinkedList.
+    public static void printList(Exercise_3 list)
+    {
+        Node stackElements =list.head;
+        // Traverse through the LinkedList
+        while(stackElements.next!=null){
+            // Print the data at current node
+            System.out.println(stackElements.data);
+            // Go to next node
+            stackElements= stackElements.next;
+        }
+
+
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
         /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
-  
-        // 
-        // ******INSERTION****** 
-        // 
-  
-        // Insert the values 
-        list = insert(list, 1); 
-        list = insert(list, 2); 
-        list = insert(list, 3); 
-        list = insert(list, 4); 
-        list = insert(list, 5); 
-  
-        // Print the LinkedList 
-        printList(list); 
-    } 
+        Exercise_3  list = new Exercise_3();
+
+        //
+        // ******INSERTION******
+        //
+
+        // Insert the values
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+
+        // Print the LinkedList
+        printList(list);
+    }
 }
