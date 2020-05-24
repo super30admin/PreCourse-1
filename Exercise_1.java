e0cb65157d2cc7707be110cb00cf0b7322bd45cb
Sarthak Sugandhi <47070701+sarthaksugandhi9@users.noreply.github.com>
2020-05-24 16:43:28 -0700

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,44 +1,59 @@
-class Stack { 
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
-        //Check for stack Overflow
-        //Write your code here
-    } 
-  
-    int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
-    } 
-  
-    int peek() 
-    { 
-        //Write your code here
-    } 
-} 
-  
-// Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+class Stack {
+	static final int MAX = 1000;
+	int top;
+	int a[];
+
+	boolean isEmpty() {
+		if (top == -1) {
+			return true;
+		}
+		return false;
+	}
+
+	Stack() {
+		top = -1;
+		a = new int[MAX];
+	}
+
+	boolean push(int x) {
+		if (top == MAX - 1) {
+			System.out.println("Stack Overflow");
+			return false;
+		}
+		a[++top] = x;
+		return true;
+	}
+
+	int pop() {
+		if (top == -1) {
+			System.out.println("Stack Underflow");
+			return 0;
+		}
+
+		int item = a[top--];
+		return item;
+	}
+
+	int peek() {
+		if (top == -1) {
+			System.out.println("Stack Underflow");
+		}
+		return a[top];
+	}
+}
+
+public class Excercise_1 {
+
+	public static void main(String[] args) {
+		Stack s = new Stack();
+		s.push(10);
+		System.out.println("Peek : " + s.peek());
+		s.push(20);
+		System.out.println("Peek : " + s.peek());
+		s.push(30);
+		System.out.println("Peek : " + s.peek());
+		System.out.println(s.pop() + " Popped from stack");
+	}
+
 }
