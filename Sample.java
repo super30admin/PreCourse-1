dd6c096f28f50853f4cb069e632437d08cbb60da
nareshkumar-ss <nareshkumar.ss@outlook.com>
2020-05-24 22:39:27 -0700

    adding complexity analis for problems

--- Sample.java
+++ Sample.java
@@ -5,3 +5,30 @@
 
 
 // Your code here along with comments explaining your approach
+
+//Problem 1: Implement Stack using Array
+
+//Time Complexity : O(1) for each action, Insiertion, Deletion, Peek..
+//Space Complexity : O(n) No of input elements to be pushed to stack
+// Any problem you faced while coding this : No
+
+
+//Problem 2: Implement Stack using LinkedList
+
+//Time Complexity : O(1) for each action, Insiertion, Deletion, Peek..
+//Space Complexity : O(n) No of input elements to be pushed to stack
+// Any problem you faced while coding this : No
+
+
+//Problem 3: Implement Singly LinkedList
+
+//Time Complexity : Takes O(n) time as we have to traverse the list and insert the element at the end. Same for printing the list as well.
+//Space Complexity : O(n) No of input elements in the List
+// Any problem you faced while coding this : No
+
+
+//problem 4: Insert an element in binary tree
+
+//Time Complexity : For 1 element instertion it would take O(log n) as per the height of the tree.
+//Space Complexity: Space complexity would also be O(log n) as parent nodes as to be pushed to the queue to insert the new node.
+// Any problem you faced while coding this : No
