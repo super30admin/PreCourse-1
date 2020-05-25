87e73faad4cc2a43610f1e40afd3b81233388c28
Prajakta Ray <47615084+prajaktaray@users.noreply.github.com>
2020-05-24 22:17:57 -0500

    Update Exercise_3.java

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,3 +1,8 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n) where n is the size of the linked list
+// Did this code successfully run on Leetcode : No
+// Any problem you faced while coding this : No
+
 import java.io.*; 
   
 // Java program to implement 
@@ -17,34 +22,52 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            this.data = d;
+            this.next = null;
         } 
     } 
   
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
     { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
+    	// Create a new node with given data 
+    	Node curr = new Node(data) ;
+    	
+    	// If the Linked List is empty, 
         // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
-
-            // Insert the new_node at last node 
-        // Return the list by head 
+    	if (list.head == null)
+    		list.head = curr;
+    	
+    	// Else traverse till the last node 
+        // and insert the new_node there 
+    	// Insert the new_node at last node 
+    	else {
+    		Node prev = list.head;
+    		while(prev.next != null) {
+    			prev = prev.next;
+    		}
+    		prev.next = curr;
+    		
+    	}
+    	// Return the list by head
+    	return list;	           
         
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+    	if (list.head == null) {
+    		System.out.println("List is empty");
+    		return;
+    	}
+    	
+    	Node curr = list.head;    	
+    	System.out.println("Contents of the list	:	");
+    	while(curr != null) {
+    		System.out.print(curr.data + "\t");
+    		curr = curr.next;
+		}
     } 
    
     // Driver code 
