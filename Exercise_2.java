784c16065f87b39d237d657d61f5b16ececc3c39
Prajakta Ray <47615084+prajaktaray@users.noreply.github.com>
2020-05-24 22:17:20 -0500

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,3 +1,7 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n) where n is the size of the stack
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 public class StackAsLinkedList { 
   
     StackNode root; 
@@ -8,31 +12,69 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
-            //Constructor here 
+            this.data = data;
+            this.next = null;
+            
         } 
     } 
     
 	
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+        return root == null; 
     } 
   
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
+        if(isEmpty()) {
+        	 StackNode curr = new StackNode(data);
+        	 root = curr;
+        	 return;
+        }
+        
+        StackNode prev = root;
+        while(prev.next != null) {
+        	prev = prev.next;
+        }
+        
+        StackNode curr = new StackNode(data);
+   	 	prev.next = curr;        
+        
     } 
   
     public int pop() 
     { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
+    	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+    	//Also return the popped element 
+    	if(isEmpty()) {
+    		System.out.println("Stack Underflow");
+    		return 0;
+    	}
+    	
+    	StackNode curr = root;
+        while(curr.next.next != null) {
+        	curr = curr.next;
+        }
+        
+        int temp = curr.next.data;
+    	curr.next = null;
+    	return temp;
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+    	if(isEmpty()) {
+    		System.out.println("Stack Underflow");
+    		return 0;
+    	}
+    	
+    	StackNode curr = root;
+        while(curr.next != null) {
+        	curr = curr.next;
+        }
+    	return curr.data;
     } 
   
 	//Driver code
@@ -40,8 +82,8 @@ public class StackAsLinkedList {
     { 
   
         StackAsLinkedList sll = new StackAsLinkedList(); 
-  
         sll.push(10); 
+        System.out.println("Top element is " + sll.peek()); 
         sll.push(20); 
         sll.push(30); 
   
