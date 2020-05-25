fea8da52bc6198628c38804e847b70193dd08186
Prajakta Ray <47615084+prajaktaray@users.noreply.github.com>
2020-05-24 22:15:56 -0500

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,33 +1,51 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n) where n is the max size of the stack
+// Did this code successfully run on Leetcode : 
+// Any problem you faced while coding this : No
 class Stack { 
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
+    int size;
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        return size == 0; 
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        size = 0; 
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        if(size == MAX) {
+        	System.out.println("Stack size exceeded! Stack Overflow");
+        	return false;
+        }
+        
+        a[size] = x;
+        size += 1;
+        return true;
+        
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if(this.isEmpty()) {
+        	System.out.println("Stack Underflow");
+        	return 0;
+        }
+        int temp = a[size];
+        a[size] = 0;
+        return temp;
     } 
   
     int peek() 
     { 
-        //Write your code here
+        return a[size];
     } 
 } 
   
