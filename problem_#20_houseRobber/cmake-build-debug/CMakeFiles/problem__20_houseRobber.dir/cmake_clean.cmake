aef04a1fb3083c303edf0995d03d40b570f37e2a
Uma Nataraj <uma.m.nataraj@gmail.com>
2020-06-02 23:56:58 -0700

    solved problem#20

---
+++ problem_#20_houseRobber/cmake-build-debug/CMakeFiles/problem__20_houseRobber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/problem__20_houseRobber.dir/main.cpp.o"
+  "problem__20_houseRobber"
+  "problem__20_houseRobber.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/problem__20_houseRobber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
