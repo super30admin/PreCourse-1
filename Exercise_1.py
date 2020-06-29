e87dd4425a1c7839b76d785a3c3a0e6d69840144
firoz1905 <30398356+firoz1905@users.noreply.github.com>
2020-06-28 23:55:23 -0700

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,37 @@
 class myStack:
-     def __init__(self):
+    def __init__(self,limit=10):
+        self.limit=limit
+        self.stk=[]
+          
          
-     def isEmpty(self):
+    def isEmpty(self):
+        return len(self.stk)<=0
          
-     def push(self, item):
+    def push(self, item):
+        if len(self.stk)>=self.limit:
+            print('Stack Overflow')
+        else:
+            self.stk.append(item)
+               
          
-     def pop(self):
+    def pop(self):
+        if len(self.stk)<=0:
+               print('Stack is UnderFlow!')
+        else:
+            return self.stk.pop()        
         
+    def peek(self):
+        if len(self.stk)<=0:
+            print('Stack is UnderFlow!')
+        else:
+               return self.stk[-1]
+          
         
-     def peek(self):
-        
-     def size(self):
+    def size(self):
+          return len(self.stk)
          
-     def show(self):
-         
-
+    def show(self):
+          return self.stk[:]         
 s = myStack()
 s.push('1')
 s.push('2')
