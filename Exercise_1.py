b71cbc8412e480758e10d04d9f0fb1fd53352907
khushbooG9 <kgn9294@gmail.com>
2020-06-30 15:15:45 -0700

    Solutions to 4 exercises

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,44 @@
 class myStack:
-     def __init__(self):
+    def __init__(self):
+        self.max_size = 1000
+        self.stack = []
+        self.length = 0
          
-     def isEmpty(self):
+    def isEmpty(self):
+        if  self.length==0:
+            return True
+        return False
          
-     def push(self, item):
+    def push(self, item):
+        if self.length <= self.max_size:
+            self.stack.append(item)
+            self.length +=1
+            return True
+        else:
+            return "The stack is full!"
+
          
-     def pop(self):
+    def pop(self):
+        if not self.isEmpty():
+            self.length -=1
+            return self.stack.pop()
+        else:
+            return "Can't pop from empty stack"
         
+    def peek(self):
+        if not self.isEmpty():
+            return self.stack[-1]
+        return "The stack is empty!"
         
-     def peek(self):
+    def size(self):
+        return self.length
+         
+    def show(self):
+        if not self.isEmpty():
+            return self.stack
+        else:
+            return "Nothing to show!The stack is empty!!"
         
-     def size(self):
-         
-     def show(self):
-         
 
 s = myStack()
 s.push('1')
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = None 
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,14 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        node  = ListNode(data)
+        if self.head:
+            temp = self.head
+            while temp.next:
+                temp = temp.next
+            temp.next = node
+        else:
+            self.head = node 
         
     def find(self, key):
         """
@@ -24,9 +34,30 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        temp = self.head
+        while temp:
+            if temp.data==key:
+                return temp.data
+            temp = temp.next
+        return None 
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if not self.head:
+            return "Linked list is already empty!"
+        if self.head.data == key:
+            self.head = None
+            return
+
+        temp = self.head
+        while temp:
+            if temp.data==key:
+                temp.val = temp.next.val
+                temp.next = temp.next.next
+                return
+            temp = temp.next 
+
+
