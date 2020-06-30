48cfdbaccc602d39f8b16cebd21e45c6ab8fb741
soumil.sarvaiya <ss8165@rit.edu>
2020-06-30 13:54:58 -0400

    Completed PreCourse_1 Ex_1

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,29 +5,58 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        if(top==-1)
+            return true;
+        else
+            return false;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top = -1;
+
     } 
   
     boolean push(int x) 
-    { 
+    {
+        boolean result = false;
+        if(top +1 < MAX){
+            a[++top]= x;
+            result = true;
+        }else{
+            System.out.println("Stack Overflow.");
+        }
         //Check for stack Overflow
         //Write your code here
+        return result;
     } 
   
     int pop() 
-    { 
+    {
+        int result = 0;
         //If empty return 0 and print " Stack Underflow"
+        if(!isEmpty()){
+            result = a[top--];
+        }else{
+            System.out.println("Stack Underflow.");
+        }
+
+        return result;
         //Write your code here
     } 
   
     int peek() 
-    { 
+    {
+        if(!isEmpty()){
+            return a[top];
+        }else{
+            System.out.println("Stack emptry");
+            return 0;
+        }
         //Write your code here
+
     } 
 } 
   
