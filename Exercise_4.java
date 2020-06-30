8325fb35faf8bb69e80b629dcd61eede8489d3c8
soumil.sarvaiya <ss8165@rit.edu>
2020-06-30 16:12:34 -0400

    Completed PreCourse_1 Ex_4

--- Exercise_4.java
+++ Exercise_4.java
@@ -32,9 +32,26 @@ public class GFG {
     /*function to insert element in binary tree */
     static void insert(Node temp, int key) 
     { 
-
+        Queue<GFG.Node> queue = new LinkedList<GFG.Node>();
+        queue.add(temp);
+    
         // Do level order traversal until we find 
-        // an empty place and add the node.  
+        // an empty place and add the node.
+        while(!queue.isEmpty()){
+            temp = queue.poll();
+            if(temp.left == null){
+                temp.left = new Node(key);
+                break;
+            }else{
+                queue.add(temp.left);
+            }
+            if(temp.right == null){
+                temp.right = new Node(key);
+                break;
+            }else{
+                queue.add(temp.right);
+            }
+        } 
     } 
        
     // Driver code 
