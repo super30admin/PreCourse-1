6db4e4677507a456ec25bf8c8fd813dd601efa60
satishsolanki1990 <satishsolanki1990@gmail.com>
2020-06-30 23:58:57 -0700

    completed Excercise 1,2 and almost half of excercise 3.

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,22 +1,41 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, data=None, next=None):
        self.data = data
-       self.next = None
+       self.next = next # next store previous nodes
  
 class Stack:
     def __init__(self):
-        
+        self.head = Node(None,None)
+        self.length=0
+
     def push(self, data):
-        
+        if self.head.data != None:
+            self.head=Node(data,self.head)
+        else:
+            self.head.data = data
+        self.length+=1
+
     def pop(self):
-        
+        return_value=self.head.data
+        self.head=self.head.next
+        return return_value
+
+    def show(self):
+        ptr=self.head
+        while True:
+            print(ptr.data)
+            if ptr.next != None:
+                ptr=ptr.next
+            else:
+                break
+
 a_stack = Stack()
 while True:
     print('push <value>')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
- 
+
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
@@ -27,4 +46,5 @@ while True:
         else:
             print('Popped value: ', int(popped))
     elif operation == 'quit':
+        a_stack.show()
         break
