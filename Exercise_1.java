2f94daa351883b57d307cdc0d9ae6e29a6b3f14b
manishar1515 <manishar1515@gmail.com>
2020-07-01 11:52:49 -0500

    Added solution for Precourse 1. Please Review

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,44 +1,51 @@
-class Stack { 
-    static final int MAX = 1000; 
-    int top; 
+class Stack {
+    static final int MAX = 1000;
+    int top;
     int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
+    Stack() {
+        top = 0;
+    }
+
+    boolean isEmpty() {
+        return top == 0;
+    }
+
+    boolean push(int x) {
         //Check for stack Overflow
-        //Write your code here
-    } 
-  
-    int pop() 
-    { 
+        if (top == MAX) {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+
+        a[++top] = x;
+        return true;
+    }
+
+    int pop() {
         //If empty return 0 and print " Stack Underflow"
-        //Write your code here
-    } 
-  
-    int peek() 
-    { 
-        //Write your code here
-    } 
-} 
-  
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        int val = a[top];
+        a[top] = 0;
+        top--;
+        return val;
+    }
+
+    int peek() {
+        return a[top];
+    }
+}
+
 // Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+class Main {
+    public static void main(String args[]) {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+    }
 }
--- Exercise_4.java
+++ Exercise_4.java
@@ -1,58 +1,78 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
 /* Removed public to remove compilation error, of class and file name mismatch*/
 class GFG {
-       
+
     /* A binary tree node has key, pointer to  
     left child and a pointer to right child */
-    static class Node { 
-        int key; 
-        Node left, right; 
-          
+    static class Node {
+        int key;
+        Node left, right;
+
         // constructor 
-        Node(int key){ 
-            this.key = key; 
-            left = null; 
-            right = null; 
-        } 
-    } 
-    static Node root; 
-    static Node temp = root; 
-      
+        Node(int key) {
+            this.key = key;
+            left = null;
+            right = null;
+        }
+    }
+
+    static Node root;
+    static Node temp = root;
+
     /* Inorder traversal of a binary tree*/
-    static void inorder(Node temp) 
-    { 
-        if (temp == null) 
-            return; 
-       
-        inorder(temp.left); 
-        System.out.print(temp.key+" "); 
-        inorder(temp.right); 
-    } 
-       
+    static void inorder(Node temp) {
+        if (temp == null)
+            return;
+
+        inorder(temp.left);
+        System.out.print(temp.key + " ");
+        inorder(temp.right);
+    }
+
     /*function to insert element in binary tree */
-    static void insert(Node temp, int key) 
-    { 
+    static void insert(Node temp, int key) {
 
         // Do level order traversal until we find 
-        // an empty place and add the node.  
-    } 
-       
+        // an empty place and add the node.
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node top = queue.poll();
+            if (top.left != null) {
+                queue.add(top.left);
+            } else {
+                top.left = new Node(key);
+                break;
+            }
+
+            if (top.right != null) {
+                queue.add(top.right);
+            } else {
+                top.right = new Node(key);
+                break;
+            }
+        }
+
+    }
+
     // Driver code 
-    public static void main(String args[]) 
-    { 
-        root = new Node(10); 
-        root.left = new Node(11); 
-        root.left.left = new Node(7); 
-        root.right = new Node(9); 
-        root.right.left = new Node(15); 
-        root.right.right = new Node(8); 
-       
-        System.out.print( "Inorder traversal before insertion:"); 
-        inorder(root); 
-       
-        int key = 12; 
-        insert(root, key); 
-       
-        System.out.print("\nInorder traversal after insertion:"); 
-        inorder(root); 
-    } 
+    public static void main(String args[]) {
+        root = new Node(10);
+        root.left = new Node(11);
+        root.left.left = new Node(7);
+        root.right = new Node(9);
+        root.right.left = new Node(15);
+        root.right.right = new Node(8);
+
+        System.out.print("Inorder traversal before insertion:");
+        inorder(root);
+
+        int key = 12;
+        insert(root, key);
+
+        System.out.print("\nInorder traversal after insertion:");
+        inorder(root);
+    }
 }
