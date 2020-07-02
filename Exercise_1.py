de3a79e8a50e56793245d4c47f269a99361763a7
Balaviknesh Sekar <bala21karthick@gmail.com>
2020-07-01 18:05:23 -0600

    Please review. Thanks.

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,32 @@
 class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self):
+        self.stack = []
+
+    def isEmpty(self):
+        if len(self.stack) == 0:
+            return True
+        else:
+            return False
+
+    def push(self, item):
+        return self.stack.append(item)
+
+    def pop(self):
+
+        if self.isEmpty():
+            print("Stack is Empty")
+        else:
+            return self.stack.pop()
+
+    def peek(self):
+        return self.stack[-1]
+
+    def size(self):
+        return len(self.stack)
+
+    def show(self):
+        return self.stack
+
 
 s = myStack()
 s.push('1')
--- Exercise_3.py
+++ Exercise_3.py
@@ -2,8 +2,12 @@ class ListNode:
     """
     A node in a singly-linked list.
     """
+
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +21,62 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        if self.head is None:
+            self.head = ListNode(data)
+        else:
+            temp = self.head
+            while temp.next:
+                temp = temp.next
+            temp.next = ListNode(data)
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        if self.head is None:
+            print("Stack is empty")
+            return
+        else:
+            temp = self.head
+            while temp:
+                if temp.data == key:
+                    return temp.data
+                else:
+                    temp = temp.next
+
+            return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head is None:
+            print("Stack is empty")
+            return
+        elif self.head.data == key:
+            self.head = self.head.next
+            return
+        else:
+            temp = self.head
+            while temp.next:
+                if temp.next.data == key:
+                    break
+                temp = temp.next
+            if temp.next is not None:
+                temp.next = temp.next.next
+                return f"item deleted {key}"
+            else:
+                print("Not Found")
+
+
+newList = SinglyLinkedList()
+
+newList.append(1)
+newList.append(2)
+newList.append(3)
+
+print(newList.remove(2))
+print(newList.find(3))
