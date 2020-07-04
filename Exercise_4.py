b16101f6eeb811f2cb3f28ab862cd98131f5f389
hackerrank <garima.adlakha2450@gmail.com>
2020-07-04 00:55:26 -0700

    Edge Case-Exercise_4

--- Exercise_4.py
+++ Exercise_4.py
@@ -2,44 +2,55 @@
 # Python program to insert element in binary tree  
 class newNode():  
   
-    def __init__(self, data):  
+    def __init__(self, data=None): 
+    
         self.key = data 
         self.left = None
         self.right = None
           
 """ Inorder traversal of a binary tree"""
-def inorder(temp): 
+def inorder(temp):
+  if not temp:
+    return
   if temp:
-      inorder(temp.left)
+      if temp.left:
+        inorder(temp.left)
       print(temp.key)
-      inorder(temp.right)    
+      if temp.right:
+        inorder(temp.right)    
   
   
 """function to insert element in binary tree """
 def insert(temp,key):
-    #print(temp) 
-    list = []  
-    list.append(temp)   
-    while (len(list)):  
-        temp = list[0]  
-        list.pop(0)  
-  
-        if not temp.left: 
-            temp.left = newNode(key)  
-            break
-        else: 
-            list.append(temp.left)  
-  
-        if not temp.right: 
-            temp.right = newNode(key)  
-            break
-        else: 
-            list.append(temp.right)
+    #to check None case
+    if temp==None:
+        temp=newNode(key)
+        #print(temp.key)
+    else:  
+        list = []  
+        list.append(temp)   
+        while (len(list)):  
+            temp = list[0]  
+            list.pop(0)  
+      
+            if not temp.left: 
+                temp.left = newNode(key)  
+                break
+            else: 
+                list.append(temp.left)  
+      
+            if not temp.right: 
+                temp.right = newNode(key)  
+                break
+            else: 
+                list.append(temp.right)
     
   
        
 # Driver code  
 if __name__ == '__main__': 
+    #to check empty case
+    #root=newNode()
     root = newNode(10)  
     root.left = newNode(11)  
     root.left.left = newNode(7)  
