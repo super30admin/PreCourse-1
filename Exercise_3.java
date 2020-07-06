bd67ffa27251742f1d64b4708da3ed1056d0fdc4
Lavanya <lavanya.ramaswamy@sjsu.edu>
2020-07-06 11:43:53 -0700

    Completed Sprint Precourse1

--- Exercise_3.java
+++ Exercise_3.java
@@ -26,7 +26,25 @@ public class LinkedList {
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
     { 
+
+        Node node = new Node(data);
+        head = list;
+       
         // Create a new node with given data 
+        if (head == null) {
+            head = node;
+            return head;
+        }
+        else{
+            Node n = head;
+        }
+
+        while(n && n.next != null) {
+            n = n.next;
+        }
+        n.next = node;
+
+        return head;
    
         // If the Linked List is empty, 
         // then make the new node as head 
@@ -42,7 +60,13 @@ public class LinkedList {
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
+        Node node = head;
+        while(node.next!= null){
+            System.out.print(node.data);
+            node = node.next;
+        }
         // Traverse through the LinkedList 
+
    
             // Print the data at current node 
        
