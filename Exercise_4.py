17270e409d63e447ad352b6803142dfe263964a0
kartavyabhatts30 <bkartavya07@gmail.com>
2020-07-05 20:17:41 -0400

    Completed Precourse_1

--- Exercise_4.py
+++ Exercise_4.py
@@ -8,15 +8,29 @@ class newNode():
           
 """ Inorder traversal of a binary tree"""
 def inorder(temp): 
-  
-    
-  
+    if temp is not None:
+        inorder(temp.left)
+        print(temp.key)
+        inorder(temp.right)
   
 """function to insert element in binary tree """
-def insert(temp,key): 
-  
-    
-  
+def insert(temp,key):
+    queue = []
+    queue.append(temp)
+
+    while len(queue) != 0:
+        curr = queue.pop(0)
+        if curr.left is not None:
+            queue.append(curr.left)
+        else:
+            curr.left = newNode(key)
+            break
+        if curr.right is not None:
+            queue.append(curr.right)
+        else:
+            curr.right = newNode(key)
+            break
+
        
 # Driver code  
 if __name__ == '__main__': 
