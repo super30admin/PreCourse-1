bd8e7d5db84374484fa8a00b49baddfaf011c3a1
Bhanu Kiran Atturu <atturu@usc.edu>
2020-08-08 13:13:43 -0700

    Completed E1

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,29 +5,46 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        return top == -1;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        //Overflow condition
+        if(top == MAX-1){
+            return false;
+        }
+        else{
+            a[++top] = x;
+            return true;
+        }
     } 
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if (top == -1) { 
+            System.out.println("Stack Underflow"); 
+            return 0; 
+        } 
+        else { 
+            int x = a[top--]; 
+            return x; 
+        } 
     } 
   
     int peek() 
     { 
-        //Write your code here
+        if (top == -1)
+        } 
+        else { 
+            int x = a[top--]; 
+            return x; 
+        } 
     } 
 } 
   
