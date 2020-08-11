5390d76dbd7b7059c7863f45f4b298792d06c845
vinaykhedekar21 <vinay.khedekar21@gmail.com>
2020-08-11 10:30:53 -0700

    Completed Sprint Precourse_1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,52 +1,91 @@
-public class StackAsLinkedList { 
+public class Exercise_2 { 
   
     StackNode root; 
   
     static class StackNode { 
         int data; 
+        
         StackNode next; 
   
         StackNode(int data) 
         { 
             //Constructor here 
+        	this.data=data;
+        	this.next=null;
         } 
     } 
     
 	
     public boolean isEmpty() 
-    { 
+    {
+		
         //Write your code here for the condition if stack is empty. 
+    	if(root==null)
+    	{
+    		System.out.println("Empty stack");
+    		return true;
+    	}
+    	return false;
     } 
   
     public void push(int data) 
     { 
         //Write code to push data to the stack. 
+    	StackNode newNode= new StackNode(data);
+    	if(root==null) {
+    		root=newNode;
+    		
+    		return;
+    	}
+    	newNode.next=root;
+    	root=newNode;
+       	
     } 
   
-    public int pop() 
-    { 	
+    public int pop()
+    {
+		
 	//If Stack Empty Return 0 and print "Stack Underflow"
+		if(root==null) {
+			System.out.println("Stack Underflow");
+			return 0;
+		}
         //Write code to pop the topmost element of stack.
+		StackNode temp=root;
+		root=temp.next;
 	//Also return the popped element 
+		return temp.data;
     } 
   
     public int peek() 
-    { 
+    {
+		if(root==null) {
+			System.out.println("Stack is Empty");
+			return 0;
+		}
         //Write code to just return the topmost element without removing it.
+    	return root.data;
     } 
   
 	//Driver code
     public static void main(String[] args) 
     { 
   
-        StackAsLinkedList sll = new StackAsLinkedList(); 
+        Exercise_2 sll = new Exercise_2(); 
   
         sll.push(10); 
-        sll.push(20); 
-        sll.push(30); 
-  
-        System.out.println(sll.pop() + " popped from stack"); 
-  
         System.out.println("Top element is " + sll.peek()); 
+        sll.push(20); 
+        System.out.println("Top element is " + sll.peek()); 
+        sll.push(30); 
+        System.out.println("Stack isempty : " + sll.isEmpty()); 
+        System.out.println("Top element is " + sll.peek()); 
+        System.out.println(sll.pop() + " popped from stack"); 
+        System.out.println(sll.pop() + " popped from stack"); 
+        System.out.println(sll.pop() + " popped from stack"); 
+        
+        System.out.println("Top element is " + sll.peek()); 
+        System.out.println("Stack isempty :" + sll.isEmpty()); 
     } 
+    
 } 
--- Exercise_4.java
+++ Exercise_4.java
@@ -1,9 +1,9 @@
 import java.util.LinkedList; 
 import java.util.Queue; 
-public class GFG { 
+public class Exercise_4 { 
        
-    /* A binary tree node has key, pointer to  
-    left child and a pointer to right child */
+    // A binary tree node has key, pointer to  
+   // left child and a pointer to right child 
     static class Node { 
         int key; 
         Node left, right; 
@@ -17,8 +17,9 @@ public class GFG {
     } 
     static Node root; 
     static Node temp = root; 
+     static Queue<Node> que = new LinkedList<Node>();
       
-    /* Inorder traversal of a binary tree*/
+    // Inorder traversal of a binary tree
     static void inorder(Node temp) 
     { 
         if (temp == null) 
@@ -29,11 +30,29 @@ public class GFG {
         inorder(temp.right); 
     } 
        
-    /*function to insert element in binary tree */
+    //function to insert element in binary tree 
     static void insert(Node temp, int key) 
     { 
-
+    	Node newNode=new Node(key);
         // Do level order traversal until we find 
+    	if(temp!=null) {
+    		que.add(temp);
+    	}
+    	while(!que.isEmpty()) {
+    	
+    	Node ex=que.remove();
+    	if(ex.left==null) {
+    		ex.left=newNode;
+    		break;
+    	}
+    	if(ex.right==null)
+    	{
+    		ex.right=newNode;
+    		break;
+    	}
+    	que.add(temp.left);
+    	que.add(temp.right);
+    	}
         // an empty place and add the node.  
     } 
        
@@ -47,13 +66,13 @@ public class GFG {
         root.right.left = new Node(15); 
         root.right.right = new Node(8); 
        
-        System.out.print( "Inorder traversal before insertion:"); 
+       System.out.print( "Inorder traversal before insertion:"); 
         inorder(root); 
        
         int key = 12; 
         insert(root, key); 
        
-        System.out.print("\nInorder traversal after insertion:"); 
-        inorder(root); 
+       System.out.print("\nInorder traversal after insertion:"); 
+     inorder(root); 
     } 
 }
