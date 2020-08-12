8e8e0f229b51f9c2b9a30c2c97ab5d6fab330c5a
csus <csus@UL-ARC1003-476.saclink.csus.edu>
2020-08-12 14:43:35 -0700

    Complted PreCourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -6,31 +6,51 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        if(top==-1)
+        return true;
+        else return false;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top=-1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(MAX == top+1){            
+            return false;
+        }
+
+        else{
+            a[++top]=x;
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(isEmpty()){
+            System.out.println(" Stack Underflow");
+            return 0;
+        }
+        else {
+            return a[top--];
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        return a[top];
     } 
 } 
-  
+
 // Driver code 
 class Main { 
     public static void main(String args[]) 
@@ -39,6 +59,11 @@ class Main {
         s.push(10); 
         s.push(20); 
         s.push(30); 
+        s.push(40); 
+        s.push(50); 
+        s.push(60); 
+       
         System.out.println(s.pop() + " Popped from stack"); 
+        
     } 
 }
