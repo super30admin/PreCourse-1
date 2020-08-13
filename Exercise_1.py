82ba6be396d3fb5f910d2068d2f426169b41fd9c
Rohit gorle <61993839+rohit-gorle@users.noreply.github.com>
2020-08-12 18:50:55 -0700

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,29 @@
 class myStack:
      def __init__(self):
-         
+         self.arr = []
+         self.length = 0
+
      def isEmpty(self):
-         
+         return None
+
      def push(self, item):
-         
+         self.arr.append(item)
+         self.length += 1
+
      def pop(self):
-        
-        
+        popped_item = self.arr[self.length-1]
+        del self.arr[self.length-1]
+        self.length -= 1
+        return popped_item
+
      def peek(self):
+       return self.arr[self.length-1]
         
      def size(self):
-         
+       return self.length-1
+
      def show(self):
+       return self.arr
          
 
 s = myStack()
