2269547edc9487afe674319fed140019b623b08c
NithyaKuchadi <49386822+NithyaKuchadi@users.noreply.github.com>
2020-08-12 17:34:05 -0700

    Precourse 1

--- Exercise_3.java
+++ Exercise_3.java
@@ -2,38 +2,41 @@ import java.io.*;
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
-  
-    Node head; // head of list 
-  
-    // Linked list Node. 
-    // This inner class is made static 
-    // so that main() can access it 
+class LinkedList { 
+	  
+    static Node head; 
+    static Node temp = head;
     static class Node { 
   
         int data; 
         Node next; 
   
-        // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+           data = d;
         } 
     } 
   
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
     { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
-
-            // Insert the new_node at last node 
-        // Return the list by head 
+    	
+    	
+    	Node n = new Node(data);
+    	
+    	if(head == null)
+    		head = n;
+    	else
+    	{
+    		 temp = head;
+    		while(temp.next != null)
+    		{
+    			temp = temp.next;
+    		}
+    		temp.next = n;
+    		
+    	}
+    	return list;	
         
     } 
   
@@ -41,10 +44,14 @@ public class LinkedList {
     public static void printList(LinkedList list) 
     {  
         // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+    	 temp = head;
+    	
+    	while(temp != null)
+    	{
+    		System.out.println(temp.data);
+    		temp = temp.next;
+    	}
+    
     } 
    
     // Driver code 
