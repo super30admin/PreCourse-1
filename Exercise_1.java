0d958c4fa2960d6e649708c0a8300c832ec7512a
gssanjana4 <gssanjana4@users.github.com>
2020-08-16 17:05:43 -0700

    Completed Sprint Precourse1 - Please review

--- Exercise_1.java
+++ Exercise_1.java
@@ -6,28 +6,50 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+    	if(top<0)
+    		return true;
+    	return false;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+    	top = -1;
+        //Initialize your constructor
+    	
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+    	if(top<=MAX)
+    	{
+    		a[++top] = x;
+    		System.out.println("Pushed " + x + " into stack");
+    		return true;
+    	}
+    	System.out.println("Stack Overflow");
+    	return false;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+    	if(top>=0 && top<=MAX) {
+    		return a[top--];
+    	}
+    	System.out.println("Stack Underflow");
+    	return 0;
+    	
     } 
   
     int peek() 
     { 
         //Write your code here
+    	if(top>=0 || top <= MAX)
+    		return a[top];
+    	return -1;
     } 
 } 
   
@@ -40,5 +62,8 @@ class Main {
         s.push(20); 
         s.push(30); 
         System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.peek() + " Top element from stack"); 
+
+        
     } 
 }
