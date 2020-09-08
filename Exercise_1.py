4fc976bc4b0a2f0d9e83c66feb3b309e76ae5a34
Lakshya0744 <lgarg@asu.edu>
2020-09-08 11:39:27 -0700

    Asg1 Completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,31 @@
 class myStack:
      def __init__(self):
-         
+         self.arr=[]
+     
+     
      def isEmpty(self):
-         
+          if self.arr==[]:
+              return True
+          else:
+               return False
      def push(self, item):
+          self.arr.append(item)
          
      def pop(self):
-        
+        self.arr.pop()
         
      def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+        return self.arr[-1]
 
+     def size(self):
+        return len(self.arr) 
+
+     def show(self):
+          return self.arr
+          
 s = myStack()
 s.push('1')
 s.push('2')
-print(s.pop())
+s.push('100')
 print(s.show())
+print(s.peek())
