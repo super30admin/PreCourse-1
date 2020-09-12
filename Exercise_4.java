b219b0cdfbc6297091221978e1c806448f314def
Hanish Talasila <t.hanishsai@gmail.com>
2020-09-11 23:03:24 -0600

    Update Exercise_4.java

--- Exercise_4.java
+++ Exercise_4.java
@@ -32,10 +32,26 @@ public class GFG {
     /*function to insert element in binary tree */
     static void insert(Node temp, int key) 
     { 
-
+        Queue<Node> q = new LinkedList<Node>();
+           q.add(temp);
         // Do level order traversal until we find 
         // an empty place and add the node.  
+           while(!q.isEmpty()){
+                  temp = q.peek();
+                  q.remove();
+                  
+                  if(temp.left == null){
+                         temp.left = new Node(key);
+                         break;
+                  }else
+                         q.add(temp.left);
+                  if(temp.right == null){
+                         temp.right = new Node(key);
+                         break;
+                  }else
+                         q.add(temp.right);
     } 
+    }
        
     // Driver code 
     public static void main(String args[]) 
