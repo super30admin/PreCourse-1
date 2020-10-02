ce0e859cea1bad3acae4ae2382fd1a4cc1e694e8
Amrutha2509 <56801612+Amrutha2509@users.noreply.github.com>
2020-10-01 22:53:47 -0400

    Implemented

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,34 @@
 class myStack:
     #initialize
      def __init__(self):
+        self.s = [] #initialize Stack
 
      def isEmpty(self):
+         return len(self.s)==0
 
      def push(self, item):
+         self.s.append(item)
 
      def pop(self):
 
+         if (self.isEmpty()):
+             print("Stack Underflow")
+         else:
+            print(self.s[len(self.s)-1])
+            self.s = self.s[:len(self.s)-1]#remove the element at top of stack pop(0)
+
 
      def peek(self):
+         if (self.isEmpty()):
+             print("Stack Underflow")
+         else:
+            print(self.s[len(self.s)-1])
 
      def size(self):
+         print(len(self.s))
 
      def show(self):
+         print(self.s)
 
 
 s = myStack()
@@ -21,3 +36,6 @@ s.push('1')
 s.push('2')
 print(s.pop())
 print(s.show())
+
+#Time complexity: O(N)
+#Space complexity: O(N)
