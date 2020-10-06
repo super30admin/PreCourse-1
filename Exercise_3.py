514da8a760ce9bbce4a57d6480187ed2471f2917
samarthaswaroopg <girish.s@husky.neu.edu>
2020-10-06 13:35:36 -0400

    Execrise 3.py and Exercise 4.py completed

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,17 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+        
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +28,9 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new_node = Node(data)
+        new_node.set_next(self.head)
+        self.head = new_node
         
     def find(self, key):
         """
@@ -24,9 +38,36 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        current = self.head
+        found = False
+        while current and found is False:
+            if current.get_data() == key:
+                found = True
+            else:
+                current = current.get_next()
+        if current is None:
+            raise ValueError("Key not in list")
+        return current
+                
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        current = self.head
+        previous = None
+        found = False
+        while current and found is False:
+            if current.get_data() == key:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+        if current is None:
+            raise ValueError("Key not in list")
+        if previous is None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
+            
