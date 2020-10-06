97a591128f62162917b9e1409bf282f3273e924e
samarthaswaroopg <girish.s@husky.neu.edu>
2020-10-05 22:29:07 -0400

    Exercise1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,18 @@
 class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
+    def __init__(self):
+        self.items = []   
+    def isEmpty(self):
+        return self.items == []     
+    def push(self, item):
+        self.items.insert(0,item)        
+    def pop(self):
+        return self.items.pop(0)              
+    def peek(self):
+        return self.items[0]       
+    def size(self):
+        return len(self.items)       
+    def show(self):
+        return self.items
          
 
 s = myStack()
