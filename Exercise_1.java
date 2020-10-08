0ea1c598c536350a456ea91de5f1db6bb0714eb3
Achyuth72 <68582633+Achyuth72@users.noreply.github.com>
2020-10-07 19:52:43 -0600

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,36 +1,72 @@
 class Stack { 
     static final int MAX = 1000; 
     int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
+    int a[] = new int[MAX]; 
+    // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
         //Write your code here 
+        if(top==-1)
+        {
+            return true;
+        }
+        return false;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+       top=-1;//Initialize your constructor 
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
-        //Write your code here
+        if(top<MAX-1)
+        {
+            a[++top]=x;
+            System.out.println(x + " pushed into stack");
+            return true;
+        }
+        else
+        {
+            System.out.print("Stack Overflow");
+        }
+		return false;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top>0)
+        {
+            return a[top--];
+        
+        }
+        else
+        {
+            System.out.print("Stack Underflow");
+            return -1;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+         if(top>0)
+        {
+            return a[top];
+        }
+        else
+        {
+            return -1;
+        }
+        
     } 
-} 
   
+// Driver code 
+}
 // Driver code 
 class Main { 
     public static void main(String args[]) 
