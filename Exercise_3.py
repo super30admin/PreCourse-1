a79668f2d0bcc1c279e49d492c384e2380c77296
ashrit deebadi <dashrit@yahoo.com>
2020-10-11 23:00:29 -0700

    First commit

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,8 +2,12 @@ class ListNode:
     """
     A node in a singly-linked list.
     """
+
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +21,46 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        new_node = ListNode(data)
+        new_node.next(self.head)
+        self.head = new_node
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+
+        current = self.head
+        found = False
+        while current and found is False:
+            if current.data == key:
+                found = True
+            else:
+                current = current.next()
+        if current is None:
+            raise ValueError("Data not in list")
+        return current
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+
+        current = self.head
+        previous = None
+        found = False
+        while current and found is False:
+            if current.data == key:
+                found = True
+        else:
+            previous = current
+            current = current.next
+        if current is None:
+            return None
+        if previous is None:
+            self.head = current.next
+        else:
+            previous.next = current.next
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,38 +1,53 @@
-# Python program to insert element in binary tree  
-class newNode():  
-  
-    def __init__(self, data):  
-        self.key = data 
+# Python program to insert element in binary tree
+class newNode():
+
+    def __init__(self, data):
+        self.key = data
         self.left = None
         self.right = None
-          
+
+
 """ Inorder traversal of a binary tree"""
-def inorder(temp): 
-  
-    
-  
-  
+
+
+def inorder(temp):
+    if temp != None:
+        inorder(temp.left)
+        print(temp.key)
+        inorder(temp.right)
+
+
 """function to insert element in binary tree """
-def insert(temp,key): 
-  
-    
-  
-       
-# Driver code  
-if __name__ == '__main__': 
-    root = newNode(10)  
-    root.left = newNode(11)  
-    root.left.left = newNode(7)  
-    root.right = newNode(9)  
-    root.right.left = newNode(15)  
-    root.right.right = newNode(8)  
-  
-    print("Inorder traversal before insertion:", end = " ") 
-    inorder(root)  
-  
+
+
+def insert(temp, key):
+
+    if temp is None:
+        return newNode(key)
+    else:
+        if temp.key == key:
+            return temp
+        elif temp.key < key:
+            temp.right = insert(temp.right, key)
+        else:
+            temp.left = insert(temp.left, key)
+    return temp
+
+    # Driver code
+if __name__ == '__main__':
+    root = newNode(10)
+    root.left = newNode(11)
+    root.left.left = newNode(7)
+    root.right = newNode(9)
+    root.right.left = newNode(15)
+    root.right.right = newNode(8)
+
+    print("Inorder traversal before insertion:", end=" ")
+    inorder(root)
+
     key = 12
-    insert(root, key)  
-  
-    print()  
-    print("Inorder traversal after insertion:", end = " ") 
-    inorder(root) 
+    insert(root, key)
+
+    print()
+    print("Inorder traversal after insertion:", end=" ")
+    inorder(root)
