2943aa9f16fc10b6264ec383c2adaed7fd373416
ishansshah20 <ishansshah2020@gmail.com>
2020-11-08 18:40:24 -0600

    PreCourse_1 completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,28 @@
+from collections import deque
+
 class myStack:
      def __init__(self):
-         
+         self.stack = deque()
+
      def isEmpty(self):
-         
+          if self.size == 0:
+               return True
+          return False
+
      def push(self, item):
-         
+          self.stack.appendleft(item)
+
      def pop(self):
-        
-        
+          return self.stack.popleft()
+
      def peek(self):
-        
+          return self.stack[0]
+
      def size(self):
-         
+          return len(self.stack)
+
      def show(self):
-         
+          return self.stack
 
 s = myStack()
 s.push('1')
--- Exercise_2.py
+++ Exercise_2.py
@@ -5,21 +5,43 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.head = Node(None)
+        self.top = self.head.next
+
     def push(self, data):
-        
+        node = Node(data)
+        node.next = self.head.next
+        self.head.next = node
+        self.top = self.head.next
+
     def pop(self):
-        
+        val = self.top
+        if val is None:
+            return None
+        self.head.next = self.top.next
+        self.top = self.head.next
+        return val.data
+
+    def print_stack(self):
+        curr = self.head.next
+        while curr:
+            print(curr.data, end = "->")
+            curr = curr.next
+        print("\n")
+
 a_stack = Stack()
 while True:
     print('push <value>')
     print('pop')
+    print('print')
     print('quit')
     do = input('What would you like to do? ').split()
  
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
+    elif operation == 'print':
+        a_stack.print_stack()
     elif operation == 'pop':
         popped = a_stack.pop()
         if popped is None:
