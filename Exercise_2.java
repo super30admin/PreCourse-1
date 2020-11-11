5a767484c5a7ae9c3b6e417333e31a498842994d
Vineet Shah <shahvineet95@gmail.com>
2020-11-11 18:48:36 -0500

    Completed Sprint Precourse1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,45 +1,74 @@
-public class StackAsLinkedList { 
-  
-    StackNode root; 
-  
-    static class StackNode { 
-        int data; 
-        StackNode next; 
-  
-        StackNode(int data) 
-        { 
-            //Constructor here 
-        } 
-    } 
-    
-	
-    public boolean isEmpty() 
-    { 
-        //Write your code here for the condition if stack is empty. 
-    } 
-  
-    public void push(int data) 
-    { 
-        //Write code to push data to the stack. 
-    } 
-  
-    public int pop() 
-    { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
-    } 
-  
-    public int peek() 
-    { 
-        //Write code to just return the topmost element without removing it.
-    } 
-  
-	//Driver code
-    public static void main(String[] args) 
-    { 
-  
-        StackAsLinkedList sll = new StackAsLinkedList(); 
+//StackAsLinkedList
+public class Exercise_2 {
+
+    StackNode root;
+
+    static class StackNode {
+        int data;
+        StackNode next;
+
+        StackNode(int data) {
+            // Constructor here
+            this.data = data;
+
+        }
+    }
+
+    public boolean isEmpty() {
+        // Write your code here for the condition if stack is empty.
+        if (root == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(int data) {
+        // Write code to push data to the stack.
+        StackNode newStackNode = new StackNode(data);
+        if (root == null) {
+            //If nothing is present in the list make the current element the root node
+            root = newStackNode;
+        } else {
+            //If List is not empty, push the incoming element at the start of the list
+            StackNode temp = root;
+            root = newStackNode;
+            newStackNode = temp;
+        }
+        System.out.println(data + " pushed to the stack");
+    }
+
+    public int pop() {
+        // If Stack Empty Return 0 and print "Stack Underflow"
+        //Initialize a minValue with the lowest most value
+        int minValue = Integer.MIN_VALUE;
+        if (root == null) {
+            //If there is no element in list
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            //if value present remove the data in the front of the list because stack is LIFO
+            minValue = root.data;
+            //make the 2nd element in the list the root element
+            root = root.next;
+        }
+        return minValue;
+    }
+
+    public int peek() {
+        if (root == null) {
+            System.out.println("Stack is Empty");
+            return Integer.MIN_VALUE;
+        } else {
+            // return the topmost element without removing it.
+            return root.data;
+        }
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+
+        Exercise_2 sll = new Exercise_2();
   
         sll.push(10); 
         sll.push(20); 
--- Exercise_4.java
+++ Exercise_4.java
@@ -1,6 +1,6 @@
 import java.util.LinkedList; 
 import java.util.Queue; 
-public class GFG { 
+public class Exercise_4 {
        
     /* A binary tree node has key, pointer to  
     left child and a pointer to right child */
@@ -32,9 +32,34 @@ public class GFG {
     /*function to insert element in binary tree */
     static void insert(Node temp, int key) 
     { 
-
+        if(temp == null){
+            root = new Node(key);
+            return;
+        }
+        Queue<Node> queue = new LinkedList<Node>();
+        //adding the temp element in the queue
+        queue.add(temp);
         // Do level order traversal until we find 
         // an empty place and add the node.  
+        while(!queue.isEmpty()){
+
+            //frontmost element in the queue is removed and we search for its left and right nodes
+            temp = queue.peek();
+            //we remove the front element
+            queue.remove();
+            if(temp.left == null){//If there is no left element present we add or current node 
+                temp.left = new Node(key);
+                break;
+            }else{
+                queue.add(temp.left); // If left node is present we add it to our queue for level order
+            }
+            if(temp.right == null){//If there is no right element present we add or current node 
+                temp.right = new Node(key);
+            }else{
+                queue.add(temp.right);// If right node is present we add it to our queue for level order
+            }
+
+        }
     } 
        
     // Driver code 
