078a91d1d0e84e4afaf073ede4742dcc24536ca9
Vasundhara Venkata Krishna Nolastname (Student) <vvvenka1@sundevils.asu.edu>
2020-11-11 14:13:04 -0700

    completed precourse1 exercise

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -10,7 +10,7 @@ class Stack {
 public: 
     int a[MAX]; // Maximum size of Stack 
   
-    Stack() { //Constructor here } 
+    Stack() {top = -1;} 
     bool push(int x); 
     int pop(); 
     int peek(); 
@@ -21,22 +21,57 @@ bool Stack::push(int x)
 { 
     //Your code here
     //Check Stack overflow as well
+    if(top>=(MAX-1)){
+    	cout<<"Stack Overflow\n";
+    	return false;
+	}
+	else{
+	    top = top+1;
+		a[top]=x;
+		cout<<x<<" Is inserted\n";
+		return true;
+	}
 } 
   
 int Stack::pop() 
 { 
     //Your code here
     //Check Stack Underflow as well 
+    if(top<0){
+    	cout<<"Stack underflow\n";
+    	return 0;
+	}
+	else{
+		int x = a[top];
+		top = top-1;
+		return x;
+	}
 } 
 int Stack::peek() 
 { 
     //Your code here
     //Check empty condition too
+    if(top<0){
+    	cout<<"Stack empty, cant peek\n";
+    	return 0;
+	}
+	else{
+		int x = a[top];
+		return x;
+	}
 } 
   
 bool Stack::isEmpty() 
 { 
     //Your code here 
+    if(top <0){
+        cout<<"is empty";
+    	return true;
+	}
+	else{
+	    cout<<"is not empty";
+		return false;
+	}
 } 
   
 // Driver program to test above functions 
@@ -46,7 +81,12 @@ int main()
     s.push(10); 
     s.push(20); 
     s.push(30); 
-    cout << s.pop() << " Popped from stack\n"; 
+    cout << s.pop() << " Popped from stack\n";
+    //cout << s.pop() << " Popped from stack\n";
+    //cout << s.pop() << " Popped from stack\n";
+    //cout << s.pop() << " Popped from stack\n";
+    //s.peek();
+    //s.isEmpty();
   
     return 0; 
 } 
