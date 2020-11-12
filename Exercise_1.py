bb1cd76a982ee0d1988e322a58dc280fff307b5f
saachi493 <61569548+saachi493@users.noreply.github.com>
2020-11-11 19:53:58 -0600

    Stack Implementation Question 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,32 @@
 class myStack:
      def __init__(self):
-         
+         self.items = []
      def isEmpty(self):
-         
+         flag = False
+         if (len(self.items)==0):
+             flag = True
+         return flag
+
      def push(self, item):
-         
+         self.items.append(item)
+         print("New item pushed", item)
+          
      def pop(self):
+         pos = len(self.items)
+         self.items.pop(pos-1)
+         print("One element has been poped from the stack")
         
         
      def peek(self):
+         item = self.items[0]
+         print("The topmost element in the Stack is:", item)
         
      def size(self):
+         print("Size of the Stack is:", len(self.items))
          
      def show(self):
+         print("Stack elements are:")
+         print(self.items)
          
 
 s = myStack()
