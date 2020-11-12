6e30665ef6d49b243af1cac03eaab11fa1f237f8
Harsh Pandya <hpandya@gleason.com>
2020-11-11 23:21:43 -0500

    Update ex1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,37 @@
 class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self):
+        stack = []
+        self.stack = stack
 
-s = myStack()
-s.push('1')
-s.push('2')
-print(s.pop())
-print(s.show())
+    def isEmpty(self):
+        return not self.stack
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.stack:
+            item = self.stack.pop()
+            return item
+        # print("Cannot remove from empty stack.")
+        return None
+
+    def peek(self):
+        if self.stack:
+            return self.stack[-1]
+        # print("Cannot peek into an empty stack.")
+        return None
+
+    def size(self):
+        return len(self.stack)
+
+    def show(self):
+        return self.stack
+
+
+if __name__ == "__main__":
+    s = myStack()
+    s.push('1')
+    s.push('2')
+    print(s.pop())
+    print(s.show())
