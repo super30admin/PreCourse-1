ed722e7d70b6d8eff19e4fb2c8555fd664594384
kritagya nepal <kritagyan@gmail.com>
2020-11-12 00:55:38 -0600

    Completed Sprint PreCourse1

--- Exercise_4.py
+++ Exercise_4.py
@@ -7,13 +7,37 @@ class newNode():
         self.right = None
           
 """ Inorder traversal of a binary tree"""
-def inorder(temp): 
-  
-    
-  
+def inorder(temp):
+    if temp:
+        inorder(temp.left)
+        print(temp.key)
+        inorder(temp.right)
+      
   
 """function to insert element in binary tree """
-def insert(temp,key): 
+def insert(temp,key):
+    if temp == None:
+        root = newNode(key)
+    else:
+       q = [] 
+       q.append(temp) 
+ 
+    while (len(q)): 
+        temp = q[0] 
+        q.pop(0) 
+ 
+        if (not temp.left):
+            temp.left = newNode(key) 
+            break
+        else:
+            q.append(temp.left) 
+ 
+        if (not temp.right):
+            temp.right = newNode(key) 
+            break
+        else:
+            q.append(temp.right)
+
   
     
   
