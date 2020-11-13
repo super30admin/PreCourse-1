29e9f5c4789621d6f2c30da46e51991f3cecc021
Helisha <helisha.sangani@gmail.com>
2020-11-13 13:20:54 -0700

    Completed sprint precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,34 @@
 class myStack:
      def __init__(self):
+          self.stack = []
+          self.n = 0
          
      def isEmpty(self):
+          if self.n == 0:
+               return True
+          return False
          
      def push(self, item):
+          self.stack.append(item)
+          self.n += 1
          
      def pop(self):
-        
+          if self.isEmpty():
+               return None
+          self.n -= 1
+          return self.stack.pop()
         
      def peek(self):
-        
+          if self.isEmpty():
+               return None
+          return self.stack[-1]
+
      def size(self):
+          return self.n
          
      def show(self):
+          return self.stack
+
          
 
 s = myStack()
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,22 +1,49 @@
 # Python program to insert element in binary tree  
 class newNode():  
-  
+
     def __init__(self, data):  
         self.key = data 
         self.left = None
         self.right = None
-          
+
+
 """ Inorder traversal of a binary tree"""
-def inorder(temp): 
-  
-    
-  
+def inorder(temp):
+    inorder_travarsal = []
+    stack = []
+    cur = root
+    while cur or stack:
+        while cur:
+            stack.append(cur)
+            cur = cur.left
+        cur = stack.pop()
+        inorder_travarsal.append(cur.key)
+        cur = cur.right
+    print(inorder_travarsal)
+    return inorder_travarsal
+
   
 """function to insert element in binary tree """
 def insert(temp,key): 
+    if temp == None:
+        return newNode(key)
+    q = [temp]
+    while q:
+        x = q.pop(0)
+        if x.left == None:
+            x.left = newNode(key)
+            return True
+        else:
+            q.append(x.left)
+        if x.right == None:
+            x.right = newNode(key)
+            return True
+        else:
+            q.append(x.right)
+    
+
   
     
-  
        
 # Driver code  
 if __name__ == '__main__': 
