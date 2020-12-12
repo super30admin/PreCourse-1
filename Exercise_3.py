ce2ab8c110c2c39cf87651b5074f32f2f51ac99d
amby2964 <75859704+amby2964@users.noreply.github.com>
2020-12-12 12:04:54 -0700

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,7 +3,9 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +19,70 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        if(self.head == None):
+            self.head = ListNode(data)
+        else:
+            ll = self.head
+            while(ll.next != None):
+                ll = ll.next
+            ll.next = ListNode(data)
+    
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        ll = self.head
+        while(ll != None):
+            if(ll.data == key):
+                return ll
+            ll = ll.next
+        print("Element not Found")
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if(self.head == None):
+            print("LL is empty")
+            return None
+        ll = self.head
+        # first node check
+        if(ll.data == key):
+            print("Element Removed: " + str(key))
+            self.print()
+            self.head = self.head.next
+            return ll
+        while(ll.next != None):
+            if(ll.next.data == key):
+                removed_node = ll.next
+                ll.next = removed_node.next
+                removed_node.next = None
+                print("Element Removed: " + str(key))
+                self.print()
+                return removed_node
+            ll = ll.next
+        print("Element Not Found to remove")
+        return None
+    
+    def print(self):
+        ll = self.head
+        while(ll != None):
+            print(ll.data, end=",")
+            ll = ll.next
+        print("")
+
+linkedlist =  SinglyLinkedList()
+linkedlist.append(1)
+linkedlist.append(2)
+linkedlist.append(3)
+linkedlist.append(4)
+print(linkedlist.find(3).data)
+linkedlist.remove(3)
+linkedlist.remove(4)
+linkedlist.remove(1)
+linkedlist.remove(7)
+linkedlist.print()
