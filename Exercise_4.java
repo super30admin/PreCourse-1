f56030bed6b321144035398d9f335b407f5990b0
Abhishek Chaudhary <abhishek.chaudhary.2812@gmail.com>
2020-12-15 00:12:20 -0600

    Completed Precourse1 code

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,59 +1,97 @@
-import java.util.LinkedList; 
-import java.util.Queue; 
-public class GFG { 
-       
-    /* A binary tree node has key, pointer to  
-    left child and a pointer to right child */
-    static class Node { 
-        int key; 
-        Node left, right; 
-          
-        // constructor 
-        Node(int key){ 
-            this.key = key; 
-            left = null; 
-            right = null; 
-        } 
-    } 
-    static Node root; 
-    static Node temp = root; 
-      
-    /* Inorder traversal of a binary tree*/
-    static void inorder(Node temp) 
-    { 
-        if (temp == null) 
-            return; 
-       
-        inorder(temp.left); 
-        System.out.print(temp.key+" "); 
-        inorder(temp.right); 
-    } 
-       
-    /*function to insert element in binary tree */
-    static void insert(Node temp, int key) 
-    { 
+import java.util.LinkedList;
+import java.util.Queue;
+class GFG {
 
-        // Do level order traversal until we find 
-        // an empty place and add the node.  
-    } 
-       
-    // Driver code 
-    public static void main(String args[]) 
-    { 
-        root = new Node(10); 
-        root.left = new Node(11); 
-        root.left.left = new Node(7); 
-        root.right = new Node(9); 
-        root.right.left = new Node(15); 
-        root.right.right = new Node(8); 
-       
-        System.out.print( "Inorder traversal before insertion:"); 
-        inorder(root); 
-       
-        int key = 12; 
-        insert(root, key); 
-       
-        System.out.print("\nInorder traversal after insertion:"); 
-        inorder(root); 
-    } 
+    /* A binary tree node has key, pointer to
+    left child and a pointer to right child */
+    static class Node {
+        int key;
+        Node left, right;
+
+        // constructor
+        Node(int key){
+            this.key = key;
+            left = null;
+            right = null;
+        }
+    }
+    static Node root;
+    static Node temp = root;
+
+    /* Inorder traversal of a binary tree*/
+    static void inorder(Node temp)
+    {
+        if (temp == null)
+            return;
+
+        inorder(temp.left);
+        System.out.print(temp.key+" ");
+        inorder(temp.right);
+    }
+
+    /*function to insert element in binary tree */
+    static void insert(Node temp, int key)
+    {
+
+        Node newnode = new Node(key);
+        // Do level order traversal until we find
+        // an empty place and add the node.
+        if(temp == null) {
+            root = newnode;
+            return;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(temp);
+
+        boolean found = false;
+
+        while(!queue.isEmpty() && !found) {
+
+            int size = queue.size();
+
+            for(int i =0;i< size; i++) {
+
+                Node currentNode = queue.poll();
+
+                if(currentNode.left!=null) {
+                    queue.add(currentNode.left);
+                } else {
+                    currentNode.left = newnode;
+                    found = true;
+                    break;
+                }
+
+                if(currentNode.right != null) {
+                    queue.add(currentNode.right);
+                } else {
+                    currentNode.right = newnode;
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+    }
+
+    // Driver code
+    public static void main(String args[])
+    {
+        root = new Node(10);
+        root.left = new Node(11);
+        root.left.left = new Node(7);
+        root.right = new Node(9);
+        root.right.left = new Node(15);
+        root.right.right = new Node(8);
+
+        System.out.print( "Inorder traversal before insertion:");
+        inorder(root);
+
+        int key = 12;
+        insert(root, key);
+
+        System.out.print("\nInorder traversal after insertion:");
+        inorder(root);
+    }
 }
