54f19e09f746b5ba6f970132abf30b1d53e41b6d
Saphal Thammu <saphalthammu@Saphals-MacBook-Pro.local>
2020-12-16 16:14:14 -0700

    Completed Exercise_2

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,3 +1,11 @@
+// Time Complexity : O(1) as there are no loops
+// Space Complexity : O(1000 *2)=O(2000) as MAX is 1000 and each has a pointer
+// Did this code successfully run on Leetcode : I did not find this question in Leet Code
+// Any problem you faced while coding this : No I just followed few youtube videos for understadning
+
+
+// Your code here along with comments explaining your approach
+
 #include <bits/stdc++.h> 
 using namespace std; 
   
@@ -10,7 +18,7 @@ public:
   
 StackNode* newNode(int data) 
 { 
-    StackNode* stackNode = new StackNode(); 
+    StackNode* stackNode = new StackNode(); //pointer variable = new data-type;
     stackNode->data = data; 
     stackNode->next = NULL; 
     return stackNode; 
@@ -18,22 +26,38 @@ StackNode* newNode(int data)
   
 int isEmpty(StackNode* root) 
 { 
-    //Your code here 
+    return !root; //root stores address so if there is no address its negations will be 1 which is boolean of true.
 } 
   
 void push(StackNode** root, int data) 
 { 
-    //Your code here 
+    StackNode* stackNode = newNode (data); //returns you a pointer variable of data type StackNode. Here newnode is the function.
+    stackNode->next = *root; // as root is double pointer we still have the address of the main variable root.
+    *root = stackNode;
+    cout << data << " pushed to stack \n";
+
+
 } 
   
 int pop(StackNode** root) 
 { 
-    //Your code here 
+    if (isEmpty (*root)) 
+    return INT_MIN;
+
+    StackNode* temp= *root;
+    *root = (*root) ->next;
+    int popped = temp->data;
+    free(temp);
+
+    return popped;
+
 } 
   
 int peek(StackNode* root) 
 { 
-    //Your code here 
+    if (isEmpty(root))
+        return INT_MIN;
+    return root->data;
 } 
   
 int main() 
