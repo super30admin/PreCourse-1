73a20c99d04caaa385b8f89b62369d518ec1289b
brdayauon <brdayauon@gmail,com>
2020-12-16 10:38:57 -0800

    Finished E1

--- Exercise_1.py
+++ Exercise_1.py
@@ -4,27 +4,27 @@
 class myStack:
      def __init__(self):
          self.stack = []
-
      def isEmpty(self):
           if not self.stack:
                return True 
           return False
-         
+     #O(1)
      def push(self, item):
          self.stack.append(item)
 
+     #O(1)
      def pop(self):
         if self.stack:
              self.stack.pop()
         else: 
              return -1
-        
+     #O(1)
      def peek(self):
           if self.stack:
                return self.stack[-1]
           else:
                return -1
-        
+
      def size(self):
          return len(self.stack)
 
@@ -34,5 +34,6 @@ class myStack:
 s = myStack()
 s.push('1')
 s.push('2')
+print(s.show())
 print(s.pop())
 print(s.show())
