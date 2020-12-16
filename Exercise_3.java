313cfaceff6b6eaac58e0c6691019c9f05a1b077
LohitaGundapaneni <lohita1114@gmail.com>
2020-12-16 18:40:44 -0500

    Excercise_3.java done

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,10 +1,11 @@
 import java.io.*; 
-  
+
+
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList { 
   
-    Node head; // head of list 
+    static Node head; // head of list 
   
     // Linked list Node. 
     // This inner class is made static 
@@ -18,6 +19,7 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+        	this.data = d;
         } 
     } 
   
@@ -25,16 +27,29 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
-   
+    	Node temp = new Node(data);
+    	temp.next = null;
         // If the Linked List is empty, 
         // then make the new node as head 
+    	if(list.head == null)
+    		list.head = temp;
         
             // Else traverse till the last node 
             // and insert the new_node there 
+            else{
+            	Node dummy = list.head;
+            	while(dummy.next!=null) {
+            	   // System.out.println("sdfghjnkm");
+            		dummy = dummy.next;
+            	}
+            	dummy.next = temp; 
 
             // Insert the new_node at last node 
         // Return the list by head 
-        
+    // 	LinkedList res_list = new LinkedList();
+    // 	res_list.head = dummy;
+            }
+        return list;
     } 
   
     // Method to print the LinkedList. 
@@ -45,6 +60,11 @@ public class LinkedList {
             // Print the data at current node 
        
             // Go to next node 
+    	Node dummy = list.head;
+            while(dummy!=null) {
+                System.out.println(dummy.data);
+            	dummy = dummy.next;
+            }
     } 
    
     // Driver code 
