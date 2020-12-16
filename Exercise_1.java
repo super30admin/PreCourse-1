4a09cb433cdbb38e048c7064033cd41f4c8c9732
Faraaz1994 <faraazahmed1994@gmail.com>
2020-12-16 11:36:28 -0800

    Precourse-1 completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,44 +1,47 @@
-class Stack { 
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+class Stack {
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
-        //Check for stack Overflow
-        //Write your code here
-    } 
-  
-    int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
-    } 
-  
-    int peek() 
-    { 
-        //Write your code here
-    } 
-} 
-  
-// Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+    boolean isEmpty() {
+        return this.top == -1;
+    }
+
+    Stack() {
+        this.top = -1;
+    }
+
+    boolean push(int x) {
+        if (this.top == MAX - 1)
+            return false;
+        a[++this.top] = x;
+        return true;
+    }
+
+    int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return a[this.top--];
+    }
+
+    int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return 0;
+        }
+        return a[this.top];
+    }
+}
+
+// Driver code
+class Main {
+    public static void main(String args[]) {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.peek() + " Peek from stack");
+    }
 }
