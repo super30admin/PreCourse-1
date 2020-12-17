73e2938e00274ee7966cfbaca27c2e399c583128
deepashree <dkulkarni2@scu.edu>
2020-12-16 16:31:39 -0800

    Execise-1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -6,28 +6,47 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        return top == -1;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
+        top++;
+        if (top == a.length) {
+            return false;
+        }
         //Write your code here
+         a[top] = x;
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if (isEmpty())
+        {
+            System.out.println("Stack underflow");
+            return 0;
+        }
+        return a[top--];
     } 
   
     int peek() 
     { 
         //Write your code here
+        if (!isEmpty())
+        {
+            return a[top];
+        }
+        return Integer.MAX_VALUE;
     } 
 } 
   
