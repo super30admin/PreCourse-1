a10086207ff78ebc6aae86324c8c9c3c0482f874
intelaashish <aashish73134@gmail.com>
2020-12-16 20:26:46 -0800

    Precourse1 Completed.

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,59 @@
+'''
+Time Complexity :
+init(): O(1)
+isEmpty(): O(1)
+push(): O(1)
+pop(): O(1)
+peek(): O(1)
+size(): O(1)
+show(): O(1)
+
+Space Complexity :
+init(): O(1)
+isEmpty(): O(1)
+push(): O(1)
+pop(): O(1)
+peek(): O(1)
+size(): O(1)
+show(): O(1)
+
+Did this code successfully run on Leetcode :
+Didnt run since the identifiers used had different names and I was running late to individually rename all.
+
+Any problem you faced while coding this :
+No.
+'''
+
+#Your code here along with comments explaining your approach
 class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self):
+        self.stackk = []
+        self.size = 0
+
+    def isEmpty(self):
+        if len(self.stackk) == 0:
+            return True
+        return False
+
+    def push(self, item):
+        self.stackk.append(item)
+        self.size += 1
+
+    def pop(self):
+        top_elem = self.stackk[-1]
+        self.stackk = self.stackk[0:len(self.stackk)]
+        self.size -= 1
+        return top_elem
+
+    def peek(self):
+        return self.stackk[-1]
+
+    def size(self):
+        return self.size
+
+    def show(self):
+        return self.stackk
+
 
 s = myStack()
 s.push('1')
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,22 +1,69 @@
+'''
+Time Complexity :
+init(): O(1)
+push(): O(1)
+pop(): O(1)
+
+Space Complexity :
+init(): O(1)
+push(): O(1)
+pop(): O(1)
+
+Did this code successfully run on Leetcode :
+Didnt run since the identifiers used had different names and I was running late to individually rename all.
+
+Any problem you faced while coding this :
+Yes, tried implementing the stack without adding the variable "prev" in the Node class but was not successful.
+'''
+
+#Your code here along with comments explaining your approach
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
 class Stack:
     def __init__(self):
-        
+        self.top = None
+
     def push(self, data):
-        
+        if self.top is None:
+            self.top = Node(data)
+        else:
+            new_node = Node(data)
+            new_node.prev = self.top
+            self.top.next = new_node
+            self.top = self.top.next
+
+
     def pop(self):
-        
+        if self.top is not None:
+            if self.top.prev is None:
+                temp = self.top.data
+                self.top = None
+                return temp
+            else:
+                temp = self.top.data
+                self.top.prev.next = None
+                self.top = self.top.prev
+                return temp
+        else:
+            return -1
+
+
+
+
+
+
 a_stack = Stack()
 while True:
     print('push <value>')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
- 
+
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
