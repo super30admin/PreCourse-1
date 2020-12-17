c3534f9a53c99722e120e56c389c3e99265d87cb
Parmeet Singh <p.ahluwalia08@gmail.com>
2020-12-16 18:41:14 -0600

    Completed Precourse 1

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,34 @@
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
 class Stack:
     def __init__(self):
-        
+        self.head=None
+    
     def push(self, data):
-        
+        if self.head == None: 
+            self.head=Node(data) 
+              
+        else: 
+            newnode = Node(data) 
+            newnode.next = self.head 
+            self.head = newnode 
+            
     def pop(self):
-        
+        if self.head == None: 
+            return None
+              
+        else: 
+            # Removes the head node and makes  
+            #the preceeding one the new head 
+            tempnode = self.head 
+            self.head = self.head.next
+            tempnode.next = None
+            return tempnode.data
 a_stack = Stack()
+
 while True:
     print('push <value>')
     print('pop')
