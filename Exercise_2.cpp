e9c2d3f60352ed19602211172aa8f2daccae3a6e
Gayathri Ganesh <gayathrig269@gmail.com>
2020-12-16 20:12:49 -0800

    Completed Precourse_1-Exercise_2.cpp

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,52 +1,83 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-  
-// A structure to represent a stack 
-class StackNode { 
-public: 
-    int data; 
-    StackNode* next; 
-}; 
-  
-StackNode* newNode(int data) 
-{ 
-    StackNode* stackNode = new StackNode(); 
-    stackNode->data = data; 
-    stackNode->next = NULL; 
-    return stackNode; 
-} 
-  
-int isEmpty(StackNode* root) 
-{ 
-    //Your code here 
-} 
-  
-void push(StackNode** root, int data) 
-{ 
-    //Your code here 
-} 
-  
-int pop(StackNode** root) 
-{ 
-    //Your code here 
-} 
-  
-int peek(StackNode* root) 
-{ 
-    //Your code here 
-} 
-  
-int main() 
-{ 
-    StackNode* root = NULL; 
-  
-    push(&root, 10); 
-    push(&root, 20); 
-    push(&root, 30); 
-  
-    cout << pop(&root) << " popped from stack\n"; 
-  
-    cout << "Top element is " << peek(root) << endl; 
-  
-    return 0; 
-} 
+//Stack using Linked List//
+
+#include <iostream>
+using namespace std;
+
+// A structure to represent a stack
+class StackNode {
+public:
+    int data;
+    StackNode *next;
+};
+
+StackNode* newNode(int data)
+{
+    StackNode* stackNode = new StackNode();
+    stackNode->data = data;
+    stackNode->next = NULL;
+    return stackNode;
+}
+
+bool isEmpty(StackNode* root)
+{
+    if(root==NULL)
+    {
+        cout<<"Stack is empty"<<endl;
+        return true;
+    }
+    else
+        return false;
+
+}
+
+void push(StackNode** root, int data)
+{
+    StackNode* stackNode = new StackNode();
+    stackNode->data = data;
+    stackNode->next = root;
+    root = stackNode;
+}
+
+int pop(StackNode** root)
+{
+        //check if stack is empty//
+        if(isEmpty(root)==true)
+        {
+            cout<<"Stack is empty"<<endl;
+            return -1;
+        }
+        else{
+            StackNode* stackNode = root;
+            root = root->next; //Assign NULL to the previous node
+            return stackNode->data;
+            delete(stackNode->data);
+        }
+}
+
+int peek(StackNode* root)
+{
+    if(isEmpty(root)==true)
+        {
+            cout<<"Stack is Empty!"<<endl;
+            return -1;
+        }
+    else
+    {
+            return root->data;
+    }
+}
+
+int main()
+{
+    StackNode* root = NULL;
+
+    push(&root, 10);
+    push(&root, 20);
+    push(&root, 30);
+
+    cout << pop(&root) << " popped from stack\n";
+
+    cout << "Top element is " << peek(&root) << endl;
+
+    return 0;
+}
