eeecef011d1194c23c57edaa2cf2f377d55cd27e
Sai Teja <saitejanagam@gmail.com>
2020-12-17 08:25:08 +0530

    Completed Sprint PreCourse_1

--- Exercise_1.java
+++ Exercise_1.java
@@ -6,33 +6,58 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+		return (top < 0);
+		
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+		top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+		
+		if(top > MAX-1){
+		System.out.println(" Stack Overflow");
+		return false;}
+		else {
+		a[++top] = x;
+		return true;	
+		}
+		
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
-    } 
+		if (top <0){
+		
+			System.out.println(" Stack Underflow");
+				return 0;
+		}
+		else {
+		return (a[top--]);
+		
+		}
+		
+		
+
+	} 
   
     int peek() 
     { 
         //Write your code here
+	return a[top];
     } 
 } 
   
 // Driver code 
-class Main { 
+public class Main { 
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
