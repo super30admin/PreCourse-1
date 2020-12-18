5c0773a4b7f77ecf1321fb02975cd24fbda46685
Asif Hasnain <mah1042@nyu.edu>
2020-12-17 23:00:49 -0500

    Completed Precourse1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,20 @@
-public class StackAsLinkedList { 
+// Time Complexity : O(1)
+// Space Complexity : O(n) where n is max size of stack
+// Did this code successfully run on Leetcode : Did not find it in leetcode
+// Any problem you faced while coding this :
+// File name was not same as class name but class was made public. This resulted in error.
+
+
+// Your code here along with comments explaining your approach
+// root will point to head of stack
+// everytime we add something to stack we put new node as root previous head as next of root
+// everytime we remove something from stack we change root to root.next
+
+
+
+
+
+class StackAsLinkedList {
   
     StackNode root; 
   
@@ -8,18 +24,23 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
-            //Constructor here 
+            this.data = data;
         } 
     } 
     
 	
     public boolean isEmpty() 
-    { 
+    {
+        if(root == null) return true;
+        return false;
         //Write your code here for the condition if stack is empty. 
     } 
   
     public void push(int data) 
-    { 
+    {
+        StackNode node = new StackNode(data);
+        node.next = root;
+        root = node;
         //Write code to push data to the stack. 
     } 
   
@@ -27,11 +48,20 @@ public class StackAsLinkedList {
     { 	
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+	//Also return the popped element
+        if(root == null) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        int data = root.data;
+        root = root.next;
+        return data;
     } 
   
     public int peek() 
-    { 
+    {
+        if(root == null) return 0;
+        return root.data;
         //Write code to just return the topmost element without removing it.
     } 
   
@@ -41,9 +71,9 @@ public class StackAsLinkedList {
   
         StackAsLinkedList sll = new StackAsLinkedList(); 
   
-        sll.push(10); 
-        sll.push(20); 
-        sll.push(30); 
+        sll.push(10);
+        sll.push(20);
+        sll.push(30);
   
         System.out.println(sll.pop() + " popped from stack"); 
   
