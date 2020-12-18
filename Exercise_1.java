464380f212202a4de525c7447e0dfd03cfd3aa15
Juhi Madhwani <jhmadhwa@ncsu.edu>
2020-12-18 13:22:44 -0500

    Completed Exercise-1: Pre-Course-1

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,29 +5,56 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Time Complexity: O(1)
+        
+        //Returns true when stack is empty i.e. top = -1 else returns false
+        return top == -1;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initializes top of the stack
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        //Time Complexity: O(1)
+       
+        //Increments the top pointer to point to the new element
+        top++;
+
+        //If the top is equal to the size of array, its a situation of stack overflow and new element 
+        //cannot be pushed onto the stack, returns false
+        if(top == a.length)
+            return false;
+        
+        //else adds the element to the index where the top points and returns true
+        a[top] = x;
+        return true;
     } 
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        //Time Complexity: O(1)
+
+        //Checks if the stack is empty i.e. top == -1, returns 0 and prints " Stack Underflow"
+        if(top == -1)
+        {
+            System.out.println(" Stack Underflow");
+            return 0;
+        }
+
+        //Else returns the element at the top of the stack and decrements the top pointer
+        return a[top--];
     } 
   
     int peek() 
     { 
-        //Write your code here
+        //Time Complexity: O(1)
+
+        //Returns the element at the top of the stack
+        return a[top];
     } 
 } 
   
