3310e86a9f3e5b00e10ef5d5e1fa732727935409
Soumyya Kanti Datta <61840898+skrantidatta@users.noreply.github.com>
2020-12-20 01:49:11 -0500

    Completed  Precourse 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,25 @@
+# Time Complexity : O(1)
+# Space Complexity : O(n) where n is the size of the array
+# Did this code successfully run on Leetcode : yes
+# Any problem you faced while coding this : no
+
+
 class myStack:
      def __init__(self):
-         
+         self.stack=[]
      def isEmpty(self):
-         
+         return self.stack.empty()# checking if the array == null
      def push(self, item):
-         
+         return self.stack.append(item)# add atthe top of the stack
      def pop(self):
-        
+        return self.stack.pop()# deleting from the top of the stack
         
      def peek(self):
-        
+        return self.stack[len(self.stack)-1]# return the value at the top of the stack
      def size(self):
-         
+        return len(self.stack)
      def show(self):
-         
+        return self.stack
 
 s = myStack()
 s.push('1')
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,15 +1,32 @@
+# Time Complexity : O(1)
+# Space Complexity : O(n) where n is the size of the array
+# Did this code successfully run on Leetcode : didnot try on leetcode
+# Any problem you faced while coding this : no
+
 class Node:
     def __init__(self, data):
        self.data = data
        self.next = None
- 
+    
 class Stack:
     def __init__(self):
+        self.top=None
         
     def push(self, data):
+        if(self.top==None):
+            self.top=Node(data)
+        else:
+            node=Node(data)# creating a new object of class node
+            node.next=self.top # adding the new node to the top of the stack
+            self.top=node
         
     def pop(self):
-        
+        if (self.top== None): # checking if the stack is empty
+            return None
+        else:
+            data=self.top.data
+            self.top=self.top.next # storing the address of the next node in top
+            return data
 a_stack = Stack()
 while True:
     print('push <value>')
