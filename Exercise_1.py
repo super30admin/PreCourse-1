51cb562462ffa322165dcc5d9f2512dec66d5ca0
ahujab <dhwanildesai@Dhwanils-MacBook-Air.local>
2020-12-20 15:33:31 -0500

    implemented

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,28 @@
 class myStack:
+     #O(1) for all
      def __init__(self):
-         
+         self.stack=[]
      def isEmpty(self):
-         
+          if len(self.stack)==0:
+              return True
+          else:
+               return False
      def push(self, item):
-         
+         self.stack.append(item)
      def pop(self):
-        
+          if self.stack:
+               pop=self.stack.pop()
+               return(pop)
+          return -1
         
      def peek(self):
+          return self.stack[-1]
         
      def size(self):
+          return len(self.stack)
          
      def show(self):
+          return(self.stack)
          
 
 s = myStack()
