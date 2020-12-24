f104bdc88c4359a02514466fd8405f3b00b323f7
Nirbhay Sibal <sibal.nirbhaytlc@gmail.com>
2020-12-24 15:59:15 -0600

    Completed Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,31 @@
 class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    '''Stack implementation using an Array.'''
+
+    def __init__(self):
+        self._storage = []
+
+    def isEmpty(self):                                  # O(1)
+        return self.size() == 0
+
+    def push(self, item):                               # O(1)
+        self._storage.append(item)
+
+    def pop(self):                                      # O(1)
+        if self.isEmpty():
+            raise Exception('Stack is Empty!')
+        return self._storage.pop()
+
+    def peek(self):                                     # O(1)
+        if self.isEmpty():
+            raise Exception('Stack is Empty!')
+        return self._storage[-1]
+
+    def size(self):                                     # O(1)
+        return len(self._storage)
+
+    def show(self):
+        return '\n'.join(reversed(self._storage))
+
 
 s = myStack()
 s.push('1')
