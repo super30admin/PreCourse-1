e53a817005dc3a081107812dd83bbde5ca9d58ad
kapish5467 <kapish5467@gmail.com>
2021-01-11 10:41:33 -0800

    Completed Sprint Precourse1

--- Exercise_3.java
+++ Exercise_3.java
@@ -2,7 +2,7 @@ import java.io.*;
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList { 
   
     Node head; // head of list 
   
@@ -18,6 +18,8 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            this.data=d;
+            this.next=null;
         } 
     } 
   
@@ -34,7 +36,17 @@ public class LinkedList {
 
             // Insert the new_node at last node 
         // Return the list by head 
-        
+        Node add= new Node(data);
+        if(list.head==null)
+            list.head=add;
+        else
+        {   Node temp= list.head;
+            while(temp.next!=null)
+                temp=temp.next;
+            
+            temp.next= add;
+        }
+        return list;
     } 
   
     // Method to print the LinkedList. 
@@ -45,6 +57,13 @@ public class LinkedList {
             // Print the data at current node 
        
             // Go to next node 
+            Node temp= list.head;
+            while(temp!=null)
+            {
+                System.out.println(temp.data);
+                temp=temp.next;
+
+            }
     } 
    
     // Driver code 
