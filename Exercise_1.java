7447bc91ad6fb2cc1cca77f75cbad8d29f5e77a2
Nirmi <nirmalasnawale@gmail.com>
2021-01-11 20:06:57 -0600

    Exercise1 in java

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,29 +5,50 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //if top is -1, i.e. no elements
+        return top == -1;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+    	//when stack is full
+        if(top == MAX){
+        	System.out.println("Stack is full.");
+        	return false;
+        }
+        else{
+        	a[++top] = x;
+        	return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if(isEmpty()){
+        	System.out.println("Stack Underflow.");
+        	return 0;
+        }
+        else{
+        	return a[top--];
+        }
     } 
   
     int peek() 
     { 
-        //Write your code here
+        //If empty return 0 and print " Stack Underflow"
+        if(isEmpty()){
+        	System.out.println("Stack Underflow.");
+        	return 0;
+        }
+        else{
+        	return a[top];
+        }
     } 
 } 
   
