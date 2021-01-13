045ded3ebb7b37bd5fce4e0f83deb58270db6867
shr1611 <shru161@users.noreply.github.com>
2021-01-13 11:34:21 -0800

    updating Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,30 +5,52 @@ class Stack {
   
     boolean isEmpty() 
     { 
+        
         //Write your code here 
-        //test command to check git status
+
+        return (top < 0);
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top = -1;
+
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
+        if(top >= MAX - 1 ){
+            System.out.println("Stack OVerflow Error!");
+            return false;
+        }
         //Write your code here
+        a[++top] = x;
+        System.out.println(x + " is added to Stack");
+        return true;
+        
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
+        if(top < 0){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
         //Write your code here
+        return a[top--];
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(top < 0){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return a[top];
     } 
 } 
   
@@ -39,7 +61,11 @@ class Main {
         Stack s = new Stack(); 
         s.push(10); 
         s.push(20); 
-        s.push(30); 
+        s.push(30);
+        s.push(100); 
+        int topElement = s.peek();
         System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(topElement + " is at the top");
+
     } 
 }
