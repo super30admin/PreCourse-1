876d9dd258e66c02aab41b90e9341c687207b86b
Akshay Elavia <akshay.elavia@sjsu.edu>
2021-01-21 17:29:27 +0530

    Completed Precourse - 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,33 +1,55 @@
 class Stack { 
     static final int MAX = 1000; 
     int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
+    int a[] = new int[MAX];
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
-        //Write your code here
+        if (top == MAX-1) {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        top = top + 1;
+        a[top] = x;
+        return true;
     } 
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            int poppedElement = a[top];
+            top = top - 1;
+            return poppedElement;
+        }
     } 
   
     int peek() 
     { 
-        //Write your code here
+        if(top > -1 && top < MAX) {
+            return a[top];
+        } else {
+            return -1;
+        }
+        
     } 
 } 
   
