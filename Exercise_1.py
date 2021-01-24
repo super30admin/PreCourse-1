790cc91a51964b1d0d96d15532bfdcbe58df4e7f
rogen-george <rogenp8@gmail.com>
2021-01-23 16:47:44 -0800

    Python Precourse 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,27 @@
 class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self):
+        self.stack = []
+
+    def isEmpty(self):
+        return ( self.stack == None )
+
+    def push(self, item):
+        self.stack.append( item )
+
+    def pop(self):
+        return self.stack.pop()
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.stack[-1]
+        else:
+            return -1
+
+    def size(self):
+        return len( self.stack )
+
+    def show(self):
+        return ( " ".join(self.stack) )
 
 s = myStack()
 s.push('1')
