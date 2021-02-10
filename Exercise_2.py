c35b2a43698616dcb602273ac6dbb8c2aadcba01
Tirthaj Rikame <tirthaj.rikame@gmail.com>
2021-02-10 21:28:27 +0530

    Completed exercises

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,14 +1,28 @@
 class Node:
-    def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
 class Stack:
     def __init__(self):
-        
-    def push(self, data):
-        
+        self.head = None
+
+    def push(self,data):
+        if self.head == None:
+            self.head = Node(data)
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+
     def pop(self):
+        if self.head == None:
+            return print('Stack is Empty')
+        else:
+            pop_node = self.head
+            self.head = self.head.next
+            pop_node.next = None
+            return pop_node.data
         
 a_stack = Stack()
 while True:
