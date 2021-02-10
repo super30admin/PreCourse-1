f9c21619148330761ef620522db5b792cbd2e793
omkar <omkarvaidya13@gmail.com>
2021-02-10 17:33:02 -0500

    Completed PreCourse 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,29 +5,48 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here  
+        return top < 0;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top >= (MAX - 1))
+            return false;
+        else
+            a[++top] = x;
+
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(isEmpty()){
+            System.out.println("Stack underflow");
+            return 0;
+        }
+
+        int data = 0;
+        data = a[top];
+        a[top--] = 0;
+
+        return data;
     } 
   
     int peek() 
     { 
         //Write your code here
+        return isEmpty() ? top : a[top];
     } 
 } 
   
@@ -35,10 +54,15 @@ class Stack {
 class Main { 
     public static void main(String args[]) 
     { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        Stack s = new Stack();
+        System.out.println(s.isEmpty());
+        System.out.println(s.peek());
+        System.out.println(s.pop());
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.peek());
+        System.out.println(s.isEmpty()); 
     } 
 }
