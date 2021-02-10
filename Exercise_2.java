aee96105484dfe0441f569a6268d8c141ebb4763
Lokeswari Umakanthan <lokeswari.umakanthan@utdallas.edu>
2021-02-10 10:05:10 -0600

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,49 +1,30 @@
-public class StackAsLinkedList {
-	StackNode root; 
-			
-	StackAsLinkedList(){
-		root = null;
-	}
-	
+public class StackAsLinkedList { 
+  
+    StackNode root; 
+  
     static class StackNode { 
         int data; 
         StackNode next; 
   
         StackNode(int data) 
         { 
-        	this.data = data;
-        	next = null;
-            //Constructor here
+            //Constructor here 
         } 
     } 
+    
 	
     public boolean isEmpty() 
     { 
-    	if(root == null) return true;
-    	else return false;
         //Write your code here for the condition if stack is empty. 
     } 
   
     public void push(int data) 
     { 
-    	StackNode node = new StackNode(data);
-    	System.out.println(node.data);
-    	node.next = root;
-    	root = node;
-    	//Write code to push data to the stack. 
+        //Write code to push data to the stack. 
     } 
   
     public int pop() 
     { 	
-    	if(isEmpty()) {
-    		System.out.println("in here Stack Underflow");
-    		return 0;
-    	}
-    	else {
-    		int temp = root.data;
-    		root = root.next;
-    		return temp;
-    	}
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
@@ -51,11 +32,6 @@ public class StackAsLinkedList {
   
     public int peek() 
     { 
-    	if(isEmpty()) {
-    		System.out.println("Stack Underflow");
-    		return 0;
-    	}
-    	return root.data;
         //Write code to just return the topmost element without removing it.
     } 
   
@@ -69,11 +45,8 @@ public class StackAsLinkedList {
         sll.push(20); 
         sll.push(30); 
   
-        System.out.println(sll.pop()+"popped from stack"); 
- 
+        System.out.println(sll.pop() + " popped from stack"); 
+  
         System.out.println("Top element is " + sll.peek()); 
-        sll.pop();
-        System.out.println(sll.pop());
-        System.out.println(sll.peek());
     } 
-}
+} 
