a3139f61c09392d216680f4a2b98475912036663
rohanbansal <rohanrb123@gmail.com>
2021-02-10 09:10:42 -0500

    completed excercise 3

---
+++ LinkedList.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.List;
+
+// Java program to implement
+// a Singly Linked List
+public class LinkedList {
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        // Constructor
+        Node(int d)
+        {
+            this.data=d;
+            this.next=null;
+        }
+    }
+    Node head; // head of list
+
+    public LinkedList(){
+        this.head=null;
+    }
+
+    public Node getHead(){
+        return this.head;
+    }
+
+    public static LinkedList insert(LinkedList list, int data)
+    {
+
+        if(list.head == null){
+            list.head = new Node(data);
+            return list;
+        }
+
+        // If the Linked List is empty,
+        // then make the new node as head
+        Node temp = list.head;
+        while (temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next = new Node(data);
+        return list;
+
+            // Else traverse till the last node
+            // and insert the new_node there
+
+            // Insert the new_node at last node
+        // Return the list by head
+
+    }
+
+    // Method to print the LinkedList.
+    public static void printList(LinkedList list)
+    {
+        // Traverse through the LinkedList
+        // Get the head node
+        if(list.head==null)
+            return;
+
+        Node currentNode = list.head;
+        while(currentNode!=null){
+            System.out.println(currentNode.data);
+            currentNode=currentNode.next;
+        }
+            // Print the data at current node
+
+            // Go to next node
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        /* Start with the empty list. */
+        LinkedList list = new LinkedList();
+
+        //
+        // ******INSERTION******
+        //
+
+        // Insert the values
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+
+        // Print the LinkedList
+        printList(list);
+    }
+}
