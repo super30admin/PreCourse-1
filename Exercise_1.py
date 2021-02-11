4314bfffbcd2c4df0e50b5a209f28dbe6be3aff7
PraveenAravendan <60367491+PraveenAravendan@users.noreply.github.com>
2021-02-10 23:20:44 -0600

    Exercises Completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,25 @@
 class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self):
+        self.stack = []
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def peek(self):
+        return self.stack[0]
+
+    def size(self):
+        return len(self.stack)
+
+    def show(self):
+        return self.stack
+
 
 s = myStack()
 s.push('1')
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,38 +1,56 @@
 # Python program to insert element in binary tree  
-class newNode():  
-  
-    def __init__(self, data):  
-        self.key = data 
+class newNode():
+
+    def __init__(self, data):
+        self.key = data
         self.left = None
         self.right = None
-          
+
+
 """ Inorder traversal of a binary tree"""
-def inorder(temp): 
-  
-    
-  
-  
+def inorder(temp):
+    inorder_list = []
+    if temp:
+        inorder_list = inorder(temp.left)
+        inorder_list.append(temp.key)
+        inorder_list = inorder_list + inorder(temp.right)
+    return inorder_list
+
+
 """function to insert element in binary tree """
-def insert(temp,key): 
-  
-    
-  
-       
+def insert(temp, key):
+    if temp is None:
+        temp = newNode(key)
+    else:
+        if temp.key < key:
+            if temp.right is None:
+                temp.right = newNode(key)
+            else:
+                insert(temp.right, key)
+        else:
+            if root.left is None:
+                root.left = newNode(key)
+            else:
+                insert(temp.left, key)
+
+
 # Driver code  
-if __name__ == '__main__': 
-    root = newNode(10)  
-    root.left = newNode(11)  
-    root.left.left = newNode(7)  
-    root.right = newNode(9)  
-    root.right.left = newNode(15)  
-    root.right.right = newNode(8)  
-  
-    print("Inorder traversal before insertion:", end = " ") 
-    inorder(root)  
-  
+if __name__ == '__main__':
+    root = newNode(10)
+    root.left = newNode(11)
+    root.left.left = newNode(7)
+    root.right = newNode(9)
+    root.right.left = newNode(15)
+    root.right.right = newNode(8)
+
+    print("Inorder traversal before insertion:", end=" ")
+    lst = inorder(root)
+    print(lst)
+
     key = 12
-    insert(root, key)  
-  
-    print()  
-    print("Inorder traversal after insertion:", end = " ") 
-    inorder(root) 
+    insert(root, key)
+
+    print()
+    print("Inorder traversal after insertion:", end=" ")
+    lst = inorder(root)
+    print(lst)
