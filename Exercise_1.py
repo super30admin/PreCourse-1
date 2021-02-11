b3479cab2222a53e51ee6ee5770741a0ca2ccfd6
saiteja-ganduri <60758084+saiteja-ganduri@users.noreply.github.com>
2021-02-10 21:31:07 -0600

    excersise1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,42 @@
 class myStack:
      def __init__(self):
+            
+            self.mylist=[]
          
      def isEmpty(self):
+        
+        istrue=False
+        if len(self.mylist)==0:
+            istrue=True
+        
+        return istrue
          
      def push(self, item):
+        
+        self.mylist.append(item)
          
      def pop(self):
+        
+        return self.mylist.pop()
+        
         
         
      def peek(self):
         
+        return self.mylist[-1]
+        
+        
+        
+        
      def size(self):
+        
+        return len(self.mylist)
          
      def show(self):
-         
+        
+        print(self.mylist)
 
+          
 s = myStack()
 s.push('1')
 s.push('2')
