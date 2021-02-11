de1406be578cd0919995e191575cb55eb9ee935f
ronak-prog <58962719+ronak-prog@users.noreply.github.com>
2021-02-10 20:33:36 -0500

    Add files via upload

--- Exercise_1.java
+++ Exercise_1.java
@@ -6,39 +6,69 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        return(top>0);
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top = -1;
+
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
-        //Write your code here
+        //Write your code 
+        if(top >= MAX-1)
+        {
+         System.out.println("Stack Overflow"); 
+         return false;  
+        }
+        else
+        {//System.out.println("Putting x");
+         top= top + 1;
+         a[top] = x;
+         return true;   
+        }
+
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top<0)
+        {
+         System.out.println("Stack Underflow"); 
+         return 0;  
+        }
+        else
+        {   int var = a[top];
+            top = top-1; 
+            return(var);
+            
+            
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
-    } 
-} 
-  
-// Driver code 
-class Main { 
+        return (a[top]);
+    }
+    //Driver code
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
         s.push(10); 
         s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        s.push(30);
+        //s.pop();
+        //System.out.println(s.peek());
+        System.out.println(s.pop() + " Popped from stack");
+        //s.peek();
+        System.out.println(s.peek());
     } 
-}
+} 
+  
--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,4 @@
-public class StackAsLinkedList { 
+class StackAsLinkedList { 
   
     StackNode root; 
   
@@ -7,32 +7,66 @@ public class StackAsLinkedList {
         StackNode next; 
   
         StackNode(int data) 
-        { 
+        { this.data = data;
             //Constructor here 
         } 
     } 
     
 	
     public boolean isEmpty() 
-    { 
+    {   
         //Write your code here for the condition if stack is empty. 
+        if(root == null)
+            {return true;}
+        else
+            return false;
     } 
   
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
+        //Write code to push data to the stack.
+        StackNode pushNode = new StackNode(data);
+
+        if(root == null)
+        {root = pushNode;}
+        else
+        {
+            StackNode tempNode= root;
+            root = pushNode;
+            pushNode.next = tempNode;
+
+        } 
+        System.out.println(data + " is pushed out");
     } 
   
     public int pop() 
     { 	
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+	//Also return the popped element
+    if(root == null)
+            {System.out.println("Stack Underflow");
+            return 0;
+
+            }
+            else
+            {
+                int popped = root.data;
+                root = root.next;
+                return popped;
+            } 
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+        if(root != null)
+        return(root.data);
+        else
+        {   
+            System.out.println("Stack Underflow");
+        }
+        return 0;
     } 
   
 	//Driver code
