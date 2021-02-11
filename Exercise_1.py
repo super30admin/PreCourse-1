5337e5bb2034eb615b184499e3ce276f69c17b2c
Sagar Rikame <sagarrikame@Sagars-MBP.lan>
2021-02-10 19:25:54 -0600

    Completed Excercise 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,28 @@
 class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self):
+        self._data = []
+    def isEmpty(self):
+        return len(self._data) == 0
+    def push(self, item):
+        self._data.append(item)
+    def pop(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self._data.pop()
+    def peek(self):
+        if self.isEmpty():
+            return 'Stack is Empty'
+        return self._data[-1]
+    def size(self):
+        return len(self._data)
+    def show(self):
+        return self._data
 
 s = myStack()
-s.push('1')
-s.push('2')
+#s.push('1')
+#s.push('2')
 print(s.pop())
 print(s.show())
+print(s.isEmpty())
+s.pop()
+print(s.isEmpty())
