f467d18962fa2046a825e8c9617735f6691a85ca
Shilpa Catherine <Shilpa.j.catherine@gmail.com>
2021-02-10 22:58:24 -0500

    Completed PreCourse_1 please review

---
+++ Exercise_2.java
@@ -0,0 +1,72 @@
+public class StackAsLinkedList {
+
+    StackNode root;
+    StackNode top;
+
+    static class StackNode {
+        int data;
+        StackNode next;
+
+        StackNode(int data)
+        {
+            //Constructor here
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    StackAsLinkedList(){
+        top=root;
+    }
+
+
+    public boolean isEmpty()
+    {
+        //Write your code here for the condition if stack is empty.
+        if(root==null){
+            return true;
+        }
+    }
+
+    public void push(int data)
+    {
+        //Write code to push data to the stack.
+        StackNode newtop =new StackNode(data);
+        top.next=newtop;
+        top=newtop;
+    }
+
+    public int pop()
+    {
+        //If Stack Empty Return 0 and print "Stack Underflow"
+        //Write code to pop the topmost element of stack.
+        //Also return the popped element
+        if(isEmpty(){
+        System.out.println("Stack Underflow");
+        return 0;
+    }else{
+
+    }
+    }
+
+    public int peek()
+    {
+        //Write code to just return the topmost element without removing it.
+        return top.data;
+    }
+
+    //Driver code
+    public static void main(String[] args)
+    {
+
+        StackAsLinkedList sll = new StackAsLinkedList();
+
+        sll.push(10);
+        sll.push(20);
+        sll.push(30);
+
+        System.out.println(sll.pop() + " popped from stack");
+
+        System.out.println("Top element is " + sll.peek());
+    }
+}
---
+++ Exercise_4.java
@@ -0,0 +1,92 @@
+import java.util.LinkedList;
+import java.util.Queue;
+public class GFG {
+
+    /* A binary tree node has key, pointer to
+    left child and a pointer to right child */
+    static class Node {
+        int key;
+        Node left, right;
+
+        // constructor
+        Node(int key){
+            this.key = key;
+            left = null;
+            right = null;
+        }
+    }
+    static Node root;
+    static Node temp = root;
+
+    /* Inorder traversal of a binary tree*/
+    static void inorder(Node temp)
+    {
+        if (temp == null)
+            return;
+
+        inorder(temp.left);
+        System.out.print(temp.key+" ");
+        inorder(temp.right);
+    }
+
+    /*function to insert element in binary tree */
+    static void insert(Node temp, int key)
+    {
+
+        Queue<Node> nodeQueue = new LinkedList<>();
+
+        if(temp == null){
+            temp= new Node(key);
+        }
+
+        else{
+            nodeQueue.add(temp);
+
+            insertHelper(nodeQueue,key);
+        }
+    }
+
+    static void insertHelper(Queue nodeQueue,int key){
+
+        if(nodeQueue.size()==0)
+        {
+            return;
+        }
+        Node temp = (Node)nodeQueue.remove();
+        if(temp.left == null )
+        {
+            temp.left =  new Node(key);
+            return;
+
+        }else if(temp.right == null){
+            temp.right =  new Node(key);
+            return;
+        }
+        else{
+            nodeQueue.add(temp.left);
+            nodeQueue.add(temp.right);
+            insertHelper(nodeQueue,key);
+            return;
+        }
+    }
+
+    // Driver code
+    public static void main(String args[])
+    {
+        root = new Node(10);
+        root.left = new Node(11);
+        root.left.left = new Node(7);
+        root.right = new Node(9);
+        root.right.left = new Node(15);
+        root.right.right = new Node(8);
+
+        System.out.print( "Inorder traversal before insertion:");
+        inorder(root);
+
+        int key = 12;
+        insert(root, key);
+
+        System.out.print("\nInorder traversal after insertion:");
+        inorder(root);
+    }
+}
