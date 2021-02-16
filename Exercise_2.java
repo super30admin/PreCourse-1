0dd7de75c8ad5282015ef037499d909e4cc82ff6
Divya arora <darora2@scu.edu>
2021-02-15 20:02:15 -0800

    Completed Exercise 1 and 2 PrecOurse1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,4 @@
-public class StackAsLinkedList { 
+public class Exercise_2 { 
   
     StackNode root; 
   
@@ -9,37 +9,75 @@ public class StackAsLinkedList {
         StackNode(int data) 
         { 
             //Constructor here 
+        	this.data=data;
         } 
     } 
     
 	
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+        //Write your code here for the condition if stack is empty.
+    	if(root==null)
+    	{
+    		return true;
+    	
     } 
+    	else
+    	{
+    		return false;
+    	}
+    }
   
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
+        //Write code to push data to the stack.
+    	StackNode newNode=new StackNode(data);
+    	StackNode temp=root;
+    	root=newNode;
+    	newNode.next=temp;
     } 
   
     public int pop() 
     { 	
 	//If Stack Empty Return 0 and print "Stack Underflow"
+    	if(root==null)
+    	{
+    		System.out.println("Stack Underflow");
+    		return 0;
+    	}
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
+    	else
+    	{
+    		int x=root.data;
+    		root=root.next;
+    		return x;
+    	}
+    	
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+    	
+    	if(root==null)
+    	{
+    		System.out.println("Stack Underflow");
+    		return 0;
+    	}
+    	else
+    	{
+    		int x=root.data;
+    		//root=root.next;
+    		return x;
+    	}
     } 
   
 	//Driver code
     public static void main(String[] args) 
     { 
   
-        StackAsLinkedList sll = new StackAsLinkedList(); 
+        Exercise_2 sll = new Exercise_2(); 
   
         sll.push(10); 
         sll.push(20); 
