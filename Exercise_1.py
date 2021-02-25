e5830cf9ee1c6d9e62cd1da630af642f3737ee8b
pranati05 <pranati05@gmail.com>
2021-02-24 22:56:44 -0800

    Completed PreCourse-1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,31 @@
 class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self):
+        self.stack = []
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def push(self, item):
+        self.stack.append(item)
+        print(item + " pushed to stack ")
+
+    def pop(self):
+        if self.stack:
+            return self.stack.pop()
+        else:
+            return -1
+
+    def peek(self):
+        if self.stack:
+            return self.stack[-1]
+        else:
+            return -1
+
+    def size(self):
+        return len(self.stack)
+
 
 s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
-print(s.show())
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,22 +1,38 @@
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
+
 class Stack:
     def __init__(self):
-        
+        self.head = None
+
     def push(self, data):
-        
+        if self.head == None:
+            self.head = Node(data)
+        else:
+            newnode = Node(data)
+            newnode.next = self.head
+            self.head = newnode
+
     def pop(self):
-        
+        if not self.head:
+            return None
+        else:
+            poppednode = self.head
+            self.head = self.head.next
+            poppednode.next = None
+            return poppednode.data
+
+
 a_stack = Stack()
 while True:
     print('push <value>')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
- 
+
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
