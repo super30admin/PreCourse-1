9f3784ea87d4c468588dfa6374253efe14d12d2c
Srikanth Reddy Nagidi <srikanth.nagidi@gmail.com>
2021-03-08 08:10:39 -0500

    Completed Exercise_1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,33 +1,51 @@
 class Stack { 
     static final int MAX = 1000; 
-    int top; 
+    public int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
         //Write your code here 
+        return top == 0;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top=0;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top == 1000){
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        a[top] = x;
+        top++;
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        int pop = a[top-1];
+        a[top-1] = 0;
+        top--;
+        return pop;
     } 
   
     int peek() 
     { 
         //Write your code here
+        return a[top-1];
     } 
 } 
   
@@ -40,5 +58,6 @@ class Main {
         s.push(20); 
         s.push(30); 
         System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.peek() + " Peek from stack"); 
     } 
 }
