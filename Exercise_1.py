e1103cae2ebe1ae8ce98d3911e4f4c813c071feb
Raj Patel <rpatel@Rajs-MacBook-Air.local>
2021-03-09 20:39:11 -0800

    Completed Exercise1 Precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,26 @@
 class myStack:
      def __init__(self):
-         
+          self.s = []
+
      def isEmpty(self):
+          return self.size() == 0
          
      def push(self, item):
+          self.s.append(item)
          
      def pop(self):
-        
-        
+          return self.s.pop()
+          
      def peek(self):
+          if self.isEmpty():
+               raise Exception("Stack empty!")
+          return self.s[-1]
         
      def size(self):
+          return len(self.s)
          
      def show(self):
+          return self.s 
          
 
 s = myStack()
