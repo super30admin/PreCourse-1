6918abd9f4b73adc0e9f3a6acd64f7127464e68f
chandini21 <chandinisri.805@gmail.com>
2021-03-10 12:52:34 -0600

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,57 @@
-class myStack:
-     def __init__(self):
+# class myStack:
+#      def __init__(self):
          
-     def isEmpty(self):
+#      def isEmpty(self):
          
-     def push(self, item):
+#      def push(self, item):
          
-     def pop(self):
+#      def pop(self):
         
         
-     def peek(self):
+#      def peek(self):
         
-     def size(self):
+#      def size(self):
          
-     def show(self):
+#      def show(self):
          
 
+# s = myStack()
+# s.push('1')
+# s.push('2')
+# print(s.pop())
+# print(s.show())
+class myStack:
+    def __init__(self):
+        self.stack = []
+        
+    def isEmpty(self):
+        return len(self.stack) <= 0
+    
+    def push(self, item):
+        self.stack.append(item)
+        
+    def pop(self):
+        self.stack = self.stack[:-1]
+        return self.stack[-1]
+        # return self.stack.pop()
+    
+    def peek(self):
+        return self.stack[-1]
+    
+    def size(self):
+        return len(self.stack)
+    
+    def show(self):
+        return self.stack
+    
 s = myStack()
-s.push('1')
-s.push('2')
-print(s.pop())
+print(s.size())
 print(s.show())
+s.push('1')
+#print("len", len(s.stack))
+#print(s.isEmpty())
+s.push('2')
+
+print(s.stack)
+print("pop", s.pop())
+print("show", s.show())
