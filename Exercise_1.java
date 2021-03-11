caee5cb4d6d99efeaa9b06a4b42d4530582d4c8b
kavya mohan <kavya4395@gmail.com>
2021-03-10 23:01:48 -0800

    Completed ex1 Precourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,67 +1,61 @@
-class Stack { 
-    static final int MAX = 1000; 
-    int top; 
+class Stack {
+    static final int MAX = 1000;
+    int top;
     int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
+
+    boolean isEmpty() {
         //Write your code here
-        if(top <= -1){
+        if (top <= -1) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
-    } 
+    }
 
-    Stack() 
-    { 
+    public Stack() {
         //Initialize your constructor
         top = -1;
-    } 
-  
-    boolean push(int x) 
-    { 
+    }
+
+    boolean push(int x) {
         //Check for stack Overflow
-        if(top >= a.length-1){
+        if (top >= a.length - 1) {
             System.out.println("Stack is full");
             return false;
         }
 
         //Write your code here
-        else{
+        else {
             top++;
             a[top] = x;
             return true;
         }
-    } 
-  
-    int pop() 
-    { 
+    }
+
+    int pop() {
         //If empty return 0 and print " Stack Underflow"
-        if(top <= -1){
+        if (top <= -1) {
             System.out.print("Satck underflow");
             return 0;
         }
         //Write your code here
-        else{
+        else {
             int pop_item = a[top];
             top--;
             return pop_item;
         }
-    } 
-  
-    int peek() 
-    { 
+    }
+
+    int peek() {
         //Write your code here
-        if(top <=-1){
+        if (top <= -1) {
             System.out.println("Stack is empty");
-            return  0;
-        }
-        else {
+            return 0;
+        } else {
             return a[top];
         }
-} 
+    }
+}
   
 // Driver code 
 class Main { 
