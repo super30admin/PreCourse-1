01b67dc73069bf4ac36dbdac81563644bac0d074
Sanjeev <sanjvija@cisco.com>
2021-03-10 22:06:04 -0800

    Completed Pre-course 1 - Please Review

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,29 +5,65 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        if (top == -1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        } 
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top = -1; 
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        top++;
+        if(top<MAX)
+        {
+            a[top]=x;
+            System.out.println("PUSH success!!");
+            return true;
+        }
+        else
+        {
+            Sytem.out.println("STACK OVERFLOW!");
+            return false;
+        }         
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(isEmpty())
+        {
+            Sytem.out.println("STACK UNDERFLOW!");
+            return 0;
+        }
+        else
+        {
+            int temp = a[top];
+            top--;
+            return temp;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(top>-1 && top<MAX)
+        {
+            return a[top];
+        }
     } 
 } 
   
