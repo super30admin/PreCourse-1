6cfc401023abad767ecd0a7b5eaa69e077c6a40a
Nicole Traboulsi <nmtraboulsi@yahoo.com>
2021-03-10 23:11:26 -0800

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,29 +5,53 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        return (top<0);
     } 
 
-    Stack() 
+    Stack(int size)
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top >=(MAX-1)) {
+            System.out.println("Stack Overflow");
+            return false;
+        } else {
+            a[++top] = x;
+            System.out.println(x + "pushed to stack");
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top<0){
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            int x = a[top--];
+            return 0;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(top<0){
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            int x = a[top];
+            return x;
+        }
     } 
 } 
   
