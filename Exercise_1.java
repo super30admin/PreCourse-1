eb71f9ec62c0cc660160ae52562b19b8fa9e0066
murdpra <79067522+murdpra@users.noreply.github.com>
2021-03-10 17:20:09 -0700

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -6,28 +6,67 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        
+        if(top == -1)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top=-1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top <= MAX)
+        {
+            top++;
+           a[top]=x;
+           System.out.println(a[top] +" is beinf pushed");
+           return true;
+           
+        }
+        else 
+        {
+            return false;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top <= -1)
+        {
+            System.out.println("The stack underflow occured");
+           return 1;
+        }
+        else if(top<= MAX) 
+        {
+            a[top]=0;
+            top--;
+            return top;
+        }
+        else{
+            return 1;
+        }
+
     } 
   
     int peek() 
     { 
         //Write your code here
+        System.out.println(  " The top most element of the stack is"
+        +a[top]);
+    return a[top];
     } 
 } 
   
@@ -39,6 +78,8 @@ class Main {
         s.push(10); 
         s.push(20); 
         s.push(30); 
+        
         System.out.println(s.pop() + " Popped from stack"); 
+        s.peek();
     } 
 }
