95977cd1c524dc512ba8bf636c5e691c81eebc1f
kavya mohan <kavya4395@gmail.com>
2021-03-10 22:26:24 -0800

    Completed ex1 Precourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,30 +5,62 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        if(top <= -1){
+            return true;
+        }
+        else{
+            return false;
+        }
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
+        if(top >= a.length-1){
+            System.out.println("Stack is full");
+            return false;
+        }
+
         //Write your code here
+        else{
+            top++;
+            a[top] = x;
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
+        if(top <= -1){
+            System.out.print("Satck underflow");
+            return 0;
+        }
         //Write your code here
+        else{
+            int pop_item = a[top];
+            top--;
+            return pop_item;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
-    } 
+        if(top <=-1){
+            System.out.println("Stack is empty");
+            return  0;
+        }
+        else {
+            return a[top];
+        }
 } 
   
 // Driver code 
