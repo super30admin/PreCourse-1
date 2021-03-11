6e23d0c410c10eabcfb2aba6c87a0d8510a8ff75
nac09 <nachiketlele307@gmail.com>
2021-03-10 16:59:40 -0800

    Precourse 1 completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,27 @@
 class myStack:
      def __init__(self):
+          self.arr=[]
          
      def isEmpty(self):
+          return self.arr==[]
          
      def push(self, item):
+          self.arr.append(item)
          
      def pop(self):
+          return self.arr.pop()
         
         
      def peek(self):
+          return self.arr[len(self.arr)-1]
         
      def size(self):
+          return len(self.arr)
          
      def show(self):
+          if self.arr:
+               for i in range(len(self.arr)):
+                    print('Current element',self.arr[i])
          
 
 s = myStack()
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data=data
+        self.next=next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,17 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        node=ListNode(data)
+        if not self.head:
+            self.head = node
+        else:
+            newnode=self.head
+            while newnode.next!=None:
+                newnode=newnode.next
+            newnode.next=node    
+            
+            
+            
         
     def find(self, key):
         """
@@ -24,9 +37,39 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        if not self.head:
+            return
+        if key==self.head.data:
+            return self.head
+        node=self.head
+        while node.next!=None:
+            if node.data==key:
+                return node
+            node=node.next
+        return     
+        
+       
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if not self.head:
+            return
+	
+	temp = self.head
+		
+	if head.data == key:
+            head = temp.next
+            print ("Deleted node is " + str(head.data))
+            return
+
+	while(temp.next):
+		if (temp.next.data == key):
+			print ("Node deleted is " + str(temp.next.data))
+			temp.next = temp.next.next
+			return
+		temp = temp.next
+	print ("Node not found")
+	return
