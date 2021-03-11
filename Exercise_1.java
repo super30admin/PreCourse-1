f6653210bafb22c827fd58e4fabd9a8c9b743fab
Bagirathan <bagirathan96@gmail.com>
2021-03-10 18:35:15 -0800

    Submitted Precourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,44 +1,63 @@
-class Stack { 
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+class Stack {
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
-        //Check for stack Overflow
-        //Write your code here
-    } 
-  
-    int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
-    } 
-  
-    int peek() 
-    { 
-        //Write your code here
-    } 
-} 
-  
-// Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+    boolean isEmpty() {
+        // Write your code here
+        if (top == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    Stack() {
+        // Initialize your constructor
+        top = 0;
+    }
+
+    boolean push(int x) {
+        // Check for stack Overflow
+        // Write your code here
+
+        if (top > MAX - 1) {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+
+        a[top++] = x;
+        return true;
+    }
+
+    int pop() {
+        // If empty return 0 and print " Stack Underflow"
+        // Write your code here
+
+        if (top <= 0) {
+            System.out.println("Stack underflow");
+            return 0;
+        }
+        int temp = a[--top];
+        return temp;
+
+    }
+
+    int peek() {
+        // Write your code here
+
+        return a[top];
+
+    }
+}
+
+// Driver code
+class Main {
+    public static void main(String args[]) {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+
+        System.out.println(s.pop() + " Popped from stack");
+    }
 }
