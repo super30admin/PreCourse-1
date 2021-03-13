291bbc23336a9f0491be97994fac72b019cf43bc
Yashasri Edukulla <yashasriedu2125@gmail.com>
2021-03-12 19:26:45 -0800

    Completed Exercise

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,19 @@
 class myStack:
      def __init__(self):
-         
+          print("Self loop")
      def isEmpty(self):
-         
+         print("self loop2")
      def push(self, item):
-         
+         print("self loop3")
      def pop(self):
-        
+        print("self loop4")
         
      def peek(self):
-        
+        print("self loop5")
      def size(self):
-         
+         print("self loop6")
      def show(self):
-         
+         print("self loop7")
 
 s = myStack()
 s.push('1')
