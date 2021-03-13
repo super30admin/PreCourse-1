0d67d02e3414f9745a55f1e74728069b39358f9f
abhijith1702 <avoudariabhijith@gmail.com>
2021-03-13 09:11:29 -0600

    Completed Sprint Precourse1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,6 @@
-public class StackAsLinkedList { 
+
+
+class StackAsLinkedList { 
   
     StackNode root; 
   
@@ -6,21 +8,33 @@ public class StackAsLinkedList {
         int data; 
         StackNode next; 
   
-        StackNode(int data) 
-        { 
-            //Constructor here 
-        } 
+        // StackNode(int data) 
+        // { 
+        //     //Constructor here
+        //     this.data = data;
+
+        // } 
     } 
+    StackAsLinkedList(){
+        this.root = null;
+    }
     
 	
     public boolean isEmpty() 
     { 
         //Write your code here for the condition if stack is empty. 
+        return root == null;
     } 
   
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
+        //Write code to push data to the stack.
+        StackNode oldRoot = root;
+        root = new StackNode();
+        root.data = data;
+        root.next = oldRoot;
+
+
     } 
   
     public int pop() 
@@ -28,11 +42,25 @@ public class StackAsLinkedList {
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
+    if(root == null){
+        System.out.println("stack Underflow");
+        return 0;
+    }
+    int value = root.data;
+    root = root.next;
+    return value;
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+        if(!isEmpty()){
+            return root.data;
+        }
+        else{
+            System.out.println("stack is empty");
+            return 0;
+        }
     } 
   
 	//Driver code
@@ -45,7 +73,7 @@ public class StackAsLinkedList {
         sll.push(20); 
         sll.push(30); 
   
-        System.out.println(sll.pop() + " popped from stack"); 
+       // System.out.println(sll.pop() + " popped from stack"); 
   
         System.out.println("Top element is " + sll.peek()); 
     } 
--- Exercise_3.java
+++ Exercise_3.java
@@ -2,7 +2,7 @@ import java.io.*;
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList { 
   
     Node head; // head of list 
   
@@ -18,6 +18,9 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+           data = d;
+           next = null;
+
         } 
     } 
   
@@ -25,7 +28,20 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
-   
+        Node temp = new Node(data);
+        temp.next = null;
+
+        if(list.head == null){
+            list.head = temp;
+        }
+        else{
+            Node last = list.head;
+            while(last.next!=null){
+                last = last.next;
+            }
+            last.next = temp;
+        }
+        return list;
         // If the Linked List is empty, 
         // then make the new node as head 
         
@@ -41,7 +57,11 @@ public class LinkedList {
     public static void printList(LinkedList list) 
     {  
         // Traverse through the LinkedList 
-   
+        Node current = list.head;
+        while(current!= null){
+            System.out.println(current.data + " ");
+            current = current.next;
+        }
             // Print the data at current node 
        
             // Go to next node 
