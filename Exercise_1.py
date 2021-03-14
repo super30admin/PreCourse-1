602bf5e7b7ab2fbf07bf11daf92ed24ef24cc72c
Spriha Deshpande <50254090+SprihaDeshpande@users.noreply.github.com>
2021-03-13 16:16:26 -0800

    Stack Using Array - Please review

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,25 @@
 class myStack:
      def __init__(self):
-         
+         self.array = []
+
      def isEmpty(self):
-         
+         if not self.array:
+             return True
+
      def push(self, item):
-         
+         self.array.append(item)
+
      def pop(self):
-        
-        
+         return self.array.pop()
+
      def peek(self):
-        
+         return self.array[-1]
+
      def size(self):
-         
+         return len(self.array)
+
      def show(self):
-         
+         return self.array
 
 s = myStack()
 s.push('1')
