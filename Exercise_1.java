49a50c174fde30a3a51dcf41d2a92fdfa3a2f449
Vishal Gadapa <vishalgadapa@Vishals-MacBook-Pro.local>
2021-03-14 22:03:18 -0700

    Completed PreCourse-1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,5 @@
+import java.lang.*;
+import java.util.*;
 class Stack { 
     static final int MAX = 1000; 
     int top; 
@@ -6,28 +8,44 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        if(this.top==0) return true;
+        return false;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        this.top=0;
+
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(this.top==MAX-1) return false;
+        a[this.top]=x;
+        ++this.top;
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(this.isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        } 
+        --top;
+        return a[top];
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(this.isEmpty()) return 0;
+        return a[top-1];
     } 
 } 
   
@@ -35,10 +53,20 @@ class Stack {
 class Main { 
     public static void main(String args[]) 
     { 
-        Stack s = new Stack(); 
+        Stack s = new Stack();      
         s.push(10); 
         s.push(20); 
         s.push(30); 
         System.out.println(s.pop() + " Popped from stack"); 
     } 
 }
+
+/*
+ *  Time Complexity: O(1) to push pop and peek for the stack as we keep the pointer to the top element. Here N is the number of elements.
+ *  Space Complexity: O(N) to store all the element in the array.
+ * 
+ *  Did this code successfully run on Leetcode : Yes
+ *  Any problem you faced while coding this : None
+ * 
+ *  Hold the pointer to the top element in the array with a variable and using that variable perform the basic operations like push pop and peek.
+ */
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,70 +1,99 @@
-import java.io.*; 
-  
+import java.io.*;
+
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
-  
+public class LinkedList {
+
     Node head; // head of list 
-  
+
     // Linked list Node. 
     // This inner class is made static 
     // so that main() can access it 
-    static class Node { 
-  
-        int data; 
-        Node next; 
-  
+    static class Node {
+
+        int data;
+        Node next;
+
         // Constructor 
-        Node(int d) 
-        { 
-            //Write your code here 
-        } 
-    } 
-  
+        Node(int d)
+        {
+            //Write your code here
+            this.data = d;
+        }
+    }
+
     // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
-    { 
-        // Create a new node with given data 
-   
+    public static LinkedList insert(LinkedList list, int data)
+    {
+        // Create a new node with given data
+        Node newNode = new Node(data);
+
+        if(list.head==null){
+            list.head = newNode;
+            return list;
+        }
+        Node currNode = list.head;
+        while(currNode.next!=null){
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+
+        return list;
         // If the Linked List is empty, 
         // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
 
-            // Insert the new_node at last node 
+        // Else traverse till the last node
+        // and insert the new_node there
+
+        // Insert the new_node at last node
         // Return the list by head 
-        
-    } 
-  
+
+    }
+
     // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
-    {  
+    public static void printList(LinkedList list)
+    {
         // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
-    } 
-   
+        Node currNode = list.head;
+        if(currNode==null) return;
+        // Print the data at current node
+        while(currNode!=null){
+            System.out.println(currNode.data);
+            currNode=currNode.next;
+        }
+        // Go to next node
+    }
+
     // Driver code 
-    public static void main(String[] args) 
-    { 
+    public static void main(String[] args)
+    {
         /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
-  
+        LinkedList list = new LinkedList();
+
         // 
         // ******INSERTION****** 
         // 
-  
+
         // Insert the values 
-        list = insert(list, 1); 
-        list = insert(list, 2); 
-        list = insert(list, 3); 
-        list = insert(list, 4); 
-        list = insert(list, 5); 
-  
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+
         // Print the LinkedList 
-        printList(list); 
-    } 
+        printList(list);
+    }
 }
+
+/*
+ *  Time Complexity: O(N) for insertion as we need to traverse through the entire list to add a new element
+ *  Space Complexity: O(N) to store all the node information.
+ * 
+ *  Did this code successfully run on Leetcode : Yes
+ *  Any problem you faced while coding this : None
+ * 
+ *  Simply traverse through all the node from the head to the last and append the next element.
+ */
+
+
