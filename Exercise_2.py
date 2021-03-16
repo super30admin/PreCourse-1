2d0aa9b097b1ea46072cbe2d4b6381543cf2a84f
RoliKhanna <khannaro@oregonstate.edu>
2021-03-16 18:42:25 -0500

    Precourse 1 completed

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,21 +2,33 @@ class Node:
     def __init__(self, data):
        self.data = data
        self.next = None
- 
+
 class Stack:
     def __init__(self):
-        
+        # Stack as a Linked list
+        self.Node = None
+
     def push(self, data):
-        
+        if(self.Node == None):
+            self.Node = Node(data)
+        else:
+            newNode = Node(data)
+            self.Node.next = newNode
+
     def pop(self):
-        
+        while(self.Node.next != None):
+            traverseNode = self.Node
+            self.Node = self.Node.next
+        self.Node = traverseNode
+        self.Node.next = None
+
 a_stack = Stack()
 while True:
     print('push <value>')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
- 
+
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,8 +3,9 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+
 class SinglyLinkedList:
+    # Isn't the solution to this the same as exercise 2?
     def __init__(self):
         """
         Create a new singly-linked list.
@@ -17,14 +18,14 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
