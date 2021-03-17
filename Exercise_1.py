e17b3b015d709a68c3123dbcbc0e35d57607ae4d
rashmi1112 <rashmibadadale@gmail.com>
2021-03-17 15:11:21 -0400

    Completed PreCourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,33 @@
 class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self):
+        self.s = []
+
+    def isEmpty(self):
+        if len(self.s) == 0:
+            return True
+        return False
+
+    def push(self, item):
+        self.s.append(item)
+
+    def pop(self):
+        if s.isEmpty():
+            raise Exception('Cannot pop from empty stack!')
+        else:
+            return self.s.pop()
+
+    def peek(self):
+        return self.s[-1]
+
+    def size(self):
+        return len(self.s)
+
+    def show(self):
+        st = []
+        for i in self.s:
+            st.append(i)
+        return st
+
 
 s = myStack()
 s.push('1')
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,22 +1,58 @@
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
+
 class Stack:
-    def __init__(self):
-        
+    def __init__(self, head=None):
+        self.head = head
+
     def push(self, data):
-        
+        if self.head is None:
+            self.head = Node(data)
+            return
+        else:
+            itr = self.head
+            while itr:
+                if itr.next is None:
+                    itr.next = Node(data)
+                    return
+                itr = itr.next
+
     def pop(self):
-        
+        if self.head is None:
+            print('Cannot pop from empty stack!')
+            return
+        else:
+            itr = self.head
+            # curr = Node()
+            prev = Node(None)
+            while itr:
+                curr = itr
+                if itr.next is None:
+                    prev.next = None
+                    return curr.data
+                prev = curr
+                itr = itr.next
+
+    def print(self):
+        itr = self.head
+        new_list = []
+        while itr:
+            new_list.append(itr.data)
+            itr = itr.next
+        return new_list
+
+
 a_stack = Stack()
 while True:
     print('push <value>')
     print('pop')
+    print('print_stack')
     print('quit')
     do = input('What would you like to do? ').split()
- 
+
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
@@ -26,5 +62,7 @@ while True:
             print('Stack is empty.')
         else:
             print('Popped value: ', int(popped))
+    elif operation == 'print_stack':
+        print(a_stack.print())
     elif operation == 'quit':
         break
