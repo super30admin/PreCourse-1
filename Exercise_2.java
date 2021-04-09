5a53a70bce9bea7982c93f09af00cc5bbf5a1dfc
AMs30-cmd <amsuper30@gmail.com>
2021-04-08 23:18:34 -0500

    Arjun Malhotra_RN02APR2021

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,6 +1,6 @@
-public class StackAsLinkedList { 
+class StackAsLinkedList { 
   
-    StackNode root; 
+    StackNode root;
   
     static class StackNode { 
         int data; 
@@ -8,40 +8,54 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
-            //Constructor here 
+            this.data = data;
         } 
     } 
     
 	
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+        //Write your code here for the condition if stack is empty.
+        if(root==null)
+            return true;
+        return false;
     } 
   
     public void push(int data) 
-    { 
-        //Write code to push data to the stack. 
+    {
+        StackNode temp=new StackNode(data);    
+        temp.next = root;
+        root=temp;
+        return;
     } 
   
     public int pop() 
-    { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
+    {
+        //If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+	    //Also return the popped element
+        if(isEmpty())
+        {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        int top = root.data;
+        root=root.next;
+        return top;
     } 
   
     public int peek() 
-    { 
+    {
         //Write code to just return the topmost element without removing it.
+        return root.data;
     } 
   
 	//Driver code
     public static void main(String[] args) 
     { 
   
-        StackAsLinkedList sll = new StackAsLinkedList(); 
-  
-        sll.push(10); 
+        StackAsLinkedList sll = new StackAsLinkedList();
+        sll.push(10);
         sll.push(20); 
         sll.push(30); 
   
