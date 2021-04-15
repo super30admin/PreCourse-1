5535ea7d6e4dc64811e42f501ff9b058f490249b
DpakJaswani <djaswani96@gmail.com>
2021-04-15 02:00:00 -0500

    [-]Exercise 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,44 +1,71 @@
-class Stack { 
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+/**
+ * @author Deepak J
+ *
+ */
+class Stack
+{
+	static final int	MAX	= 1000;
+	int					top;
+	int					a[]	= new int[MAX]; // Maximum size of Stack
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
-        //Check for stack Overflow
-        //Write your code here
-    } 
-  
-    int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
-    } 
-  
-    int peek() 
-    { 
-        //Write your code here
-    } 
-} 
-  
-// Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+	boolean isEmpty()
+	{
+		return (top < 0);
+	}
+
+	Stack()
+	{
+		top = -1;
+	}
+
+	boolean push(int x)
+	{
+		boolean isPushed = false;
+		if (top >= (MAX - 1))
+		{
+			System.out.println("Stack Overflow! Can not push " + x
+					+ " in stack");
+		}
+		else
+		{
+			top++;
+			a[top] = x;
+			isPushed = true;
+			System.out.println(x + " pushed in stack!");
+		}
+		return isPushed;
+	}
+
+	int pop()
+	{
+		int poppedElement = 0;
+		if (isEmpty())
+		{
+			System.out.println("Stack Underflow!");
+		}
+		else
+		{
+			poppedElement = a[top];
+			top--;
+		}
+		return poppedElement;
+	}
+
+	int peek()
+	{
+		return a[top];
+	}
+}
+
+// Driver code
+class Main
+{
+	public static void main(String args[])
+	{
+		Stack s = new Stack();
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		System.out.println(s.pop() + " Popped from stack");
+	}
 }
