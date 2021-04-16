b54f5b975d1258322c976e08992723de454d2171
kanksha92 <kanksha1992@gmail.com>
2021-04-16 16:37:20 -0500

    exercise 1,2,3

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,33 @@
 class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self):
+        self.stack = []
+
+    def isEmpty(self):
+        if not self.stack:
+            return True
+        return False
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.isEmpty():
+            print("Stack Underflow")
+            return 0
+        return self.stack.pop()
+
+    def peek(self):
+        if self.isEmpty():
+            print("Stack Underflow")
+            return 0
+        return self.stack[-1]
+
+    def size(self):
+        return len(self.stack)
+
+    # not sure why we have both show() and peek()
+    def show(self):
+        return self.peek()
 
 s = myStack()
 s.push('1')
--- Exercise_2.py
+++ Exercise_2.py
@@ -2,21 +2,45 @@ class Node:
     def __init__(self, data):
        self.data = data
        self.next = None
- 
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insertAtBegin(self, data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+        self.head = new_node
+
+    def removeFromBegin(self):
+        if not self.head:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        return temp
+
+
 class Stack:
     def __init__(self):
-        
+        self.stack_list = LinkedList()
+
     def push(self, data):
-        
+        self.stack_list.insertAtBegin(data)
+
     def pop(self):
-        
+        top = self.stack_list.removeFromBegin()
+        if top:
+            return top.data
+        return None
+
 a_stack = Stack()
 while True:
     print('push <value>')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
- 
+
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
