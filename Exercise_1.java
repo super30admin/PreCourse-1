c1bf0f146c3a266c6c2aec8ea1e18fe48d3b983d
Suresh Murali <sureshmurali@Sureshs-MacBook-Pro.local>
2021-04-17 01:37:21 -0400

    Completed Pre-Course-1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,44 +1,73 @@
-class Stack { 
+class Stack {
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        if (top < 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        if(top > 100) {
+            System.out.println("Stack Overflow!");
+            return false;
+        }
+        else{
+            top++;
+            a[top] = x;
+            System.out.println("Element" + x + "is pushed into the stack");
+            return true;
+
+        }
     } 
   
     int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+    {
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else{
+            int x = a[top--];
+            System.out.println("Element" + x + "is popped from the stack");
+            return x;
+        }
     } 
   
     int peek() 
     { 
-        //Write your code here
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+            return 0;
+        }
+        else{
+            int x = a[top];
+            return x;
+        }
     } 
 } 
   
 // Driver code 
-class Main { 
-    public static void main(String args[]) 
+class Main {
+    public static void main(String args[])
     { 
         Stack s = new Stack(); 
         s.push(10); 
         s.push(20); 
         s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.peek());
     } 
 }
--- Exercise_3.java
+++ Exercise_3.java
@@ -17,38 +17,60 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            //Write your code here
+            data = d;
         } 
     } 
   
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
     { 
-        // Create a new node with given data 
+        // Create a new node with given data
+        Node node = new Node(data);
    
         // If the Linked List is empty, 
-        // then make the new node as head 
+        // then make the new node as head
+
+        if (list.head == null)
+        {
+            list.head = node;
+        }
         
             // Else traverse till the last node 
-            // and insert the new_node there 
+            // and insert the new_node there
+        else{
+            Node nextNode = list.head;
+            while(nextNode.next != null)
+            {
+                nextNode = nextNode.next;
+            }
+            // Insert the new_node at last node
+            nextNode.next = node;
+        }
 
-            // Insert the new_node at last node 
-        // Return the list by head 
+
+        // Return the list by head
+        return list;
         
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
-    {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+    {
+        Node curr_node = list.head;
+        // Traverse through the LinkedList
+        while(curr_node != null) {
+
+            // Print the data at current node
+            System.out.println(curr_node.data);
+
+            // Go to next node
+            curr_node = curr_node.next;
+        }
     } 
    
-    // Driver code 
-    public static void main(String[] args) 
+    // Driver code
+     public static void main(String[] args)
     { 
         /* Start with the empty list. */
         LinkedList list = new LinkedList(); 
