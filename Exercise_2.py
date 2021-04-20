b80d9ddba0f11d5df787ea02575b19936d30472f
srichakradhar <srichakra3nsr3@gmail.com>
2021-04-20 11:33:26 -0400

    submit s30

--- Exercise_2.py
+++ Exercise_2.py
@@ -5,10 +5,53 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.size = 0
+
     def push(self, data):
+        if self.size == 0:
+            self.head = Node(data)
+            self.tail = self.head
+        else:
+            node = self.head
+            while node.next:
+                node = node.next
+            node.next = Node(data)
+            self.tail = node.next
+        self.size += 1
         
     def pop(self):
+        if self.size == 0:
+            return
+        if self.size == 1:
+            data = self.head.data
+            self.head = None
+            self.tail = self.head
+            self.size -= 1
+            return data
+        node = self.head
+        while node.next.next:
+            node = node.next
+        data = node.next.data
+        node.next = None
+        self.tail = node
+        self.size -= 1
+        return data
+
+    def peek(self):
+        if self.tail:
+            return self.tail.data
+    
+    def show(self):
+        stack = []
+        node = self.head
+        while node:
+            stack.append(node.data)
+            node = node.next
+        return stack
+    
+    def size(self):
+        return self.size
+
         
 a_stack = Stack()
 while True:
@@ -23,8 +66,17 @@ while True:
     elif operation == 'pop':
         popped = a_stack.pop()
         if popped is None:
-            print('Stack is empty.')
+            print('>>> Stack is empty.')
         else:
-            print('Popped value: ', int(popped))
-    elif operation == 'quit':
+            print('>>> Popped value: ', int(popped))
+    elif operation == 'peek':
+        peeked = a_stack.peek()
+        if peeked is None:
+            print('>>> Stack is empty.')
+        else:
+            print('>>> Peeked value: ', int(peeked))
+    elif operation == 'show':
+        stack = a_stack.show()
+        print(">>> ", stack)
+    elif operation.startswith('q') or operation == 'exit':
         break
