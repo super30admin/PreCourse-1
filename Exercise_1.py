f489b8084f1d8222b890115551f1a91d7e9c502a
tiwariarjun91 <tiwariarjun91@gmail.com>
2021-05-17 23:26:06 +0530

    Completed Exercise 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,31 @@
 class myStack:
      def __init__(self):
+     	self.stk = []
          
      def isEmpty(self):
+     	return len(self.stk)
          
      def push(self, item):
-         
+         self.stk.append(item)
      def pop(self):
+     	if len(self.stk)<=0:
+     		return 0
+     	else:
+     		return self.stk.pop()
         
         
      def peek(self):
+     	if len(self.stk <= 0):
+     		print("Stack Underflow")
+     		return 0
+     	else:
+     		return self.stk[-1]
         
      def size(self):
+     	return len(self.stk)
          
      def show(self):
+     	print(self.stk)
          
 
 s = myStack()
