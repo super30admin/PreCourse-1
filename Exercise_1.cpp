5858e2890d0d6e4057d3c8b0b77b1aec9287af0b
Radhika Kulkarni <radhikankulkarni@gmail.com>
2021-05-17 23:48:59 +0530

    Commit 1 for cpp exercise-1

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -10,7 +10,9 @@ class Stack {
 public: 
     int a[MAX]; // Maximum size of Stack 
   
-    Stack() { //Constructor here } 
+    Stack() { //Constructor here
+        top=0;
+    } 
     bool push(int x); 
     int pop(); 
     int peek(); 
@@ -21,22 +23,42 @@ bool Stack::push(int x)
 { 
     //Your code here
     //Check Stack overflow as well
+    bool stat = false;
+    if(top < MAX) {
+        a[top] = x;
+        top++;
+        stat = true;
+    }
+    return stat;
+
 } 
   
 int Stack::pop() 
 { 
     //Your code here
     //Check Stack Underflow as well 
+    int retVal = -1; //Assumption is that -1 will be returned incase of underflow
+    if(top != 0) {
+        top--;
+        retVal = a[top];
+    }
+    return retVal;
 } 
 int Stack::peek() 
 { 
     //Your code here
     //Check empty condition too
+    int retVal = -1; //Assumption is that -1 will be returned incase of empty stack
+    if(top != -1) {
+        retVal = a[top - 1];
+    }
+    return retVal;
 } 
   
 bool Stack::isEmpty() 
 { 
     //Your code here 
+    return (top == 0) ? true : false;
 } 
   
 // Driver program to test above functions 
