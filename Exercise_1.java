74b7a8ed76a300d4ffffa336e1161fb71ccc936b
sakshi <sakshi@sakshis-MacBook-Pro.local>
2021-05-17 18:16:59 -0400

    Completed Exercise-1

--- Exercise_1.java
+++ Exercise_1.java
@@ -6,10 +6,12 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+    	return top==-1;
     } 
 
     Stack() 
     { 
+    	top=-1;
         //Initialize your constructor 
     } 
   
@@ -17,16 +19,32 @@ class Stack {
     { 
         //Check for stack Overflow
         //Write your code here
+    	if(top==MAX-1)
+    	{
+    		System.out.println("Stack is Full");
+    		return false;
+    	}
+    	else {
+    	a[++top]=x;
+    	return true;
+    	}
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+    	if(isEmpty()) {
+    		System.out.println("Stack Underflow");
+            return top;
+    	}
+        else{
+    	return a[top--];}
     } 
   
     int peek() 
     { 
+    	return a[top];
         //Write your code here
     } 
 } 
