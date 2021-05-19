6e03c2d4a55d30e4e7bd6498635881a44e0602e9
odsouza <odsouza@akamai.com>
2021-05-19 09:29:38 +0530

    Precourse_1 Completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,33 @@
 class myStack:
      def __init__(self):
-         
+         self.stack = []
+         return
+
      def isEmpty(self):
-         
+          if len(self.stack)==0:
+               return True
+          else:
+               return False
+
      def push(self, item):
+          self.stack.append(item)
+
          
      def pop(self):
-        
+          if len(self.stack)==0:
+               return -1
+          return self.stack.pop()
         
      def peek(self):
+          top = len(self.stack)-1
+          return self.stack[top]
         
      def size(self):
+          return len(self.stack)
          
      def show(self):
+          return self.stack
+
          
 
 s = myStack()
