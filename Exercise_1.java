f146cd2d2606469b4111d94a12b54c7b2085ffd5
SatwikBhandiwad <satwik.s_bhandiwad@nokia.com>
2021-05-20 02:33:22 +0530

    Completed Sprint Precourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -6,28 +6,55 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+    	if(top < 0)
+    		return true;
+    	else
+    		return false;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+    	top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
+    	if(top >= (MAX -1)) {
+    		System.out.println("Stack Overflow");
+    		return false;
         //Write your code here
+    	}
+    	else{
+    		a[++top] = x;
+    		System.out.println(x + " pushed into stack");
+    		return true;
+    	}
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
+    	if(top < 0) {
+    		System.out.println("Stack Underflow");
+    		return 0;
+    	} else {
+    		return a[top--];
+    	}
+    	
         //Write your code here
     } 
   
     int peek() 
     { 
         //Write your code here
+    	if(top < 0) {
+    		System.out.println("Stack Underflow");
+    		return 0;
+    	} else {
+    		return a[top];
+    	}
     } 
 } 
   
