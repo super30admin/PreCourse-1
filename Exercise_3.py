bbc86601842675bfd659dcec0826492ffd1313f5
sonalgupta276 <sonalgupta276@gmail.com>
2021-05-19 01:44:03 -0500

    Complete Precourse-1

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data  
+        self.next = next 
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,16 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        newNode = ListNode(data)
+        if self.head is None:
+            self.head = newNode
+            return
+ 
+        last = self.head
+        while (last.next):
+            last = last.next
+ 
+        last.next =  newNode
         
     def find(self, key):
         """
@@ -24,9 +36,62 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        temp = self.head
+        while (temp):
+            if temp.data == key:
+                print(key)
+                return key
+            temp = temp.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        temp = self.head
+
+        if (temp is not None):
+            if (temp.data == key):
+                self.head = temp.next
+                temp = None
+                return
+ 
+        # Search for the key to be deleted, keep track of the
+        # previous node as we need to change 'prev.next'
+        while(temp is not None):
+            if temp.data == key:
+                break
+            prev = temp
+            temp = temp.next
+ 
+        # if key was not present in linked list
+        if(temp == None):
+            return
+            
+        # Unlink the node from linked list
+        prev.next = temp.next
+ 
+        temp = None
+
+    def printList(self):
+        current = self.head
+        while(current is not None):
+            print(current.data, end = " ")
+            current = current.next
+        print("\n")
+
+
+if __name__=='__main__':
+  
+    llist = SinglyLinkedList()
+    llist.append(1)
+    llist.append(2)
+    llist.append(3)
+    llist.append(4)
+    llist.printList()
+    llist.remove(3)
+    llist.printList()
+    llist.find(3)
+    llist.find(2)
+  
