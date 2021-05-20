24fac7f0c03124f3e2221610488a2e2f2fc85fd2
kgajwan1 <36811665+kgajwan1@users.noreply.github.com>
2021-05-19 22:09:24 -0400

    Update Exercise_2.cpp

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -7,7 +7,9 @@ public:
     int data; 
     StackNode* next; 
 }; 
-  
+
+
+
 StackNode* newNode(int data) 
 { 
     StackNode* stackNode = new StackNode(); 
@@ -15,25 +17,58 @@ StackNode* newNode(int data)
     stackNode->next = NULL; 
     return stackNode; 
 } 
-  
+ 
+
+StackNode *top;
 int isEmpty(StackNode* root) 
 { 
     //Your code here 
+    return top ==NULL;
+    
 } 
   
 void push(StackNode** root, int data) 
 { 
     //Your code here 
+    StackNode *temp = new StackNode(); 
+   if(!temp){
+       cout<<"stack is full\n";
+       exit(1);   
+       
+   }
+   temp->data=data;
+   temp->next = top;
+   top = temp;
+    
 } 
   
 int pop(StackNode** root) 
 { 
     //Your code here 
+    StackNode *temp = new StackNode(); 
+    if(top ==NULL){
+        cout<< "Empty stack\n";
+        exit(1);
+    }
+    else{
+        temp = top;
+        top = top->next;
+        temp->next = NULL;
+        return temp->data;
+    }
 } 
   
 int peek(StackNode* root) 
 { 
     //Your code here 
+    if(!isEmpty(root)){
+        return top->data;
+    }
+    else{
+        cout<<"stack is empty\n";
+        exit(1);
+        //return -1;
+    }
 } 
   
 int main() 
@@ -44,6 +79,8 @@ int main()
     push(&root, 20); 
     push(&root, 30); 
   
+    cout << pop(&root) << " popped from stack\n"; 
+    cout << pop(&root) << " popped from stack\n"; 
     cout << pop(&root) << " popped from stack\n"; 
   
     cout << "Top element is " << peek(root) << endl; 
