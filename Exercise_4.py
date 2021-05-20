660ed74edd484c4e0b655cfd7870879b20d248b9
JidnyeshSankhe <jidnyeshsankhe304@gmail.com>
2021-05-19 23:27:09 -0500

    completes exercise 4

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,38 +1,56 @@
 # Python program to insert element in binary tree  
-class newNode():  
-  
-    def __init__(self, data):  
-        self.key = data 
+
+class newNode():
+
+    def __init__(self, data):
+        self.key = data
         self.left = None
         self.right = None
-          
+
 """ Inorder traversal of a binary tree"""
-def inorder(temp): 
-  
-    
-  
-  
+def inorder(temp):
+    if not temp:
+        return
+    inorder(temp.left)
+    print(temp.key)
+    inorder(temp.right)
+
+
+
+
 """function to insert element in binary tree """
-def insert(temp,key): 
-  
-    
-  
-       
+def insert(temp,key):
+    if temp.key:
+        if key < temp.key:
+            if temp.left is None:
+                temp.left = newNode(key)
+            else:
+                insert(temp.left,key)
+        elif key > temp.key:
+            if temp.right is None:
+                temp.right = newNode(key)
+            else:
+                insert(temp.right,key)
+    else:
+        temp.key = key
+
+
+
 # Driver code  
-if __name__ == '__main__': 
-    root = newNode(10)  
-    root.left = newNode(11)  
-    root.left.left = newNode(7)  
-    root.right = newNode(9)  
-    root.right.left = newNode(15)  
-    root.right.right = newNode(8)  
-  
-    print("Inorder traversal before insertion:", end = " ") 
-    inorder(root)  
-  
+if __name__ == '__main__':
+    root = newNode(10)
+    root.left = newNode(11)
+    root.left.left = newNode(7)
+    root.right = newNode(9)
+    root.right.left = newNode(15)
+    root.right.right = newNode(8)
+
+    print("Inorder traversal before insertion:", end = " ")
+    inorder(root)
+
     key = 12
-    insert(root, key)  
-  
-    print()  
-    print("Inorder traversal after insertion:", end = " ") 
-    inorder(root) 
+    insert(root, key)
+
+    print()
+    print("Inorder traversal after insertion:", end = " ")
+    inorder(root)
