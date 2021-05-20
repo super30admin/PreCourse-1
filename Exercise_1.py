c07920ca0759134a85c36695ec2f6ecbd6b1b085
XDsubhash <subhashxd@gmail.com>
2021-05-19 20:55:27 -0700

    Completed Sprint Precourse 1
    Signed-off-by: XDsubhash <subhashxd@gmail.com>

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,38 @@
 class myStack:
      def __init__(self):
+          self.stack = list()
+          self.maxSize = 10
+          self.top = 0
          
      def isEmpty(self):
-         
+          return self.top <= 0
+
      def push(self, item):
+          if self.top >= self.maxSize:
+               print('Stack is Full.',item, ' cannot be pushed.')
+               return -1
+          self.stack.append(item)
+          self.top += 1
+          print(item, ' pushed to stack')
+          return item
          
      def pop(self):
-        
-        
+          if self.isEmpty():
+               return 'Stack is Empty'
+          item = self.stack.pop()
+          self.top -= 1
+          return item         
+     
      def peek(self):
+          if self.isEmpty():
+               return 'Stack is Empty'
+          return self.stack[self.top-1]          
         
      def size(self):
+          return self.top
          
      def show(self):
+          return self.stack[::-1]
          
 
 s = myStack()
--- Exercise_4.py
+++ Exercise_4.py
@@ -7,14 +7,38 @@ class newNode():
         self.right = None
           
 """ Inorder traversal of a binary tree"""
-def inorder(temp): 
-  
+def inorder(temp):
+    if temp:
+        inorder(temp.left)
+        print(temp.key, end = " ")
+        inorder(temp.right) 
     
   
   
 """function to insert element in binary tree """
-def insert(temp,key): 
-  
+def insert(temp,key):
+    if not temp:
+        root = newNode(key)
+        return
+
+    q = []
+    q.append(temp)
+
+    while (len(q)):
+        temp = q[0]
+        q.pop(0)
+
+        if (not temp.left):
+            temp.left = newNode(key)
+            break
+        else:
+            q.append(temp.left)
+
+        if (not temp.right):
+            temp.right = newNode(key)
+            break
+        else:
+            q.append(temp.right)
     
   
        
@@ -35,4 +59,4 @@ if __name__ == '__main__':
   
     print()  
     print("Inorder traversal after insertion:", end = " ") 
-    inorder(root) 
+    inorder(root)
