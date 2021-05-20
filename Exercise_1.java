7cfde27b6bef950d885a57399ee192c3005cf435
Vishal Puri <vishalpuri2594@gmail.com>
2021-05-19 20:39:21 -0500

    Completed Sprint PreCourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,44 +1,61 @@
-class Stack { 
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+// Time Complexity : All operations are O(1)
+// Space Complexity : As we have defined the array of MAX size, so SC is O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this : Class name should be kept Exercise_1 or Stack?
+// I wanted to keep it as "Stack" for better reference but followed the video provided and didn't change the
+// name of the class from "Exercise_1" to "Stack".
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
+class Stack {
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack 
+
+    boolean isEmpty() {
+        return top == 0 ? true : false;
+    }
+
+    Stack() {
+        //Initialize your constructor
+        top = 0;
+    }
+
+    boolean push(int x) {
         //Check for stack Overflow
-        //Write your code here
-    } 
-  
-    int pop() 
-    { 
+        if (top == MAX)
+            return false;
+        a[top++] = x;
+        return true;
+    }
+
+    int pop() {
         //If empty return 0 and print " Stack Underflow"
-        //Write your code here
-    } 
-  
-    int peek() 
-    { 
-        //Write your code here
-    } 
-} 
-  
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return a[--top];
+    }
+
+    int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return a[top - 1];
+    }
+}
+
 // Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+class Exercise_1 {
+    public static void main(String args[]) {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.pop();
+        s.pop();
+        s.pop();
+        s.peek();
+        System.out.println(s.pop() + " Popped from stack");
+    }
 }
