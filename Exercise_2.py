51038c51e3f9054e74ff256969b61303aeb50888
Syed Ahmed <syed.hope@gmail.com>
2021-05-20 22:45:23 -0700

    Completed Sprint PreCourse-1

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,22 +1,40 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, next = None):
        self.data = data
-       self.next = None
- 
+       self.next = next
+
 class Stack:
     def __init__(self):
-        
+        self.head = Node('this is actually tail')
+        self.max_len = 10000 # limiting to a number. Not necessary if you want to provide freedom to the user to
+        # use your entire heap size
+        self.len_ = 0 # to keep track of the current length
+
     def push(self, data):
-        
+        if self.len_ >= self.max_len:
+            print('Stack size reached, {} cannot be pushed'.format(data))
+        else:
+            new_node = Node(data, self.head)
+            self.head = new_node
+            self.len_ += 1
+
     def pop(self):
-        
+        if self.len_ <= 0:
+            print('Nothing to pop')
+            return
+        else:
+            result = self.head.data
+            self.head = self.head.next
+            self.len_ -= 1
+            return result
+
 a_stack = Stack()
 while True:
     print('push <value>')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
- 
+
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
@@ -28,3 +46,12 @@ while True:
             print('Popped value: ', int(popped))
     elif operation == 'quit':
         break
+
+'''
+n = number of elements in the stack
+Space Complexity: O(n)
+
+Time Complexity:
+push: O(1) in worst case which is better than array implementation
+pop: O(1)
+'''
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,7 +3,9 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+        self.val = data
+        self.next = next
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +19,52 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        new_node = ListNode(data)
+        current = self.head
+        if not current:
+            self.head = new_node
+        else:
+            while current.next:
+                current = current.next
+            current.next = new_node
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        current = self.head
+        while current:
+            if current.val == key:
+                return current.val
+            current = current.next
+        return
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        deleted = False
+        current = self.head
+        prev = current
+        if self.head.val == key: # edge case. handle head assignment separately
+            self.head = current.next
+            deleted = True
+        while current: # for the rest of the nodes
+            if current.val == key:
+                prev.next = current.next
+                deleted = True
+                break
+            prev = current
+            current = current.next
+        return deleted
+
+'''
+n = number of nodes in the linkedlist
+Space Complexity: O(n)
+
+Time Complexity:
+satisfies per requirement
+'''
