44671c5ec960e4df196a843ebf50497b4b09c6bc
krishnaattapu <krishna.attapu@gmail.com>
2021-05-22 00:47:04 -0500

    PreCourse-1 completed

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,52 +1,68 @@
-public class StackAsLinkedList { 
-  
-    StackNode root; 
-  
-    static class StackNode { 
-        int data; 
-        StackNode next; 
-  
-        StackNode(int data) 
-        { 
-            //Constructor here 
-        } 
-    } 
-    
-	
-    public boolean isEmpty() 
-    { 
-        //Write your code here for the condition if stack is empty. 
-    } 
-  
-    public void push(int data) 
-    { 
-        //Write code to push data to the stack. 
-    } 
-  
-    public int pop() 
-    { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
-    } 
-  
-    public int peek() 
-    { 
-        //Write code to just return the topmost element without removing it.
-    } 
-  
-	//Driver code
-    public static void main(String[] args) 
-    { 
-  
-        StackAsLinkedList sll = new StackAsLinkedList(); 
-  
-        sll.push(10); 
-        sll.push(20); 
-        sll.push(30); 
-  
-        System.out.println(sll.pop() + " popped from stack"); 
-  
-        System.out.println("Top element is " + sll.peek()); 
-    } 
-} 
+class StackAsLinkedList {
+
+	StackNode root;
+
+	static class StackNode {
+		int data;
+		StackNode next;
+
+		StackNode(int data) {
+			this.data = data;
+		}
+	}
+
+	public boolean isEmpty() {
+		return root == null;
+	}
+
+	public void push(int data) {
+		StackNode newNode = new StackNode(data);
+		if (!isEmpty()) {
+			StackNode oldRoot = root;
+			root = newNode;
+			newNode.next = oldRoot;
+		} else {
+			root = newNode;
+		}
+	}
+
+	public int pop() {
+		if (!isEmpty()) {
+			int val = root.data;
+			root = root.next;
+			return val;
+		} else {
+			System.out.println("Stack Underflow");
+			return 0;
+		}
+	}
+
+	public int peek() {
+		if (!isEmpty()) {
+			return root.data;
+		} else {
+			System.out.println("Stack Underflow");
+			return 0;
+		}
+	}
+
+}
+
+class Exercise_2 {
+	// Driver code
+	public static void main(String[] args) {
+
+		StackAsLinkedList s = new StackAsLinkedList();
+
+		for (int i = 1; i <= 1000; i++) {
+			s.push(i);
+		}
+
+		System.out.println("peek from stack " + s.peek());
+
+		for (int i = 1000; i > 0; i--) {
+			s.pop();
+		}
+		System.out.println("underflow status " + s.pop());
+	}
+}
