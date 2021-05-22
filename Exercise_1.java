374fcfb4833293f3cc4ca5c7e3ac0238bbef24e3
vigneshpurushothaman97 <vigneshpurushothaman97@gmail.com>
2021-05-22 17:25:34 +0530

    Completed Precourse-1 Exercise-1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,44 +1,58 @@
-class Stack { 
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+class Stack {
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
-        //Check for stack Overflow
-        //Write your code here
-    } 
-  
-    int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
-    } 
-  
-    int peek() 
-    { 
-        //Write your code here
-    } 
-} 
-  
-// Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+    boolean isEmpty()
+    {
+        return (top==-1);
+    }
+
+    Stack()
+    {
+        top=-1;
+    }
+    boolean push(int x)
+    {
+        if(!isFull())
+        {
+            a[++top]= x;
+            return true;
+        }
+        else
+        {
+            System.out.println("Stack overflow");
+            return false;
+        }
+    }
+
+    int pop()
+    {
+        if(!isEmpty())
+        {
+            int number = a[top];
+            top--;
+            return number;
+        }
+        else {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+    }
+    int peek()
+    {
+        return a[top];
+    }
+    boolean isFull()
+    {
+        return (top > MAX);
+    }
+    public static void main(String args[])
+    {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+    }
 }
