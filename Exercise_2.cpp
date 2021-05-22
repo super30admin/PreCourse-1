890331da444a2fe1ae669d37208309125fd6c62c
chaitanya003 <chaitanyakhinvasara33@gmail.com>
2021-05-22 17:02:00 +0530

    Added solutions to cpp files for PreCourse-1

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -19,21 +19,51 @@ StackNode* newNode(int data)
 int isEmpty(StackNode* root) 
 { 
     //Your code here 
+    return root ==NULL;
+    
 } 
   
 void push(StackNode** root, int data) 
 { 
     //Your code here 
+    StackNode *temp = newNode(data);
+    temp->next = *root;
+    *root = temp;
 } 
   
 int pop(StackNode** root) 
 { 
     //Your code here 
+    if(isEmpty(*root))                 //check if the stack is empty. If yes you cant delete the element
+    {
+        return -1;
+    }
+    else
+    {
+        /* code */
+     StackNode *temp = *root;
+     *root = (*root)->next;
+     int item = temp->data;
+     free(temp);
+     return item;   
+       
+    }
+    
 } 
   
 int peek(StackNode* root) 
 { 
     //Your code here 
+    if(isEmpty(root))
+    {
+        return -1;
+    }
+    else
+    {
+        /* code */
+        return root->data;
+    }
+    
 } 
   
 int main() 
@@ -50,3 +80,6 @@ int main()
   
     return 0; 
 } 
+
+// Time Complexity 
+// Time complexity is O(1) for all the functions.
