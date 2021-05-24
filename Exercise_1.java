28d99bdc38886375e85cd57d9dc8ded3bd1e28c8
DhirenParyani <sunny.paryani@gmail.com>
2021-05-23 21:33:47 -0700

    Completed Sprint PreCourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,29 +5,59 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        if(top==-1)
+        return true;
+
+        return false;
+        
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top=-1;
+
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        top++;
+        if(top<(MAX))
+        {
+            
+            a[top]=x;
+            return true;
+        }
+        return false;
+
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(isEmpty())
+        {
+            System.out.println("Stack Underflow");
+            return 0;
+
+        }
+        int poppedElement=a[top];
+        top--;
+        return poppedElement;
+        
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(isEmpty())
+        {
+            return 0;
+
+        }
+        return a[top];
     } 
 } 
   
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,10 @@
-import java.io.*; 
+import java.io.*;
+
+//import org.graalvm.compiler.graph.Node; 
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+public class Exercise_3 { 
   
     Node head; // head of list 
   
@@ -18,14 +20,15 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            this.data=d;
         } 
     } 
   
     // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
+    public static Exercise_3 insert(Exercise_3 list, int data) 
     { 
         // Create a new node with given data 
-   
+        Node node=new Node(data);    
         // If the Linked List is empty, 
         // then make the new node as head 
         
@@ -34,24 +37,45 @@ public class LinkedList {
 
             // Insert the new_node at last node 
         // Return the list by head 
+        if(list!=null && list.head==null)
+        list.head=node;
+        else{
+        Node temp=list.head;
+        while(temp.next!=null)
+        temp=temp.next;
+
+        temp.next=new Node(data);
+        }   
+
+        return list;
         
     } 
   
     // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
+    public static void printList(Exercise_3 list) 
     {  
+        Node temp=list.head;
         // Traverse through the LinkedList 
-   
+        while(temp!=null)
+        {
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
             // Print the data at current node 
        
-            // Go to next node 
+            // Go to next node
+            
     } 
+
+    class G115{
+
+    }
    
     // Driver code 
-    public static void main(String[] args) 
+    public static void main(String[] args)
     { 
         /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
+        Exercise_3 list = new Exercise_3(); 
   
         // 
         // ******INSERTION****** 
