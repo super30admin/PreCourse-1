fee6a74afb1a6a5436461290d09ae75e70dd2e0a
nishigandhamhaisne <nishmhaisne@gmail.com>
2021-05-24 17:24:23 -0700

    Completed Sprint PreCourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,38 @@
 class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    stack_array = []
+    max_size = 10000
+
+    def __init__(self):
+        self.stack_array = []
+
+    def isEmpty(self):
+        return len(self.stack_array) == 0
+
+    def push(self, item):
+        if len(self.stack_array) <= self.max_size:
+            self.stack_array.append(item)
+        else:
+            print("Stack Full!!")
+
+    def pop(self):
+        if (len(self.stack_array) != 0):
+            return self.stack_array.pop()
+        else:
+            print("Stack is Empty!!")
+
+    def peek(self):
+        # check if empty
+        if len(self.stack_array) == 0:
+            print("Stack is Empty")
+        else:
+            return self.stack_array[len(self.stack_array) - 1]
+
+    def size(self):
+        return len(self.stack_array)
+
+    def show(self):
+        return self.stack_array
+
 
 s = myStack()
 s.push('1')
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,38 +1,65 @@
-# Python program to insert element in binary tree  
-class newNode():  
-  
-    def __init__(self, data):  
-        self.key = data 
+# Python program to insert element in binary tree
+class newNode():
+
+    def __init__(self, data):
+        self.key = data
         self.left = None
         self.right = None
-          
+
+
 """ Inorder traversal of a binary tree"""
-def inorder(temp): 
-  
-    
-  
-  
+
+
+def inorder(temp):
+    if temp == None:
+        return
+
+    inorder(temp.left)
+    print (temp.key, " ")
+    inorder(temp.right)
+
+
 """function to insert element in binary tree """
-def insert(temp,key): 
-  
-    
-  
-       
-# Driver code  
-if __name__ == '__main__': 
-    root = newNode(10)  
-    root.left = newNode(11)  
-    root.left.left = newNode(7)  
-    root.right = newNode(9)  
-    root.right.left = newNode(15)  
-    root.right.right = newNode(8)  
-  
-    print("Inorder traversal before insertion:", end = " ") 
-    inorder(root)  
-  
+
+
+def insert(temp, key):
+    queue_for_level_order = []
+    queue_for_level_order.append(temp)
+
+    while (len(queue_for_level_order) > 0):
+        node = queue_for_level_order.pop(0)
+        if node.left is None:
+            node.left = newNode(key)
+            break
+        else:
+            queue_for_level_order.append(node.left)
+
+        if node.right is None:
+            node.right = newNode(key)
+            break
+        else:
+            queue_for_level_order.append(node.right)
+
+
+def main():
+    root = newNode(10)
+    root.left = newNode(11)
+    root.left.left = newNode(7)
+    root.right = newNode(9)
+    root.right.left = newNode(15)
+    root.right.right = newNode(8)
+
+    print("Inorder traversal before insertion:")
+    inorder(root)
+
     key = 12
-    insert(root, key)  
-  
-    print()  
-    print("Inorder traversal after insertion:", end = " ") 
-    inorder(root) 
+    insert(root, key)
+
+    print()
+    print("Inorder traversal after insertion:")
+    inorder(root)
+
+
+# Driver code
+if __name__ == '__main__':
+    main()
