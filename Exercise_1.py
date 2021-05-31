0d34d76faaf8d83261673418ce41f598cfc802ca
sushmakallapur <sushma.kallapur@gmail.com>
2021-05-31 12:38:47 -0700

    Sprint PreCourse1 completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,42 @@
 class myStack:
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    s_array = []
+    max = 1000
+
+    def __init__(self):
+        self.s_array = []
+
+    def isEmpty(self):
+        return len(self.s_array) == 0
+
+    def push(self, item):
+        if len(self.s_array) >= self.max:
+            print("Stack Overflow")
+        else:
+            self.s_array.append(item)
+        print("Pushed", item)
+
+    def pop(self):
+        if self.isEmpty():
+            print("Stack Underflow")
+        else:
+            self.s_array.pop()
+
+    def peek(self):
+        if self.isEmpty():
+            print("Stack Underflow")
+        else:
+            self.s_array[(len(self.s_array)) - 1]
+
+    def size(self):
+        return len(self.s_array)
+
+    def show(self):
+        return self.s_array
+
 
 s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
 print(s.show())
+
--- Exercise_2.py
+++ Exercise_2.py
@@ -5,15 +5,38 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.head = None
+
+
     def push(self, data):
+        if self.head == None:
+            self.head = Node(data)
+        else:
+            n_node = Node(data)
+            n_node.next = self.head
+            self.head = n_node
         
     def pop(self):
-        
+        if self.head == None: #Stack Underflow
+            return None
+
+        else:
+            node_pop = self.head
+            self.head = self.head.next
+            return node_pop.data
+
+    def peek(self):
+        if self.head == None:
+            return None
+        else:
+            return self.head.data
+
+
 a_stack = Stack()
 while True:
     print('push <value>')
     print('pop')
+    print('display')
     print('quit')
     do = input('What would you like to do? ').split()
  
@@ -26,5 +49,10 @@ while True:
             print('Stack is empty.')
         else:
             print('Popped value: ', int(popped))
+    elif operation == 'display':
+        node = a_stack.head
+        while node:
+            print(node.data)
+            node = node.next
     elif operation == 'quit':
         break
