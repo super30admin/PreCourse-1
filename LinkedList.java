f2e2005ef7be7eee71133bb1173a0244e9982467
Aditya Dasani <dasani.ad@northeastern.edu>
2021-06-13 17:30:44 -0400

    PreCourse-1 Completed

--- LinkedList.java
+++ LinkedList.java
@@ -1,4 +1,4 @@
-import java.io.*; 
+
   
 // Java program to implement 
 // a Singly Linked List 
@@ -18,6 +18,8 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            data = d;
+            next = null;
         } 
     } 
   
@@ -25,26 +27,43 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
+        Node node = new Node(data);
+        node.next = null;
    
         // If the Linked List is empty, 
         // then make the new node as head 
-        
+        if(list.head == null){
+            list.head = node;
+        } else {
             // Else traverse till the last node 
             // and insert the new_node there 
-
+            Node last = list.head;
+            while(last.next != null){
+                last = last.next;
+            }
             // Insert the new_node at last node 
+            last.next = node;
+        }
+           
+            
         // Return the list by head 
-        
+        return list;
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
+        Node current = list.head;
+        System.out.print("Linked List => ");
+
         // Traverse through the LinkedList 
-   
+
+        while(current != null) {
             // Print the data at current node 
-       
+            System.out.print(current.data + ",");
             // Go to next node 
+            current = current.next;
+        }
     } 
    
     // Driver code 
