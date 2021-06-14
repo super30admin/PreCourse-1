af591bdc1036847cf179c9448173d91dc5c05389
Mehar Shahani <mehar.shahani@gmail.com>
2021-06-14 12:43:08 -0500

    Completed Spring Precourse 1

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,4 +1,9 @@
-# Python program to insert element in binary tree  
+# Insert element in binary tree  
+
+# Any problem you faced while coding this : insert operation
+# Time complexity : O(log n)
+# Space complexity : O(n)
+
 class newNode():  
   
     def __init__(self, data):  
@@ -6,17 +11,41 @@ class newNode():
         self.left = None
         self.right = None
           
-""" Inorder traversal of a binary tree"""
+# Inorder traversal of a binary tree
 def inorder(temp): 
-  
+    if not temp:
+        return
     
+    inorder(temp.left)
+    print(temp.key, end = " ")
+    inorder(temp.right)
   
   
-"""function to insert element in binary tree """
+# function to insert element in binary tree
 def insert(temp,key): 
-  
-    
-  
+    if not temp:
+        root = newNode(key)
+        return
+
+    my_queue = []
+    my_queue.append(temp)
+
+    while len(my_queue):
+        temp = my_queue[0]
+        my_queue.pop(0)
+
+        if not temp.left:
+            temp.left = newNode(key)
+            break
+        else:
+            my_queue.append(temp.left)
+
+        if not temp.right:
+            temp.right = newNode(key)
+            break
+        else:
+            my_queue.append(temp.right)
+     
        
 # Driver code  
 if __name__ == '__main__': 
