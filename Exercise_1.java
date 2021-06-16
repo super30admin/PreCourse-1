1f8c966ee50afc04e345b5c34d6be22a8e9f9ab7
Suraj Vangala <surajvangala6@gmail.com>
2021-06-16 16:33:41 -0400

    Suraj completed Sprint PreCourse 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,44 +1,71 @@
-class Stack { 
+//Time Complexity:O(1)
+//Space Complexity:O(n)
+class Stack {
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        if(top==-1) return true;
+        return false;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top=-1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top!=MAX){
+            top++;
+            a[top]=x;
+            return true;
+        }
+        System.out.println("Stack Overflow");
+        return false;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        int m= a[top];
+        top--;
+        return m;
     } 
   
     int peek() 
     { 
         //Write your code here
-    } 
-} 
-  
-// Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        if(isEmpty()) {
+            System.out.println("Stack is Empty");
+            return -1;
+        }
+        return a[top];
     } 
 }
+class Main {
+    public static void main(String args[])
+    {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.peek());
+         System.out.println(s.pop() + " Popped from stack");
+         s.pop();
+         s.pop();
+         s.pop();
+    }
+}
+  
