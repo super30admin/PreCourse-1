505ae7370bd4381a08d5c977029d6cdb26e1b5fa
ml <mahaviredx@gmail.com>
2021-06-17 00:58:22 -0400

    Completed Sprint Precourse1 with Python3

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -10,13 +12,24 @@ class SinglyLinkedList:
         Create a new singly-linked list.
         Takes O(1) time.
         """
+        #head points to fron end of the node and tail points to the rear end of the node
         self.head = None
+        self.tail = None
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        #If there is no element in the list in the list insert at the front else insert at the rear
+        if self.head ==  None:
+            self.head = ListNode(data)
+            return
+        self.tail = self.head
+        while(self.tail.next):
+            self.tail = self.tail.next
+        self.tail.next = ListNode(data)
+        
         
     def find(self, key):
         """
@@ -24,9 +37,59 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        if self.head ==  None:
+            return
+        self.tail = self.head
+        while(self.tail):
+            if self.tail.data == key:
+                return self.tail.data
+            self.tail = self.tail.next
+        return None
+            
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head ==  None:
+            return
+        if self.head.data ==  key:
+            self.head = self.head.next
+            return 
+        self.tail = self.head
+        while(self.tail and self.tail.next):
+            if self.tail.next.data == key:
+                self.tail.next  = self.tail.next.next
+            self.tail = self.tail.next
+    
+    def show(self):
+        """
+        Prints all the elements of the list
+        """
+        if self.head ==  None:
+            return
+        self.tail = self.head
+        while(self.tail):
+            print(self.tail.data)
+            self.tail = self.tail.next
+        return None
+            
+ll = SinglyLinkedList()
+ll.append(2)
+ll.append(3)
+ll.append(4)
+ll.append(5)
+print("Show element")
+ll.show()
+print(ll.find(5))
+print(ll.find(11))
+ll.remove(4)
+print("Show element without 4")
+ll.show()
+print("Show element without 5")
+ll.remove(5)
+ll.show()
+print("Show element without 2")
+ll.remove(2)
+ll.show()
--- Exercise_4.py
+++ Exercise_4.py
@@ -7,13 +7,39 @@ class newNode():
         self.right = None
           
 """ Inorder traversal of a binary tree"""
-def inorder(temp): 
+def inorder(temp):
+    """ 
+    Inorder traversal of a binary tree left part , then root and then right part
+    Takes O(total nodes(n)) time.
+    """
+    if not temp:
+        return
+    inorder(temp.left)
+    print(temp.key)
+    inorder(temp.right)
+     
   
     
   
   
 """function to insert element in binary tree """
-def insert(temp,key): 
+def insert(temp,key, flag):
+    """
+    Function to add element inside tree. We use flag to check if the element has been inserted else we might insert duplicates
+    Takes O(height (h)) time.
+    """
+    if flag:
+        return
+    if not temp.left and not flag:
+        temp.left =  newNode(key)
+        flag.append("1")
+    if not temp.right and not flag:
+        temp.right =  newNode(key)
+        flag.append("1")
+    if not flag:
+        insert(temp.left,key, flag)
+    if not flag:
+        insert(temp.right,key,flag)
   
     
   
@@ -31,7 +57,8 @@ if __name__ == '__main__':
     inorder(root)  
   
     key = 12
-    insert(root, key)  
+    flag = []
+    insert(root, key, flag)  
   
     print()  
     print("Inorder traversal after insertion:", end = " ") 
