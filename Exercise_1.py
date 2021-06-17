d66fd15eada96dd57808e779ee15787cbf47b007
Neha Poonia <sihagneha91@gmail.com>
2021-06-16 19:29:36 -0700

    completed Sprint Precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,33 @@
+#Implement Stack using Array
 class myStack:
      def __init__(self):
+       self.l=[]
          
      def isEmpty(self):
+      if self.size()==0:
+         return True
+      else:
+          return False
          
      def push(self, item):
+       self.l.append(item)
+
          
      def pop(self):
-        
+        if self.size()==0:
+          print("Stack Empty")
+        else:
+          return self.l.pop()
+
         
      def peek(self):
+       return self.l[-1]
         
      def size(self):
+       return len(self.l)
          
      def show(self):
+       return self.l
          
 
 s = myStack()
