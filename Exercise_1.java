b644c09268b3113fc4c673198e960172ea207c89
DaryaniLeena <daryani.l@husky.neu.edu>
2021-06-16 20:05:59 -0700

    precourse1 code commit

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,29 +5,52 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        if(top==-1){
+            return true;
+        }
+        return false;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top<MAX){
+            a[++top] = x;
+            return true;
+        }else{
+            System.out.println("Stack Overflow");
+            return false;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top == -1) {
+            System.out.println("Stack Underflow");
+            return 0;
+        } else{
+            return a[top--];
+        }
     } 
   
     int peek() 
     { 
-        //Write your code here
+        if(top == -1){
+            System.out.println("Stack Underflow");
+            return 0;
+        }else{
+            return a[top];
+        }
     } 
 } 
   
