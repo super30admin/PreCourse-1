3a664611f482a3dc9bb0e144ff5269da54eae1f5
Ramya Kothapalli <sairamyakothapalli@gmail.com>
2021-06-18 14:20:29 -0700

    Completed Sprint Precourse1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,6 +1,8 @@
 public class StackAsLinkedList { 
-  
-    StackNode root; 
+      
+    static StackNode root = null; 
+    //static StackNode top = null;
+    int size;
   
     static class StackNode { 
         int data; 
@@ -8,34 +10,66 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
-            //Constructor here 
+            //Constructor here
+            this.data=data;
+            this.next = null;
+            
         } 
     } 
-    
-	
+        
     public boolean isEmpty() 
     { 
         //Write your code here for the condition if stack is empty. 
+        if(size == 0)
+            return true;
+        else
+            return false;
     } 
   
     public void push(int data) 
     { 
         //Write code to push data to the stack. 
+        StackNode newNode = new StackNode(data);
+        if(root == null) {
+            root = newNode;
+        }else {
+            StackNode oldRoot = root;
+            root = newNode;
+            root.next = oldRoot;
+        }
+        size++;
     } 
   
     public int pop() 
-    { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
+    {   
+    //If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+    //Also return the popped element 
+        if(isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }else {
+            int val = root.data;
+            root = root.next;
+            size--;
+            return val;
+        }
+        
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+        if(!isEmpty())
+            return root.data;
+        else {
+            System.out.println("Stack is Empty");
+            return -1;
+        }
+            
     } 
   
-	//Driver code
+    //Driver code
     public static void main(String[] args) 
     { 
   
