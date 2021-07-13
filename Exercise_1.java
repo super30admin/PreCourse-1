333f1cc6416fc0e107a79049dbcb440a2d783bce
skalapa2 <skalapa2@asu.edu>
2021-07-13 12:50:17 -0700

    PreCourse 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -5,40 +5,55 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        return top<0;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
+        if(top>=a.length) {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        a[++top] = x;
+        return true;
         //Write your code here
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
+        if(top < 0){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return a[top--];
         //Write your code here
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(top<0)return -1;
+        else return a[top];
     } 
 } 
   
 // Driver code 
-class Main { 
+class Exercise_1 {
     public static void main(String args[]) 
     { 
-        Stack s = new Stack(); 
+        Stack s = new Stack();
         s.push(10); 
         s.push(20); 
-        s.push(30); 
+        s.push(30);
         System.out.println(s.pop() + " Popped from stack"); 
     } 
 }
