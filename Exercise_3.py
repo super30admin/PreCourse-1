aeae3b013364eac036777601037424769ea19e2e
Sayanti Roy <sayantiroy90@gmail.com>
2021-07-14 14:10:06 -0600

    July Precourse 1

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,32 +1,46 @@
-class ListNode:
-    """
-    A node in a singly-linked list.
-    """
-    def __init__(self, data=None, next=None):
-    
-class SinglyLinkedList:
-    def __init__(self):
-        """
-        Create a new singly-linked list.
-        Takes O(1) time.
-        """
-        self.head = None
 
-    def append(self, data):
-        """
-        Insert a new element at the end of the list.
-        Takes O(n) time.
-        """
-        
-    def find(self, key):
-        """
-        Search for the first element with `data` matching
-        `key`. Return the element or `None` if not found.
-        Takes O(n) time.
-        """
-        
-    def remove(self, key):
-        """
-        Remove the first occurrence of `key` in the list.
-        Takes O(n) time.
-        """
+
+
+
+
+class Node:
+ 
+
+    def __init__(self, values):
+        self.values = values  
+        self.next = None  
+ 
+
+class Singly_Linked_list:
+ 
+
+    def __init__(self):
+        self.head = None
+ 
+   
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print (temp.values)
+            temp = temp.next
+ 
+ 
+
+def main():
+ 
+
+    list = Singly_Linked_list()
+ 
+    list.head = Node(1)
+    second = Node(23)
+    third = Node(67)
+    fourth =Node(98)
+ 
+    list.head.next = second; 
+    second.next = third;
+    third.next=fourth;
+ 
+    list.printList()
+    
+    
+main()
--- Exercise_4.py
+++ Exercise_4.py
@@ -1,38 +1,54 @@
 # Python program to insert element in binary tree  
-class newNode():  
-  
-    def __init__(self, data):  
-        self.key = data 
+
+class Insert_BST:
+
+    def __init__(self, val):
+        self.val = val
         self.left = None
         self.right = None
-          
-""" Inorder traversal of a binary tree"""
-def inorder(temp): 
-  
+
+   
+    def insert(self, val):
+        
+        if val == self.val:
+            return
+        if val < self.val:
+                    
+            if self.left:
+                self.left.insert(val)
+            else:
+                self.left = Insert_BST(val)
+        else:
+            
+            if self.right:
+                self.right.insert(val)
+            else:
+                self.right = Insert_BST(val)
+                
+                
+                
+    def printBST(self):
     
-  
-  
-"""function to insert element in binary tree """
-def insert(temp,key): 
-  
+        values = []
     
-  
-       
-# Driver code  
-if __name__ == '__main__': 
-    root = newNode(10)  
-    root.left = newNode(11)  
-    root.left.left = newNode(7)  
-    root.right = newNode(9)  
-    root.right.left = newNode(15)  
-    root.right.right = newNode(8)  
-  
-    print("Inorder traversal before insertion:", end = " ") 
-    inorder(root)  
-  
-    key = 12
-    insert(root, key)  
-  
-    print()  
-    print("Inorder traversal after insertion:", end = " ") 
-    inorder(root) 
+        if self.right:
+        	values += self.right.printBST()
+
+        
+        if self.left:
+            values += self.left.printBST()  
+        
+
+        values.append(self.val)
+        return values
+                
+numbers = [34123429,63457,454345435,1290873,754354355,4453455,85453451,3454345,554345,54353,243532,53553,3535353,353591]
+bst = Insert_BST(numbers[0])
+
+
+for i in range(1, len(numbers)):
+    bst.insert(numbers[i])
+
+bst.insert(8990)
+    
+print(bst.printBST())
