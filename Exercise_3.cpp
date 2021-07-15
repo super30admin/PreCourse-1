529e8b1e1b248cddac875b21c7d1f636f57cedf8
poojalokhandwala <30384366+poojalokhandwala@users.noreply.github.com>
2021-07-14 22:22:24 -0400

    Completed exercise3

--- Exercise_3.cpp
+++ Exercise_3.cpp
@@ -1,80 +1,89 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-  
-// A linked list node (changes) 
-class Node  
-{  
-    public: 
-    int data;  
-    Node *next;  
-};  
-  
-/* Given a reference (pointer to pointer) 
-to the head of a list and an int, inserts 
+#include <bits/stdc++.h>
+using namespace std;
+
+// A linked list node (changes)
+class Node
+{
+    public:
+    int data;
+    Node *next;
+};
+Node* head=NULL;
+/* Given a reference (pointer to pointer)
+to the head of a list and an int, inserts
 a new node on the front of the list. */
-void push(Node** head_ref, int new_data)  
-{  
-    /* 1. allocate node */ 
-  
-    /* 2. put in the data */  
-  
-    /* 3. Make next of new node as head */ 
-  
-    /* 4. move the head to point to the new node */
-}  
-  
-/* Given a node prev_node, insert a new node after the given  
+void push(Node** head_ref, int new_data)
+{
+    *head_ref = new Node();
+    (*head_ref)->data = new_data;
+
+    (*head_ref)->next = NULL;
+    if(head == NULL){
+        head = *head_ref;
+    }
+    else{
+       (*head_ref)->next = head;
+       head = *head_ref;
+    }
+}
+
+/* Given a node prev_node, insert a new node after the given
 prev_node */
-void insertAfter(Node* prev_node, int new_data)  
-{  
-    /*1. check if the given prev_node is NULL */ 
-  
-    /* 2. allocate new node */ 
-  
-    /* 3. put in the data */ 
-  
-    /* 4. Make next of new node as next of prev_node */
-  
-    /* 5. move the next of prev_node as new_node */ 
-}  
-  
-/* Given a reference (pointer to pointer) to the head  
+void insertAfter(Node* prev_node, int new_data)
+{
+    prev_node = head->next;
+    if(prev_node == NULL){
+        printf("Error: Invalid node pointer.");
+    }
+    else{
+        Node *new_node = new Node();
+        new_node->data = new_data;
+        new_node->next = (head->next)->next;
+        (head->next)->next = new_node;
+    }
+}
+
+/* Given a reference (pointer to pointer) to the head
 of a list and an int, appends a new node at the end */
-void append(Node** head_ref, int new_data)  
-{  
-    /* 1. allocate node */ 
-  
-    /* 2. put in the data */ 
-  
-    /* 3. This new node is going to be  
-    the last node, so make next of  
-    it as NULL*/  
-  
-    /* 4. If the Linked List is empty, 
-    then make the new node as head */
-  
-    /* 5. Else traverse till the last node */
-  
-    /* 6. Change the next of last node */ 
-}  
-  
-// This function prints contents of 
-// linked list starting from head  
-void printList(Node *node)  
-{  
-    //Your code here
-}  
-  
+void append(Node** head_ref, int new_data)
+{
+    *head_ref = new Node();
+    (*head_ref)->data = new_data;
+    (*head_ref)->next = NULL;
+    if(head == NULL){
+        head = *head_ref;
+    }
+    else{
+        Node* curr = head;
+        while(curr->next != NULL){
+            curr = curr->next;
+        }
+        curr->next = *head_ref;
+    }
+}
+
+// This function prints contents of
+// linked list starting from head
+void printList(Node *node)
+{
+    node = head;
+    while (node != NULL) {
+        cout<<node->data<<" ";
+        node = node->next;
+    }
+    cout<<endl;
+}
+
 /* Driver code*/
-int main()  
-{  
-    Node* head = NULL;   
-    append(&head, 6);  
-    push(&head, 7);  
-    push(&head, 1);    
-    append(&head, 4);    
-    insertAfter(head->next, 8);  
-    cout<<"Created Linked list is: ";  
-    printList(head);  
-    return 0;  
-}  
+int main()
+{
+    Node* head = NULL;
+    append(&head, 6);
+    push(&head, 7);
+    push(&head, 1);
+    append(&head, 4);
+    insertAfter(head->next, 8);
+    cout<<"Created Linked list is: ";
+    printList(head);
+    return 0;
+}
