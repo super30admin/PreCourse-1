572a321636f3fa2789625ed4e6a8b19c3de8642e
Durgesh Patil <patildurgesh1998@gmail.com>
2021-07-14 20:41:12 -0400

    Updated_Exercise_02.cpp

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,52 +1,93 @@
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 10000
+int size = 0;
+// A structure to represent a stack
+class StackNode {
+public:
+    int data;
+    StackNode* next;
+};
   
-// A structure to represent a stack 
-class StackNode { 
-public: 
-    int data; 
-    StackNode* next; 
-}; 
+StackNode* newNode(int data)
+{
+    StackNode* stackNode = new StackNode();
+    stackNode->data = data;
+    stackNode->next = NULL;
+    return stackNode;
+}
   
-StackNode* newNode(int data) 
-{ 
-    StackNode* stackNode = new StackNode(); 
-    stackNode->data = data; 
-    stackNode->next = NULL; 
-    return stackNode; 
-} 
+int isEmpty(StackNode* root)
+{
+    //Your code here
+    return (root == NULL);
+}
   
-int isEmpty(StackNode* root) 
-{ 
-    //Your code here 
-} 
+void push(StackNode** root, int data)
+{
+    //Your code here
+    StackNode* temp = new StackNode();
+    try{
+        if(size == MAX)
+            throw(10);
+        else
+        {
+            temp->data = data;
+            temp->next = *root;
+            *root = temp;
+            size++;
+        }
+    }
+    catch(int num)
+    {
+        cout<<"Stack Overflow"<<endl;
+    }
+}
   
-void push(StackNode** root, int data) 
-{ 
-    //Your code here 
-} 
+int pop(StackNode** root)
+{
+    //Your code here
+    StackNode* temp = new StackNode();
+    try{
+        if(size == 0)
+            throw(10);
+        else
+        {
+            temp = *root;
+            *root = temp->next;
+            size--;
+            return temp->data;
+        }
+    }
+    catch(int num)
+    {
+        cout<<"Stack Underflow"<<endl;
+    }
+}
   
-int pop(StackNode** root) 
-{ 
-    //Your code here 
-} 
+int peek(StackNode* root)
+{
+    //Your code here
+    if(root!=NULL)
+        return root->data;
+    else
+        exit(1);
+}
   
-int peek(StackNode* root) 
-{ 
-    //Your code here 
-} 
+int main()
+{
+    StackNode* root = NULL;
   
-int main() 
-{ 
-    StackNode* root = NULL; 
+    push(&root, 10);
+    push(&root, 20);
+    push(&root, 30);
   
-    push(&root, 10); 
-    push(&root, 20); 
-    push(&root, 30); 
+    cout << pop(&root) << " popped from stack\n";
   
-    cout << pop(&root) << " popped from stack\n"; 
+    cout << "Top element is " << peek(root) << endl;
   
-    cout << "Top element is " << peek(root) << endl; 
-  
-    return 0; 
-} 
+    return 0;
+}
+
+
