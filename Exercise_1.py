570f5ee081d34503d9cc8e005297c6442c77a149
Preksha Butani <prekshabutani@Prekshas-MacBook-Pro.local>
2021-07-14 23:09:44 -0700

    Completed PreCourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,47 @@
+"""
+Time Complexity : O(1)
+Space Complexity : O(n)
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this :
+"""
+#defining MAX here to check upper limit
+MAX = 1000
 class myStack:
      def __init__(self):
-         
+         self.data = []
      def isEmpty(self):
-         
+          if(len(self.data) == 0):
+               return True
+          else:
+               return False
      def push(self, item):
-         
+          """return if the array/list already has MAX=1000 
+             elements as it would be a Overflow condition
+             else just add/append to the list"""
+          if(len(self.data) == MAX):
+               print("Stack Overflow")
+          else:
+               self.data.append(item)
      def pop(self):
-        
-        
+          """return if the array/list is empty as 
+             it would be a Underflow condition
+             else just remove that from the list"""
+          if(self.isEmpty()):
+               print("Stack Underflow")
+               return 0
+          else:
+               element = self.data[-1]
+               del self.data[-1]
+               return element
      def peek(self):
-        
+          """return the last element of the list
+              as it is stack so last element would 
+              be the topmost element"""
+          return self.data[len(self.data) - 1]
      def size(self):
-         
+         return len(self.data)
      def show(self):
-         
+         return self.data
 
 s = myStack()
 s.push('1')
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,16 @@
+"""
+Time Complexity : O(n)
+Space Complexity : O(n)
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this :
+"""
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = None
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,16 +25,57 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        """To add the element at the end of the list 
+            we need to traverse through the list and 
+            insert at the very end of the list
+        """
+        if self.head == None:
+            self.head = ListNode(data,None)
+        else:
+            newNode = ListNode(data,None)
+            endNode = self.head
+            while(endNode.next != None):
+                endNode = endNode.next
+            endNode.next = newNode
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        """To find the element from the list 
+            we need to traverse through the list unless 
+            the element is found just in the head
+        """
+        currentNode = self.head
+        while(currentNode.next != None):
+            if currentNode.data == key:
+                return True
+            else:
+                currentNode = currentNode.next
+        return None
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        """To remove the element from the list 
+            we need to traverse through the list unless 
+            the element is found just in the head and 
+            also keep track of the previous pointer.
+            Once found we need to assign the pointer first 
+            to the next of next and then break the link
+        """
+        currentNode = self.head
+        if self.head.data == key:
+            currentNode = self.head
+            self.head = currentNode.next
+            currentNode = None
+        else:
+            while(currentNode.next != None):
+                if currentNode.data == key:
+                    break
+                prev = currentNode
+                currentNode = currentNode.next
+            prev.next = currentNode.next
+            currentNode = None
