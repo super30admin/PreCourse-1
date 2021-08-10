bfda718246ec90a42cc191ebe73c07504880300a
Smriti0311 <smritigrao@gmail.com>
2021-08-10 14:45:37 -0400

    Completed Exercise_4 of Prcourse-1

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,3 +1,10 @@
+# Time Complexity: O(n) as inorder and insert require traversal of entire tree
+# Space Complexity: O(n) for inorder traversal which is recursive
+# Did this code successfully run on Leetcode: Yes
+# Any problem you faced while coding this: Took some time to realise that you have to insert a new value as a newNode
+
+
+from queue import Queue
 # Python program to insert element in binary tree  
 class newNode():  
   
@@ -8,12 +15,37 @@ class newNode():
           
 """ Inorder traversal of a binary tree"""
 def inorder(temp): 
-  
-    
-  
+    if not temp:
+        return
+    inorder(temp.left)
+    print(temp.key)
+    inorder(temp.right)
   
 """function to insert element in binary tree """
 def insert(temp,key): 
+    if not temp:
+        temp = newNode(key)
+        temp.key = key
+    else:
+        mynode = newNode(key)
+        mynode.key = key
+        q1 = Queue(10)
+        q1.put(temp)
+        while not(q1.empty()):
+            root = q1.get() 
+            if root.left is not None:
+                q1.put(root.left)
+            else:
+                root.left = mynode
+                return 'Succesfully inserted '+str(key)+' as left child of '+ str(root.key)
+            
+            if root.right is not None:
+                q1.put(root.right)
+            else:
+                root.right = mynode
+                return 'Succesfully inserted '+str(key)+' as right child of '+ str(root.key)
+    
+
   
     
   
@@ -31,7 +63,7 @@ if __name__ == '__main__':
     inorder(root)  
   
     key = 12
-    insert(root, key)  
+    print(insert(root, key))
   
     print()  
     print("Inorder traversal after insertion:", end = " ") 
