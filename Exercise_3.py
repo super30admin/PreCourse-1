8bd5fde1c4a38f03cff13d747f87716c8f310096
Smriti0311 <smritigrao@gmail.com>
2021-08-10 12:20:10 -0400

    Completed Exercise_3 of Precourse-1

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,6 +1,6 @@
 # Time Complexity: O(n) 
 # Space Complexity: O(1)
-# Did this code successfully run on Leetcode: Yes, on problem 155 - MinStack
+# Did this code successfully run on Leetcode: Yes
 # Any problem you faced while coding this: NO
 
 class ListNode:
