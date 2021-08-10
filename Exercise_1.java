5fb3ec10401e72b0adac0ba827cd8fd422d50280
rajchoksi1997 <rajchoksi1997@gmail.com>
2021-08-10 13:37:43 -0700

    complete precourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,4 @@
+// Exercise_1 : Implement Stack using Array.
 class Stack { 
     static final int MAX = 1000; 
     int top; 
@@ -5,29 +6,47 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        return this.top == -1;
+
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        this.top = -1; 
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
-        //Write your code here
+        //Write your code here       
+        if(this.top == MAX-1){
+            return false;
+        }
+        this.top++;
+        this.top = x;
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(this.top == -1){
+            System.out.println(" Stack Underflow");
+            return 0;
+        }
+        int temp = this.top;
+        top--;
+        return temp;
+
     } 
   
     int peek() 
     { 
         //Write your code here
+        return this.top;
     } 
 } 
   
