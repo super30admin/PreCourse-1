a022535a0c062003d4b0310ff8025ae0d1c4281f
ami-doshi <ami.doshi@outlook.com>
2021-08-09 22:40:26 -0700

    Completed partail Spring Precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,35 @@
 class myStack:
      def __init__(self):
+         #initializing the stack as a list
+         self.array = []
          
      def isEmpty(self):
-         
+         #Checking if the Stack is empty
+         print(len(self.array)==0)
+     
      def push(self, item):
-         
+         #Pushing item into the stack at the last position
+         self.array.append(item)
+     
      def pop(self):
-        
+         #popping item which was last inserted (LIFO)
+         self.array = self.array[:-1]
         
      def peek(self):
-        
+         #Just viewing the last/top element of stack
+         return self.array[-1]
+     
      def size(self):
-         
+         #finding size of the stack
+         print(len(self.array),"   ",self.array)
+        
      def show(self):
-         
+         #Displaying the stack elements
+        print(self.array)         
 
 s = myStack()
 s.push('1')
 s.push('2')
+s.size()
+s.isEmpty()
 print(s.pop())
-print(s.show())
--- Exercise_2.py
+++ Exercise_2.py
@@ -6,11 +6,19 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.array = []
+
     def push(self, data):
-        
+        self.array.append(data)
+        print(self.array)
+    
     def pop(self):
-        
+        #pop if stack not empty
+        if len(self.array) > 0:
+            popped = self.array[-1]
+            self.array = self.array[:-1]
+            return popped
+    
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
