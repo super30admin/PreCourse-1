3a9c4470998f116da67fc9ddd244f66b459624e6
Sampada Samaka <sampadasamaka@gmail.com>
2021-08-09 22:14:04 -0500

    Added the implementation of stack using list

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,29 @@
 class myStack:
      def __init__(self):
+          self.stack = []
          
      def isEmpty(self):
+          if len(self.stack) == 0:
+               return True
+          else:
+               return False
          
      def push(self, item):
+          self.stack.append(item)
          
      def pop(self):
+          a = self.stack.pop()
+          print('Element popped is', a)
         
         
      def peek(self):
+          return(self.stack[-1])
         
      def size(self):
-         
+         return(len(self.stack))
+
      def show(self):
+          return(self.stack)
          
 
 s = myStack()
@@ -20,3 +31,4 @@ s.push('1')
 s.push('2')
 print(s.pop())
 print(s.show())
+
