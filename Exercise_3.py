67ecd21b335a372bc467b74aaf4946eb3b6312ae
Smriti0311 <smritigrao@gmail.com>
2021-08-10 12:11:55 -0400

    Completed Exercise_3 of Precourse-1

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,15 @@
+# Time Complexity: O(n) 
+# Space Complexity: O(1)
+# Did this code successfully run on Leetcode: Yes, on problem 155 - MinStack
+# Any problem you faced while coding this: NO
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +24,14 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        if self.head is None:
+            self.head = ListNode(data, None)
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = ListNode(data, None)
+        return "Successfully appended " + str(data)
         
     def find(self, key):
         """
@@ -24,9 +39,42 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        temp = self.head
+        while temp.next is not None:
+            if temp.data == key:
+                return temp.data
+            temp = temp.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+
+        if self.head.data == key:
+            if self.head.next is None:
+                self.head.data = None
+            else:
+                self.head = self.head.next
+        temp = self.head
+        while temp.next is not None:
+            if temp.next.data == key:
+                temp.next = temp.next.next
+                break
+        return "Successfully removed " + str(key)
+
+    def printLL(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.data)
+            temp = temp.next
+
+s1 = SinglyLinkedList()
+print(s1.append(1))
+print(s1.append(2))
+print(s1.append(1))
+print(s1.append(2))
+print(s1.find(1))
+print(s1.remove(2))
+s1.printLL()
