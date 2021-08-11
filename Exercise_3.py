7ccb3aea265a70816463fe26abd44249a0fb6e13
meghasn <meghoos99@gmail.com>
2021-08-11 22:44:15 +0530

    impleented singly linkedlist

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data=data
+        self.next=None
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,14 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        newnode=ListNode(data)
+        if self.head==None:
+            self.head=newnode
+        else:
+            current=self.head
+            while(current.next):
+                current=current.next
+            current.next=newnode
         
     def find(self, key):
         """
@@ -24,9 +34,61 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        current=self.head
+        while(current.next):
+            
+            if current.data==int(key):
+                
+                return key
+            current=current.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        temp=self.head
+        current=temp.next
+        while(temp.next):
+            if current.data==int(key):
+                
+                temp.next=current.next
+                current.next=None
+                current=temp.next
+                return
+            current=current.next
+            temp=temp.next
+    def print(self):
+        current=self.head
+        while(current.next):
+            print(current.data)
+            current=current.next
+        print(current.data)
+
+
+
+ll=SinglyLinkedList()
+while True:
+    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    print('append<value>')
+    print('find<value>')
+    print('remove<value>')
+    print('display')
+    do = input('What would you like to do? ').split()
+    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    operation = do[0].strip().lower()
+    if operation == 'append':
+        ll.append(int(do[1]))
+    elif operation == 'find':
+        val = ll.find(do[1])
+        if val is None:
+            print('key not found')
+        else:
+            print(val,"found")
+    elif operation == 'remove':
+        ll.remove(do[1])
+    elif operation == 'print':
+        ll.print()
+
+
