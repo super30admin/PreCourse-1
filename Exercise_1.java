f0243dd293bd112ae600dca366a7941f9a48d284
LAPTOP-APCK6UTA\lenovo s340 <manikyaudaykiran@gmail.com>
2021-08-11 16:07:16 -0500

    excercise 1,2,3

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,44 +1,73 @@
 class Stack { 
-    static final int MAX = 1000; 
+    static final int MAX = 5; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+    	if(top==-1)
+    	{
+    		return true;
+    	}
+    	else { return false;}
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+    	this.top=-1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
-        //Write your code here
+        if(top==MAX-1)
+        {
+        	System.out.println("stack is full cannot push :"+ x);
+        	return false;
+        }
+        else
+        {
+        	a[++top]=x;
+        	return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if(top<0)
+        {
+        	return 0;
+        }
+        else 
+        {
+        	int b = a[top--];
+        	return b;
+        }
     } 
   
     int peek() 
     { 
-        //Write your code here
+    	if(top<0)
+        {
+        	return 0;
+        }
+        else 
+        {
+        	return a[top];
+        }
     } 
 } 
   
-// Driver code 
+//Driver code 
 class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+ public static void main(String args[]) 
+ { 
+     Stack s = new Stack(); 
+     s.push(10); 
+     s.push(20); 
+     s.push(30); 
+     System.out.println(s.pop() + " Popped from stack"); 
+ } 
 }
