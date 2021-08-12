26b941240d8e387af67fb09ec98d12eb00061e49
agowdagr <abhiishekgowda@gmail.com>
2021-08-11 23:49:56 -0700

    Completed Sprint Precourse-1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,43 @@
 class myStack:
      def __init__(self):
+          self.list = []
          
      def isEmpty(self):
+          if self.list == []:
+               return True
+          else:
+               return False
          
      def push(self, item):
+          self.list.append(item)
+          return "The element has been successfully inserted"
          
      def pop(self):
+          if self.isEmpty():
+               return "There is not any element in the stack"
+          else:
+               return self.list.pop()
         
         
      def peek(self):
+          if self.isEmpty():
+               return "There is not any element in the stack"
+          else:
+               return self.list[len(self.list) - 1]
         
      def size(self):
+          return len(self.list)
          
      def show(self):
+          return self.list
+
          
 
 s = myStack()
 s.push('1')
 s.push('2')
+s.push('3')
+s.push('4')
+s.push('5')
 print(s.pop())
 print(s.show())
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,32 +1,117 @@
-class ListNode:
-    """
-    A node in a singly-linked list.
-    """
-    def __init__(self, data=None, next=None):
-    
-class SinglyLinkedList:
-    def __init__(self):
-        """
-        Create a new singly-linked list.
-        Takes O(1) time.
-        """
-        self.head = None
+class Node:
+    def __init__(self,value=None):
+        self.value=value
+        self.next=None
 
-    def append(self, data):
-        """
-        Insert a new element at the end of the list.
-        Takes O(n) time.
-        """
-        
-    def find(self, key):
-        """
-        Search for the first element with `data` matching
-        `key`. Return the element or `None` if not found.
-        Takes O(n) time.
-        """
-        
-    def remove(self, key):
-        """
-        Remove the first occurrence of `key` in the list.
-        Takes O(n) time.
-        """
+
+class SLinkedlist:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        #self.size=0
+
+    def __iter__(self):
+        node=self.head
+        while node:
+            yield node
+            node=node.next
+
+    def insertnode(self,value,location):
+        newnode= Node(value)
+        if self.head is None:
+            self.head=newnode
+            self.tail=newnode
+        else:
+            if location==0:
+                newnode.next=self.head
+                self.head=newnode
+            elif location==-1:
+                #print('Inserting at the end')
+                newnode.next=None
+                self.tail.next= newnode
+                self.tail=newnode
+            else:
+                tempnode=self.head
+                index=0
+                while(index<location-1):
+                    tempnode=tempnode.next
+                    index+=1
+                nextnode = tempnode.next
+                tempnode.next = newnode
+                newnode.next=nextnode
+                if self.head==self.tail:
+                    self.tail=newnode
+    def Slinkedlist_traverse(self):
+        if self.head is None:
+            print('There are no nodes to traverse')
+        else:
+            node=self.head
+            while node:
+                print(node.value)
+                node=node.next
+
+    def slinkedlist_search(self,nodevalue):
+        if self.head is None:
+            print('The linked list is empty')
+        else:
+            node=self.head
+            while node:
+                if node.value==nodevalue:
+                    return node.value
+                node=node.next
+            return 'The node does not exist in linked list'
+
+    def Slinkedlist_deletion(self,location):
+        if self.head is None:
+            print('the linked list doesnt exist')
+        else:
+            if location==0:
+                if self.head==self.tail:
+                    self.head=None
+                    self.tail=None
+                else:
+                    self.head=self.head.next
+            elif location==-1:
+                if self.head==self.tail:
+                    self.head=None
+                    self.tail=None
+                else:
+                    prevnode=self.head
+                    while prevnode:
+                        if prevnode.next==self.tail:
+                            break
+                        prevnode=prevnode.next
+                    prevnode.next=None
+                    self.tail=prevnode
+            else:
+                tempnode=self.head
+                index=0
+                while index<location-1:
+                    tempnode=tempnode.next
+                    index+=1
+                nextnode=tempnode.next
+                tempnode.next=nextnode.next
+
+
+
+
+singlelinkedlist= SLinkedlist()
+singlelinkedlist.insertnode(7,-1)
+singlelinkedlist.insertnode(3,-1)
+singlelinkedlist.insertnode(9,1)
+singlelinkedlist.insertnode(4,-1)
+singlelinkedlist.insertnode(99,0)
+singlelinkedlist.insertnode(100,0)
+singlelinkedlist.insertnode(44,0)
+
+
+print([node.value for node in singlelinkedlist])
+#singlelinkedlist.Slinkedlist_traverse()
+#print(singlelinkedlist.slinkedlist_search(50))
+singlelinkedlist.Slinkedlist_deletion(3)
+print([node.value for node in singlelinkedlist])
+
+
+
+
+
