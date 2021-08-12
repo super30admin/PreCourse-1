54eaaabf37e5df1448b53c24d26e4adcafade2e6
laxmisravyarudraraju <laxmisravyarudraraju@gmail.com>
2021-08-11 21:05:09 -0400

    Implemented stack using linked list

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,6 +1,12 @@
-public class StackAsLinkedList { 
+// Time complexity: 
+// PUSH - O(1)
+// POP - O(1)
+// PEEK - O(1)
+// SUCCESS in Leetcode
+
+class StackAsLinkedList { 
   
-    StackNode root; 
+    StackNode root;
   
     static class StackNode { 
         int data; 
@@ -8,31 +14,43 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
-            //Constructor here 
-        } 
-    } 
-    
+            this.data = data;
+            this.next = null;
+        }
+    }
 	
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+        return root == null;
     } 
   
     public void push(int data) 
     { 
         //Write code to push data to the stack. 
+        StackNode head = new StackNode(data);
+        head.next = root;
+        root = head;
     } 
   
     public int pop() 
     { 	
 	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
+    //Write code to pop the topmost element of stack.
 	//Also return the popped element 
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            int data = root.data;
+            root = root.next;
+            return data;
+        }
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+        return root.data;
     } 
   
 	//Driver code
@@ -43,7 +61,11 @@ public class StackAsLinkedList {
   
         sll.push(10); 
         sll.push(20); 
-        sll.push(30); 
+        System.out.println(sll.pop() + " popped from stack"); 
+        System.out.println("Top element is " + sll.peek());
+        sll.push(30);
+        System.out.println("Top element is " + sll.peek());
+        sll.push(40); 
   
         System.out.println(sll.pop() + " popped from stack"); 
   
