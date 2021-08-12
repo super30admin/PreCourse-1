99f6d5b8ccf73427a5111c5a8aba1e8d1f7819da
supreeth101 <supreeth97tamvada@gmail.com>
2021-08-12 00:25:36 -0500

    Added precourse 1 program solutions

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,28 @@
 class myStack:
      def __init__(self):
+          self.stack = []
          
      def isEmpty(self):
+          if len(self.stack) == 0:
+               return True
+          return False
          
      def push(self, item):
+          self.stack.append(item)
          
      def pop(self):
+          self.stack.pop()
         
         
      def peek(self):
+          top = self.stack[-1]
+          return top
         
      def size(self):
+          return len(self.stack)
          
      def show(self):
+          return (self.stack)
          
 
 s = myStack()
