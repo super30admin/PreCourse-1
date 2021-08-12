a1e5cd3cea8d41edfc9b0349eaa7053d9574f724
ami-doshi <ami.doshi@outlook.com>
2021-08-11 18:04:10 -0700

    Precourse 1 completed. Please Review

--- Exercise_4.py
+++ Exercise_4.py
@@ -13,7 +13,7 @@ def inorder(temp):
       #  print("L", temp.key)
         inorder(temp.left)
 
-        print(temp.key)
+        print(temp.key, end = " ") 
 
        # print("R", temp.key)
         inorder(temp.right)
@@ -22,10 +22,42 @@ def inorder(temp):
   
 """function to insert element in binary tree """
 def insert(temp,key): 
-  pass
-    
+    #if tree is empty
+    if not temp:
+      temp = newNode(key)
+      return
+   #Note: help taken from youtube to understand difference between BST and Binary Tree.
+   #Was unsure on how to do the insertion for Binary Level Order Tree
+
+    queue_node = []
+    #basically assigning root node object location to queue, 
+    #and converting the tree into a queue, each element being the node object
+    #this is due to level order tree, would be different in BST
+    queue_node.append(temp)
   
-       
+   
+    while (len(queue_node)):
+        #updating the original root to the new one which where a node would be inserted
+        temp = queue_node[0]
+        
+        #taking each node out and checking if it has right and left node
+        queue_node.pop(0)
+        
+        #checking the child, if empty we instert and stop
+        if (not temp.left):
+           temp.left = newNode(key)
+           break
+        else:
+            queue_node.append(temp.left)
+ 
+        if (not temp.right):
+            temp.right = newNode(key)
+            break
+        else:
+            queue_node.append(temp.right)
+   #while not(traverse_stack):
+                
+
 # Driver code  
 if __name__ == '__main__': 
     root = newNode(10)  
@@ -41,6 +73,6 @@ if __name__ == '__main__':
     key = 12
     insert(root, key)  
 #  
-#    print()  
-#    print("Inorder traversal after insertion:", end = " ") 
-#    inorder(root) 
+    print()  
+    print("Inorder traversal after insertion:", end = " ") 
+    inorder(root) 
