5d6f4c96a034f732e129de902898d7d12fdf729f
veeceey <varun_6april@hotmail.com>
2021-08-11 23:28:55 -0700

    Completed Sprint Precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,19 +1,55 @@
+"""
+Using LIFO for stack implementation in list
+"""
 class myStack:
+
      def __init__(self):
-         
+          """
+          Initialize the itemlist as a list
+          """
+          self.itemlist=[]
+
      def isEmpty(self):
-         
+          """
+          We check if the list stack is empty or not
+          """
+          if self.itemlist==[]:
+               return "Stack is empty"
+          else:
+               return "Stack is not empty"
+
      def push(self, item):
-         
+          """
+          Pushing the item into the stack like appending in an array at the back
+          """
+          self.itemlist.append(item)
+
      def pop(self):
-        
-        
+          """Popping the last element inserted from the stack"""
+          self.itemlist.pop()
+
      def peek(self):
+          """Checking the last element inserted in the stack"""
+          if self.itemlist:
+               return self.itemlist[-1]
+          else:
+               return None                        
         
      def size(self):
+          """
+          Checking the size of the stack
+          """
+          if self.itemlist:
+               return len(self.itemlist)
+          else:
+               return None
          
      def show(self):
-         
+          """
+          The show function to print the stack
+          """
+          return self.itemlist
+
 
 s = myStack()
 s.push('1')
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,52 @@
+"""
+Using LinkedList to implement Stack LIFO
+"""
 
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, nextNode=None):
+       """
+       Initializing the Class Node
+       Where NextNode pointer is None
+       And Data is the value
+       """
        self.data = data
-       self.next = None
+       self.nextNode = nextNode
  
 class Stack:
     def __init__(self):
-        
+       """
+       The init method to initialize
+       head of the Linkedlist to None
+       size of the linkedlist to 0
+       :return: None
+       """
+       self.head=None
+       self.size=0
+
     def push(self, data):
+        """
+        Pushing the element to the head of the node initially empty
+        and assigning the nextnode as the head
+        :param data:
+        :return: None
+        """
+        self.head=Node(data, self.head)
+        # self.nextNode=self.head
+        self.size+=1
         
     def pop(self):
-        
+        """
+        To avoid any exception we first check if the size of the list is >0 and in only that condition we could pop
+        We pop the element on the head size and assign the head to the next node after the element we popped
+        :return: None or Popped Element
+        """
+        if self.size==0:
+            return None
+        else:
+            poppedelement=self.head.data
+            self.head=self.head.nextNode
+            self.size-=1
+            return poppedelement
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
@@ -22,6 +58,7 @@ while True:
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
+
     elif operation == 'pop':
         popped = a_stack.pop()
         if popped is None:
