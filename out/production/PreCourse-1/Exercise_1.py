57252152fb63dbb720f95cfe0f5c63099b53465b
ghodkesujay <ghodke.su@northeastern.edu>
2021-08-11 20:41:41 -0400

    Completed Sprint PreCourse-1

---
+++ out/production/PreCourse-1/Exercise_1.py
@@ -0,0 +1,22 @@
+class myStack:
+     def __init__(self):
+         
+     def isEmpty(self):
+         
+     def push(self, item):
+         
+     def pop(self):
+        
+        
+     def peek(self):
+        
+     def size(self):
+         
+     def show(self):
+         
+
+s = myStack()
+s.push('1')
+s.push('2')
+print(s.pop())
+print(s.show())
