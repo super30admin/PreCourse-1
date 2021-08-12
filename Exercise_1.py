9730d8905183523221bb3a915f1f0871cfcc24a8
Rutuja Haridas <rutuja.haridas@tweeny.in>
2021-08-12 08:05:24 +0530

    Precourse 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,45 @@
 class myStack:
      def __init__(self):
+          self.top = -1
+          self.max = 100
+          self.data = []
          
+     def isFull(self):
+          if self.top == self.max:
+               return True
+          return False
+     
      def isEmpty(self):
+          if self.top == -1:
+               return True    
+          return False
          
      def push(self, item):
+          # Check if stack is full
+          if self.isFull():
+               return "Stack overflow"
+          
+          self.data.append(item)
+          self.top += 1
          
      def pop(self):
-        
-        
+        # Check if stack is empty
+          if self.isEmpty():
+             return "Stack is empty"
+          self.data.pop()
+          self.top -= 1
+          
+
      def peek(self):
+          if self.isEmpty():
+                 return "Stack is empty"
+          return self.data[self.top]
         
      def size(self):
-         
+          return "Size of stack is {}".format(len(self.data))
+
      def show(self):
+          return "Stack is {}".format(self.data)
          
 
 s = myStack()
@@ -20,3 +47,4 @@ s.push('1')
 s.push('2')
 print(s.pop())
 print(s.show())
+print(s.peek())
