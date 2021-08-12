74b9a74003f96de0c870fc090a82d18fbfbcc7e1
laxmisravyarudraraju <laxmisravyarudraraju@gmail.com>
2021-08-11 21:05:47 -0400

    Implemented Singly Linked list

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,13 @@
+// Time complexity: 
+// INSERT - O(N)
+// PRINT LIST - O(N)
+// SUCCESS in Leetcode
+
 import java.io.*; 
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList { 
   
     Node head; // head of list 
   
@@ -17,7 +22,8 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            this.data = d;
+            this.next = null;
         } 
     } 
   
@@ -25,26 +31,35 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
-   
+        Node node = new Node(data);
         // If the Linked List is empty, 
         // then make the new node as head 
+        if (list.head == null) {
+            list.head = node;
+        } else {
+            Node temp = list.head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
         
             // Else traverse till the last node 
             // and insert the new_node there 
 
             // Insert the new_node at last node 
         // Return the list by head 
-        
+        return list;
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+        Node temp = list.head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
     } 
    
     // Driver code 
