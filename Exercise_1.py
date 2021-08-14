5e1d761b5fdf28f6ff90286ae08c0dfa1f258e15
Chandra Mohan <chandan.anniyan@gmail.com>
2021-08-14 16:59:35 -0700

    Pushing exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,23 +2,45 @@ class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+      self.arr = []
+
      def isEmpty(self):
-         
+
+      if not self.arr:
+        return True
+      else:
+        return False
+
      def push(self, item):
-         
+      self.arr.append(item)
+
      def pop(self):
-        
+      if not self.arr:
+        return
+      else:
+        self.arr.pop()
         
      def peek(self):
-        
+      if not self.arr:
+        return
+      else:
+        return self.arr[-1]
+
      def size(self):
-         
+       return len(self.arr)        
+     
      def show(self):
-         
+      return self.arr
 
 s = myStack()
 s.push('1')
 s.push('2')
+s.push('2')
+print(s.isEmpty())
+print(s.peek())
+print(s.size())
+print(s.pop())
+print(s.pop())
+print(s.pop())
 print(s.pop())
 print(s.show())
