2decc2f7c21e8334fde5775e8d0b881e215664cc
Aman Sharma <amansharma@Amans-MacBook-Air.local>
2021-08-15 17:49:56 -0500

    Complete Python Exercises 1,2,3

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,24 +1,42 @@
+# Time Complexity : O(1)
+# Space Complexity : O(N)
+# Did this code successfully run on Leetcode : Couldn't find on Leetcode
+# Any problem you faced while coding this : None
 
+
+# Your code here along with comments explaining your approach
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
+
 class Stack:
     def __init__(self):
-        
+        self.root = Node(None)
+
     def push(self, data):
-        
+        node = Node(data)
+        node.next = self.root
+        self.root = node
+        print("Pushed " + str(data) + " to stack")
+
     def pop(self):
-        
+        if self.root.data is None:
+            return None
+        popped = self.root.data
+        self.root = self.root.next
+        return popped
+
+
 a_stack = Stack()
+print('push <value>')
+print('pop')
+print('quit')
 while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    print('push <value>')
-    print('pop')
-    print('quit')
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,9 +1,20 @@
+# Time Complexity : O(N)
+# Space Complexity : O(N)
+# Did this code successfully run on Leetcode : Couldn't find on Leetcode
+# Any problem you faced while coding this : None
+
+
+# Your code here along with comments explaining your approach
 class ListNode:
     """
     A node in a singly-linked list.
     """
+
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +28,88 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        node = ListNode(data)  # the next is set as None by default
+        current_node = self.head
+        if self.head is None:
+            self.head = node
+            print("Appended")
+            return node.data
+        while current_node.next is not None:
+            current_node = current_node.next
+        print("Appended")
+        current_node.next = node
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        if self.head is None:
+            print("Empty List")
+            return None
+        elif self.head.data == key:
+            return self.head.data
+
+        current_node = self.head
+
+        while current_node.next is not None:
+            current_node = current_node.next  # Because head has already been checked
+            if current_node.data == key:
+                return current_node.data
+        print("Element not found, returning None")
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head is None:
+            print("Empty List")
+            return None
+        elif self.head.data == key:
+            self.head = self.head.next
+            print("Removed")
+            return None
+        current_node = self.head
+
+        while current_node.next is not None:
+            if current_node.next.data == key:
+                current_node.next = current_node.next.next
+                print("Removed")
+                return None
+            current_node = current_node.next
+
+        print("Element not found, nothing removed")
+        return None
+
+
+'''
+# Driver code
+ll = SinglyLinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+ll.append(5)
+ll.append(6)
+print(ll.find(3))
+print(ll.find(7))
+ll.remove(3)
+ll.remove(7)
+ll.remove(3)
+ll.remove(4)
+print(ll.find(3))
+print(ll.find(4))
+print(ll.find(5))
+ll.remove(6)
+print(ll.find(6))
+ll.append(6)
+ll.append(7)
+print(ll.find(7))
+ll.remove(7)
+print(ll.find(5))
+print(ll.find(6))
+print(ll.find(7))
+'''
