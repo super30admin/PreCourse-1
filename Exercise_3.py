d9c75ae0daab2edd7e15b95d24ecbd8d938c8b80
Rohit Sikrewal <63307686+sikrewalrohit@users.noreply.github.com>
2021-08-17 18:08:32 -0700

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,16 +19,55 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        tail = self.head
+        new_node = ListNode(data)
         
+        if self.head == None:
+            self.head = new_node
+            return
+            
+        while tail.next != None:
+            tail = tail.next
+            
+        tail.next = new_node
+        return
+            
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        tail = self.head
+        
+        while tail is not None:
+            if tail.data == key:
+                return tail
+            
+            tail = tail.next
+            
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head == None:
+            print("Empty List!")
+            return
+        
+        tail = self.head
+        
+        if tail.data == key:
+            self.head = self.head.next
+            return
+        
+        while (tail.next != None) and (tail.next.data != key):
+            tail = tail.next
+         
+        if tail.next == None:
+            print("Element not found!")
+            return
+        
+        tail.next = tail.next.next
