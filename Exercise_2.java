7505bd70b5d74fab4c97fb2d4580692d1a6d74f7
Infernus <Infernus101@users.noreply.github.com>
2021-09-08 14:07:20 -0400

    done with precourse 1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,6 @@
-public class StackAsLinkedList { 
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+class StackAsLinkedList {
   
     StackNode root; 
   
@@ -8,31 +10,39 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
-            //Constructor here 
-        } 
+            this.data = data;
+            next = null;
+        }
     } 
     
 	
     public boolean isEmpty() 
-    { 
-        //Write your code here for the condition if stack is empty. 
-    } 
+    {
+        return root == null;
+    }
   
     public void push(int data) 
-    { 
-        //Write code to push data to the stack. 
+    {
+        StackNode temp = new StackNode(data);
+        temp.next = root;
+        root = temp;
     } 
   
     public int pop() 
-    { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+    {
+        if(isEmpty()){
+            System.out.println("Stack underflow");
+            return 0;
+        }else{
+            StackNode ret = root;
+            root = root.next;
+            return ret.data;
+        }
     } 
   
     public int peek() 
-    { 
-        //Write code to just return the topmost element without removing it.
+    {
+        return root.data;
     } 
   
 	//Driver code
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,9 @@
-import java.io.*; 
+// Time Complexity : O(n)
+// Space Complexity : O(n)
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList {
   
     Node head; // head of list 
   
@@ -16,35 +17,38 @@ public class LinkedList {
   
         // Constructor 
         Node(int d) 
-        { 
-            //Write your code here 
-        } 
-    } 
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    public boolean isEmpty()
+    {
+        return head == null;
+    }
   
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
-    { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
-
-            // Insert the new_node at last node 
-        // Return the list by head 
+    {
+        if(list.isEmpty())  list.head = new LinkedList.Node(data);
+        else{
+            LinkedList.Node temp = list.head;
+            while(temp.next != null)    temp = temp.next;
+            temp.next = new LinkedList.Node(data);
+        }
+        return list;
         
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
-    {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+    {
+        LinkedList.Node temp = list.head;
+        while(temp != null){
+            System.out.print(temp.data + "\n");
+            temp = temp.next;
+        }
     } 
    
     // Driver code 
