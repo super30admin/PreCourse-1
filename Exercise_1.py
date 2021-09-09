2e3241a24e3784342bb835e398556b724cda5ec1
akshay <akulkarni10@hawk.iit.edu>
2021-09-09 00:51:21 -0500

    Add comments

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,4 +1,4 @@
-# // Time Complexity : append O(1), Pop O(1), Peek O(1), isEmpty O(1), Size O(1)
+# // Time Complexity : append O(1), Pop O(n), Peek O(1), isEmpty O(1), Size O(1)
 # // Space Complexity : O(1)
 # // Did this code successfully run on Leetcode : Yes
 # // Any problem you faced while coding this : No
@@ -9,18 +9,23 @@ class myStack:
     # Please read sample.java file before starting.
     # Kindly include Time and Space complexity at top of each file
     def __init__(self):
+      # Initializing my data structure
         self.stack = []
 
     def isEmpty(self):
+      #if length of stack is zero return true else false
         return len(self.stack) == 0
 
     def push(self, item):
+      #appending an item to the end of the list
         self.stack.append(item)
 
     def pop(self):
+       #removing an item to the end of the list
         return self.stack.pop()
 
     def peek(self):
+      #getting an item to the end of the list
         return self.stack[-1]
 
     def size(self):
