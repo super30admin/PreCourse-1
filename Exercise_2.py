62b812ca98cbe0b986eca21be70b13896ee7660a
akshatha-p <56571864+akshatha-p@users.noreply.github.com>
2021-09-09 03:13:24 -0400

    Completed Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -6,10 +6,24 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head = None
         
     def push(self, data):
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            NewNode = Node(data)
+            NewNode.next = self.head
+            self.head = NewNode
         
     def pop(self):
+        if self.head is None:
+            return None
+        else:
+            popped = self.head.data
+            self.head = self.head.next
+            return popped
+        
         
 a_stack = Stack()
 while True:
