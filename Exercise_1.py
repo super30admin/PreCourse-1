274aeedde5827448809cf13ec9767a7c127b0444
akshay <akulkarni10@hawk.iit.edu>
2021-09-08 21:43:33 -0500

    Implement Stack using Array

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,37 @@
+# // Time Complexity : append O(1), Pop O(1), Peek O(1), isEmpty O(1), Size O(1)
+# // Space Complexity : O(1)
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : No
+
+
+# // Your code here along with comments explaining your approach
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    # Please read sample.java file before starting.
+    # Kindly include Time and Space complexity at top of each file
+    def __init__(self):
+        self.stack = []
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        self.stack.pop()
+
+    def peek(self):
+        return self.stack[len(self.stack) - 1]
+
+    def size(self):
+        return len(self.stack)
+
+    def show(self):
+        return self.stack
+
 
 s = myStack()
-s.push('1')
-s.push('2')
+s.push("1")
+s.push("2")
 print(s.pop())
 print(s.show())
