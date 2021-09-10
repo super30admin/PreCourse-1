77a0245da4ccec016661b75200d49bdf08c34dc3
vgirkar <vgirkar@uncc.edu>
2021-09-09 22:03:44 -0400

    Completed - Please Review

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,37 +1,60 @@
-class Stack { 
-    //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+class Stack {
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
-        //Check for stack Overflow
-        //Write your code here
-    } 
-  
-    int pop() 
-    { 
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack
+
+    boolean isEmpty() {
+        return top < 0;
+    }
+
+    Stack() {
+        //Initialize your constructor
+        top = -1;
+
+    }
+
+    boolean push(int x) {
+        if (top > MAX - 1) {
+            //Check for stack Overflow
+            System.out.println("Stack OverFlow");
+            return false;
+
+        } else {
+            //Write your code here
+            a[++top] = x;
+            return true;
+
+        }
+
+
+    }
+
+    int pop() {
         //If empty return 0 and print " Stack Underflow"
+        if (top < 0) {
+            System.out.println("Stack UnderFlow");
+            return 0;
+
+        } else {
+            int x = a[top--];
+            return x;
+        }
         //Write your code here
-    } 
-  
-    int peek() 
-    { 
+    }
+
+    int peek() {
+        if (top < 0) {
+            System.out.println("Stack UnderFlow");
+            return 0;
+
+        } else {
+            int x = a[top];
+            return x;
+        }
         //Write your code here
-    } 
-} 
+    }
+}
   
 // Driver code 
 class Main { 
