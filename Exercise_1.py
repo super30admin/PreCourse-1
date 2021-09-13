b4faa7550d4a99c606110c6d27ca936fe2022436
ag-tiwari <90532652+ag-tiwari@users.noreply.github.com>
2021-09-13 01:11:21 -0500

    Completed Precourse-1

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,23 +2,47 @@ class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+       self.stack=list()
+
+     # O(1) Time Complexity    
      def isEmpty(self):
-         
+         if len(self.stack)==0:
+           return True
+         else:
+           return False
+    
+    # O(1) Time Complexity   
      def push(self, item):
-         
+         self.stack.append(item)
+
+    # O(1) Time Complexity
      def pop(self):
-        
-        
+        if len(self.stack)>0:
+          return self.stack.pop()
+        else:
+          print('Stack Underflow')
+          return 0
+
+    # O(1) Time Complexity
      def peek(self):
-        
+        if len(self.stack)>0:
+          return self.stack[-1]
+        else:
+          return None
+     
+     # O(1) Time Complexity
      def size(self):
-         
+        return len(self.stack) 
+    
+    # O(1) Time Complexity
      def show(self):
-         
+         return self.stack
 
 s = myStack()
 s.push('1')
-s.push('2')
+s.push('3')
+print(s.peek())
+print(s.size())
 print(s.pop())
 print(s.show())
+print(s.isEmpty())
--- Exercise_2.py
+++ Exercise_2.py
@@ -6,16 +6,42 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.top=None
+
     def push(self, data):
-        
+        new_node=Node(data)
+        if new_node is None:
+            return
+        new_node.data = data 
+        new_node.next = self.top
+        self.top = new_node
+
     def pop(self):
-        
+        if self.top is None:
+            print("Stack Underflow")
+            return 0
+        temp=self.top.data
+        self.top = self.top.next
+        return temp
+
+    def isEmpty(self):
+        return self.top is None
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.top.data
+        else:
+            print("The stack is empty")
+            return -1
+
 a_stack = Stack()
+
+
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
     print('pop')
+    print('peek')
     print('quit')
     do = input('What would you like to do? ').split()
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
@@ -30,3 +56,5 @@ while True:
             print('Popped value: ', int(popped))
     elif operation == 'quit':
         break
+    elif operation == 'peek':
+        print(a_stack.peek())
