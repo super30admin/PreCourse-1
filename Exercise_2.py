c862abe7b333197982cdc3208b18b7849e7b2974
yashgovilkar <yashgovilkar14@gmail.com>
2021-09-29 13:46:24 +0530

    Completed Sprint Precourse1

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,4 +1,8 @@
-
+"""
+Time Complexity: O(1)
+Space Complexity: O(n)
+Code ran successfully
+"""
 class Node:
     def __init__(self, data):
        self.data = data
@@ -6,10 +10,23 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head=None
         
     def push(self, data):
+        if self.head == None:
+            self.head=Node(data)
+        else:
+            new_node=Node(data)
+            new_node.next=self.head
+            self.head=new_node
         
     def pop(self):
+        if self.head == None:
+            return None
+        else:
+            removed_data=self.head.data
+            self.head=self.head.next
+            return removed_data
         
 a_stack = Stack()
 while True:
