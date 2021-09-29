c23e5754fdce389e73039aed64db83f833935a75
ramkr508 <ramkr508@gmail.com>
2021-09-29 00:02:02 -0500

    Revert "Completed Sprint Precourse1"
    This reverts commit e01306b1f6ac41a263e5f443e63dde19dbb8abd2.

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,6 +1,5 @@
- class StackAsLinkedList {
-     //time complexity O(1) for push, pop and peek
-     //space complexity is O(n) for n elements
+public class StackAsLinkedList { 
+  
     StackNode root; 
   
     static class StackNode { 
@@ -9,60 +8,31 @@
   
         StackNode(int data) 
         { 
-           this.data = data;
+            //Constructor here 
         } 
     } 
     
 	
     public boolean isEmpty() 
-    {
-        if (root == null) {
-            return true;
-        }
-        else
-            return false;
+    { 
+        //Write your code here for the condition if stack is empty. 
     } 
   
     public void push(int data) 
-    {
-        StackNode newNode = new StackNode(data);
-
-        if (root == null) {
-            root = newNode;
-        }
-        else {
-            StackNode temp = root;
-            root = newNode;
-            newNode.next = temp;
-        }
+    { 
+        //Write code to push data to the stack. 
     } 
   
     public int pop() 
     { 	
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
-	//Also return the popped element
-
-        int popped = 0;
-        if (root == null) {
-            System.out.println("Stack Underflow");
-        }
-        else {
-            popped = root.data;
-            root = root.next;
-        }
-        return popped;
+	//Also return the popped element 
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
-        if (root == null) {
-            return 0;
-        }
-        else {
-            return root.data;
-        }
     } 
   
 	//Driver code
@@ -73,10 +43,10 @@
   
         sll.push(10); 
         sll.push(20); 
-        sll.push(30);
-
-        System.out.println(sll.pop() + " popped from stack");
-
-        System.out.println("Top element is " + sll.peek());
+        sll.push(30); 
+  
+        System.out.println(sll.pop() + " popped from stack"); 
+  
+        System.out.println("Top element is " + sll.peek()); 
     } 
 } 
