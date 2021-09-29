ba759f8a1b8771dcff293347c69711f157a6d463
Spandana Edupuganti <spandanaedupuganti31@gmail.com>
2021-09-28 22:22:35 -0400

    PreCourse-1 completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,39 +1,72 @@
+// Time Complexity : O(1)
+// Space Complexity : O(MAX)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+// as stack is First In Last Out, we will add the elements to the end of the array and remove the elements from the end of the array
 class Stack { 
-    //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
+ 
     boolean isEmpty() 
     { 
-        //Write your code here 
+    	//check whether the top is at index 0
+        if(top==0){
+            return true;
+        }else{
+            return false;
+        }
+       
     } 
 
+    //initialize top with zero
     Stack() 
     { 
-        //Initialize your constructor 
+        top=0;
     } 
   
+    
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+    	//check if the array size is within the max capacity
+        if(top<a.length-1){
+        	//assign the new element to array value at index "top" and increment top
+            a[top]=x;
+            top++;
+            return true;
+        }else{
+            return false;
+        }
     } 
+  
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+    	//check if array is empty
+        if(isEmpty()){
+        	//print appropriate message if array is empty
+            System.out.println("Stack Underflow");
+            return 0;
+        }else{
+        	//assign array value at index "top-1" to a temp variable and then remove the element by making it to 0 and decrement top
+            int result= a[top-1];
+            a[top-1]=0;
+            top--;
+            return result;
+        }
     } 
   
+    //return the element present at the top index in the array
     int peek() 
     { 
-        //Write your code here
+        return a[top];
     } 
 } 
   
-// Driver code 
 class Main { 
     public static void main(String args[]) 
     { 
--- Exercise_2.java
+++ Exercise_2.java
@@ -1,38 +1,86 @@
-public class StackAsLinkedList { 
+// Time Complexity : O(N) where N is size of the stack
+// Space Complexity : O(N) where N is size of the stack
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+// as stack is First In Last Out, we will add the elements to the end of list and remove the elements from the end of the list
+class StackAsLinkedList { 
   
     StackNode root; 
   
     static class StackNode { 
         int data; 
         StackNode next; 
-  
+        
+        //initialize node with data
         StackNode(int data) 
         { 
-            //Constructor here 
+        	this.data=data;
         } 
     } 
     
 	
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+    	//check for root node if it is null
+    	if(root==null) {
+    		return true;
+    	}else {
+    		return false;
+    	}
     } 
   
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
+    	StackNode node=new StackNode(data);
+    	//check if root node is null and if it is make the new node as root
+    	if(isEmpty()) {
+    		root=node;
+    		root.next=null;
+    	}else {
+    		//traverse through the linked list and link the new node at the end of the list
+    		StackNode curr=root;
+    		while(curr.next!=null) {
+    			curr=curr.next;
+    		}
+    		curr.next=node;
+    		//as this is the last node point the next to null
+    		node.next=null;
+    	}
     } 
   
     public int pop() 
     { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+    	//check if the stack is empty and if it is print appropriate message
+    	if(isEmpty()) {
+    		System.out.println("Stack Underflow");
+    		return 0;
+    	}
+    	//traverse through the linked list until the last but one node 
+    	StackNode curr=root;
+		while(curr.next!=null && curr.next.next!=null) {
+			curr=curr.next;
+		}
+		//assign the last node to a temp node
+		int result=curr.next.data;
+		// point the last but one node's next to null hearby removing the last node from the linked list  
+		curr.next=null;
+		return result;
     } 
   
     public int peek() 
     { 
-        //Write code to just return the topmost element without removing it.
+    	//check if stack is empty and if it is return 0
+    	if(isEmpty()) return 0;
+    	StackNode curr=root;
+    	//traverse through the linked list until the last node and return the last node's data
+		while(curr.next!=null) {
+			curr=curr.next;
+		}
+		int result=curr.data;
+		return result;
     } 
   
 	//Driver code
