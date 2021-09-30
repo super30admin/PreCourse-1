d0c6c593c24c58d197397446acbb4cdbcff85b6d
jaidevmane <devmane.j@northeastern.edu>
2021-09-29 23:55:06 -0400

    Completed Pre-Course 1 in Python : Please review

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,14 @@
+#Exercise 2 : Implement a stack using LinkedList
+
+#Time Complexity : O(1)
+#The time complexity in this case to push or pop an element is O(1), as all we are doing for creating a new node is changing the last node pointer to the new node,
+#and it is added to the stack, and in the case of pop, removing the pointer of the current head node and making it point to the next node, effectively popping(or removing)
+#the element
+
+
+#Space Complexity : O(N)
+#The time complexity is O(N) as we would require space as it scales up, and this space is proportional to the length of the stack.
+
 
 class Node:
     def __init__(self, data):
@@ -6,10 +17,23 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head = None
         
     def push(self, data):
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
         
     def pop(self):
+        if self.head is None:
+            return None
+        else:
+            popped = self.head.data
+            self.head = self.head.next
+            return popped
         
 a_stack = Stack()
 while True:
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,9 +1,20 @@
+#Exercise 3 : Implement singly Linked List
+
+#Time Complexity : The time complexity for all of the methods(except initialization) mentioned below is O(N), as for every operation we have to traverse the length of the linked list, and 
+#unlike stack it is not constant time operation.
+
+
+#Space Complexity : The space complexity is O(N) as it will increase as the number of elements(length) of the linked list increases.
+
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,6 +28,13 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        if not self.head:
+            self.head = ListNode(data=data)
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+            curr.next = ListNode(data=data) 
         
     def find(self, key):
         """
@@ -24,9 +42,28 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        curr = self.head
+        while curr and curr.data != key:
+            curr = curr.next
+        return curr
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        curr = self.head
+        prev = None
+        while curr and curr.data != key:
+            prev = curr
+            curr = curr.next
+        if prev is None:
+            self.head = curr.next
+        elif curr:
+            prev.next = curr.next
+            curr.next = None
+
+
+
+
+
