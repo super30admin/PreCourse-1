7e4d2ae5889c310e3acae6fed93c7a5a46721a21
Prashant Mittal <prashant.mittal2@globallogic.com>
2021-09-29 22:00:35 -0500

    Completed PreCourse 1 in python

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,66 @@
+# Time Complexity : mentioned in every function
+# Space Complexity : mentioned in every function
+# Did this code successfully run on Leetcode : Didn't tried
+# Any problem you faced while coding this : No
+
+# Your code here along with comments explaining your approach
+
+
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+
+    def __init__(self):
+        """
+        Time Complexity : O(1)
+        Space Complexity : O(1)
+        """
+        self.data = []
+
+    def isEmpty(self):
+        """
+        Time Complexity : O(1)
+        Space Complexity : O(1)
+        """
+        return self.data == []
+
+    def push(self, item):
+        """
+        Time Complexity : O(1)
+        Space Complexity : O(1)
+        No additional variable used hence constant space complexity.
+        """
+        self.data.append(item)
+
+    def pop(self):
+        """
+        Time Complexity : O(1)
+        Space Complexity : O(1)
+        No additional variable used hence constant space complexity.
+        """
+        return self.data.pop()
+
+    def peek(self):
+        """
+        Time Complexity : O(1)
+        Space Complexity : O(1)
+        """
+        return self.data[-1]
+
+    def size(self):
+        """
+        Time Complexity : O(1)
+        Space Complexity : O(1)
+        built-in len() function stores the length of any list in a separate variable. Hence returning
+        value is a O(1) time complexity.
+        """
+        return len(self.data)
+
+    def show(self):
+        """
+        Time Complexity : O(1)
+        Space Complexity : O(1)
+        """
+        return self.data
+
 
 s = myStack()
 s.push('1')
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,7 +3,10 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -16,17 +19,69 @@ class SinglyLinkedList:
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
+        check head, if it doesn't exists add new node to head else traverse to end & add new node to end.
         """
+        new_node = ListNode(data)
+        if not self.head:
+            self.head = new_node
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = new_node
         
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
+        starting head search the key, if it exists return the data else return None
         """
+        curr = self.head
+        while curr:
+            if curr.data == key:
+                return curr.data
+            curr = curr.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
+        check if head exists or not & key matches head data, if yes make head.next new head else traverse through
+        linklist to find key, if found remove the node else return null if key not found
         """
+        curr = self.head
+        if self.head and self.head.data == key:
+            self.head = self.head.next
+            curr = None
+            return
+        prev = None
+        while curr:
+            prev = curr
+            curr = curr.next
+            if curr.data == key:
+                prev.next = curr.next
+                curr = None
+                break
+        else:
+            return
+
+    def print(self):
+        cur = self.head
+        while cur:
+            print(cur.data,end='->')
+            cur = cur.next
+
+
+ll = SinglyLinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+ll.print()
+print()
+print(ll.find(2))
+ll.print()
+print(ll.remove(4))
+ll.print()
