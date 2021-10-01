3890e964ef4d3ce5d266599b6408916ef155a38b
1026sumanrawat <rawat.su@northeastern.edu>
2021-10-01 17:11:37 -0400

    fin_assignment2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,24 +1,48 @@
 
+
+# Class to create Node for linked-list
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
+# Class to create Stack
+
+
 class Stack:
+
+    # Constructor
     def __init__(self):
-        
+        self.head = None
+
+# Push
     def push(self, data):
-        
+
+        if self.head == None:
+            self.head = Node(data)
+        else:
+            # set the `.next` pointer of the new node to point to the current
+            # head node of the list
+            newNode = Node(data)
+            newNode.next = self.head
+            newNode.head = self.head
+            self.head = newNode
+
     def pop(self):
-        
+        popped = self.head
+        self.head = self.head.next
+        popped.next = None
+        return popped.data
+
+
 a_stack = Stack()
 while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
