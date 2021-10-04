ac0dde11ce0abddc4cabe954cf20434ba85a14d5
DaneshDhamejani <daneshdhamejani24@gmail.com>
2021-10-04 16:49:20 -0700

    Completed Sprint Precourse-1

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,32 +1,51 @@
-
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
+
 class Stack:
+
+    # Initializing a Stack
     def __init__(self):
-        
+        self.head = None
+
+    # Push a value into the head of the stack
     def push(self, data):
-        
+
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+
+    # Remove and return a value in the head of the stack
     def pop(self):
-        
+
+        if self.head is None:
+            return None
+        else:
+            remove = self.head.data
+            self.head = self.head.next
+            return remove
+
+
 a_stack = Stack()
 while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    print('push <value>')
-    print('pop')
-    print('quit')
-    do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    print("push <value>")
+    print("pop")
+    print("quit")
+    do = input("What would you like to do? ").split()
+
     operation = do[0].strip().lower()
-    if operation == 'push':
+    if operation == "push":
         a_stack.push(int(do[1]))
-    elif operation == 'pop':
+    elif operation == "pop":
         popped = a_stack.pop()
         if popped is None:
-            print('Stack is empty.')
+            print("Stack is empty.")
         else:
-            print('Popped value: ', int(popped))
-    elif operation == 'quit':
+            print("Popped value: ", int(popped))
+    elif operation == "quit":
         break
