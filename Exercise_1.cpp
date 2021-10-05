c8655a55f8ce2d23c2ff6ba5d97c0c7715a7a347
vagdevichidella@gmail.com <vagdevi161@gmail.com>
2021-10-05 01:19:00 -0700

    Completed PreCourse-1 : Please review

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h> 
   
 using namespace std; 
-  
+   // Time Complexity of Push, Pop , Peak and IsEmpty is O(1) (constant time)
+   // Space Complexity of Push, Pop , Peak and IsEmpty is O(1) (constant time)
 #define MAX 1000 
   
 class Stack {
-  //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
     int top; 
   
 public: 
     int a[MAX]; // Maximum size of Stack 
   
-    Stack() { //Constructor here } 
+    Stack() { 
+        // Initialise the top to 0.
+        top = 0;
+    }//Constructor here } 
     bool push(int x); 
     int pop(); 
     int peek(); 
@@ -21,24 +23,55 @@ public:
   
 bool Stack::push(int x) 
 { 
-    //Your code here
-    //Check Stack overflow as well
+    // Stack overflow condition
+    if(top == MAX-1){
+        // top reached the max capacity of the stack , so cannot push.
+        return false;
+    }
+
+    //Increment the top as the new element is pushed
+    top++;
+    // Add the element value to the stack
+    a[top] = x;
+    return true;
 } 
   
 int Stack::pop() 
 { 
-    //Your code here
-    //Check Stack Underflow as well 
+    //Stack Underflow
+    if(top == -1){
+        // cannot pop the element from the stack
+        return false;
+
+    }
+
+    // Store the value in a temp to return in the function 
+    int ele = a[top];
+    //Decrement  the top as the element is removed
+    top--;
+    // Return the element at the top
+    return ele;
 } 
 int Stack::peek() 
 { 
-    //Your code here
-    //Check empty condition too
+    //Empty Stack condition
+     if(top == -1){
+        // cannot get the element from the stack as stack is empty
+        return false;
+
+    }
+    return a[top];
 } 
   
 bool Stack::isEmpty() 
 { 
-    //Your code here 
+    //Check the top index 
+    if(top == -1){
+        return true;
+    }
+    else{
+        return false;
+    }
 } 
   
 // Driver program to test above functions 
--- Exercise_3.cpp
+++ Exercise_3.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h> 
 using namespace std; 
-  
+// N : Number of nodes in the linked list
+ // Time Complexity of Push, insertAfter , append is O(N) (traversing through head to end)
+// Space Complexity of Push, insertAfter , append is O(1) (pointers)  
 // A linked list node (changes) 
 class Node  
 {  
@@ -15,12 +17,13 @@ a new node on the front of the list. */
 void push(Node** head_ref, int new_data)  
 {  
     /* 1. allocate node */ 
-  
+    Node* temp = new Node();
     /* 2. put in the data */  
-  
+    temp->data = new_data;
     /* 3. Make next of new node as head */ 
-  
+    temp->next = (*head_ref);
     /* 4. move the head to point to the new node */
+    (*head_ref) = temp;
 }  
   
 /* Given a node prev_node, insert a new node after the given  
@@ -28,14 +31,20 @@ prev_node */
 void insertAfter(Node* prev_node, int new_data)  
 {  
     /*1. check if the given prev_node is NULL */ 
-  
+    if(prev_node == NULL){
+        return;
+    }
+    else{
     /* 2. allocate new node */ 
-  
+        Node* temp = new Node();
     /* 3. put in the data */ 
-  
+        temp->data = new_data;
     /* 4. Make next of new node as next of prev_node */
-  
+        temp->next = prev_node ->next;
     /* 5. move the next of prev_node as new_node */ 
+        prev_node->next = temp;
+    }
+   
 }  
   
 /* Given a reference (pointer to pointer) to the head  
@@ -43,19 +52,31 @@ of a list and an int, appends a new node at the end */
 void append(Node** head_ref, int new_data)  
 {  
     /* 1. allocate node */ 
-  
+    Node* temp =  new Node();
     /* 2. put in the data */ 
-  
+
+    temp->data = new_data;
+    Node *last = *head_ref;
+
     /* 3. This new node is going to be  
     the last node, so make next of  
     it as NULL*/  
-  
+    temp->next = NULL;
+
     /* 4. If the Linked List is empty, 
     then make the new node as head */
-  
+    if(*head_ref == NULL){
+        *head_ref = temp;
+         return;
+    }
+
     /* 5. Else traverse till the last node */
-  
-    /* 6. Change the next of last node */ 
+    while(last->next != NULL)
+        last = last->next;
+
+    /* 6. Change the next of last node */
+    last->next = temp;
+    return; 
 }  
   
 // This function prints contents of 
@@ -63,6 +84,10 @@ void append(Node** head_ref, int new_data)
 void printList(Node *node)  
 {  
     //Your code here
+    while(node != NULL){
+        cout << " " << node->data;
+        node = node->next;
+    }
 }  
   
 /* Driver code*/
