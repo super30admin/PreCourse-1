344aeb90e9d5b4e3e8ab0ef691cdface16009c97
Nidhi <nidhisvkulkarni@gmail.com>
2021-10-05 01:22:20 -0700

    Completed sprintPreCourse1

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,3 +1,6 @@
+//Time complexity for pop=1
+//Space complexity=1000*ItemType(4 for int)=4000
+
 #include <bits/stdc++.h> 
   
 using namespace std; 
@@ -7,38 +10,80 @@ using namespace std;
 class Stack {
   //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
-    int top; 
+    int top;
+    int itemCount;
+    int x; 
   
 public: 
     int a[MAX]; // Maximum size of Stack 
   
-    Stack() { //Constructor here } 
+    Stack() { int x=0; int itemCount=0; } 
     bool push(int x); 
     int pop(); 
     int peek(); 
     bool isEmpty(); 
+    void  display();
 }; 
   
 bool Stack::push(int x) 
 { 
     //Your code here
     //Check Stack overflow as well
+	bool result=false; //set resukt as false
+	if(itemCount < 1000)//if the stack is not full, push the element in stack
+	{
+		a[itemCount]=x;//push the value as the last element in array
+		itemCount++;//Increase the count
+		result=true;//return true if push is successful
+	}
+	else
+	{
+		cout<<"No Capacity"<<endl;//else, return as stack is full
+	}
+	return result;
 } 
   
 int Stack::pop() 
 { 
     //Your code here
-    //Check Stack Underflow as well 
+    //Check Stack Underflow as well
+	
+	if(itemCount==0)//If stack is empty, nothing can be popped
+	{
+		cout<<"No items to pop"<<endl;
+	}
+	else
+	{
+		
+		top=a[itemCount-1];//Return the count element at count -1th position in array
+		itemCount--;//Decreement the count after popping 
+		
+	
+	} 
+	return top;
 } 
 int Stack::peek() 
 { 
     //Your code here
     //Check empty condition too
+	if(itemCount==0)//Check if stack is empty
+	cout<<"No elements in stack "<<endl;
+	else
+	top=a[itemCount-1];//Show the top element
+
+	
+
+	return top;
 } 
   
 bool Stack::isEmpty() 
 { 
-    //Your code here 
+    //Your code here
+	bool result=true;
+	if(itemCount>0)
+	result= false;
+	 
+	return result;
 } 
   
 // Driver program to test above functions 
@@ -48,7 +93,7 @@ int main()
     s.push(10); 
     s.push(20); 
     s.push(30); 
-    cout << s.pop() << " Popped from stack\n"; 
-  
+    cout << s.pop() << " Popped from stack\n";
+    cout<<s.peek() <<" Last element in stack"<<endl;
     return 0; 
 } 
--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -18,22 +18,53 @@ StackNode* newNode(int data)
   
 int isEmpty(StackNode* root) 
 { 
-    //Your code here 
+   int result;
+    if(root==NULL)
+    result=0;
+    else
+    result=1;
+    
+    return result;
 } 
   
 void push(StackNode** root, int data) 
 { 
-    //Your code here 
+    //Your code here
+    StackNode *nodetopush = new StackNode;
+    nodetopush->data=data;
+    nodetopush->next= (*root);
+    (*root)=nodetopush; 
+
 } 
   
 int pop(StackNode** root) 
 { 
-    //Your code here 
+    StackNode *popnode=new StackNode;
+    int a;
+   if(*root!=NULL)
+   {
+   	popnode=*root;
+   	(*root)=(*root)->next;
+   	a=popnode->data;
+   	delete popnode;
+   	
+   }   
+   return a;
 } 
   
 int peek(StackNode* root) 
 { 
-    //Your code here 
+    StackNode *peeknode=new StackNode;
+    int pn;
+   if(root!=NULL)
+   {
+   	peeknode=root;
+   	root=root->next;
+   	pn=peeknode->data;
+   	
+   	
+   }   
+   return pn;
 } 
   
 int main() 
@@ -43,7 +74,7 @@ int main()
     push(&root, 10); 
     push(&root, 20); 
     push(&root, 30); 
-  
+  cout << "Top element is " << peek(root) << endl; 
     cout << pop(&root) << " popped from stack\n"; 
   
     cout << "Top element is " << peek(root) << endl; 
