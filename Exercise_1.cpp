b2c7130e63adc5b3fb7b3e13dd9b19d88f84f3b2
Akarsh <92701572+cAkarsh@users.noreply.github.com>
2021-11-16 15:05:05 -0800

    Upadate

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,4 +1,11 @@
-#include <bits/stdc++.h> 
+#include <iostream> 
+
+// Time Complexity : all opertations are O(1) 
+// Space Complexity : O(1)
+
+
+
+// Your code here along with comments explaining your approach
   
 using namespace std; 
   
@@ -12,7 +19,7 @@ class Stack {
 public: 
     int a[MAX]; // Maximum size of Stack 
   
-    Stack() { //Constructor here } 
+    Stack() { top=0;  } 
     bool push(int x); 
     int pop(); 
     int peek(); 
@@ -23,22 +30,47 @@ bool Stack::push(int x)
 { 
     //Your code here
     //Check Stack overflow as well
+    if(top == MAX)
+    {
+        return 0;
+    }
+    else
+    {
+        a[top]=x;
+        top++;
+        return 1;
+    }
 } 
   
 int Stack::pop() 
 { 
-    //Your code here
-    //Check Stack Underflow as well 
+     if (this->top==0)
+    {
+        return -111;
+    }
+    else
+    {
+        int tempRet = this->a[(this->top)-1];
+        this->top--;
+        return tempRet;
+    }
 } 
 int Stack::peek() 
 { 
-    //Your code here
-    //Check empty condition too
+    if (this->top==0)
+    {
+        return -111;
+    }
+    else
+    {
+        return this->a[(this->top)-1];
+    }
 } 
   
 bool Stack::isEmpty() 
 { 
     //Your code here 
+    return (this->top==0);
 } 
   
 // Driver program to test above functions 
