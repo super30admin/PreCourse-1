59c44b6299747640e8f3c286aa88db2e7e004a69
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2021-11-15 22:39:28 -0800

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -8,31 +8,57 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+    	if(top == -1)
+    		return true;
+    	else
+    		return false;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+    	top =-1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+    	
+    	if(top>=a.length-1) {
+    		System.out.println("Stack Overflow");
+    		return false;
+    		}
+    	else {
+    		top++;
+    		a[top] = x;
+    		return true;
+    	}
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+    	int x = 0;
+    	if(top == -1) {
+    		System.out.println("Stack UnderFlow");
+    		
+    	}
+    	else {
+    		x = a[top];
+    		top--;
+    	}
+    	return x;
     } 
   
     int peek() 
     { 
         //Write your code here
+    	
+    	return a[top];
     } 
 } 
-  
 // Driver code 
 class Main { 
     public static void main(String args[]) 
