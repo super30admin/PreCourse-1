b73f846d2fa11015ec07194dbd11b427ad90d522
Akarsh <92701572+cAkarsh@users.noreply.github.com>
2021-11-16 15:05:42 -0800

    Update Exercise_2.cpp

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,7 +1,10 @@
-#include <bits/stdc++.h> 
+#include <iostream> 
 using namespace std; 
   
 // A structure to represent a stack 
+// Time Complexity : isEmpty = O(1), push = O(1) , pop = O(1),peek =O(1)
+// Space Complexity : O(1)
+
 class StackNode { 
 public: 
     int data; 
@@ -18,22 +21,34 @@ StackNode* newNode(int data)
   
 int isEmpty(StackNode* root) 
 { 
-    //Your code here 
+    if (root == nullptr) return 1;
+    else return 0;
 } 
   
 void push(StackNode** root, int data) 
 { 
-    //Your code here 
+    StackNode *temp=newNode(data);
+    temp->next=(*root);
+    (*root)=temp;
 } 
   
 int pop(StackNode** root) 
 { 
     //Your code here 
+    if (*root == nullptr)
+    {
+        return -1;
+    }
+    StackNode *tempRoot= *root;
+    *root=tempRoot->next;
+    int retTemp=tempRoot->data;
+    delete tempRoot;
+    return retTemp;
 } 
   
 int peek(StackNode* root) 
 { 
-    //Your code here 
+    return root->data; 
 } 
   
 int main() 
