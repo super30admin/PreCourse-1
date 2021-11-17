37d5389829537a2a334ff70d179fb939c929ad12
Jay <dsrujay@gmail.com>
2021-11-17 00:55:49 -0800

    commiting exercise-1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,46 +1,75 @@
-class Stack { 
+class Stack {
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
+    boolean isEmpty()
+    {
+        //Write your code here
+        if(top == -1)
+            return true;
+        else
+            return false
+    }
+
+    Stack()
+    {
+        //Initialize your constructor
+        top = -1;
+    }
+
+    boolean push(int x)
+    {
         //Check for stack Overflow
         //Write your code here
-    } 
-  
-    int pop() 
-    { 
+        if(top == (MAX-1)){
+            System.out.print("Stack Overflow");
+            return false;
+        }
+        else {
+            top++;
+            a[top] = x;
+            return true;
+        }
+
+    }
+
+    int pop()
+    {
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
-    } 
-  
-    int peek() 
-    { 
+        if(top == -1){
+            System.out.println("Stack underflow");
+            return 0;
+        }
+        else {
+            int x = a[top];
+            top--;
+            return x;
+        }
+    }
+
+    int peek()
+    {
         //Write your code here
-    } 
-} 
-  
-// Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+        if(top == -1){
+            System.out.println(" Stack is empty");
+            return -1;
+        }
+        return a[top];
+    }
+}
+
+// Driver code
+class Main {
+    public static void main(String args[])
+    {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+    }
 }
