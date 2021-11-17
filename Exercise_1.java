a624cc65be5b04b17a5771d4729f8eb152b678cc
Tejaswi Konduri <tejaswik5592@gmail.com>
2021-11-18 02:25:24 +0530

    Completed Sprint PreCourse-1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,4 +1,6 @@
-class Stack { 
+// Time Complexity : O(1) for isEmpty, push, pop and peek functions.
+// Space Complexity :
+class Stack {
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
@@ -7,29 +9,50 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        if (top < 0)
+            return true;
+        return false;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top >= (MAX-1)) {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        a[++top] = x;
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        int ret = a[top--];
+        return ret;
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return a[top];
     } 
 } 
   
