18ee6452a1924e6b82cdec21232b1b1ef65af934
Yadnesh Kulkarni <ykulkar2@uncc.edu>
2021-11-17 14:25:47 -0500

    Exercise-3 Completed

--- Exercise_3.cpp
+++ Exercise_3.cpp
@@ -1,7 +1,26 @@
-#include <bits/stdc++.h> 
+#include <iostream>
 using namespace std; 
   
-// A linked list node (changes) 
+// A linked list node (changes)
+// Approach : Implementing Singly Linked List
+//          1. push :
+//              a. Push requires node to be pushed at the front.
+//              b. Hence, create a node and make its next as current head.
+//              c. Point head to newly created node.
+//              d. Time Complexity : O(1)  Constant Time.
+//              e. Space Complexity : O(1) Constant Space.
+//          2. insertAfter :
+//              a. Create a new node.
+//              b. Point previous node -> next to newNode/
+//              c. Time Complexity : O(1)  Constant Time.
+//              d. Space Complexity : O(1) Constant Space.
+//          3. append :
+//              a. Create a new node with data.
+//              b. Assign next to null as it is going to be the last node.
+//              c. Check if list is empty. If it is then assign this node as head node.
+//              d. Else traverse till the last node and insert new node at the end.
+//              e. Time Complexity : O(n)
+//              f. Space Complexity : O(1) Constant Space
 class Node  
 {  
     public: 
@@ -15,12 +34,16 @@ a new node on the front of the list. */
 void push(Node** head_ref, int new_data)  
 {  
     /* 1. allocate node */ 
-  
+    Node* newData = new Node();
+
     /* 2. put in the data */  
-  
+    newData->data = new_data;
+
     /* 3. Make next of new node as head */ 
-  
+    newData->next = *head_ref;
+
     /* 4. move the head to point to the new node */
+    *head_ref = newData;
 }  
   
 /* Given a node prev_node, insert a new node after the given  
@@ -28,14 +51,21 @@ prev_node */
 void insertAfter(Node* prev_node, int new_data)  
 {  
     /*1. check if the given prev_node is NULL */ 
-  
+    if(prev_node == nullptr)
+    {
+        return;
+    }
+
     /* 2. allocate new node */ 
-  
+    Node* newNode = new Node();
     /* 3. put in the data */ 
-  
+    newNode->data = new_data;
+
     /* 4. Make next of new node as next of prev_node */
-  
-    /* 5. move the next of prev_node as new_node */ 
+    newNode->next = prev_node->next;
+
+    /* 5. move the next of prev_node as new_node */
+    prev_node->next = newNode; 
 }  
   
 /* Given a reference (pointer to pointer) to the head  
@@ -43,19 +73,36 @@ of a list and an int, appends a new node at the end */
 void append(Node** head_ref, int new_data)  
 {  
     /* 1. allocate node */ 
-  
+    Node* newNode = new Node();
+    Node* traverseNode = *head_ref;
     /* 2. put in the data */ 
-  
+    newNode->data = new_data;
+    
     /* 3. This new node is going to be  
     the last node, so make next of  
     it as NULL*/  
-  
+    newNode->next = nullptr;
+
     /* 4. If the Linked List is empty, 
     then make the new node as head */
-  
+    if(traverseNode == nullptr)
+    {
+        *head_ref = newNode; 
+        return;
+    }
+
     /* 5. Else traverse till the last node */
-  
+    else
+    {
+        while(traverseNode->next != nullptr)
+        {
+            traverseNode = traverseNode->next;
+        }
+    }
+
+
     /* 6. Change the next of last node */ 
+    traverseNode->next = newNode;
 }  
   
 // This function prints contents of 
@@ -63,6 +110,13 @@ void append(Node** head_ref, int new_data)
 void printList(Node *node)  
 {  
     //Your code here
+    cout << endl;
+    while(node != nullptr)
+    {
+        cout << node->data << " -> ";
+        node = node->next;
+    }
+    cout << "null" << endl;
 }  
   
 /* Driver code*/
