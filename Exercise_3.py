12f9c6313a6a62318c69d602785f4550d9a258c9
Chirag Rajpal <chiragbrajpal@gmail.com>
2021-11-17 23:05:19 -0800

    Completed PreCourse-1

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,32 +1,73 @@
-class ListNode:
-    """
-    A node in a singly-linked list.
-    """
-    def __init__(self, data=None, next=None):
-    
-class SinglyLinkedList:
+class Node():
+    def __init__(self, data=None):
+        self.data = data,
+        self.next = None
+
+
+class SinglyLinkedList():
     def __init__(self):
-        """
-        Create a new singly-linked list.
-        Takes O(1) time.
-        """
         self.head = None
 
     def append(self, data):
-        """
-        Insert a new element at the end of the list.
-        Takes O(n) time.
-        """
-        
+        if(self.head == None):
+            self.head = Node(data)
+            # print(self.head.data)
+            self.head.data = data
+            self.head.next = None
+        else:
+            temp = self.head
+            # print("Checking the value assigned to temp",self.head.data)
+            while temp.next != None:
+                temp = temp.next
+            newNode = Node(data)
+            newNode.data = data
+            temp.next = newNode
+            temp.data = newNode.data
+
+    def display(self):
+        dispNode = self.head
+        print("self.head is", self.head.data)
+        while dispNode != None:
+            print(dispNode.data)
+            dispNode = dispNode.next
+
     def find(self, key):
-        """
-        Search for the first element with `data` matching
-        `key`. Return the element or `None` if not found.
-        Takes O(n) time.
-        """
-        
-    def remove(self, key):
-        """
-        Remove the first occurrence of `key` in the list.
-        Takes O(n) time.
-        """
+        incr = 0
+        resarr = []
+        findNode = self.head
+        while(key != findNode.data and findNode.next != None):
+            # print("Key: ",key,"findNode.data is",findNode.data)
+            findNode = findNode.next
+            incr = incr+1
+            if(findNode.data == key):
+                resarr.append(findNode.data)
+                resarr.append(incr)
+                return resarr
+        return None
+
+    def Remove(self, key):
+        arrres = []
+        incr = 0
+        nodeRemove = self.head
+        while(key != nodeRemove.data):
+            nodeRemove = nodeRemove.next
+            incr += 1
+            if(nodeRemove.data == key):
+                arrres.append(nodeRemove.data)
+                arrres.append(incr)
+                return arrres
+        return None
+
+
+node1 = SinglyLinkedList()
+
+node1.append(25)
+node1.append(26)
+node1.append(28)
+node1.append(30)
+node1.append(32)
+
+node1.display()
+print(node1.find(28))
+print(node1.Remove(32))
+
