f9ed051f8caed3758d529442e2ef5fba6e684d79
Mariya <62721583+mariyav96@users.noreply.github.com>
2021-11-18 14:33:48 -0700

    Completed sprint1 Precouse_1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,45 @@
 class myStack:
+    # Time Complexity :
+    # Space Complexity :
+    # Did this code successfully run on Leetcode : Yes
+    # Any problem you faced while coding this :
+
+
+    
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
+    def __init__(self):
         
-        
-     def peek(self):
-        
-     def size(self):
+        self.list1 = []
          
-     def show(self):
+    def isEmpty(self):
+        if len(self.list1) == 0:
+             return True
+        else:
+            return False
+         
+    def push(self, item):
+        self.list1.append(item)
+       
+    def pop(self):
+        if self.size == 0:
+             return "Stack is empty"
+        else:
+            return self.list1.pop()
+        
+    def peek(self):
+        return self.list1[-1]
+        
+    def size(self):
+         return len(self.list1)
+        
+    def show(self):
+        for i in range(len(self.list1)):
+            print(self.list1[i])
+        
          
 
 s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
-print(s.show())
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,9 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+        
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,16 +20,29 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        newNode = Node(data)
+        if self.head == None:
+            self.head = newNode
+        else:
+            nextNode = self.head.next
+            while(nextNode.next):
+                nextNode = nextNode.next
+            nextNode.next = newNode
+            
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        while(self.head.next):
+            if self.head.data == key:
+                return self.head.data
+            self.head = self.head.next
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+       
