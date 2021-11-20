8079419ba20253392c2379944f5f43919a5fb414
sai <srsaikumarreddy@gmail.com>
2021-11-20 01:42:30 -0700

    Pre Course 1 all qns done

---
+++ LinkedList.java
@@ -0,0 +1,96 @@
+//TC: Print O(N) INSERT O(N) in worst case at end of list
+//SC: O(N)
+
+import java.io.*; 
+  
+// Java program to implement 
+// a Singly Linked List 
+
+public class LinkedList { 
+  
+    Node head; // head of list 
+  
+    // Linked list Node. 
+    // This inner class is made static 
+    // so that main() can access it 
+    static class Node { 
+  
+        int data; 
+        Node next; 
+  
+        // Constructor 
+        Node(int d) 
+        { 
+            //Write your code here 
+            this.data=d;
+            this.next=null;
+        } 
+    } 
+  
+    // Method to insert a new node 
+    public static LinkedList insert(LinkedList list, int data) 
+    { 
+        // Create a new node with given data 
+        Node newNode=new Node(data);
+        newNode.next=null;
+   
+        // If the Linked List is empty, 
+        // then make the new node as head 
+        if(list.head==null)
+            list.head=newNode;
+        else
+        {   Node curr=list.head;
+            while(curr.next!=null)
+            {
+                curr=curr.next;
+            }
+            curr.next=newNode;
+        }
+        return list;
+        
+            // Else traverse till the last node 
+            // and insert the new_node there 
+
+            // Insert the new_node at last node 
+        // Return the list by head 
+        
+    } 
+  
+    // Method to print the LinkedList. 
+    public static void printList(LinkedList list) 
+    {  
+        // Traverse through the LinkedList 
+   
+            // Print the data at current node 
+       
+            // Go to next node 
+        Node curr=list.head;
+        while(curr!=null)
+        {
+            System.out.println(curr.data);
+            curr=curr.next;
+        }
+
+    } 
+   
+    // Driver code 
+    public static void main(String[] args) 
+    { 
+        /* Start with the empty list. */
+        LinkedList list = new LinkedList(); 
+  
+        // 
+        // ******INSERTION****** 
+        // 
+  
+        // Insert the values 
+        list = insert(list, 1); 
+        list = insert(list, 2); 
+        list = insert(list, 3); 
+        list = insert(list, 4); 
+        list = insert(list, 5); 
+  
+        // Print the LinkedList 
+        printList(list); 
+    } 
+}
