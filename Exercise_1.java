56ce608ff4bdcf0e79d7cfc1f3fe1dbcc8210e3b
akulprabhudesai <ap2559@njit.edu>
2021-11-22 01:08:12 -0500

    Completed Sprint PreCourse-1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,94 @@
+/*
+ * Question 1:  Implement Stack using Array
+ * 
+ * Time Complexity:
+ * 1. isEmpty - O(1)
+ * 2. Push operation - O(1)
+ * 3. Pop operation - o(1)
+ * 4. Peek operation - O(1)
+ * 
+ * 
+ * Space Complexity:
+ * 1. isEmpty - O(1)
+ * 2. Push operation - O(1)
+ * 3. Pop operation - o(1)
+ * 4. Peek operation - O(1)
+ * 
+ * 
+ * This code will run successfully on Leetcode
+ * 
+ * Any problems you face while coding this - No
+ * 
+ * Approach: 
+ * Using a pointer top to implement FIFO pattern. top is initialized to -1. 
+ * on every push operation top will be incremented first and then element will be inserted.
+ * 
+ * 1. for is empty operation, result will be true if top = -1 as it shows stack is empty
+ * 2. for push operation, if stack is full (ie if top == max -1) then false is return else 
+ * 	  top is incremented and then element is inserted into stack
+ * 3. for pop operation if stack is empty(ie is top = -1) then 0 is returned and 
+ * 	  message "stack underflow" is printed on console else element currently pointed by top 
+ * 	  is returned and top is decremented by 1 which shows this element is also removed from stack
+ * 4. for peek operation if stack is empty(ie is top = -1) then 0 is returned and 
+ * 	  message "stack underflow" is printed on console else element currently pointed by top 
+ * 	  is returned and top remains same that shows, current element is not removed from stack
+ */
+
+
 class Stack { 
-    //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
+    
+    static final int MAX = 5; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        if(this.top == -1)return true;
+        return false;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor \
+    	this.top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+    	if(this.top ==  this.MAX-1) {
+    		//code for overflow
+    		System.out.println("Stack is full");
+    		return false;
+    	}else {
+    		this.a[++this.top] = x;
+    		return true;
+    	}
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+    	if(this.top == -1) {
+    		System.out.println("Stack Underflow");
+    		return 0;
+    	}else {
+    		//Write your code here
+    		return this.a[this.top--];
+    	}
+        
     } 
   
     int peek() 
     { 
-        //Write your code here
+    	if(this.top == -1) {
+    		System.out.println("Stack Underflow");
+    		return 0;
+    	}else {
+    		//Write your code here
+    		return this.a[this.top];
+    	}
     } 
 } 
   
@@ -38,9 +97,14 @@ class Main {
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
+        System.out.println("10 is " + s.push(10));
+        System.out.println("20 is " + s.push(20));
+        System.out.println("30 is " + s.push(30));
+        System.out.println("40 is " + s.push(40));
+        System.out.println("50 is " + s.push(50));
+        System.out.println("60 is " + s.push(60));
         System.out.println(s.pop() + " Popped from stack"); 
+        
     } 
 }
+
