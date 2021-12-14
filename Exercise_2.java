2c1fd7f04200f08a8cf86fb2ffd106cde7ba7584
Pradnya Yeole <pradnyayeole@Pradnyas-MacBook-Air.local>
2021-12-14 13:47:02 -0800

    Pradnya_Yeole-preCourse-1-commit

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,4 @@
-public class StackAsLinkedList { 
+class StackAsLinkedList {
   
     StackNode root; 
   
@@ -7,32 +7,69 @@ public class StackAsLinkedList {
         StackNode next; 
   
         StackNode(int data) 
-        { 
-            //Constructor here 
+        {
+            this.data=data;
         } 
-    } 
-    
-	
+    }
+
+    StackNode top;
+    StackAsLinkedList()
+    {
+        this.top = null;
+    }
     public boolean isEmpty() 
-    { 
-        //Write your code here for the condition if stack is empty. 
+    {
+        return top == null;
     } 
   
     public void push(int data) 
-    { 
-        //Write code to push data to the stack. 
+    {
+        // create new node temp and allocate memory
+        StackNode temp = new StackNode(data);
+
+        // check if stack (heap) is full. Then inserting an
+        //  element would lead to stack overflow
+        if (temp == null) {
+            System.out.print("\nHeap Overflow");
+            return;
+        }
+
+        // initialize data into temp data field
+        temp.data = data;
+
+        // put top reference into temp link
+        temp.next = top;
+
+        // update top reference
+        top = temp;
     } 
   
     public int pop() 
-    { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+    {
+        // check for stack underflow
+        if (top == null) {
+            System.out.print("\nStack Underflow");
+            return 0;
+        }
+        int x=0;
+        x= top.data;
+        // update the top pointer to point to the next node
+        top = (top).next;
+        return x;
+
+
+
     } 
   
     public int peek() 
-    { 
-        //Write code to just return the topmost element without removing it.
+    {
+        if (!isEmpty()) {
+            return top.data;
+        }
+        else {
+            System.out.println("Stack is empty");
+            return -1;
+        }
     } 
   
 	//Driver code
