7140c951a388577702ce6a4823c8b343376aaac6
LiberoPat <patrick.trias@gmail.com>
2021-12-14 23:58:40 -0800

    completed precourse through python

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,40 @@
+
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
+# Implement Stack using array
+# Time Complexity : O(1)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes..except the show function. I am getting response that says None. Not sure why
+# Any problem you faced while coding this : MY push function only occurs onces and doesn't push again
+
+
+# Your code here along with comments explaining your approach
+
+
+
      def __init__(self):
-         
+      self.items = [1,2,3,4];  #initializing an array
      def isEmpty(self):
+      return self.items == []; #is empty
          
      def push(self, item):
+       self.items.append(item);
          
      def pop(self):
+       self.items.pop(); #built in function that removes last int
         
         
      def peek(self):
+       return self.items[len(self.items) - 1];
         
      def size(self):
+       return len(self.items)
          
      def show(self):
+       return self.items
          
 
 s = myStack()
-s.push('1')
-s.push('2')
-print(s.pop())
-print(s.show())
+s.push(12)
+s.push(2) #not working
+print(s.pop())  #not working
+print(s.show()) #not working =(
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,16 @@
 class ListNode:
     """
+    implement singly list
     A node in a singly-linked list.
     """
+    # Time Complexity : O(1)
+    # Space Complexity : O(N)
+    # Did this code successfully run on Leetcode : Yes. 
+    # Any problem you faced while coding this :
+
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = None
     
 class SinglyLinkedList:
     def __init__(self):
@@ -11,12 +19,23 @@ class SinglyLinkedList:
         Takes O(1) time.
         """
         self.head = None
+        self.tail = None
+        self.count = 0
+   
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        node = ListNode(data);
+        if self.tail:
+            self.tail.next = node
+            self.tail = node
+        else:
+            self.head = node
+            self.tail = node
+        self.count += 1
         
     def find(self, key):
         """
@@ -24,9 +43,47 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+         # Initialize current to head
+        current = self.head
+ 
+        # loop till current not equal to None
+        while current != None:
+            if current.data == key:
+                return True # data found
+             
+            current = current.next
+         
+        return False # Data Not found
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        # Store head node
+        temp = self.head
+ 
+        # If head node itself holds the key to be deleted
+        if (temp is not None):
+            if (temp.data == key):
+                self.head = temp.next
+                temp = None
+                return
+ 
+        # Search for the key to be deleted, keep track of the
+        # previous node as we need to change 'prev.next'
+        while(temp is not None):
+            if temp.data == key:
+                break
+            prev = temp
+            temp = temp.next
+ 
+        # if key was not present in linked list
+        if(temp == None):
+            return
+ 
+        # Unlink the node from linked list
+        prev.next = temp.next
+ 
+        temp = None
+ 
