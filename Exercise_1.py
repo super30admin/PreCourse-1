d78bd60d93f290ad03b3fc102e51a984879cc98d
Nikhil Mandge <nikhilmandge@Nikhils-MacBook-Pro.local>
2021-12-15 15:50:27 -0500

    soution added

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,70 @@
+#Time Complexity is O(1) (Constant Time)
+#Space Complexity is O(1) (Constant Time)
 class myStack:
+  
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+       self.init_max=1000
+       self.data = [0]*self.init_max
+       self.top=-1
+    
+     #Time Complexity Of this function is O(1) (Constant Time)
+     #Space Complexity Of this function is O(1) (Constant Time)
      def isEmpty(self):
-         
+       if self.top<0:
+         return True
+       else:
+          return False
+
+     #Time Complexity Of this function is O(1) (Constant Time)
+     #Space Complexity Of this function is O(1) (Constant Time)
      def push(self, item):
-         
+       if self.top >= self.init_max:
+          print("Stack Is Full!!!")
+          return
+       else:
+         self.top=self.top+1
+         self.data[self.top] = item
+         print("Item "+ item +" is pushed into Stack")
+
+     #Time Complexity Of this function is O(1) (Constant Time)
+     #Space Complexity Of this function is O(1) (Constant Time)            
      def pop(self):
-        
-        
+       if self.isEmpty():
+          print("Stack Is Empty!!!")
+          return
+       else:
+         item =self.data[self.top]
+         self.top=self.top-1
+         print("Item "+ str(item) +" is poped out from Stack")
+         return item
+       
+     #Time Complexity Of this function is O(1) (Constant Time)
+     #Space Complexity Of this function is O(1) (Constant Time)   
      def peek(self):
-        
+       if self.top<0:
+         print("Stack is empty")
+       else:
+        return self.data[self.top]
+
+     #Time Complexity Of this function is O(1) (Constant Time)
+     #Space Complexity Of this function is O(1) (Constant Time)   
      def size(self):
+       return len(self.data)
          
      def show(self):
+       for x in range(0,self.top+1):
+         print(str(self.data[x])+" ")
+       return "\n"
          
 
 s = myStack()
 s.push('1')
+s.push('3')
 s.push('2')
+s.push('3')
+s.push('5')
+print(s.pop())
 print(s.pop())
 print(s.show())
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,4 +1,5 @@
-
+#Time Complexity is O(n) 
+#Space Complexity is O(1) constant space
 class Node:
     def __init__(self, data):
        self.data = data
@@ -6,10 +7,37 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.data = None
+        pass
         
     def push(self, data):
+        if self.data == None:
+            self.data=Node(data)
+            return data
+
+        temp = self.data
+
+        while temp !=None:
+            if temp.next == None:
+                temp.next = Node(data)
+                return data
+            temp = temp.next
         
     def pop(self):
+        if self.data == None:
+            return None
+        if self.data.next == None:
+            d = self.data.data
+            self.data = None
+            return d
+
+        pop_node = self.data
+        while pop_node.next.next != None:
+            pop_node = pop_node.next
+        d= pop_node.next.data
+        pop_node.next = None
+        return d
+        
         
 a_stack = Stack()
 while True:
