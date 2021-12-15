86bb0aa3419e973108609aa20e831828a951b145
Akhila5diddi <akhila5netha@gmail.com>
2021-12-15 04:39:32 -0800

    Precourse 1 assignment submission

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,59 @@
+//Time complexity : O(1) for all the push, pop, peek, isEmpty operations
+//Space Complxity : O(1) all the time, we initaialize a predefined Array of provided Max size
+
 class Stack { 
-    //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+    
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Top holds value to the stack's last element's index, if stack is empty it's value will be = -1
+        return top == -1;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initializing top to -1, creating an empty stack
+        this.top = -1;   
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        //check if stack is used at its maximum capacity
+        if (top == MAX - 1){
+            System.out.println("Stack Overflow");
+            return false;       
+        } else {
+            //increment top's value and  insert value to be pushed at top's index
+            a[++top] = x;
+            return true;   
+        }        
     } 
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        //check if stack is empty
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        } else { 
+            //return value at the top and decrement top
+            return a[top--];   
+        }
     } 
   
     int peek() 
     { 
-        //Write your code here
+        //check if stack is empty
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {   
+            //return value at the to
+            return a[top];   
+        }    
     } 
 } 
   
@@ -41,6 +65,13 @@ class Main {
         s.push(10); 
         s.push(20); 
         s.push(30); 
+        System.out.println("Stack is empty : " + s.isEmpty()); 
+        System.out.println(s.peek() + " Peek value of the stack");
         System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.peek() + " Peek value of the stack");
+        System.out.println("Stack is empty : " + s.isEmpty()); 
     } 
 }
