09e81faefc56cbf93d56215be21810a395e246b7
shirin <shirin32@gmail.com>
2021-12-15 23:49:00 -0800

    Completed Sprint Precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,37 @@
+  #Exercise_1 : Implement Stack using Array.
+  
+  #Time Complexity : O(1) for all operations
+  #Space Complexity : O(n) 
+  #Did this code successfully run on VSCode : Yes
+  #Any problem you faced while coding this : None
+
+ 
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
+
+    def __init__(self):
+      self.items = []   #constructor                    
          
-     def isEmpty(self):
+    def isEmpty(self):
+      return self.items == []   #testing to see if stack is empty and return boolean value
          
-     def push(self, item):
+    def push(self, item):
+      self.items.append(item)   #taking parameter as item. Returns nothing but modifies stack by appending item. 
          
-     def pop(self):
+    def pop(self):
+      return self.items.pop()   #returns item being popped and modifies the stack
         
+    def peek(self):
+      return self.items[len(self.items)-1]  #returns only the top element without modifying the stack. Peeking into stack to just know what is the top element
         
-     def peek(self):
-        
-     def size(self):
+    def size(self):
+      return len(self.items)  #returning length of the array to know size of stack
          
-     def show(self):
-         
+    def show(self):
+      return self.items   #returns the array itself 
 
 s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
 print(s.show())
+
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,15 +1,37 @@
+  #Exercise_2 : Implement Stack using Linked List.
+  
+  #Time Complexity : O(1) for push and peek but O(n) for pop 
+  #Space Complexity : O(n)
+  #Did this code successfully run on VSCode : Yes
+  #Any problem you faced while coding this : None
 
-class Node:
-    def __init__(self, data):
+
+  #Your code here along with comments explaining your approach
+
+class Node:                         #Node class to hold item and its reference
+    def __init__(self, data):       #constructor with instance variablles 'data' and 'next' 
        self.data = data
        self.next = None
  
-class Stack:
-    def __init__(self):
+class Stack:                        #Stack class to construct Stack
+    def __init__(self):             #Constructor. Initizlizing first element (head) to 'None' (empty list)
+        self.head = None
         
-    def push(self, data):
+    def push(self, data):           #pushing adds node data to the beginning (head) of the linked list in Stack
+        if self.head is None:       #if list is empty, add node and point 'head' to the node address
+            self.head = Node(data)   
+        else:                       ##Else, add new node to the beginning, point head to the new node and the old (original) 'head' to the next node
+            new_node = Node(data)   
+            new_node.next = self.head
+            self.head = new_node
         
-    def pop(self):
+    def pop(self):                     #popping removes node from the beginning and returns the remomved node data
+        if self.head is None:           #If there is no node, list is empty and return None
+            return None
+        else:                           #Else return the data of the node being popped. Change head to next node
+            popped_node = self.head.data
+            self.head = self.head.next
+            return popped_node
         
 a_stack = Stack()
 while True:
