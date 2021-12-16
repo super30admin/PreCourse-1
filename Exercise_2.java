00bd7bae524b8236ac775aabb949360a74ceafa3
Saloni Desai <spdesai4@asu.edu>
2021-12-15 22:53:28 -0800

    Completed Sprint PreCourse-1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,3 +1,9 @@
+// Time Complexity : Insert Operation, Pop operation and Peek - all three require us to traverse the linked list atleast once  - hence O(N).
+// Space Complexity : Depends on the list of elements, if N elements are stored, O(N).
+// Did this code successfully run on Leetcode : Not Applicable
+// Any problem you faced while coding this : 1) Made a mistake in order of updating the current and previous node pointers, 
+//the prev must be updated to store the current value, before the we do curr = curr.next.
+
 public class StackAsLinkedList { 
   
     StackNode root; 
@@ -9,6 +15,8 @@ public class StackAsLinkedList {
         StackNode(int data) 
         { 
             //Constructor here 
+            this.data = data;
+            this.next = null;
         } 
     } 
     
@@ -16,22 +24,74 @@ public class StackAsLinkedList {
     public boolean isEmpty() 
     { 
         //Write your code here for the condition if stack is empty. 
+        if(root == null) return true;
+        else return false;
+
     } 
   
     public void push(int data) 
     { 
         //Write code to push data to the stack. 
+        StackNode node = new StackNode(data);
+        
+        if(root == null)
+        {
+            root = node;
+        }
+        else
+        {
+            StackNode curr = root;
+            while(curr.next!= null)
+            {
+                curr = curr.next;
+            }
+            curr.next = node;
+
+        }
     } 
   
     public int pop() 
-    { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
+    {
+	    //If Stack Empty Return 0 and print "Stack Underflow"
+        if(isEmpty()) 
+        {
+            System.out.println("Stack Underflow.");
+            return 0;
+        }
+        else
+        {
+            StackNode curr = root;
+            StackNode prev = null;
+            while(curr.next!= null)
+            {
+                
+                prev= curr;
+                curr= curr.next;
+                System.out.println("The value of curr is: "+ curr.data + " and prev is: "+ prev.data);
+            }
+            
+            if (prev!= null) prev.next = null;
+            else {
+                //we are deleting the root node
+                root = null; 
+            }
+
+            return curr.data;
+        }
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
     } 
   
     public int peek() 
-    { 
+    {
+        if(isEmpty()) return 0;
+        StackNode curr = root;
+        while(curr.next!= null)
+        {
+            curr= curr.next;
+        }
+        return curr.data; 
+       
         //Write code to just return the topmost element without removing it.
     } 
   
@@ -46,6 +106,10 @@ public class StackAsLinkedList {
         sll.push(30); 
   
         System.out.println(sll.pop() + " popped from stack"); 
+        System.out.println(sll.pop() + " popped from stack"); 
+        System.out.println(sll.pop() + " popped from stack"); 
+        System.out.println(sll.pop() + " popped from stack"); 
+        sll.push(40); 
   
         System.out.println("Top element is " + sll.peek()); 
     } 
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,4 +1,11 @@
-import java.io.*; 
+ // Time Complexity : Insert Operation requires us to traverse the linked list atleast one  - while inserting the last element, hence O(N).
+// Space Complexity : Depends on the list of elements, if N elements are stored, O(N).
+// Did this code successfully run on Leetcode : Not Applicable
+// Any problem you faced while coding this : When inserting into a linked-list there are two cases to be considered:
+// 1. If the list is empty, simply set the newly created node as head 
+// 2. If the list is not empty, iterate to the last element and set it's next to point to the newly created node  
+// I forgot to use the "else" in the if-else for 1 and 2   (if 1 else 2), was simply checking for the first condition 
+//and not the second, and hence was entering into an infinite loop.
   
 // Java program to implement 
 // a Singly Linked List 
@@ -18,6 +25,8 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            this.data =d;
+            this.next = null;
         } 
     } 
   
@@ -25,15 +34,31 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
-   
+        Node node = new Node(data);
         // If the Linked List is empty, 
         // then make the new node as head 
-        
+        if(list.head == null)
+        {
+            list.head = node;
+        }
+        else
+        {
             // Else traverse till the last node 
-            // and insert the new_node there 
-
+            // and insert the new_node there
+            Node curr = list.head;
+            
+            while(curr.next!= null)
+            {
+            //System.out.println("This is curr: " + curr.data + " This is next: " + curr.next.data);
+            curr = curr.next;
+            }
             // Insert the new_node at last node 
+            curr.next = node;
+
+        }
         // Return the list by head 
+
+        return list;
         
     } 
   
@@ -41,10 +66,14 @@ public class LinkedList {
     public static void printList(LinkedList list) 
     {  
         // Traverse through the LinkedList 
-   
+        Node curr = list.head;
+        while(curr!= null)
+        {
             // Print the data at current node 
-       
+            System.out.println(curr.data);
             // Go to next node 
+            curr = curr.next;
+        }
     } 
    
     // Driver code 
@@ -52,7 +81,7 @@ public class LinkedList {
     { 
         /* Start with the empty list. */
         LinkedList list = new LinkedList(); 
-  
+        
         // 
         // ******INSERTION****** 
         // 
