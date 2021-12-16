e8c66513786679158d138a77d9d4dba6c72d389e
akashamin01 <aakashjamin1239@gmail.com>
2021-12-16 01:46:08 -0800

    Finished Precourse 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,6 +1,11 @@
-class Stack { 
+import java.util.*;
+
+class StackArr { 
     //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+    //Kindly include Time and Space complexity at top of each file
+    //Time complexity is O(1) as no loops are involved
+    //Space complexity is O[1] as well 
+
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
@@ -8,28 +13,60 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        //Maintained a variable top that gets to current array index
+        //Array Index cannot be less than 0 hence that qualifies for Empty check condition
+        if(top<0){
+            return true;
+        }else{
+            return false;
+        } 
     } 
 
-    Stack() 
+    StackArr() 
     { 
         //Initialize your constructor 
+        top = -1;
+
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
-        //Write your code here
+        //Pushed element to the current top condition
+        if(top >= MAX-1){
+            System.out.print("Stack Overflow");
+            return false;
+        }else{
+            a[++top] = x;
+            System.out.print("Top " + top);
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        //Popped element from the current top condition
+        if(top<0){
+            System.out.print("Stack Underflow");
+            return 0;
+        }else{
+            int x = a[top--];
+            return x;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        //Consoles the element at the current top condition 
+        if(top<0){
+            System.out.print("Stack Underflow");
+            return 0;
+        }else{
+            int x = a[top];
+            return x;
+        }
     } 
 } 
   
@@ -37,10 +74,16 @@ class Stack {
 class Main { 
     public static void main(String args[]) 
     { 
-        Stack s = new Stack(); 
+        StackArr s = new StackArr(); 
+        // Test Case 1
         s.push(10); 
         s.push(20); 
         s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack");
+        // Test Case 2
+        s.push(10); 
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.pop() + " Popped from stack");
     } 
 }
+
