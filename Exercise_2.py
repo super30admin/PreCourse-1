f018539bfce44a550bec695d413d80aa0fb9ed72
Aniruddha Ingle <aniruddhaingle@aniruddhas-mbp.lan>
2021-12-16 23:27:00 -0600

    Sprint: Pre-Course1 Completed

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,12 @@
+# Time Complexity : 
+# For Push: O(1) and For Pop: O(N)
+
+# Space Complexity : 
+# O(N) worst case space complexity.
+
+# Did this code successfully run on Leetcode : NA
+
+# Any problem you faced while coding this : No
 
 class Node:
     def __init__(self, data):
@@ -6,10 +15,40 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head = None
+        self.stackTop = None
+
         
     def push(self, data):
+        newNode = Node(data)
+
+        if self.head == None:
+            self.head = newNode
+            self.stackTop = self.head
+            return
+
+        self.stackTop.next = newNode
+        self.stackTop = newNode
+
         
     def pop(self):
+        if self.head == None:
+            return None
+
+        if self.head.next == None:
+            ret = self.head.data
+            self.head = None
+            return ret
+        
+        curr = self.head
+        while curr.next.next != None:
+            curr = curr.next
+        ret = curr.next.data
+        curr.next = None
+        return ret
+        
+
+        
         
 a_stack = Stack()
 while True:
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,13 @@
+from typing import List
+
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -11,12 +16,23 @@ class SinglyLinkedList:
         Takes O(1) time.
         """
         self.head = None
+        self.stackTop = None
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
-        Takes O(n) time.
+        Using stacktop, it can be done in O(1) time.
         """
+        newNode = ListNode(data)
+
+        if self.head == None:
+            self.head = newNode
+            self.stackTop = self.head
+            return
+        
+        self.stackTop.next = newNode
+        self.stackTop = self.stackTop.next
+
         
     def find(self, key):
         """
@@ -24,9 +40,50 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        if self.head == None:
+            return None
+        
+        curr = self.head
+        while curr != None and curr.data != key:
+            curr = curr.next
+        
+        return None if curr == None else curr.data
+
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head == None:
+            print("List is Empty!")
+            return None
+        
+
+        prev = None
+        curr = self.head
+
+        while curr != None:
+            if curr.data == key:
+                if prev == None:
+                    self.head = curr.next
+                    return
+                else:
+                    prev.next = curr.next
+                    return
+            
+            prev = curr
+            curr = curr.next
+        
+        print("Key not found in List")
+        return None
+
+
+        
+test = SinglyLinkedList()
+test.append(1)
+test.append(2)
+print(test.find(12))
+print(test.find(2))
+print(test.remove(2))
+print(test.find(2))
