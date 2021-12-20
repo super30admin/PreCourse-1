26744bb2c84877c41715930e80fb05cfafb99dd1
Jayavardhan Karampudi <jay@Jais-MacBook-Pro.local>
2021-12-19 21:08:01 -0700

    Precourse-1 Complete

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,32 @@
+# Time Complexity : 
+   # push - O(1)
+   # pop - O(1)
+   # isempty - O(1)
+   # display - O(n)
+# Space Complexity: O(n) where n is the number of elements in the list
+# Did this code successfully run on Leetcode : NA
+# Any problem you faced while coding this : No
+
+# Your code here along with comments explaining your approach 
+
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+        self.stack = []
      def isEmpty(self):
-         
+        return not bool(self.stack)
      def push(self, item):
-         
+        self.stack.append(item)
      def pop(self):
-        
+        if self.isEmpty():
+              return None
+        return self.stack.pop()
         
      def peek(self):
-        
+        return None if self.isEmpty() else self.stack[-1]
      def size(self):
-         
+        return len(self.stack)
      def show(self):
-         
+        return self.stack
 
 s = myStack()
 s.push('1')
