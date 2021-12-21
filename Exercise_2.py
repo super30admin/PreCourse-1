2b4a95d9afefe8f17e7af20cffc0d10bfbb63f1d
rohitsonawane <rohit2790@gmail.com>
2021-12-20 22:31:55 -0800

    Complete Sprint Precourse-1

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,24 +1,51 @@
+# Time Complexity : O(1)
+# Space Complexity : O(N) : number of elements
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+
+# Your code here along with comments explaining your approach
+"""
+1. initializing stack with head
+2. push : assign new node to head and move previous head to next of new head
+3. pop : return data of head and assign head.next to head
+
+"""
+
 
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
+
 class Stack:
     def __init__(self):
-        
+        self.head = None
+
     def push(self, data):
-        
+        if self.head:
+            node = Node(data)
+            node.next = self.head
+            self.head = node
+        else:
+            self.head = Node(data)
+
     def pop(self):
-        
+        if self.head:
+            node = self.head
+            self.head = node.next
+            return node.data
+        else:
+            return None
+
+
 a_stack = Stack()
 while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,9 +1,30 @@
+# Time Complexity : append, find , remove  O(N)
+# Space Complexity : O(N) : number of elements
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+
+# Your code here along with comments explaining your approach
+"""
+1. initializing singly LL with head
+2. append : if the head is empty update head with new linknode else loop through next of node and at the end append the new node
+3. find : if the head data is key return key  else loop through next of node till the end and check the each data with key if matches return the key
+4. remove : if the head is data to remove , update head with head.next .
+else loop though nodes check the next node whether matches with key if yes then remove and update the next
+
+"""
+
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
+
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = None
+
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +38,56 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        node = ListNode(data)
+        if not self.head:
+            self.head = node
+            return
+
+        pos = self.head
+        while pos.next:
+            pos = pos.next
+        pos.next = node
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        pos = self.head
+        if pos.data == key:
+            return key
+        while pos:
+            if pos.data == key:
+                return key
+            pos = pos.next
+
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+
+        if self.head.data == key:
+            self.head = self.head.next
+            return
+
+        pos = self.head
+        while pos and pos.next:
+            node = pos.next
+            if node.data == key:
+                pos.next = node.next
+                return
+            pos = pos.next
+
+
+l = SinglyLinkedList()
+l.append(1)
+l.append(2)
+l.append(3)
+print(l.find(3))
+l.remove(2)
+l.remove(3)
+print(l.find(2))
