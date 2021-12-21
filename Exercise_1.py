c9937dc699b8325a2047755ac5fbfea0402e4744
pavanghuge <pbg7@njit.edu>
2021-12-20 20:13:09 -0500

    Precourse Submission

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,40 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
+
+# Time Complexity : O(n)
+# Space Complexity :O(n)
+# Did this code successfully run on Leetcode : yes
+# Any problem you faced while coding this :
+# concepts of stack
+
      def __init__(self):
-         
+        self.stack = []
+        self.top = -1
      def isEmpty(self):
+       if self.top == -1:
+         return True
          
      def push(self, item):
-         
+       self.top = self.top + 1
+       print("Pushed element is: ", item)
+       self.stack.append(item)
+
      def pop(self):
-        
+        if  self.isEmpty():
+          print("Stack is empty")
+        else:
+          return self.stack.pop()
         
      def peek(self):
-        
+       if self.top != -1:
+         print(self.stack[self.top])
+
      def size(self):
+       return len()
          
      def show(self):
-         
+       return self.stack
 
 s = myStack()
 s.push('1')
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,19 @@
+
+# // Time Complexity : O(n)
+# // Space Complexity :O(n)
+# // Did this code successfully run on Leetcode : 
+# // Any problem you faced while coding this : I did not get this pro
+# I had to revise singly linkedlist and concepts fo stack
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+class stackEmpty(Exception):
+    pass
     
 class SinglyLinkedList:
     def __init__(self):
@@ -11,22 +22,70 @@ class SinglyLinkedList:
         Takes O(1) time.
         """
         self.head = None
+        self.size = 0 
+
+    def __len__(self):
+        return self.size
+
+    
+    def isEmpty(self):
+        return self.size == 0
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+
+        self.head = ListNode(data, self.head)
+        self.size += 1 
+
         
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
-        """
+        """ 
+        current = self.head
+ 
+        # loop till current not equal to None
+        while current != None:
+            if current.data == x:
+                return True # data found
+             
+            current = current.next
+         
+        return False
+
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.isEmpty():
+            raise stackEmpty('This stack is empty')
+
+        temp = self.head
+        if (temp is not None):
+            if (temp.data == key):
+                self.head = temp.next
+                temp = None
+                return
+
+
+
+
+        while(temp is not None):
+            if temp.data == key:
+                break
+            prev = temp
+            temp = temp.next
+
+        if(temp == None):
+            return
+ 
+        prev.next = temp.next
+ 
+        temp = None
