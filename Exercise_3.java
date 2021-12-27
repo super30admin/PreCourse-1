489522e4b113932a73f03013b597b313f00730f9
Sapna Patel <sapna@Sapnas-MacBook-Pro.local>
2021-12-27 14:01:33 -0500

    precourse-1

--- Exercise_3.java
+++ Exercise_3.java
@@ -17,30 +17,50 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            this.data = d;
+            this.next = null;
         } 
     } 
   
     // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
+    public static LinkedList insert(Exercise_3 list, int data) 
     { 
         // Create a new node with given data 
-   
+
+        Node newnode = new Node(data);
+        newnode.next = null;
+
         // If the Linked List is empty, 
         // then make the new node as head 
-        
+
+        if(list.head == null){
+            list.head = newnode;
+        }else{
             // Else traverse till the last node 
             // and insert the new_node there 
-
-            // Insert the new_node at last node 
+            Node last = list.head;
+            while(last.next != null){
+                last = last.head;
+            }
+             // Insert the new_node at last node 
         // Return the list by head 
-        
+            last.next = newnode;
+            return list;
+
+        }
+
     } 
   
     // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
+    public static void printList(Exercise_3 list) 
     {  
         // Traverse through the LinkedList 
+
+        Node current = list.head;
+        while(current != null){
+            System.out.println(current.data + " ");
+            current = current.next;
+        }
    
             // Print the data at current node 
        
@@ -51,7 +71,7 @@ public class LinkedList {
     public static void main(String[] args) 
     { 
         /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
+        Exercise_3 list = new Exercise_3(); 
   
         // 
         // ******INSERTION****** 
