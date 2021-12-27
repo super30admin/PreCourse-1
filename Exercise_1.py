cc1501d8f52b784860d5f983574c7e01c1f9f539
Ovini Nanayakkara <ovi@Ovinis-MacBook-Pro.local>
2021-12-27 13:28:00 -0800

    Added precourse 1 solutions

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,49 @@
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
+# Time Complexity :
+  # isEmpty operation: O(n) [We traverse the array to find the length,
+  #                          If we keep a counter then it will be O(1)]
+  # push operation: O(1)
+  # pop operation: O(1)
+  # peek operation: O(1)
+  # size operation: O(n)
+  # show operation: O(1)
+
+# Space Complexity :
+# Did this code successfully run on Leetcode :
+# -> No
+# Any problem you faced while coding this :
+# -> Was unable to determing the Time and Space complexity correctly
+
+
      def __init__(self):
-         
+       self.stack = []
+
      def isEmpty(self):
-         
+       if len(self.stack) == 0:
+         return True
+       else:
+         return False
+
      def push(self, item):
-         
+       self.stack.append(item)
+
+     # Pop the last added element
      def pop(self):
-        
-        
+       self.stack.pop()
+
      def peek(self):
-        
+       return self.stack[-1]
+
      def size(self):
-         
+       return len(self.stack)
+
      def show(self):
-         
+       return self.stack
+
 
 s = myStack()
 s.push('1')
 s.push('2')
+print(s.show())
 print(s.pop())
 print(s.show())
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,41 @@
+# Implement stack as a linked list
 
 class Node:
     def __init__(self, data):
        self.data = data
        self.next = None
- 
+
 class Stack:
     def __init__(self):
-        
+        self.head = None
+
+# In order to maintain Push operation as O(1) I am adding the new node to the front of the list
     def push(self, data):
-        
+        if self.head == None:
+            newNode = ListNode(data)
+            self.head = NewNode
+        else:
+            newNode = ListNode(data)
+            newNode.next = self.head
+            self.head = newNose
+
+# In order to maintain Pop operation as O(1) I am removing the node to the front of the list (where last added would be)
     def pop(self):
-        
+        if self.head == None:
+            return None
+        else:
+            temp = self.head
+            self.head = self.head.next
+            temp.next = None
+            return temp.data
+
+    def peek(self):
+        if self.head == None:
+            return None
+        else:
+            return self.head.data
+
+
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
