9a9c35071d27ba22cc8e8b64e90afea498f804a6
trisha-m <65079062+trisha-m@users.noreply.github.com>
2021-12-30 23:46:39 -0800

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,13 @@
+#time complexity:O(n)
+  #space complexity:O(n)
 class ListNode:
     """
     A node in a singly-linked list.
     """
+    
     def __init__(self, data=None, next=None):
+        self.data= data
+        self.next= next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -12,11 +17,22 @@ class SinglyLinkedList:
         """
         self.head = None
 
+
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        newnode=ListNode(data)
+        current= newnode
+        if(self.head):
+                current = self.head
+                while(current.next):
+                  current = current.next
+                current.next = newnode
+        else:
+               self.head = newnode
+
         
     def find(self, key):
         """
@@ -24,9 +40,28 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        if(self.head):
+                current = self.head
+                while(current.next):
+                  if(current.next== None):
+                      return None
+                  if(current.data == key): 
+                    return current.data
+                  else:
+                      current=current.next
+            
+
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if(self.head):
+                current = self.head
+                while(current.next):
+                  if(current.next== None):
+                      return None
+                  if(current.data == key): 
+                    current.next= current.next
+                
