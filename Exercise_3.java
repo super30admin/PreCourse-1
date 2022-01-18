5783eb526f1b7d72e41918170aaca49aa1b5d92b
Shimpli Kalse <skalse@uncc.edu>
2022-01-18 13:51:52 -0500

    Completed sprint precourse1

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,3 +1,6 @@
+//Time complexity: o(1)
+//space complexity : o(n)
+
 import java.io.*; 
   
 // Java program to implement 
@@ -17,7 +20,8 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            data =d;
+            next=null;
         } 
     } 
   
@@ -25,12 +29,24 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
+        Node n = new Node(data);
    
         // If the Linked List is empty, 
         // then make the new node as head 
-        
-            // Else traverse till the last node 
+        if(list.head==null){
+            list.head=n;
+        }else   // Else traverse till the last node 
             // and insert the new_node there 
+        {
+            Node l=list.head;
+            while(l.next!=null){
+                l=l.next;
+            }
+            l.next=n;
+
+        }
+        
+          return list;
 
             // Insert the new_node at last node 
         // Return the list by head 
@@ -45,6 +61,14 @@ public class LinkedList {
             // Print the data at current node 
        
             // Go to next node 
+
+            Node node=list.head;
+            while(node.next !=null){
+                System.out.println(node.data);
+                node=node.next;
+
+            }
+            System.out.println(node.data);
     } 
    
     // Driver code 
