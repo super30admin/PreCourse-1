4db53f4af47d3179831b95f1549ad9e2ad6fb460
Aryyama Kumar Jana <aryyamakumarjana@Aryyamas-MacBook-Pro-2.local>
2022-01-18 18:31:15 -0700

    Completed Sprint Precourse1

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,15 @@
+# Time Complexity : O(n)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : Could not find the problem in Leetcode
+# Any problem you faced while coding this : No
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,16 +24,44 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        node = ListNode(data)
+        if self.head:
+            curr = self.head
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = node
+        else:
+            self.head = node
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        curr = self.head
+        while curr:
+            if curr.data == key:
+                return curr
+            curr = curr.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head.data == key:
+            temp = self.head
+            self.head = self.head.next
+            temp = None
+        else:
+            prev = self.head
+            curr = prev.next
+            while curr:
+                if curr.data == key:
+                    prev.next = curr.next
+                    curr = None
+                    break
+                prev = curr
+                curr = curr.next
