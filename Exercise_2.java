f160112a14ca0c76af90579b6494f581a83f2154
Rupin Singh Thakur <rupin.thakur23@gmail.com>
2022-01-19 18:43:30 -0600

    Completed Precourse

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,52 +1,64 @@
-public class StackAsLinkedList { 
-  
-    StackNode root; 
-  
-    static class StackNode { 
-        int data; 
-        StackNode next; 
-  
-        StackNode(int data) 
-        { 
-            //Constructor here 
-        } 
-    } 
-    
-	
-    public boolean isEmpty() 
-    { 
-        //Write your code here for the condition if stack is empty. 
-    } 
-  
-    public void push(int data) 
-    { 
-        //Write code to push data to the stack. 
-    } 
-  
-    public int pop() 
-    { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
-    } 
-  
-    public int peek() 
-    { 
-        //Write code to just return the topmost element without removing it.
-    } 
-  
-	//Driver code
-    public static void main(String[] args) 
-    { 
-  
-        StackAsLinkedList sll = new StackAsLinkedList(); 
-  
-        sll.push(10); 
-        sll.push(20); 
-        sll.push(30); 
-  
-        System.out.println(sll.pop() + " popped from stack"); 
-  
-        System.out.println("Top element is " + sll.peek()); 
-    } 
-} 
+public class StackAsLinkedList {
+
+    StackNode root;
+
+    static class StackNode {
+        int data;
+        StackNode next;
+
+        StackNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (root == null)
+            return true;
+        else
+            return false;
+    }
+
+    public void push(int data) {
+        StackNode node = new StackNode(data);
+        if (root == null)
+            root = node;
+        else {
+            node.next = root;
+            root = node;
+        }
+    }
+
+    public int pop() {
+        if (root == null) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        StackNode top = root;
+        root = root.next;
+        return top.data;
+
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return root.data;
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+
+        StackAsLinkedList sll = new StackAsLinkedList();
+
+        sll.push(10);
+        sll.push(20);
+        sll.push(30);
+
+        System.out.println(sll.pop() + " popped from stack");
+
+        System.out.println("Top element is " + sll.peek());
+    }
+}
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,70 +1,72 @@
-import java.io.*; 
-  
+import java.io.*;
+
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
-  
-    Node head; // head of list 
-  
-    // Linked list Node. 
-    // This inner class is made static 
-    // so that main() can access it 
-    static class Node { 
-  
-        int data; 
-        Node next; 
-  
-        // Constructor 
-        Node(int d) 
-        { 
-            //Write your code here 
-        } 
-    } 
-  
-    // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
-    { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
+public class LinkedList {
 
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
-    } 
-  
-    // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
-    {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
-    } 
-   
-    // Driver code 
-    public static void main(String[] args) 
-    { 
+    Node head; // head of list
+
+    // Linked list Node.
+    // This inner class is made static
+    // so that main() can access it
+    static class Node {
+
+        int data;
+        Node next;
+
+        // Constructor
+        Node(int d) {
+            int data = d;
+            Node next = null;
+        }
+    }
+
+    // Method to insert a new node
+    public static LinkedList insert(LinkedList list, int data) {
+        Node newNode = new Node(data);
+
+        if (list.head == null) {
+            list.head = newNode;
+            return list;
+        } else {
+            Node current = list.head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+            return list;
+        }
+
+    }
+
+    // Method to print the LinkedList.
+    public static void printList(LinkedList list) {
+        if (list.head == null)
+            System.out.println("No Data exists in the list");
+        Node current = list.head;
+        while (current != null) {
+            System.out.print(current.data + "-> ");
+            current = current.next;
+        }
+    }
+
+    // Driver code
+    public static void main(String[] args) {
         /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
-  
-        // 
-        // ******INSERTION****** 
-        // 
-  
-        // Insert the values 
-        list = insert(list, 1); 
-        list = insert(list, 2); 
-        list = insert(list, 3); 
-        list = insert(list, 4); 
-        list = insert(list, 5); 
-  
-        // Print the LinkedList 
-        printList(list); 
-    } 
+        LinkedList list = new LinkedList();
+
+        //
+        // ******INSERTION******
+        //
+
+        // Insert the values
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+
+        // Print the LinkedList
+        printList(list);
+    }
 }
