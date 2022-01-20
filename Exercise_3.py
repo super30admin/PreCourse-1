1d4372ec61d8920d98ddb05576b39b0bd3cf3988
Akash Ashok <akashashokwork@gmail.com>
2022-01-19 22:27:07 -0800

    Completed PreCourse-1

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,19 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        nodeToAppend = ListNode(data)
+        if self.head is None:
+            self.head = nodeToAppend
+            nodeToAppend.next = None
+            return
+
+        last = self.head
+        
+        
+        while (last.next != None):
+            last = last.next
+        last.next = nodeToAppend
+
         
     def find(self, key):
         """
@@ -24,9 +39,56 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        temp = self.head
+        if (temp != None):
+            if (temp.data == key):
+                return key
+            else:
+                while(temp.next != None):
+                    if (temp.data == key):
+                        return key
+                    temp = temp.next
+        else:
+            return "None"
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        prev = None
+        current = self.head
+  
+        while (current != None):
+            if current.data == key:
+                if current == self.head:
+                    self.head = self.head.next
+                    current = self.head
+                else:  
+                    prev.next = current.next
+                    current = current.next
+                return
+            else:
+                prev = current
+                current = current.next
+  
+        # return head (if key not in list)
+
+    def printSLL(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+        
+sll = SinglyLinkedList()
+sll.append(11)
+sll.append(22)
+sll.append(33)
+sll.append(44)
+sll.append(33)
+sll.append(33)
+# sll.printSLL()
+sll.remove(33)
+# sll.printSLL()
+sll.remove(33)
+sll.printSLL()
