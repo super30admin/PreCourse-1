7245877391e087030c72f719e079540659e5149b
SRaval20 <raval.sagar.sr@gmail.com>
2022-01-22 00:39:11 -0800

    Completed Precourse-1 Exercise-1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,67 @@
-class Stack { 
+// Time Complexity: 1
+// Space Complexity: n
+// Yes, this code ran successfully on Leetcode Playground
+// No, I didn't face any problem in this problem
+
+class Stack {
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        if(top > -1)        // Stack is not Empty
+            return true ;
+        else
+            return false ;  // Stack is Empty
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        top++ ;
+        if(top > MAX)       // Stack is full
+        {
+            top-- ;
+            return false ;
+        }
+        else                // Stack is not full
+        {
+            a[top] = x ;
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top < 0)         // Stack is Empty
+        {
+            System.out.println(" Stack Underflow");
+            return 0 ;
+        }
+        else                // Stack is not Empty
+        {
+            int temp = a[top] ;
+            top-- ;
+            return temp ;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        return a[top] ;     // returning top element
     } 
 } 
   
@@ -41,6 +73,6 @@ class Main {
         s.push(10); 
         s.push(20); 
         s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack");
     } 
 }
---
+++ out/production/PreCourse-1/Exercise_1.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h> 
+  
+using namespace std; 
+  
+#define MAX 1000 
+  
+class Stack {
+  //Please read sample.java file before starting.
+  //Kindly include Time and Space complexity at top of each file
+    int top; 
+  
+public: 
+    int a[MAX]; // Maximum size of Stack 
+  
+    Stack() { //Constructor here } 
+    bool push(int x); 
+    int pop(); 
+    int peek(); 
+    bool isEmpty(); 
+}; 
+  
+bool Stack::push(int x) 
+{ 
+    //Your code here
+    //Check Stack overflow as well
+} 
+  
+int Stack::pop() 
+{ 
+    //Your code here
+    //Check Stack Underflow as well 
+} 
+int Stack::peek() 
+{ 
+    //Your code here
+    //Check empty condition too
+} 
+  
+bool Stack::isEmpty() 
+{ 
+    //Your code here 
+} 
+  
+// Driver program to test above functions 
+int main() 
+{ 
+    class Stack s; 
+    s.push(10); 
+    s.push(20); 
+    s.push(30); 
+    cout << s.pop() << " Popped from stack\n"; 
+  
+    return 0; 
+} 
