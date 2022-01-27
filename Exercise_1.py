827b001760b5194ec14112047a6f439ccb17bd4d
sujana <sujana99j@gmail.com>
2022-01-27 13:15:53 -0800

    Precourse1_submission

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,38 @@
+##Exercise_1 : Implement Stack using Array.
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self): 
+        self.L=[]
+        self.size=0
 
+    def isEmpty(self):
+        return self.size == 0
+
+    def push(self,item):
+        # tc - o(1)
+        # sc - o(1)
+        self.L.append(item)
+        self.size=self.size+1
+
+    def pop(self):
+        # tc - o(1)
+        # sc - o(1
+        self.size=self.size-1
+        return self.L.pop()
+
+    def peak(self):
+        # tc - o(n)
+        # sc - o(1)
+        return max(self.L)
+    
+    def size(self):
+        # tc - o(1)
+        # sc - o(1)
+        return self.size
+
+    def show(self):
+        # tc - o(1)
+        # sc - o(1)
+        return self.L
 s = myStack()
 s.push('1')
 s.push('2')
