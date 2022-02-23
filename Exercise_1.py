dd4ae8ad460722dd14c706186d528b7c4a046a14
nisargushah <58926548+nisargushah@users.noreply.github.com>
2022-02-23 17:02:31 -0600

    Completed Problem  1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,27 @@
+# Time Complexity : O(1) adding, removing and showing an array is constant time
+# Space Complexity : O(n) depends on how many numbers we want to insert
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+         self.stack = []
      def isEmpty(self):
-         
+         if len(self.stack) > 0:
+             return False
+         else:
+             return True
      def push(self, item):
-         
+         self.stack.append(item)
      def pop(self):
-        
-        
+         return self.stack.pop()
+
      def peek(self):
-        
+         return self.stack[0]
      def size(self):
-         
+         return len(self.stack)
      def show(self):
-         
+         return self.stack
 
 s = myStack()
 s.push('1')
