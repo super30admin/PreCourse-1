723c813b5af3f74b4b0e21bdf8ac86c0fc4e1a67
Sai Varma Penmatsa <67587377+SAIVARMA571@users.noreply.github.com>
2022-02-23 13:13:26 -0500

    Update Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,5 @@
+#push and pop operations are O(1).
+
 
 class Node:
     def __init__(self, data):
@@ -6,27 +8,24 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head = None
         
     def push(self, data):
+        if(self.head == None):
+            self.head = Node(data)
+        else:
+            newNode = Node(data)
+            newNode.next = self.head
+            self.head = newNode
+        return self.head
         
     def pop(self):
+        if(self.head == None):
+            return None
+        else:
+            temp = self.head
+            self.head = temp.next
+            temp.next = None
+            return temp.data
         
 a_stack = Stack()
-while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    print('push <value>')
-    print('pop')
-    print('quit')
-    do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    operation = do[0].strip().lower()
-    if operation == 'push':
-        a_stack.push(int(do[1]))
-    elif operation == 'pop':
-        popped = a_stack.pop()
-        if popped is None:
-            print('Stack is empty.')
-        else:
-            print('Popped value: ', int(popped))
-    elif operation == 'quit':
-        break
