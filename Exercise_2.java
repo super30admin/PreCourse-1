ed788c3123b22febf1f8ebb8be1191478e0d8a04
sofiamh16 <sofiamh16@gmail.com>
2022-02-22 18:50:15 -0600

    Completed Sprint PreCourse1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,38 +1,60 @@
+// Time Complexity : O(1) for all operations (Since done at the root/head)
+// Space Complexity : O(n), n is the number of nodes in the list
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : N/A
+
 public class StackAsLinkedList { 
   
     StackNode root; 
-  
+
     static class StackNode { 
         int data; 
         StackNode next; 
-  
+        
         StackNode(int data) 
         { 
             //Constructor here 
+            this.data = data;
         } 
     } 
     
 	
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+        //Condition for stack is empty
+        return root == null;
     } 
   
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
+        StackNode node = new StackNode(data); //Creating a new node with the data given
+        node.next = root;                     //linking the new node to root
+        root = node;                          //updating the root   
     } 
   
     public int pop() 
     { 	
 	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+        if(root==null)
+        {
+            System.out.println("Stack Underflow");
+            return 0;
+        }else
+        {
+            StackNode x = root;   //storing root in a temporary node
+            root = root.next;     //moving root to the next node
+            return x.data;        //returning data from temporary node
+        }
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+        if(!isEmpty())
+        {
+            return root.data;
+        }else
+            return -1;
     } 
   
 	//Driver code
@@ -46,7 +68,7 @@ public class StackAsLinkedList {
         sll.push(30); 
   
         System.out.println(sll.pop() + " popped from stack"); 
-  
         System.out.println("Top element is " + sll.peek()); 
+
     } 
 } 
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,14 @@
+// Time Complexity : O(n) (as we traverse to the last node and then add a new node)
+// Space Complexity : O(n) (n-number of nodes)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Had to change the name of the class as it didn't //run on leetcode
+
+
 import java.io.*; 
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+public class LinkedList1 { 
   
     Node head; // head of list 
   
@@ -17,41 +23,57 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            //Initializing fields
+            data = d;
+            next = null;
         } 
     } 
   
     // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
+    public static LinkedList1 insert(LinkedList1 list, int data) 
     { 
         // Create a new node with given data 
-   
+        Node node = new Node(data);
+        node.next = null;
         // If the Linked List is empty, 
         // then make the new node as head 
-        
+        if(list.head==null)
+            list.head = node;
             // Else traverse till the last node 
             // and insert the new_node there 
-
+        else
+        {
+            Node temp = list.head;
+            while(temp.next!=null)
+            {
+                temp = temp.next;
+            }
             // Insert the new_node at last node 
+            temp.next = node;
+        }
         // Return the list by head 
-        
+        return list;    
     } 
   
     // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
+    public static void printList(LinkedList1 list) 
     {  
+        Node temp = list.head;
         // Traverse through the LinkedList 
-   
+        while(temp!=null)
+        {
             // Print the data at current node 
-       
+            System.out.println(temp.data);
             // Go to next node 
+            temp = temp.next;
+        }         
     } 
    
     // Driver code 
     public static void main(String[] args) 
     { 
         /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
+        LinkedList1 list = new LinkedList1(); 
   
         // 
         // ******INSERTION****** 
