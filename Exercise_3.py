cce26a97b25933900ce882fef37ca0a6ed1169b5
architakale <architakale@knights.ucf.edu>
2022-02-23 20:35:15 -0500

    Exercise 2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,14 @@
+from ast import While
+from email import header
+
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -10,13 +16,17 @@ class SinglyLinkedList:
         Create a new singly-linked list.
         Takes O(1) time.
         """
-        self.head = None
+        self.head = ListNode()
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        current_node = self.head
+        while current_node.next != None:
+            current_node = current_node.next
+        current_node.next = ListNode(data)
         
     def find(self, key):
         """
@@ -24,9 +34,38 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        current_node = self.head
+        while current_node != None:
+            if current_node.data == key:
+                return current_node
+            current_node = current_node.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        current_node = self.head
+        prev_node = None
+        while current_node != None:
+            if current_node.data == key:
+                prev_node.next = current_node.next
+            prev_node = current_node
+            current_node = current_node.next
+    
+    def print_list(self):
+        current_node = self.head
+        while current_node!= None:
+            print(current_node.data)
+            current_node = current_node.next
+
+# x = SinglyLinkedList()
+# x.append(1)
+# x.append(2)
+# x.append(3)
+# x.append(4)
+# x.print_list()
+# print(x.find(3).data)
+# x.remove(2)
+# x.print_list()
