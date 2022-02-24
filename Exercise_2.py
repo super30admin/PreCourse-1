1edd8289256bb945bc06de688061a389df799f42
architakale <architakale@knights.ucf.edu>
2022-02-23 21:16:23 -0500

    Exercise 2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,29 @@
-
+# Time Complexity is O(1)
+# Space Complexity is O(n)
 class Node:
-    def __init__(self, data):
+    def __init__(self, data=None):
        self.data = data
        self.next = None
  
 class Stack:
     def __init__(self):
+        self.head = Node()
         
     def push(self, data):
-        
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
     def pop(self):
-        
+        if self.head != None:
+            top_value = self.head.data
+            self.head = self.head.next
+            return top_value
+        return None
+    
+    def peek(self):
+        return self.head.data if self.head != None else 0 
+
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
