6759b9539ca77ee99cdcfa484729d7a4bd9b9f4c
Aditya Desai <adidesi95@gmail.com>
2022-02-27 19:52:08 -0700

    Exercise completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,67 @@
-class Stack { 
-    //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
+/*
+Time Complexity : O(1) since everything is done in constant amount of operation irrespective of size
+Space Complexity : O(n) since we create memory when stack in initialized
+
+Did this code successfully run on Leetcode :
+Finished in 104 ms
+40 Peeked from stack
+40 Popped from stack
+30 Popped from stack
+20 Popped from stack
+10 Popped from stack
+Stack Underflow
+0 Peeked from stack
+
+Any problem you faced while coding this :
+Renamed the class due to Leetcode giving compiling issues due to Conflict with java.util.Stack class.
+
+Your code here along with comments explaining your approach :
+Straight forward approach.
+*/
+class StackUsingArray { 
+    private static final int MAX = 1000; 
+    private int top; 
+    private int []a;
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        return (top == -1);
     } 
 
-    Stack() 
+    StackUsingArray() 
     { 
-        //Initialize your constructor 
+        this.a = new int[MAX]; // Maximum size of Stack 
+        this.top = -1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        if(top+1<MAX){
+            a[++top] = x;
+            return true;
+        }
+        System.out.println("Stack Overflow");
+        return false;
+
     } 
   
     int pop() 
-    { 
+    {
         //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if(top == -1){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return a[top--];
     } 
   
     int peek() 
-    { 
-        //Write your code here
+    {
+        if(top == -1){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return a[top];
     } 
 } 
   
@@ -37,10 +69,18 @@ class Stack {
 class Main { 
     public static void main(String args[]) 
     { 
-        Stack s = new Stack(); 
+        StackUsingArray s = new StackUsingArray(); 
+        System.out.println(s.peek() + " Peeked from stack"); 
         s.push(10); 
         s.push(20); 
         s.push(30); 
+        s.push(40); 
+        System.out.println(s.peek() + " Peeked from stack"); 
         System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.peek() + " Peeked from stack"); 
+        
     } 
 }
