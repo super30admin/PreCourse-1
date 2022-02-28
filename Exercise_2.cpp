b48af053aafa755dd7f0de34d058926f02fac975
Sanjana Reddy <sanjanakommareddy@gmail.com>
2022-02-28 17:50:35 +0530

    completed PreCourse-1

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -19,21 +19,37 @@ StackNode* newNode(int data)
 int isEmpty(StackNode* root) 
 { 
     //Your code here 
+    return !root;
 } 
   
 void push(StackNode** root, int data) 
 { 
     //Your code here 
+   StackNode* temp = newNode(data);
+    temp->next = *root;
+    *root = temp;
+
 } 
   
 int pop(StackNode** root) 
 { 
     //Your code here 
+    if(isEmpty(*root))
+        return INT_MIN;
+    StackNode* temp = *root;
+    *root = (*root)->next;
+    int k=temp->data;
+    free(temp);
+    return k;
+
 } 
   
 int peek(StackNode* root) 
 { 
     //Your code here 
+    if(isEmpty(root))
+        return INT_MIN;
+    return root->data;
 } 
   
 int main() 
--- Exercise_3.cpp
+++ Exercise_3.cpp
@@ -15,12 +15,14 @@ a new node on the front of the list. */
 void push(Node** head_ref, int new_data)  
 {  
     /* 1. allocate node */ 
+    Node* temp = new Node();
   
     /* 2. put in the data */  
-  
+    temp->data = new_data;
     /* 3. Make next of new node as head */ 
-  
+    temp->next = (*head_ref);
     /* 4. move the head to point to the new node */
+    (*head_ref) = temp;
 }  
   
 /* Given a node prev_node, insert a new node after the given  
@@ -28,14 +30,17 @@ prev_node */
 void insertAfter(Node* prev_node, int new_data)  
 {  
     /*1. check if the given prev_node is NULL */ 
-  
+    if(prev_node==NULL){
+        return;
+    }
     /* 2. allocate new node */ 
-  
+    Node* temp = new Node(); 
     /* 3. put in the data */ 
-  
+    temp->data = new_data;
     /* 4. Make next of new node as next of prev_node */
-  
+    temp->next = prev_node->next;
     /* 5. move the next of prev_node as new_node */ 
+    prev_node->next = temp;
 }  
   
 /* Given a reference (pointer to pointer) to the head  
@@ -43,19 +48,29 @@ of a list and an int, appends a new node at the end */
 void append(Node** head_ref, int new_data)  
 {  
     /* 1. allocate node */ 
-  
+    Node* temp = new Node();
     /* 2. put in the data */ 
-  
+    temp->data = new_data;
     /* 3. This new node is going to be  
     the last node, so make next of  
-    it as NULL*/  
+    it as NULL*/
+    temp->next = NULL;  
   
     /* 4. If the Linked List is empty, 
     then make the new node as head */
-  
+    if(*head_ref == NULL)
+    {
+        *head_ref = temp;
+        return;
+    }
     /* 5. Else traverse till the last node */
-  
+    while((*head_ref)->next != NULL)
+    {
+        (*head_ref)=(*head_ref)->next;
+    }
     /* 6. Change the next of last node */ 
+    (*head_ref)->next = temp;
+    return;
 }  
   
 // This function prints contents of 
@@ -63,6 +78,11 @@ void append(Node** head_ref, int new_data)
 void printList(Node *node)  
 {  
     //Your code here
+    while(node!=NULL)
+    {
+        cout << node->data << " ";
+        node = node->next;
+    }
 }  
   
 /* Driver code*/
