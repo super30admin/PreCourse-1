6cffffe5e5a574a9b2d699a24aceef61d171ae21
Kavyasree <kolliparakavyasree@gmail.com>
2022-03-06 15:45:01 -0800

    Precourse-1 completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -2,34 +2,59 @@ class Stack {
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
-    int top; 
+    int top;
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        return top == -1;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        // Checking if array is full
+        if (top + 1 == MAX) {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        else {
+            top = top + 1;
+            a[top] = x;
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            int temp = a[top];
+            top--;
+            return temp;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+            return top;
+        } else
+            return a[top];
     } 
 } 
   
@@ -41,6 +66,6 @@ class Main {
         s.push(10); 
         s.push(20); 
         s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack "+ s.peek()); 
     } 
 }
--- Exercise_3.java
+++ Exercise_3.java
@@ -17,7 +17,9 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            //Write your code here
+            this.data = d;
+            this.next = null;  
         } 
     } 
   
@@ -25,26 +27,43 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
+        Node temp = new Node(data);
+        temp.next = null;
    
         // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
-
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
+        // then make the new node as head
+        if (list.head == null) {
+            list.head = temp;
+        } else {
+            // Else traverse till the last node
+            // and insert the new_node there
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+   
+            // Insert the new_node at last node
+            last.next = temp;
+        }
+   
+        // Return the list by head
+        return list;
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+        Node temp = list.head;
+
+        // Traverse through the LinkedList
+        while (temp != null) {
+            // Print the data at current node
+            System.out.print(temp.data + " ");
+    
+            // Go to next node
+            temp = temp.next;
+        }
+        System.out.println("");
     } 
    
     // Driver code 
