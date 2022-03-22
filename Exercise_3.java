ad1492be5262933c182fa9fa04905d9170b1fe8a
skatkar <skatkar225@gmail.com>
2022-03-21 23:28:54 -0500

    Exercise_3.java

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,12 +1,12 @@
-import java.io.*; 
-  
-// Java program to implement 
+import java.io.*;
+
+// Java program to implement
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList {
   
-    Node head; // head of list 
-  
-    // Linked list Node. 
+    Node head; // head of list
+
+    // Linked list Node.
     // This inner class is made static 
     // so that main() can access it 
     static class Node { 
@@ -17,35 +17,52 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            this.data = d;
+            this.next = null;
         } 
     } 
   
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
     { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
+        if(isListNull(list)){
+            System.out.println("list is null");
+            return null;
+        }
 
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
+        Node tempNode = new Node(data);
+
+        if(list.head == null) {
+            list.head = tempNode;
+        }else{
+            Node currentNode = list.head;
+            while(currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = tempNode;
+        }
+        return list;
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
-    } 
+        if(isListNull(list)){
+            System.out.println("List is null");
+            return;
+        }
+        Node currentNode = list.head;
+        System.out.print("head -> ");
+        while(currentNode.next != null){
+            System.out.print(currentNode.data + " -> ");
+            currentNode = currentNode.next;
+        }
+        System.out.println(" end");
+    }
+
+    private static boolean isListNull(LinkedList list) {
+        return null == list;
+    }
    
     // Driver code 
     public static void main(String[] args) 
