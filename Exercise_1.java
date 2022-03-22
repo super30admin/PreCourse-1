155316ffb89bf35f9f582bf56aa3c65a78c4202e
Pratik Kamble <54627841+pratik-kamble@users.noreply.github.com>
2022-03-22 17:54:38 -0400

    Sprint 1 Exercise 1 completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -7,29 +7,46 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+      return (top < 0);  
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        if((top + 1) == MAX){
+            return false;
+        }
+        a[++top] = x;
+        return true;
+        
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        }else{
+            return a[top--];
+        }
+
+
     } 
   
     int peek() 
     { 
-        //Write your code here
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        }else{
+            return a[top];
+        }
     } 
 } 
   
