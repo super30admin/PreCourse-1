e65dce79dbf11908813f222639711f179dc715d7
AayushSanghai <61236166+AayushSanghai@users.noreply.github.com>
2022-03-23 18:08:02 -0500

    Completed sprint Precourse-1

--- Exercise_1.java
+++ Exercise_1.java
@@ -37,10 +37,6 @@ class Stack {
 class Main { 
     public static void main(String args[]) 
     { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println("a");
     } 
 }
--- Exercise_2.java
+++ Exercise_2.java
@@ -8,7 +8,7 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
-            //Constructor here 
+            int a = 10;
         } 
     } 
     
