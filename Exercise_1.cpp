5d06ac308a66fa075f73fbae97130f27511b6655
Jigar <gayatrimaa@jigars-mbp.mynetworksettings.com>
2022-03-23 00:08:12 -0400

    complete the first problem

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,3 +1,4 @@
+//time complexity is o(1) for every function and space is o(1)
 #include <bits/stdc++.h> 
   
 using namespace std; 
@@ -12,7 +13,9 @@ class Stack {
 public: 
     int a[MAX]; // Maximum size of Stack 
   
-    Stack() { //Constructor here } 
+    Stack() { //Constructor here
+      top = -1;
+    } 
     bool push(int x); 
     int pop(); 
     int peek(); 
@@ -23,22 +26,46 @@ bool Stack::push(int x)
 { 
     //Your code here
     //Check Stack overflow as well
+   if(top >= MAX-1){
+	cout<<"stack overflow"<<endl;
+	return false;
+   } else{
+   	 a[++top] = x;
+   	 return true;
+   }
 } 
   
 int Stack::pop() 
 { 
     //Your code here
     //Check Stack Underflow as well 
+    if (top == -1) {
+    	return 0;
+    } else {
+    	return a[top--];
+    }
 } 
+
 int Stack::peek() 
 { 
     //Your code here
     //Check empty condition too
+ if(top == -1) {
+	 return 0;
+ } else {
+ 	return a[top];
+ }
+ 
 } 
   
 bool Stack::isEmpty() 
 { 
     //Your code here 
+ if(top == -1){
+	return true;
+  } else {
+  	return false;
+  }
 } 
   
 // Driver program to test above functions 
@@ -51,4 +78,4 @@ int main()
     cout << s.pop() << " Popped from stack\n"; 
   
     return 0; 
-} 
+}
