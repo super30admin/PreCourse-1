dff7de1cf8b2d41bb4aea6c0f7dfadf40c808625
Keerthy43 <keerthygandra526@gmail.com>
2022-03-23 07:13:02 +0530

    Precourse1 Java solutions

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,46 +1,84 @@
-class Stack { 
+import java.util.Arrays;
+
+/**
+ * Space complexity of this solution is O(1)
+ */
+class Stack {
     //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+    //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
     int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
+    int[] a = new int[MAX]; // Maximum size of Stack
+
+    /**
+     * Time complexity of isEmpty() operation is O(1)
+     */
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        return top == 0 && a[top] == 0;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
-    } 
-  
+        //Initialize your constructor
+    }
+
+    /**
+     * Time complexity of push() operation is O(1)
+     */
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
-    } 
-  
+        if(top+1 == a.length)
+            return false;
+        else{
+            a[top] = x;
+            top = top + 1;
+            return true;
+        }
+    }
+
+    /**
+     * Time complexity of pop() operation is O(1)
+     */
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
-    } 
-  
+        if(isEmpty()){
+            return 0;
+        }else{
+            int poppedVal = a[top-1];
+            a[top - 1] = 0;
+            top = top -1;
+            return poppedVal;
+        }
+    }
+
+    /**
+     * Time complexity of peek() operation is O(1)
+     */
     int peek() 
     { 
         //Write your code here
+        return a[top-1];
     } 
 } 
   
 // Driver code 
 class Main { 
-    public static void main(String args[]) 
+    public static void main(String[] args)
     { 
         Stack s = new Stack(); 
         s.push(10); 
         s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        s.push(30);
+        System.out.println(Arrays.toString(s.a));
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(Arrays.toString(s.a));
+        System.out.println(s.peek() + " Top element from stack");
+        System.out.println(Arrays.toString(s.a));
     } 
 }
--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,7 @@
-public class StackAsLinkedList { 
+/**
+ * Space complexity of this solution is O(n)
+ */
+class StackAsLinkedList {
   
     StackNode root; 
   
@@ -8,45 +11,74 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
-            //Constructor here 
+            //Constructor here
+            this.data = data;
         } 
-    } 
-    
-	
+    }
+
+    /**
+     * Time complexity of isEmpty() operation is O(1)
+     */
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
-    } 
-  
+        //Write your code here for the condition if stack is empty.
+        return root == null;
+    }
+
+    /**
+     * Time complexity of push() operation is O(1)
+     */
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
-    } 
-  
+        //Write code to push data to the stack.
+        StackNode newStackNode = new StackNode(data);
+        if (!isEmpty()) {
+            newStackNode.next = root;
+        }
+        root = newStackNode;
+    }
+
+    /**
+     * Time complexity of pop() operation is O(1)
+     */
     public int pop() 
     { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
-    } 
-  
+	    //If Stack Empty Return 0 and print "Stack Underflow"
+        // Write code to pop the topmost element of stack.
+	    //Also return the popped element
+        if(isEmpty())
+            return 0;
+        else{
+            int poppedValue = root.data;
+            root = root.next;
+            return poppedValue;
+        }
+    }
+
+    /**
+     * Time complexity of peek() operation is O(1)
+     */
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+        if(isEmpty())
+            return 0;
+        else
+            return root.data;
     } 
   
 	//Driver code
     public static void main(String[] args) 
     { 
   
-        StackAsLinkedList sll = new StackAsLinkedList(); 
-  
+        StackAsLinkedList sll = new StackAsLinkedList();
+        System.out.println("Is stack empty : " + sll.isEmpty());
         sll.push(10); 
         sll.push(20); 
-        sll.push(30); 
-  
+        sll.push(30);
+        System.out.println("Top element is : " +  sll.peek());
         System.out.println(sll.pop() + " popped from stack"); 
-  
-        System.out.println("Top element is " + sll.peek()); 
+        System.out.println("Top element is : " + sll.peek());
+        System.out.println("Is stack empty : " + sll.isEmpty());
     } 
 } 
