d140f671a899864a73836ea068aad557006b6f56
LipsaKhant <89164452+LipsaKhant@users.noreply.github.com>
2022-03-23 18:34:12 -0400

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,44 +1,80 @@
+// Time Complexity:
+//Space Complexity:
+
+
 class Stack { 
     //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+    //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Check top
+    	if (top==-1)
+    	{
+    		return true;
+    	}
+    	else
+    		return false;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Intialize top
+    	top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
-        //Write your code here
+    	if (top>=MAX-1)
+    	{
+    		System.out.println("Stack Overflow");
+    		return false;
+    	}
+    	else{
+    	    top++;
+    		a[top] = x;
+    	}
+    // 	System.out.println(a);
+    		return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+    	if (isEmpty())
+    	{
+    		System.out.println("Stack Underflow");
+    		return 0;
+    	}
+    	else
+    	{
+    	    
+    		return a[top];
+    	}
     } 
   
     int peek() 
     { 
         //Write your code here
+    	if (isEmpty()) 
+    	{
+    		System.out.println("Stack is empty");
+    	}
+    	return a[top];
     } 
 } 
   
 // Driver code 
-class Main { 
+public class Main { 
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
-        s.push(10); 
+        s.push(10);
+        // System.out.println(s.a);
         s.push(20); 
         s.push(30); 
         System.out.println(s.pop() + " Popped from stack"); 
