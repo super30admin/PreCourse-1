aeeefcf3e08c37c89611ae985c6259c746c89836
rakeshamireddy <rakeshkumar.amireddy@sjsu.edu>
2022-03-23 20:42:14 -0700

    Modified

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,28 @@
+from collections import deque
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+     	self.container = deque()
          
      def isEmpty(self):
+
+     	 return len(self.container)==0
          
      def push(self, item):
+     	 return self.container.append(item)
          
      def pop(self):
-        
+         return self.container.pop()
         
      def peek(self):
+     	 return self.container[-1]
         
      def size(self):
+     	 return len(self.container)
          
      def show(self):
+     	 return self.container
          
 
 s = myStack()
