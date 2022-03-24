20d3b0e6b73a7d81cbe039760e9497dd3fa2a3bd
Ramya269 <mikkilineniramya6@gmail.com>
2022-03-23 22:14:57 -0500

    Precourse1 completed please review

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,41 @@
-class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+#Time Complexity : O(1)
+#Space Complexity : O(1)
+#Did this code successfully run on Leetcode : Yes
+#Any problem you faced while coding this : No
 
+class myStack:
+    #function to create empty stack
+     def __init__(self):
+         self.array_stack=[]
+    #checks if stack is empty or not and return true or false
+     def isEmpty(self):
+         return len(self.array_stack)==0  
+    #inserts the elements into the stack       
+     def push(self, item):
+         return self.array_stack.append(item)
+    #removes the last element inserted into the stack
+     def pop(self):
+         return self.array_stack.pop() 
+    #returns the last element of the stack       
+     def peek(self):
+         return self.array_stack[-1]
+    #returns size of the stack
+     def size(self):
+         return len(self.array_stack)
+    #returns the stack
+     def show(self):
+         return self.array_stack
+        
 s = myStack()
+print(s.isEmpty())
 s.push('1')
 s.push('2')
+print(s.isEmpty())
+print(s.show())
 print(s.pop())
+s.push('3')
+s.push('4')
+print(s.show())
+print(s.peek())
+print(s.size())
 print(s.show())
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,12 @@
+#Did this code successfully run on Leetcode : Yes
+#Any problem you faced while coding this : No
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+         self.data = data
+         self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +21,17 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        #Create a node for the data to insert
+        nextNode = ListNode(data) 
+        #checking if linked list is empty or not and making new node ie., nextNode as head
+        if self.head is None:
+            self.head = nextNode 
+        #if not traverse till end
+        lastNode = self.head
+        while (lastNode.next):
+            lastNode = lastNode.next 
+        #updating the nextNode of lastNode
+        lastNode.next = nextNode
         
     def find(self, key):
         """
@@ -24,9 +39,40 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        #assign the search node to head and loop until searchNode != None
+        searchNode = self.head 
+        while (searchNode != None):
+            if (searchNode.data == key):
+                #if element is found
+                return searchNode
+            
+            searchNode = searchNode.next
+        #if element is not found 
+        return None 
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        removeNode = self.head
+        #checking if initial node has the key that has to be removed
+        if (removeNode != None):
+            if (removeNode.data == key):
+                self.head = removeNode.next
+                removeNode = None
+                return
+        #checking for the key to be removed and making note of previous node
+        while(removeNode != None):
+            if removeNode.data == key:
+                break
+            previousNode = removeNode
+            removeNode = removeNode.next
+ 
+        # checking if key is present or not in linked list
+        if(removeNode == None):
+            return 
+        #removing the node from linked list
+        previousNode.next = removeNode.next 
+        removeNode = None
+
