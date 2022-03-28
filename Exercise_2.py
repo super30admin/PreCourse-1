2398453bbc3e4894dae266495fafb8e0098eba47
Aishwarya Lad <aishwarya.lad@udemy.com>
2022-03-27 18:32:41 -0700

    Precourse1 completed

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,15 +1,33 @@
+"""
+Time complexity O(1) and Space Complexity O(n)
+"""
 
 class Node:
     def __init__(self, data):
        self.data = data
        self.next = None
- 
+
+
 class Stack:
     def __init__(self):
-        
+        self.top = None
+
     def push(self, data):
+        new_node = Node(data)
+        if self.top is None:
+            new_node.next = None
+        else:
+            new_node.next = self.top
+        self.top = new_node
         
     def pop(self):
+        if self.top is None:
+            return None
+        else:
+            temp_node = self.top
+            self.top = self.top.next
+            return temp_node.data
+
         
 a_stack = Stack()
 while True:
