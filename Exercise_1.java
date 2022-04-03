4a0c0272178ec8ae0055d5bf721b7e6acd9920cd
Rashmi <rashmipatil018@gmail.com>
2022-04-02 20:01:35 -0400

    Completed PreCourse-1 - Please Review.

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,46 +1,82 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run: Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
 class Stack { 
-    //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+    //Exercise_1 : Implement Stack using Array.
+
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
-
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
-        //Check for stack Overflow
-        //Write your code here
-    } 
-  
-    int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
-    } 
-  
-    int peek() 
-    { 
-        //Write your code here
-    } 
-} 
-  
-// Driver code 
-class Main { 
+     
+    //Driver code
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
         s.push(10); 
         s.push(20); 
         s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " popped from stack"); 
+        System.out.print("Elements present in stack :");
+        s.print();
     } 
-}
+    boolean isEmpty() 
+    {
+        return top<0;
+    } 
+
+    Stack() 
+    { 
+        //Initialize your constructor 
+        top=-1;
+    } 
+  
+    boolean push(int x) 
+    { 
+        //Check for stack Overflow
+        if(top>=MAX-1){
+            System.out.println("Stack overflow");
+            return false;
+        }
+        else{
+            a[++top]=x;
+            System.out.println(x+" pushed into stack");
+            return true;
+        }
+    } 
+  
+    int pop() 
+    { 
+        //If empty return 0 and print " Stack Underflow"
+        if(top<0){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        //Write your code here
+        else{
+            int x=a[top--];
+            return x;
+        }
+    } 
+  
+    int peek() 
+    { 
+        //Write your code here
+        if(top<0){
+            System.out.println("Stack Underflow");
+            return 0;
+        }else{
+            int x=a[top];
+            return x;
+        }
+    } 
+    void print(){
+        for(int i = top;i>-1;i--){
+          System.out.print(" "+ a[i]);
+        }
+    }
+} 
+  
