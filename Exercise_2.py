46778540036c332f8f485b22bda597d0ee5ab019
Shrey Patel <shrey1124@gmail.com>
2022-04-03 00:51:40 -0500

    solutions added

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,15 +1,56 @@
+# Time Complexity : all operations O(1).
+# Space Complexity : O(N) where N is total elements pushed
+# Any problem you faced while coding this : no
 
 class Node:
     def __init__(self, data):
        self.data = data
        self.next = None
  
+
 class Stack:
     def __init__(self):
+        self.MAX = 1000
+        self.root = None
+        self.counts = 0
         
+    
     def push(self, data):
+        # check if capacity reached
+        if self.counts == self.MAX:
+            return None
+        if self.root is None: # first element
+            self.root = Node(data)
+        else: # insert from root so that pop operations are O(1)
+            new_node = Node(data)
+            new_node.next = self.root
+            self.root = new_node
+        self.counts += 1
         
+    
     def pop(self):
+        if self.root is None:
+            print("Stack Underflow")
+            return 0
+        node = self.root
+        self.root = self.root.next
+        self.counts -= 1
+        return node.data
+    
+
+    def isEmpty(self):
+        return self.root is None
+    
+
+    def peek(self):
+        if self.root is None:
+            return 0
+        else:
+            return self.root.data
+
+
+
+
         
 a_stack = Stack()
 while True:
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,15 @@
+# Time Complexity : See comments
+# Space Complexity : O(N) where N is total elements pushed
+# Any problem you faced while coding this : no
+
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -12,21 +19,89 @@ class SinglyLinkedList:
         """
         self.head = None
 
-    def append(self, data):
+    def append(self, data): # O(n)
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        if self.head is None:
+            self.head = ListNode(data)
+        else:
+            p = self.head
+            while p.next is not None:
+                p = p.next
+            p.next = ListNode(data)
+
+        return self.head
         
-    def find(self, key):
+    def find(self, key): # O(n)
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        if self.head is None:
+            return None
+        p = self.head
+        while p is not None and p.data != key:
+            p = p.next
+        return p
         
-    def remove(self, key):
+    def remove(self, key): # O(n)
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head is None:
+            return
+        if self.head.data == key:
+            self.head = self.head.next
+            return
+
+        p = self.head
+        while p.next is not None and p.next.data != key:
+            p = p.next
+        
+        p.next = p.next.next
+
+    def print(self): # O(n)
+        p = self.head
+        ll = []
+        while p is not None:
+            ll.append(str(p.data))
+            p = p.next
+        
+        print(" -> ".join(ll))
+
+
+
+
+llist = SinglyLinkedList()
+llist.append(1)
+llist.append(2)
+llist.append(4)
+llist.append(6)
+print("find 4", llist.find(4))
+print("find 14", llist.find(14))
+llist.print()
+
+print("remove 2 ")
+llist.remove(2)
+llist.print()
+
+print("remove 1 ")
+llist.remove(1)
+llist.print()
+
+print("remove 6 ")
+llist.remove(6)
+llist.print()
+
+print("remove 4 ")
+llist.remove(4)
+llist.print()
+
+
+
+
+
