32e636cf2522d2d0543ec0487d5129d101d9b5da
lohitb009 <89905158+lohitYahoo96@users.noreply.github.com>
2022-04-17 01:29:30 +0530

    Exercise 1 and Exercise 2
    Exercise 1 and Exercise 2 completed
    17-April-2022.

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,20 +2,37 @@ class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+        self.stackList = []
+        
      def isEmpty(self):
          
+         # If length of list == 0
+         if len(self.stackList) == 0:
+             return True
+         else:
+             return False
+    
      def push(self, item):
-         
+        # Append the element to the list
+        self.stackList.append(item)
+        return self.peek()
+    
      def pop(self):
-        
-        
+        # Remove the last element from the list
+        # Slice the list
+        self.stackList = self.stackList[0:len(self.stackList)-1]
+        return self.peek
+    
      def peek(self):
-        
+        # Return the stack
+        return self.stackList
+    
      def size(self):
-         
+        return len(self.stackList)
+    
      def show(self):
-         
+        # Return the stack
+        return self.stackList
 
 s = myStack()
 s.push('1')
--- Exercise_2.py
+++ Exercise_2.py
@@ -6,10 +6,61 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.head = None
+        self.tail = None
+        self.length = 0
+         
     def push(self, data):
+        # Create an obj of class node
+        objNewNode = Node(data)
+        
+        # Check if length > 0
+        if self.length == 0:
+            # No element in the linkedlist
+            self.head = objNewNode
+            self.tail = objNewNode
+        
+        else:
+            # Elements already available in the linked-list
+            # 1. pt "next" of tail to "objNewNode"
+            self.tail.next = objNewNode
+            
+            # 2. Update tail to "objNewNode"
+            self.tail = objNewNode
+         
+        # Increase the length of linked-list
+        self.length +=1
         
     def pop(self):
+        
+        if self.length == 0:
+            # No elements in the linked-list
+            return "Empty linked-list"
+        
+        elif self.length == 1:
+            # Only 1 element in the linked-list
+            # head and tail are same
+            self.head = None
+            self.tail = None
+            
+        else:
+            # For other approach
+            
+            # 1. Create ptr currentNode
+            currentNode = self.head
+            
+            # 2. Iterate till currentNode.next != tail
+            while currentNode.next != self.tail:
+                currentNode = currentNode.next
+                continue
+            
+            # 3. Reached currentNode, now update the rfr's
+            self.tail = None
+            currentNode.next = self.tail
+            self.tail = currentNode
+            currentNode = None
+        
+        self.length -= 1
         
 a_stack = Stack()
 while True:
