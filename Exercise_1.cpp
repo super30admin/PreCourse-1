6b5fbc141b85369fd0ea11ffd2f521e84079fddd
Kumar Shubham <kumarshubham@Kumars-MacBook-Pro.local>
2022-04-22 16:56:57 -0500

    Completed Sprint Precourse-1

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h> 
+#include <iostream> 
   
 using namespace std; 
   
@@ -12,34 +12,96 @@ class Stack {
 public: 
     int a[MAX]; // Maximum size of Stack 
   
-    Stack() { //Constructor here } 
+    Stack() { top= -1; } 
     bool push(int x); 
     int pop(); 
     int peek(); 
     bool isEmpty(); 
 }; 
-  
+
+// Time Complexity of push : O(1)
+// Space Complexity of push : O(1)
+
 bool Stack::push(int x) 
 { 
     //Your code here
     //Check Stack overflow as well
+
+    //If top is not pointing to the max then we can push element to the stack 
+    //else there is no space in the stack.
+
+    if (top < (MAX - 1)) {
+
+        a[++top] = x;
+        cout << x << " is pushed to the top of stack.\n";
+        return true;
+        
+    }
+    else {
+        cout << "Stack Overflow occured.";
+        return false;
+    }
 } 
-  
+
+// Time Complexity of pop : O(1)
+// Space Complexity of pop : O(1)
+
 int Stack::pop() 
 { 
     //Your code here
     //Check Stack Underflow as well 
-} 
+
+    //If top is not pointing to the less than 0 then we can pop element from the stack 
+    //else there is no element in the stack.
+
+    if (top >= 0) {
+
+        int temp = a[top--];
+        return temp;
+        
+    }
+    else {
+        cout << "Stack Underflow occured.";
+        return 0;
+    }
+}
+
+// Time Complexity of peek : O(1)
+// Space Complexity of peek : O(1)
+
 int Stack::peek() 
 { 
     //Your code here
     //Check empty condition too
+
+    //If top is not pointing to the less than 0 then we can peek element from the stack 
+    //else there is no element in the stack.
+
+    if (top >= 0) 
+    {
+        return a[top];
+    }
+    else {
+        cout << "Stack is Empty.";
+        return 0;
+    }
 } 
-  
+
+// Time Complexity of isEmpty : O(1)
+// Space Complexity of isEmpty : O(1)
+
 bool Stack::isEmpty() 
 { 
-    //Your code here 
+    //Your code here
+    if (top < 0)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
 } 
+
   
 // Driver program to test above functions 
 int main() 
@@ -49,6 +111,5 @@ int main()
     s.push(20); 
     s.push(30); 
     cout << s.pop() << " Popped from stack\n"; 
-  
-    return 0; 
+    
 } 
--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -15,25 +15,87 @@ StackNode* newNode(int data)
     stackNode->next = NULL; 
     return stackNode; 
 } 
-  
+
+// Time Complexity of isEmpty : O(1)
+// Space Complexity of isEmpty : O(1)
+
 int isEmpty(StackNode* root) 
 { 
     //Your code here 
+
+    // If head is pointing to NULL then list is empty.
+
+    if (root == NULL)
+        return -1;
+    else return 0;
 } 
-  
+
+// Time Complexity of push : O(1)
+// Space Complexity of push : O(1)
+
 void push(StackNode** root, int data) 
 { 
     //Your code here 
+
+    // Create a new node.
+    StackNode* newnode = newNode(data);
+
+    // Point next of newnode points to head. 
+    newnode->next = (*root);
+
+    // Newnode becomes the head
+    (*root) = newnode;
+    cout<<data<<" is pushed into stack.\n";
+    return;
 } 
-  
+
+// Time Complexity of pop : O(1)
+// Space Complexity of pop : O(1)
+
 int pop(StackNode** root) 
 { 
     //Your code here 
+
+    // Create a new node.
+    StackNode* newnode = newNode(0);
+
+    // If root is not null that means stack is not empty.
+    if (root != NULL)
+    {
+        // Create a new node.
+        newnode = (*root);
+
+        (*root) = (*root)->next;
+        int popped_data = newnode->data;
+
+        // Free the space.
+        free(newnode);
+
+        // return the popped element.
+        return popped_data;
+    }
+    else
+    {
+        cout << "Stack Underflow occured.\n" ;
+        return -1;
+    }
 } 
-  
+
+// Time Complexity of peek : O(1)
+// Space Complexity of peek : O(1)
+
 int peek(StackNode* root) 
 { 
     //Your code here 
+    if (isEmpty(root) == -1)
+    {
+        cout<<"empty.\n";
+        return -1;
+    }
+    else
+    {
+        return root->data;
+    }
 } 
   
 int main() 
