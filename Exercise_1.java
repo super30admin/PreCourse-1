e70ab7d2c06a4d327c3ce9eded61953024044c6a
srinidhisunkara@outlook.com <srinidhisunkara@outlook.com>
2022-04-23 09:41:06 +0530

    Completed PreCourse 1 Srinidhi Sunkara_RN34APR2022 Please Review

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,46 +1,68 @@
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
+
+    //Time and space complexity are O(top) and in worst case O(MAX)
+    static final int MAX = 1000;
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        return top==-1;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top=-1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+
+
+        if(top==MAX-1){
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        top=top+1;
+        a[top]=x;
+        return true;
+
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        top=top-1;
+        return a[top+1];
+
     } 
   
     int peek() 
     { 
         //Write your code here
+         return a[top];
     } 
 } 
   
 // Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+class Main {
+    public static void main(String args[])
+    {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+    }
 }
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,7 +1,8 @@
 import java.io.*; 
   
 // Java program to implement 
-// a Singly Linked List 
+// a Singly Linked List
+//Space Complexity O(1) and time complexity is O(N); Where N is the number of nodes.
 public class LinkedList { 
   
     Node head; // head of list 
@@ -17,23 +18,38 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            //Write your code here
+            data=d;
+            next=null;
         } 
     } 
   
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
     { 
-        // Create a new node with given data 
+        // Create a new node with given data
+        Node newNode=new Node(data);
    
         // If the Linked List is empty, 
-        // then make the new node as head 
+        // then make the new node as head
+        if(list.head==null){
+            list.head=newNode;
+
+        }
         
             // Else traverse till the last node 
-            // and insert the new_node there 
+            // and insert the new_node there
+        else{
+            Node temp=list.head;
+            while(temp.next!=null){
+                temp=temp.next;
+            }
+            temp.next=newNode;
+        }
 
             // Insert the new_node at last node 
-        // Return the list by head 
+        // Return the list by head
+        return list;
         
     } 
   
@@ -42,7 +58,13 @@ public class LinkedList {
     {  
         // Traverse through the LinkedList 
    
-            // Print the data at current node 
+            // Print the data at current node
+        Node temp=list.head;
+        while(temp.next!=null){
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+        System.out.println(temp.data);
        
             // Go to next node 
     } 
