b01d8e709b856aeccf6caeb87f4f48feed5d2b25
reetesh13 <reeteshzope@gmail.com>
2022-04-23 01:50:23 -0400

    Completed Sprint PreCourse-1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,71 @@
+from operator import le
+
+
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+  # Please read sample.java file before starting.
+  # Kindly include Time and Space complexity at top of each file
+    def __init__(self):
+        self.stk = []
+
+    """
+        Check if stack is empty.
+        Takes O(1) time.
+    """
+
+    def isEmpty(self):
+        if len(self.stk) == 0:
+            return True
+        else:
+            return False
+
+    """
+        Push element to stack.
+        Takes O(1) time.
+    """
+
+    def push(self, item):
+        self.stk.append(item)
+
+    """
+        Push element from stack.
+        Takes O(1) time.
+    """
+
+    def pop(self):
+        self.stk.pop()
+
+    """
+        Peek element from stack.
+        Takes O(1) time.
+    """
+
+    def peek(self):
+        return self.stk[len(self.stk) - 1]
+
+    """
+        Check size of the stack
+        Takes O(1) time.
+    """
+
+    def size(self):
+        return len(self.stk)
+
+    """
+        Show all elements in the stack
+        Takes O(1) time.
+    """
+
+    def show(self):
+        return self.stk
+
 
 s = myStack()
+print(s.isEmpty())
 s.push('1')
+s.push('3')
 s.push('2')
 print(s.pop())
 print(s.show())
+print(s.size())
+print(s.peek())
+print(s.isEmpty())
--- Exercise_3.py
+++ Exercise_3.py
@@ -2,31 +2,89 @@ class ListNode:
     """
     A node in a singly-linked list.
     """
+
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = None
+
+
 class SinglyLinkedList:
     def __init__(self):
         """
         Create a new singly-linked list.
         Takes O(1) time.
         """
-        self.head = None
+        self.head = ListNode()
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        curr = self.head
+
+        while curr:
+            prev = curr
+            curr = curr.next
+        prev.next = ListNode(data)
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        if self.head.next == None:
+            return None
+        curr = self.head.next
+
+        while curr:
+            if curr.data == key:
+                return curr.data
+            else:
+                curr = curr.next
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head.next == None:
+            return None
+        curr = self.head.next
+        prev = self.head
+        while curr:
+            if curr.data == key:
+                prev.next = prev.next.next
+                return curr.data
+            else:
+                prev = curr
+                curr = curr.next
+
+        return None
+
+
+a_list = SinglyLinkedList()
+while True:
+    print('append <value>')
+    print('find <value>')
+    print('remove <value>')
+    do = input('What would you like to do? ').split()
+    operation = do[0].strip().lower()
+    if operation == 'append':
+        a_list.append(int(do[1]))
+    elif operation == 'remove':
+        removed = a_list.remove(int(do[1]))
+        if removed is None:
+            print('List is empty or element not found')
+        else:
+            print('Removed value: ', int(removed))
+    elif operation == 'find':
+        found = a_list.find(int(do[1]))
+        if found is None:
+            print('Not found')
+        else:
+            print('Found value: ', int(found))
+    elif operation == 'quit':
+        break
