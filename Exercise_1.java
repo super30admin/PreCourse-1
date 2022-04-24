c82d7f743df87e669d3cb49554a26dd515f3523b
Abhishek Chaudhary <abhishek.chaudhary.2812@gmail.com>
2022-04-24 17:36:57 -0500

    Completed Precourse 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -8,28 +8,53 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        return a.length == 0;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+
+        if(top < MAX)
+        {
+            top++;
+            a[top] = x;
+            return true;
+        }
+        else 
+        {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top < 0)
+        {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else
+        {
+            return a[top--];
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        return a[top];
     } 
 } 
   
--- Exercise_2.java
+++ Exercise_2.java
@@ -1,14 +1,18 @@
-public class StackAsLinkedList { 
+public class Exercise_2 
+{ 
   
     StackNode root; 
   
-    static class StackNode { 
+    static class StackNode 
+    { 
         int data; 
         StackNode next; 
   
         StackNode(int data) 
         { 
             //Constructor here 
+            this.data = data;
+            this.next = null;
         } 
     } 
     
@@ -16,11 +20,15 @@ public class StackAsLinkedList {
     public boolean isEmpty() 
     { 
         //Write your code here for the condition if stack is empty. 
+        return this.root == null;
     } 
   
     public void push(int data) 
     { 
         //Write code to push data to the stack. 
+        StackNode newnode = new StackNode(data);
+        newnode.next = this.root;
+        this.root = newnode;
     } 
   
     public int pop() 
@@ -28,18 +36,29 @@ public class StackAsLinkedList {
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
+        if(this.root == null)
+        {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+
+        int value = this.root.data;
+        this.root = this.root.next;
+
+        return value;
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+        return root.data;
     } 
   
 	//Driver code
     public static void main(String[] args) 
     { 
   
-        StackAsLinkedList sll = new StackAsLinkedList(); 
+        Exercise_2 sll = new Exercise_2(); 
   
         sll.push(10); 
         sll.push(20); 
