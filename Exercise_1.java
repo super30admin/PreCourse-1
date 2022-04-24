bd0ac2de708b0a90925b12ebf2535294b7d4f4ad
Rutuja Mandlik <mandlik.r@northeastern.edu>
2022-04-23 23:06:10 -0400

    Precourse-1 Completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,116 +1,72 @@
-// Time Complexity : O(1) Reason: because we do not run any for loop 
-// Space Complexity : O(1)  Reason: we can push() and pop() through the array everytime one at a time
-// Did this code successfully run on Leetcode : The code ran successfully in the Visual Code Studio but on leetcode it is unable to detect print()
-// Any problem you faced while coding this : Starting with the basics, i did not know what was Stack and how to write all methods for stack,
-//  but i took help from google and understood Stacks and started implementing Stacks using java where i realized that i had to implement it using Array, so i studied what are Arrays and how does it work.
-// I faced many errors but then at the end i was able to fix all the problems, but i am not quite sure why there are no problems in VS while i am getting some issues on Leetcode.
-
-
+// Time Complexity :  O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+ 
 
 // Your code here along with comments explaining your approach
 
+class Stack {
+	// Please read sample.java file before starting.
+	// Kindly include Time and Space complexity at top of each file
+	static final int MAX = 1000;
+	int top;
+	int a[] = new int[MAX]; // Maximum size of Stack
 
+	boolean isEmpty() { //TC: O(1)
+		// Write your code here
 
+		return top < 0;
 
-class Stack { 
+	}
 
-  //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-        return (top < 0);
-    } 
+	Stack() {
+		// Initialize your constructor
+		top = -1;
+	}
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-        top = -1;
+	boolean push(int x) { //TC: O(1)
+		// Check for stack Overflow
+		// Write your code here
+		if (top > MAX - 1) {
+			System.out.println("Stack Overflow");
+			return false;
+		} else {
+			top++;
+			a[top] = x;
+			return true;
+		}
 
-    } 
-  
-    boolean push(int x) 
-    { 
-        //Check for stack Overflow
-        
-        if(top >= (MAX - 1)) {
+	}
 
-            System.out.println("Stack is full");
-            return false;
+	int pop() { //TC: O(1)
+		// If empty return 0 and print " Stack Underflow"
+		// Write your code here
+		if (top < 0) {
+			System.out.println("Stack Underflow");
+			return 0;
 
-        }
-         
-        // Write your code here
+		} else {
 
-        else{
-            a[++top] = x;
-            return true;
-        }
-        
+			return a[top--];
 
-    }
-    
+		}
 
-  
-    int pop() 
-    { 
-        //  If empty return 0 and print " Stack Underflow"
-        if(top < 0)
-        {
-            System.out.println("Stack Underflow");
-            return 0;
+	}
 
-        }
-        //Write your code here
-        else {
-            int x = a[top--];
-            return x;
-        }
-        
-     
-        
-    } 
-  
-    int peek() 
-    { 
-        //Write your code here
-        if(top < 0)
-        {
-            System.out.println("Stack is empty");
-            return 0;
+	int peek() { //TC: O(1)
+		// Write your code here
+		return a[top];
+	}
+}
 
-        }
-        //Write your code here
-        else {
-            int x = a[top];
-            return x;
-        }
-   
-    } 
-    void print(){
-        for(int i = top;i>-1;i--){
-          System.out.print(" "+ a[i]);
-        }
-      }
-} 
-  
 // Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-
-       
-      System.out.println(s.pop() + " Popped from stack"); 
-      s.print();
-      
-
-        
-    } 
+class Main {
+	public static void main(String args[]) {
+		Stack s = new Stack();
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		System.out.println(s.pop() + " Popped from stack");
+	}
 }
--- Exercise_2.java
+++ Exercise_2.java
@@ -1,40 +1,21 @@
-//The time complexity for all push(), pop(), and peek() operations is:
-// O(1) as we are not performing any kind of traversal over the list. 
-//We perform all the operations through the current pointer only.
+//TC: O(1) 
+//SC: O(1)
 
-//Space complexity:O(1), which means that the complexity is constant. Every time we push an element, 
-//we'll perform exactly the same number of operations, add element, and change two pointers.
-
-// Did this code successfully run on Leetcode: I am getting one error while initializing constructor at StackNode(int data) 
-// Any problem you faced while coding this : I had to first understan how to create a node class and how linkedlist works. Then initially 
-// i faced syntactical errors and also struggled with writing the code for push()
-
-
-import static java.lang.System.exit;
-
-//Create Stack using Linked list
-public class StackAsLinkedList { 
+class StackAsLinkedList { 
   
-    
     StackNode root; 
-  
-    //Linked list node
+
+   
     static class StackNode { 
-        int data;  //integer data
-        StackNode next;  //reference variable node type
-    
-        //Create global top reference variable global
-        StackNode top; 
+        int data; 
+        StackNode next; 
   
-        StackNode(int data)
-        
+        StackNode(int data) 
         { 
-            
             //Constructor here 
-           d = data;
-           next = null;
-            
-           
+        	
+        	this.data = data;
+        	
         } 
     } 
     
@@ -42,81 +23,57 @@ public class StackAsLinkedList {
     public boolean isEmpty() 
     { 
         //Write your code here for the condition if stack is empty. 
-        return top == null;
+    	
+    		return (root == null);
+    	
+    	
     } 
   
-    public void push(int d) 
+    public void push(int data) 
     { 
+    	StackNode newnode = new StackNode(data);
         //Write code to push data to the stack. 
-        //Create an empty node
-        StackNode temp = new StackNode();
-        
-        //check is stack is full
-        if(top == null){
-            System.out.println("Stack overflow");
-            return;
-        }
-        
-        //initialize data into temp data file
-        
-        temp.data = d;
-        
-        //put top reference into temp link
-        
-        temp.next = top;
-        
-        //update top reference
-        top = temp;
-            
+    	if(root == null) {
+    		root = newnode;
+        	
+    	} else {
+    		StackNode temp = root;
+    		root = newnode;
+    		newnode.next = temp;
+        	  	
+    	}
+    	
     } 
   
     public int pop() 
     { 	
 	//If Stack Empty Return 0 and print "Stack Underflow"
-        if(top == null){
-            System.out.println("Stack Underflow");
-            return 0;
-        }
-        
-           // update the top pointer to point to the next node
-        top = (top).next;
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+    	int poppedEl = Integer.MIN_VALUE;
+    	if(root == null) {
+    		System.out.println("Stack Underflow");
+    		
+    	} else {
+    		poppedEl = root.data;
+    		root = root.next;
+    		
+    	}
+    	return poppedEl;
+    	
     } 
   
     public int peek() 
     { 
-        //Write code to just return the topmost element without removing it.
-        //check stack is underflow
+    	
+    	if(root == null) {
+    		return Integer.MIN_VALUE;
+    	} else {
+    		return root.data;
+    	}
+    	
         
-        if(!isEmpty()){
-            return top.d;
-        }else{
-            System.out.println("Stack underflow");
-            return -1;
-        
-        }
+			
     } 
-    
-    public void display(){
-        
-        if (top == null) {
-            System.out.printf("\nStack Underflow");
-            exit(1);
-        }
-        else {
-            StackNode temp = top;
-            while (temp != null) {
- 
-                // print node data
-                System.out.printf("%d->", temp.data);
- 
-                // assign temp link to temp
-                temp = temp.next;
-            }
-        }
-    }
-
+  
 	//Driver code
     public static void main(String[] args) 
     { 
@@ -126,13 +83,9 @@ public class StackAsLinkedList {
         sll.push(10); 
         sll.push(20); 
         sll.push(30); 
-        
-        sll.display();
-        sll.pop();
   
         System.out.println(sll.pop() + " popped from stack"); 
   
         System.out.println("Top element is " + sll.peek()); 
     } 
-
 }
