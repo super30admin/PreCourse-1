104b4ade0b1a6801d7e315e185c90860edaf0d09
Lakshmanan Palaniappan <lpalaniappan@twitter.com>
2022-04-25 19:00:36 -0700

    Completed Sprint PreCourse1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,19 @@
-public class StackAsLinkedList { 
+/**
+ *  Time Complexity
+ *
+ *      isEmpty() - O(1)
+ *
+ *      push(x) - O(1)
+ *
+ *      pop() - O(1)
+ *
+ *      peek() - O(1)
+ *
+ *  Space Complexity - O(N) where N is the number of elements inserted into the stack
+ */
+
+
+public class StackAsLinkedList {
   
     StackNode root; 
   
@@ -8,31 +23,49 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
-            //Constructor here 
+            this.data = data;
+            this.next = null;
         } 
     } 
     
 	
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+        return root == null;
     } 
   
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
+        StackNode newNode = new StackNode(data);
+
+        if(isEmpty()) {
+            root = newNode;
+        } else {
+            newNode.next = root;
+            root = newNode;
+        }
     } 
   
     public int pop() 
     { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+	      if(isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            StackNode first = root;
+            root = root.next;
+            return first.data;
+        }
     } 
   
     public int peek() 
-    { 
-        //Write code to just return the topmost element without removing it.
+    {
+        if(isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            return root.data;
+        }
     } 
   
 	//Driver code
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,4 +1,11 @@
-import java.io.*; 
+/**
+ * printList(list) - O(N) where N is the number of the elements in the linked list
+ *
+ * insert(list, data) - O(N) where N is the number of elements in the linked list
+ *
+ */
+
+import java.io.*;
   
 // Java program to implement 
 // a Singly Linked List 
@@ -17,34 +24,40 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            this.data = d;
+            this.next = null;
         } 
     } 
   
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
     { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
+        Node newNode = new Node(data);
 
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
+        if(list.head == null ) {
+            list.head = newNode;
+        } else {
+
+            Node runner = list.head;
+
+            while(runner.next != null)
+                runner = runner.next;
+
+            runner.next = newNode;
+        }
+
+        return list.head;
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+        Node runner = list.head;
+        while (runner != null ) {
+            Sytsem.out.println(runner.val" --> ")
+            runner = runner.next;
+        }
+        
     } 
    
     // Driver code 
