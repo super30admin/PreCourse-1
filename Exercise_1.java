88b28dfe442a42aa291450b94c75e13ef7738dc9
aashutoshtaikar <aashutoshtaikar@gmail.com>
2022-04-26 17:42:29 -0400

    completed precourse 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -4,32 +4,64 @@ class Stack {
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
-  
+    
+    /**
+     * Time complexity: O(1)  
+     * Space complexity: O(1)
+     */
     boolean isEmpty() 
     { 
-        //Write your code here 
+        return top < 0;
     } 
 
+    /**
+     * Time complexity: O(1)  
+     * Space complexity: O(1)
+     */
     Stack() 
     { 
         //Initialize your constructor 
+        this.top = -1;
     } 
-  
+    
+    /**
+     * Time complexity: O(1)  
+     * Space complexity: O(1)
+     * @param x to be pushed
+     * @return true if the push is successful
+     */
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if (top < MAX) {
+            a[++top] = x;
+            return true;
+        }
+        return false;
     } 
   
+    /**
+     * Time complexity: O(1)  
+     * Space complexity: O(1)
+     * @return -1 If empty, 
+     */
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if (top < 0) {
+            System.out.println(" Stack Underflow");
+            return -1;
+        }
+        int poppedElement = a[top];
+        a[top--] = 0;
+        return poppedElement;
     } 
   
     int peek() 
     { 
-        //Write your code here
+        if(top < 0) return -1;
+        return a[top];
     } 
 } 
   
@@ -38,9 +70,11 @@ class Main {
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
+        System.out.println(s.isEmpty());
         s.push(10); 
         s.push(20); 
         s.push(30); 
         System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.isEmpty());
     } 
 }
--- Exercise_3.java
+++ Exercise_3.java
@@ -2,7 +2,7 @@ import java.io.*;
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList { 
   
     Node head; // head of list 
   
@@ -14,37 +14,54 @@ public class LinkedList {
         int data; 
         Node next; 
   
-        // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            this.data = d;
         } 
     } 
   
-    // Method to insert a new node 
+    /**
+     * Method to insert a new node
+     * Time complexity: O(n)  
+     * Space complexity: O(n)
+     */
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
-   
+        Node newNode = new Node(data);
         // If the Linked List is empty, 
         // then make the new node as head 
-        
+        if(list.head == null) list.head = newNode; 
             // Else traverse till the last node 
             // and insert the new_node there 
-
-            // Insert the new_node at last node 
-        // Return the list by head 
+        else {
+            Node current = list.head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = newNode; // Insert the new_node at last node 
+        }
+        return list;    // Return the list by head 
         
     } 
   
-    // Method to print the LinkedList. 
+    /**
+     * Method to print the LinkedList.
+     * Time complexity: O(n)  
+     * Space complexity: O(n)
+     */
     public static void printList(LinkedList list) 
     {  
-        // Traverse through the LinkedList 
-   
+        // Traverse through the LinkedList
+        Node current = list.head; 
+        System.out.print("HEAD -> ");
+        while(current!=null && current.next!= null){
             // Print the data at current node 
-       
-            // Go to next node 
+            System.out.print(current.data);
+            System.out.print(" -> ");
+            current = current.next; // Go to next node 
+        }
+        System.out.println("null");            
     } 
    
     // Driver code 
