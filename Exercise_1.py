23c4cab97f763f4453d7de67c0f82b6579ea09c1
spatil29bing <86124914+spatil29bing@users.noreply.github.com>
2022-04-27 22:49:17 -0400

    update exercise 1 solution

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,38 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
+    def __init__(self):
+        self.container = []
          
-     def isEmpty(self):
+    def isEmpty(self):
+        return len(self.container) == 0
          
-     def push(self, item):
-         
-     def pop(self):
+    def push(self, item):
+        self.container.append(item)
         
         
-     def peek(self):
-        
-     def size(self):
          
-     def show(self):
+    def pop(self):
+           return self.container.pop()
+            
+    def peek(self):
+        return self.container[-1]
+        
+    def size(self):
+        return len(self.container)
+         
+    def show(self):
+        return self.container
          
 
 s = myStack()
+print(s.isEmpty())
 s.push('1')
 s.push('2')
+s.push('ddef')
+s.push('[[]]')
 print(s.pop())
 print(s.show())
+print(s.peek())
+print(s.isEmpty())
+print(s.size())
