71e2f53a3f6098cdb0183368e65109d1edb778e6
username-iron <Sanjay@MacBook-Pro.local>
2022-04-30 17:30:09 -0700

    Updating exercise 1 solution

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,6 +1,8 @@
 class Stack { 
     //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+    //Kindly include Time and Space complexity at top of each file
+    // Time complexity is O(1)
+    // Space complexity is O(n)
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX];  // Maximum size of Stack 
