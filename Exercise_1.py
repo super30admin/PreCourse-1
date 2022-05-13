f0b0452858c2d2d740342b2dd7d5052293fb992e
Mukta Parab <78190285+mukta2695@users.noreply.github.com>
2022-05-13 01:42:50 -0400

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,9 +1,11 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-  
+
   #Time complexity: O(1)
   #Space complexity: O(1)
+  #Did this code successfully run on Leetcode : Yes
+  #Any problem you faced while coding this : No 
      def __init__(self):
          self.stack=[]
          
