887e767ca154aa135f98edaa0ea48f9dde768d26
poojitha bijjam <poojitha1589@gmail.com>
2022-05-17 07:26:20 -0500

    Exercise 2

--- Exercise_2.py
+++ Exercise_2.py
@@ -6,10 +6,21 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
-    def push(self, data):
-        
-    def pop(self):
+        self.top = None
+    def push(self, data):#Insert at the begining
+        temp = Node(data)
+        temp.next = self.top
+        self.top = temp
+        print(self.top.data)
+    def pop(self):# Delete at the begining
+        if self.top == None:
+            return None
+        else:
+            popped = self.top.data
+        temp = self.top
+        self.top = temp.next
+        temp.next = None
+        return popped
         
 a_stack = Stack()
 while True:
