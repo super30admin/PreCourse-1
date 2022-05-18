fa38269e320db10a5d4eea2e85b88a0c4f0478d1
Dixika Grewal <59345244+dixg@users.noreply.github.com>
2022-05-18 02:28:21 -0700

    Question 1 and 2 - completed

--- Exercise_2.py
+++ Exercise_2.py
@@ -6,12 +6,31 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head=None
+
         
     def push(self, data):
-        
+        if self.head==None:
+            self.head=Node(data)
+        else:
+            print("before", self.head.data) 
+            newnode=Node(data)
+            newnode.next= self.head
+            self.head=newnode            
+            print("after", self.head.data) 
+    
     def pop(self):
+        if self.head==None:
+            return None
+        pop_node_data=self.head.data
+        self.head=self.head.next
+        self.head.next=None
+        print("head after popping is ", self.head.data)
+        return pop_node_data
         
 a_stack = Stack()
+
+
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
