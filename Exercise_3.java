1daa1990929360c51d3d00c9be91bd32656599f8
Khushil-Modi <modikhushil@gmail.com>
2022-05-20 13:38:40 -0700

    Update Exercise_3.java

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,5 +1,8 @@
 import java.io.*; 
-  
+
+//Time Complexity: O(1)
+//Space complexity: O(n) 
+
 // Java program to implement 
 // a Singly Linked List 
 public class LinkedList { 
@@ -18,6 +21,8 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+			this.data=d;
+            this.next=null;
         } 
     } 
   
@@ -25,7 +30,19 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
-   
+		 Node n=new Node(data);
+        n.next=null;
+        if(list.head==null){
+            list.head=n;
+        }
+        else{
+            Node t=list.head;
+            while(t.next!=null){
+                t=t.next;
+            }
+            t.next=n;
+        }
+        return list
         // If the Linked List is empty, 
         // then make the new node as head 
         
@@ -34,6 +51,12 @@ public class LinkedList {
 
             // Insert the new_node at last node 
         // Return the list by head 
+		
+		Node t=list.head;
+        while(t!=null){
+            System.out.println(t.data);
+            t=t.next;
+        }
         
     } 
   
