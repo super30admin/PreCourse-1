a7d5f42fe87f9143d53d6fef5719a0a949cdaf90
Gsubhashreddy <gsubhashreddy1@gmail.com>
2022-05-21 12:37:28 -0700

    Completed Pre-course1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,46 +1,80 @@
-class Stack { 
-    //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+import java.io.*;
+import java.util.*;
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
+//Time Complexity O(1)
+// Space Complexity O(Max_Value_Of_Integer)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this : No
+
+class Stack {
+    //Please read sample.java file before starting.
+    //Kindly include Time and Space complexity at top of each file
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack
+
+    boolean isEmpty()
+    {
+        //Write your code here
+        if(top==-1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    Stack()
+    {
+        this.top=-1;
+        this.a=a;
+        //Initialize your constructor
+    }
+
+    boolean push(int x)
+    {
+        if(top==MAX){
+            return false;
+
+        }
+        this.top+=1;
+        this.a[this.top]=x;
+        return true;
         //Check for stack Overflow
         //Write your code here
-    } 
-  
-    int pop() 
-    { 
+    }
+
+    int pop()
+    {
+        if(isEmpty())
+        {
+            System.out.println(" Stack Under Flow");
+            return 0;
+        }
+        else {
+            int temp=this.a[this.top];
+            this.top-=1;
+            return temp;
+
+        }
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
-    } 
-  
-    int peek() 
-    { 
+    }
+
+    int peek()
+    {
+
+        return this.a[this.top];
         //Write your code here
-    } 
-} 
-  
-// Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+    }
+}
+class Main {
+    public static void main(String args[])
+    {
+        Stack s = new Stack();
+        Stack s1 = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+    }
 }
--- Exercise_2.java
+++ Exercise_2.java
@@ -1,52 +1,80 @@
-public class StackAsLinkedList { 
-  
-    StackNode root; 
-  
-    static class StackNode { 
-        int data; 
-        StackNode next; 
-  
-        StackNode(int data) 
-        { 
-            //Constructor here 
-        } 
-    } 
-    
-	
-    public boolean isEmpty() 
-    { 
-        //Write your code here for the condition if stack is empty. 
-    } 
-  
-    public void push(int data) 
-    { 
-        //Write code to push data to the stack. 
-    } 
-  
-    public int pop() 
-    { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
-    } 
-  
-    public int peek() 
-    { 
-        //Write code to just return the topmost element without removing it.
-    } 
-  
-	//Driver code
-    public static void main(String[] args) 
-    { 
-  
-        StackAsLinkedList sll = new StackAsLinkedList(); 
-  
-        sll.push(10); 
-        sll.push(20); 
-        sll.push(30); 
-  
-        System.out.println(sll.pop() + " popped from stack"); 
-  
-        System.out.println("Top element is " + sll.peek()); 
-    } 
-} 
+import java.io.*;
+import java.util.*;
+
+//Time Complexity O(1)
+// Space Complexity O(Max_Value_Of_Integer)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this : No
+
+class Stack {
+    //Please read sample.java file before starting.
+    //Kindly include Time and Space complexity at top of each file
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack
+
+    boolean isEmpty()
+    {
+        //Write your code here
+        if(top==-1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    Stack()
+    {
+        this.top=-1;
+        this.a=a;
+        //Initialize your constructor
+    }
+
+    boolean push(int x)
+    {
+        if(top==MAX){
+            return false;
+
+        }
+        this.top+=1;
+        this.a[this.top]=x;
+        return true;
+        //Check for stack Overflow
+        //Write your code here
+    }
+
+    int pop()
+    {
+        if(isEmpty())
+        {
+            System.out.println(" Stack Under Flow");
+            return 0;
+        }
+        else {
+            int temp=this.a[this.top];
+            this.top-=1;
+            return temp;
+
+        }
+        //If empty return 0 and print " Stack Underflow"
+        //Write your code here
+    }
+
+    int peek()
+    {
+
+        return this.a[this.top];
+        //Write your code here
+    }
+}
+class Main {
+    public static void main(String args[])
+    {
+        Stack s = new Stack();
+        Stack s1 = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+    }
+}
