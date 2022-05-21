e6f7d39a2497be86cd0bd795daa68d3ea180922c
Ruthwik12 <ratnaruthwikreddy@gmail.com>
2022-05-21 14:07:21 -0700

    Completed PreCourse 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,19 +2,23 @@ class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+       self.l = list()
          
      def isEmpty(self):
-         
+         return len(self.l) == 0
      def push(self, item):
+       self.l.append(item)
          
      def pop(self):
-        
+        return self.l.pop()
         
      def peek(self):
-        
+        return self.l[-1]
      def size(self):
+       return len(self.l)
          
      def show(self):
+       return self.l
          
 
 s = myStack()
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,16 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        temp = ListNode(data)
+        if(not self.head):
+            self.head = temp
+        else:
+            t = self.head
+            while(t.next):
+                t = t.next
+            t.next = temp
+
+
         
     def find(self, key):
         """
@@ -24,9 +36,52 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        t = self.head
+        while(t):
+            if(t.data == key):
+                return key
+            else:
+                t = t.next
+        return None
+    
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        prev = ListNode(0)
+        prev.next = self.head
+        curr = self.head
+        nxt = curr.next
+
+        while(nxt):
+            if(key == curr.data):
+                prev.next = nxt
+                curr.next =  None
+                return self.head
+            else:
+                prev = curr
+                curr = nxt
+                nxt = nxt.next
+        if(key == curr.data):
+                prev.next = nxt
+                curr.next =  None
+                return self.head
+        else:
+            prev = curr
+            curr = nxt
+            nxt = nxt.next
+
+l =   SinglyLinkedList()
+l.append(10)
+l.append(20)
+l.append(30)
+print(l.find(10))
+k = l.remove(20)
+print(k)
+while(k):
+    print(k.data)
+    k = k.next
+
+
+
