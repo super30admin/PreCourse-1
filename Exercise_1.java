5c0c90078a0c87bf6e5d86fe9aec469c62b0ad06
deepakn1717 <deepakn1717@gmail.com>
2022-05-20 23:27:26 -0500

    more drive code logic added

--- Exercise_1.java
+++ Exercise_1.java
@@ -56,11 +56,12 @@ class Stack {
 class Main {
     public static void main(String args[]) {
         Stack s = new Stack();
-        s.peek();
+        System.out.println(s.peek() + " peek value from stack"); 
         s.push(10);
         s.push(20);
         s.push(30);
-        s.peek();
+        System.out.println(s.peek() + " peek value from stack"); 
         System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.peek() + " peek value from stack"); 
     }
 }
