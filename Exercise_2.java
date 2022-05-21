b73d1860e65580a68739fd36ffd1102e61ef54c7
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-05-21 11:07:58 -0400

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,45 +1,77 @@
-public class StackAsLinkedList { 
+class Node
+{
+    int data;  
+    Node next;      
+}
   
-    StackNode root; 
+class StackNode 
+{ 
+        private Node top;
+        private int count;
   
-    static class StackNode { 
-        int data; 
-        StackNode next; 
-  
-        StackNode(int data) 
+        StackNode() 
         { 
-            //Constructor here 
+            this.top=null;
+            this.count=0;
         } 
-    } 
     
 	
     public boolean isEmpty() 
     { 
+        return top==null;
         //Write your code here for the condition if stack is empty. 
     } 
   
-    public void push(int data) 
+    public void push(int x) 
     { 
+        Node node = new Node();
+        if (node == null)
+        {
+            System.out.println("Stack Overflow");
+            return;
+        }
         //Write code to push data to the stack. 
+        node.data = x;
+        node.next = top;
+        top = node;
+        this.count += 1;
     } 
   
     public int pop() 
-    { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
+    {
+        
+	    //If Stack Empty Return 0 and print "Stack Underflow"
+        if (isEmpty())
+        {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
         //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+        int top = peek();
+        this.count -= 1;
+        this.top = (this.top).next;
+        //Also return the popped element 
+        return top;
     } 
   
     public int peek() 
-    { 
-        //Write code to just return the topmost element without removing it.
-    } 
+    {
+        if (isEmpty()) 
+        {
+            System.out.println("The stack is empty");
+            return 0;
+        }
+        return top.data;
+    }
+}
+public class Main
+{
   
 	//Driver code
     public static void main(String[] args) 
     { 
   
-        StackAsLinkedList sll = new StackAsLinkedList(); 
+        StackNode sll = new StackNode(); 
   
         sll.push(10); 
         sll.push(20); 
