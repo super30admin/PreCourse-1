b3729995133edd9abf9768fef8794e3eb46f2297
pasarkarg <pasarkar.g@northeastern.edu>
2022-05-21 20:02:22 -0400

    Wrote code in java for precourse1-Gauri Pasarkar

--- LinkedList.java
+++ LinkedList.java
@@ -18,6 +18,8 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            data = d;
+            next = null;
         } 
     } 
   
@@ -34,6 +36,43 @@ public class LinkedList {
 
             // Insert the new_node at last node 
         // Return the list by head 
+        Node new_node = new Node(data);
+
+new_node.next = null;
+
+// If the Linked List is empty,
+
+// then make the new node as head
+
+if (list.head == null) {
+
+list.head = new_node;
+
+}
+
+else {
+
+// Else traverse till the last node
+
+// and insert the new_node there
+
+Node last = list.head;
+
+while (last.next != null) {
+
+last = last.next;
+
+}
+
+// Insert the new_node at last node
+
+last.next = new_node;
+
+}
+
+// Return the list by head
+
+return list;
         
     } 
   
@@ -45,6 +84,23 @@ public class LinkedList {
             // Print the data at current node 
        
             // Go to next node 
+            Node currNode = list.head;
+
+System.out.print("LinkedList: ");
+
+// Traverse through the LinkedList
+
+while (currNode != null) {
+
+// Print the data at current node
+
+System.out.print(currNode.data + " ");
+
+// Go to next node
+
+currNode = currNode.next;
+
+}
     } 
    
     // Driver code 
--- Sample.java
+++ Sample.java
@@ -5,3 +5,23 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+
+// Excercise_1: 
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+
+
+// Excercise_2 :
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+
+
+// Excercise_3 :
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+
