f62a2dbf7c8d76c5b1435da6e4a76029130c956a
Shivani31996 <shivani.chavan96@gmail.com>
2022-05-22 16:52:21 -0400

    Pre-course 1 completed

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,11 @@
-import java.io.*; 
-  
+import java.io.*;
+
+  //Time complexity: O(1) for insert operation at head, O(n) for insert operation at tail and O(n) printList operation
+  //Space complexity: O(n) as the singly linked list grows
+
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList {
   
     Node head; // head of list 
   
@@ -18,6 +21,7 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            this.data = d;
         } 
     } 
   
@@ -25,26 +29,47 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
+        Node newNode = new Node(data);
    
         // If the Linked List is empty, 
         // then make the new node as head 
+        if(list.head == null)
+        {
+            list.head = newNode;
+            return list;
+        }
         
             // Else traverse till the last node 
             // and insert the new_node there 
+            
+            Node temp = list.head;
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+
+            temp.next = newNode;
 
             // Insert the new_node at last node 
         // Return the list by head 
+        return list;
         
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+        // Traverse through the LinkedList
+        // Print the data at current node        
+        // Go to next node 
+
+        Node temp = list.head;
+
+        while(temp.next != null)
+        {
+            System.out.println(temp.data);
+            temp = temp.next;
+        } 
     } 
    
     // Driver code 
