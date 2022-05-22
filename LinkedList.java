788108fae6653c7cff185fe94092e221e4b35ce3
saiteja akula <saiteja.akula@globalrelay.net>
2022-05-21 17:26:55 -0700

    Precourse1

--- LinkedList.java
+++ LinkedList.java
@@ -18,6 +18,9 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            this.data=d;
+            this.next=null;
+
         } 
     } 
   
@@ -25,7 +28,20 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
-   
+        
+            Node node= new Node(data);
+            if(list.head==null)list.head=node;
+            else{
+                Node current=list.head;
+                while(current.next!=null){
+                    current=current.next;
+                }
+                current.next=node;
+                node.next=null;
+            }
+            return list;
+
+            
         // If the Linked List is empty, 
         // then make the new node as head 
         
@@ -34,6 +50,7 @@ public class LinkedList {
 
             // Insert the new_node at last node 
         // Return the list by head 
+        
         
     } 
   
@@ -45,6 +62,15 @@ public class LinkedList {
             // Print the data at current node 
        
             // Go to next node 
+            Node current=list.head;
+            while(current.next!=null)
+            {
+                System.out.println(current.data);
+                current=current.next;
+
+            }
+            System.out.println(current.data);
+
     } 
    
     // Driver code 
