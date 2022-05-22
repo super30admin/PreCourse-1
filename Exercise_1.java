b358e6d6e2d50f18c9650ffb1013bb75aa3aad01
Rushi Sharma <rushisharma64@gmail.com>
2022-05-21 18:12:46 -0700

    Solutions

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,8 @@
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
@@ -7,31 +12,69 @@ class Stack {
   
     boolean isEmpty() 
     { 
+        if (top == -1)
+        {
+            return true;
+        } 
+        else
+            return false;
         //Write your code here 
     } 
 
     Stack() 
     { 
+        top = -1;
         //Initialize your constructor 
     } 
   
     boolean push(int x) 
     { 
+        if (top == MAX){
+
+            System.out.println("Stack Overflow");
+            return false;
+        }
+
+        else {
+            top++;
+            a[top] = x;
+            return true;
+        }
         //Check for stack Overflow
         //Write your code here
     } 
   
     int pop() 
     { 
+        if (isEmpty())
+        {
+            System.out.println("Stack Underflow");
+            return 0;
+            
+        }
+        else 
+        {
+            return a[top--];
+        }
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
     } 
   
     int peek() 
-    { 
+    {
+
+        if (isEmpty())   
+        {  
+            System.out.println("Empty Stack");  
+            return 0; 
+        }  
+        else  
+        {  
+            return a[top];  
+        }  
         //Write your code here
     } 
-} 
+}
   
 // Driver code 
 class Main { 
@@ -41,6 +84,6 @@ class Main {
         s.push(10); 
         s.push(20); 
         s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack");
     } 
 }
