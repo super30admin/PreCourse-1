3584b87b91202ae497917c0bf9f47f32e15a34c7
Thirunaa <thirunaavukkarasu.thiru@gmail.com>
2022-05-22 00:31:59 -0400

    Test commit

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,22 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
-
+  def __init__(self):
+      pass
+  def isEmpty(self):
+      pass
+  def push(self, item):
+      pass
+  def pop(self):
+      pass
+      
+  def peek(self):
+      pass
+  def size(self):
+      pass
+  def show(self):
+      pass
+#Test line
 s = myStack()
 s.push('1')
 s.push('2')
