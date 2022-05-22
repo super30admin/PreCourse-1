e3a76805831807519602fb91d2cca1b75792fb10
rahul-gajwani <rahul.gajwani10@gmail.com>
2022-05-22 08:05:50 +0530

    Update Exercise_1.java
    Completed Sprint PreCourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -7,29 +7,51 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        return top == -1;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+	  top = -1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        if(top == MAX-1)  
+        {  
+            System.out.println("Overflow !!");  
+            return false;  
+        }  
+        else   
+        {  
+            top++;  
+            a[top]=x;  
+            return true;  
+        }  
     } 
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if (top == -1)  
+        {  
+            System.out.println("Underflow !!");  
+            return 0;  
+        }  
+        else   
+        {  
+            return a[top--];
+        }
     } 
   
     int peek() 
     { 
-        //Write your code here
+         if (top == -1)  
+        {  
+            System.out.println("Underflow !!");  
+            return 0;  
+        }  
+		return a[top];
     } 
 } 
   
