88212d4e5f5cbe73cf79edf6773e0b71b709f657
rahul-gajwani <rahul.gajwani10@gmail.com>
2022-05-22 08:08:47 +0530

    Update Exercise_3.java
    Completed Sprint Precourse1

--- Exercise_3.java
+++ Exercise_3.java
@@ -17,34 +17,40 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            data = d;
+            next = null; 
         } 
     } 
   
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
     { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
-
-            // Insert the new_node at last node 
-        // Return the list by head 
+        Node node = new Node(data);
+        node.next = null;
+        if(list.head == null)
+            list.head = node;
+        else
+        {
+            Node lastNode = list.head;
+            while(lastNode.next != null)
+            {
+                lastNode = lastNode.next;
+            }
+            lastNode.next = node;
+        }
+        return list; 
         
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+       Node currNode = list.head;
+        while(currNode != null)
+        {
+            System.out.println(currNode.data);
+            currNode = currNode.next;
+        }
     } 
    
     // Driver code 
