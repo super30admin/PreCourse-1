23f7ffeb350933c61cc1fef37e0025bb5a89576a
Rajvi Shah <rajvishah2309@gmail.com>
2022-05-22 09:02:31 -0700

    update

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,29 +1,64 @@
+# Time Complexity : O(1) for push(), pop()
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
 
 class Node:
-    def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+
+    # Class to create nodes of linked list
+    # constructor initializes node automatically
+    def __init__(self ,data):
+        self.data = data
+        self.next = None
+
 class Stack:
+
+    # head is default NULL
     def __init__(self):
-        
-    def push(self, data):
-        
+        self.head = None
+
+
+
+    # Method to add data to the stack
+    # adds to the start of the stack
+    def push(self ,data):
+
+        if self.head == None:
+            self.head = Node(data)
+
+        else:
+            newnode = Node(data)
+            newnode.next = self.head
+            self.head = newnode
+
     def pop(self):
-        
+        if self.head == None:
+            return None
+
+        else:
+            # Removes the head node and makes
+            # the preceding one the new head
+            r_node = self.head
+            self.head = self.head.next
+            r_node.next = None
+            return r_node.data
+
+
 a_stack = Stack()
 while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    print('push <value>')
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    print('push 10')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
     elif operation == 'pop':
         popped = a_stack.pop()
+        # print(popped)
         if popped is None:
             print('Stack is empty.')
         else:
