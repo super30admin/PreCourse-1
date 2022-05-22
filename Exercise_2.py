3b4b0acf38bd8b808acd79924d2f235a9e927c29
Thirunaa <thirunaavukkarasu.thiru@gmail.com>
2022-05-22 04:13:46 -0400

    Ex2 and Ex3 completed

--- Exercise_2.py
+++ Exercise_2.py
@@ -6,22 +6,40 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head = None
         
     def push(self, data):
-        
+        if self.head == None:
+            self.head=Node(data)
+             
+        else:
+            newnode = Node(data)
+            newnode.next = self.head
+            self.head = newnode
     def pop(self):
+        if self.head is None:
+                return None             
+        else:
+            poppednode = self.head
+            self.head = self.head.next
+            poppednode.next = None
+            return poppednode.data
         
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    print("")
     print('push <value>')
     print('pop')
     print('quit')
-    do = input('What would you like to do? ').split()
+    do = input('What would you like to do? ').lower().split()
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
     if operation == 'push':
-        a_stack.push(int(do[1]))
+        try:
+            a_stack.push(int(do[1]))
+        except:
+            print("Invalid element to push. Pass something to push. Check typo")
     elif operation == 'pop':
         popped = a_stack.pop()
         if popped is None:
@@ -30,3 +48,5 @@ while True:
             print('Popped value: ', int(popped))
     elif operation == 'quit':
         break
+    else:
+        print("Invalid input! Try again!")
--- Exercise_3.py
+++ Exercise_3.py
@@ -2,31 +2,92 @@ class ListNode:
     """
     A node in a singly-linked list.
     """
-    def __init__(self, data=None, next=None):
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+        
+    # defining getter and setter for data and next
+    def getData(self):
+        return self.data
+    
+    def setData(self, data):
+        self.data = data
+    
+    def getNextNode(self):
+        return self.next
+    
+    def setNextNode(self, node):
+        self.next = node
     
 class SinglyLinkedList:
-    def __init__(self):
+    def __init__(self, head=None):
         """
         Create a new singly-linked list.
         Takes O(1) time.
         """
-        self.head = None
+        self.head = head
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        node = ListNode(data, self.head)
+        self.head = node
+        return True
+    
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        curr = self.head
+        while curr:
+            if curr.getData() == key:
+                return True
+            else:
+                curr = curr.getNextNode()
+        return False
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        prev = None
+        curr = self.head
+        while curr:
+            if curr.getData() == key:
+                if prev:
+                    prev.setNextNode(curr.getNextNode())
+                else:
+                    self.head = curr.getNextNode()
+                return True
+            
+            prev = curr
+            curr = curr.getNextNode()
+            
+        return False
+    
+    # print the linked list
+    def show(self):
+        curr = self.head
+        while curr:
+            print(curr.data)
+            curr = curr.getNextNode()
+    
+mySinglyLinkedList = SinglyLinkedList()
+print("Inserting")
+print(mySinglyLinkedList.append(5))
+print(mySinglyLinkedList.append(15))
+print(mySinglyLinkedList.append(25))
+
+mySinglyLinkedList.show()
+
+print(mySinglyLinkedList.find(25))
+
+print(mySinglyLinkedList.remove(25))
+
+mySinglyLinkedList.show()
+
