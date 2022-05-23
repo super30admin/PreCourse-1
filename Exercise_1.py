9c02f0f6acf86277c54a3a03abf97c88d8d30cd4
plokaiah <plokaiah@asu.edu>
2022-05-22 17:41:16 -0700

    Added solutions to Precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,36 @@
+# Time Complexity : O(1) for push, pop, peek
+# Space Complexity : O(N) where N is the number of elements in the array.
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+        self.list = []
+
+
      def isEmpty(self):
+       return len(self.list) == 0
          
      def push(self, item):
+        self.list.append(item)
          
      def pop(self):
-        
-        
+       if len(self.list) == 0:
+         return "List is Empty" 
+       else:
+        k= self.list[-1]
+        self.list = self.list[:-1]        
+        return k
      def peek(self):
+       if len(self.list) == 0:
+         return "List is Empty" 
+       else:
+        return self.list[-1]
         
      def size(self):
+       return len(self.list)
          
      def show(self):
-         
+         return self.list
 
 s = myStack()
 s.push('1')
