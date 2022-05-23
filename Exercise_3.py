ef4e93bf6f39bf795d18bc2bb36847baeb68a004
vineetvk19 <67041692+vineetvk19@users.noreply.github.com>
2022-05-23 00:22:15 -0400

    Precourse-1, Exercise_3 python submission

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,16 @@
+#Author: Vineet Khanna
+#Implementing singly linked list
+
+from ast import And
+
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +25,19 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        #Creating a node of type Listnode with data to append.
+        node1 = ListNode(data)
+
+        #If empty assign head to node.
+        if self.head == None:
+            self.head = node1
+        
+        #Else, traverse to end and add.
+        else:
+            while self.head.next:
+                self.head = self.head.next
+            self.head.next = node1
+            
         
     def find(self, key):
         """
@@ -24,9 +45,44 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        #Traverse the list and if found return that else returning none.
+        while self.head.next:
+            if self.head.data == key:
+                return key
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        #Checking if the head element itself is the key.
+        if self.head is not None and self.head.data == key:
+            self.head = self.head.next
+            return
+        
+        # Traversing linked list until finding key or end 
+        # while keeping track of the previous node so that
+        # its next can be assigned to next of deleted node.
+        while self.head is not None:
+            if self.head.data == key:
+                break
+            prev = self.head
+            self.head = self.head.next
+
+        if self.head == None:
+            return None
+
+        prev.next = self.head.next
+
+
+
+# Some testing.
+l1 = SinglyLinkedList()
+l1.append(1)
+l1.append(2)
+l1.append(3)
+l1.append(4)
+print(l1.find(3))
+l1.remove(3)
+print(l1.find(3))
