5bc0882e622789466b6a5b37d6379e576cee2fff
Dash_Cosmatos77 <53261974+Dhairya1999@users.noreply.github.com>
2022-05-22 22:21:34 -0500

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,52 @@
-class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+"""
+Time Complexity : O(1) for all methods
+Space Complexity : O(n)
+"""
 
-s = myStack()
-s.push('1')
-s.push('2')
-print(s.pop())
-print(s.show())
+
+class StackUsingArray:
+    stack = []
+
+    def __init__(self):
+        self.stack = []
+        self.top = -1
+
+    def isEmpty(self):
+        if self.top == -1:
+            return True
+        else:
+            return False
+
+    def push(self, element):
+        self.stack.append(element)
+
+    def pop(self):
+        if (len(self.stack) != 0):
+            lastelement = self.stack[-1]
+            del (self.stack[-1])
+            return lastelement
+        else:
+            print('the Array is empty')
+
+    def peek(self):
+        return self.stack[self.top]
+
+    def size(self):
+        if self.isEmpty() == True:
+            return 'Stack is empty'
+        else:
+            return self.top + 1
+
+    def outputstack(self):
+        print(self.stack)
+
+
+if __name__ == "__main__":
+    s = StackUsingArray()
+    s.push(1)
+    s.push(2)
+    s.push(3)
+    s.push(4)
+    s.pop()
+    s.peek()
+    s.outputstack()
