8380d483eb747a75daeed2fa993125f36114db55
RiyaGupta7195 <riyag621@gmail.com>
2022-05-23 12:07:23 -0500

    Precourse1 in Python

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,24 +1,68 @@
-
+"""
+Time Complexity : O(1) for all methods(push, pop)
+o(n) for display function
+Space Complexity : O(n)
+Did this code successfully run on Leetcode : N/A
+Any problem you faced while coding this : No
+"""
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
+
 class Stack:
     def __init__(self):
-        
+        #creating stack
+        self.top = None
+
     def push(self, data):
-        
+        '''
+        create a temp node with data= new_data and next pointer pointing none.
+        kept top of stack at head instead keeping at end because
+        at head time complexity will be O(1) abd at end it will be O(n)
+        '''
+        temp_node = Node(data)
+        if self.top == None:
+            self.top = temp_node
+            # self.top.data = data
+            # self.top.next = None
+        else:
+            # temp_node.data = data
+            temp_node.next = self.top
+            self.top = temp_node
+
     def pop(self):
-        
+        '''
+        assigning next pointer of top to next node of top.
+        and returning data from old top.(temp_node was used for this)
+        '''
+        # data = None
+        # temp_node = Node()
+        if self.top == None:
+            return None
+        else:
+            temp_node = self.top
+            self.top = self.top.next
+            temp_node.next = None
+            return temp_node.data
+
+    def display(self):
+      self.t = self.top
+      while self.t != None:
+        print(self.t.data, "->", end=" ")
+        self.t = self.t.next
+
+
 a_stack = Stack()
+
 while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
@@ -28,5 +72,7 @@ while True:
             print('Stack is empty.')
         else:
             print('Popped value: ', int(popped))
+    elif operation == 'dis':
+        a_stack.display()
     elif operation == 'quit':
         break
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,18 @@
+"""Time Complexity : O(1) to create
+O(n) to append, find, remove
+Space Complexity : O(n)
+Did this code successfully run on Leetcode : N/A
+Any problem you faced while coding this : No """
+
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data= data
+        self.next= next
+
     
 class SinglyLinkedList:
     def __init__(self):
@@ -12,11 +22,27 @@ class SinglyLinkedList:
         """
         self.head = None
 
+
+
+
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new_node = ListNode(data)
+        if self.head == None:
+            self.head = new_node
+
+        else:
+            temp = self.head
+            while True:
+
+                if temp.next is None:
+                    temp.next = new_node
+                    break
+                temp = temp.next
+
         
     def find(self, key):
         """
@@ -24,9 +50,52 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        index = 0
+        temp = self.head
+        while temp:
+            if temp.data == key:
+                return index
+            temp = temp.next
+            index += 1
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        prev = self.head
+        temp = prev.next
+
+        if prev.data == key and prev is self.head:  # delete value if it is Head
+            self.head = temp
+        else:
+            while temp:  # delete non-Head values
+                if temp.data == key:
+                    prev.next = temp.next
+                    break
+                prev = prev.next
+                temp = prev.next
+
+    def print(self):
+        if self.head == None:
+            return "Linked List is empty"
+            return
+        itr= self.head
+        llstr= ''
+        while itr:
+            llstr+= str(itr.data) + "-->"
+            itr= itr.next
+        print(llstr)
+
+ll= SinglyLinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+ll.print()
+ll.remove(3)
+ll.print()
+res = ll.find(3)
+print(res)
+
+
