2cc69b67f1e4cbee406783e5e69fb1614108d5ef
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-05-26 19:37:58 -0400

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,87 +1,107 @@
+//****DESIGN STACK USING LINKEDLIST****
 //time complexity : o(1);
 //space complexity:
 
-class Node
-{
-    int data;  
-    Node next;      
-}
-  
-class StackNode 
-{ 
-        private Node top;
-        private int count;
-  
-        StackNode() 
-        { 
-            this.top=null;
-            this.count=0;
-        } 
-    
-	
-    public boolean isEmpty() 
-    { 
-        return top==null;
-        //Write your code here for the condition if stack is empty. 
-    } 
-  
-    public void push(int x) 
-    { 
-        Node node = new Node();
-        if (node == null)
+// Java program to Implement a stack
+// using singly linked list
+// import package
+import static java.lang.System.exit;
+
+// Create Stack Using Linked list
+class StackUsingLinkedlist {
+
+	// A linked list node
+	private class Node {
+        int data; 
+		Node next;
+	}
+	Node top;
+	// Constructor
+	StackUsingLinkedlist()
+	{
+		this.top = null;
+	}
+	public void push(int x) // insert at the beginning
+	{
+	    Node temp=new Node();
+	    temp.data=x;
+	    temp.next=top;
+	    top=temp;
+		
+	}
+
+	public boolean isEmpty()
+	{
+		return top==null;
+	}
+
+	public int peek()
+	{
+	    if(top==null)
+	    {
+	        return 0;
+	    }
+	    else
+	    {
+	        return top.data;
+	    }
+		
+	}
+
+	public void pop() // remove at the beginning
+	{
+	    top=top.next;
+		
+	}
+
+	public void display()
+	{
+        if (top == null) 
         {
-            System.out.println("Stack Overflow");
-            return;
+            System.out.printf("\nStack Underflow");
+            
         }
-        //Write code to push data to the stack. 
-        node.data = x;
-        node.next = top;
-        top = node;
-        this.count += 1;
-    } 
-  
-    public int pop() 
-    {
-        
-	    //If Stack Empty Return 0 and print "Stack Underflow"
-        if (isEmpty())
+        else 
         {
-            System.out.println("Stack Underflow");
-            return 0;
+            Node temp = top;
+            while (temp != null) 
+            {
+ 
+                System.out.printf("%d->", temp.data);
+ 
+                temp = temp.next;
+		    }
         }
-        //Write code to pop the topmost element of stack.
-        int top = peek();
-        this.count -= 1;
-        this.top = (this.top).next;
-        //Also return the popped element 
-        return top;
-    } 
-  
-    public int peek() 
-    {
-        if (isEmpty()) 
-        {
-            System.out.println("The stack is empty");
-            return 0;
-        }
-        return top.data;
     }
 }
-public class Main
+// main class
+public class GFG 
 {
-  
-	//Driver code
-    public static void main(String[] args) 
-    { 
-  
-        StackNode sll = new StackNode(); 
-  
-        sll.push(10); 
-        sll.push(20); 
-        sll.push(30); 
-  
-        System.out.println(sll.pop() + " popped from stack"); 
-  
-        System.out.println("Top element is " + sll.peek()); 
-    } 
-} 
+	public static void main(String[] args)
+	{
+		// create Object of Implementing class
+		StackUsingLinkedlist obj = new StackUsingLinkedlist();
+		// insert Stack value
+		obj.push(11);
+		obj.push(22);
+		obj.push(33);
+		obj.push(44);
+
+		// print Stack elements
+		obj.display();
+
+		// print Top element of Stack
+		System.out.printf("\nTop element is %d\n", obj.peek());
+
+		// Delete top element of Stack
+		obj.pop();
+		obj.pop();
+
+		// print Stack elements
+		obj.display();
+
+		// print Top element of Stack
+		System.out.printf("\nTop element is %d\n", obj.peek());
+	}
+}
+
