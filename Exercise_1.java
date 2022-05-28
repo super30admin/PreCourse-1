042262abd560ef52a87edd2167d3369044d84e16
harshparmar <parmarharsh4296@gmail.com>
2022-05-28 09:08:41 -0400

    Assignment Completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,63 @@
-class Stack { 
+import java.sql.SQLOutput;
+
+class Stack {
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
+
+    // Time Complexity:
+    // isEmpty() - O(1)
+    // push() - O(1)
+    // pop() - O(1)
+    // peek() - O(1)
+    // Space Complexity for these - O(1)
+    private static final int MAX = 1000;
+    private int top;
+    private int a[] = new int[MAX]; // Maximum size of Stack
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+
+        return top == -1;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        a = new int[MAX];
+        this.top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if (top != MAX - 1) {
+            a[++top] = x;
+            return true;
+        }
+        return false;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if (top != -1) {
+            return a[top--];
+        }
+        System.out.println("Stack Underflow");
+        return 0;
     } 
   
     int peek() 
     { 
         //Write your code here
+        if (top != -1) {
+            return a[top];
+        }
+        System.out.println("Stack Empty");
+        return 0;
     } 
 } 
   
@@ -41,6 +69,12 @@ class Main {
         s.push(10); 
         s.push(20); 
         s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println((s.peek() + "is Stack's peek"));
+        System.out.println("Is Stack's empty: " + s.isEmpty());
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println("Is Stack's empty: " + s.isEmpty());
+    }
 }
