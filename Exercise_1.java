3c828bb97848a0ef9170748385dc30b8f8ebe5e0
Bhargav Shah <bhargavpshah98@gmail.com>
2022-05-30 15:05:55 -0700

    Code Uploaded

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,52 @@
-class Stack { 
+//Time Complexity:- O(N)
+//Space Complexity:- O(1)
+class Stack {
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
-    int top; 
+    int top = -1;
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
-    { 
-        //Write your code here 
+    {
+        return top==-1;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top=-1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        if(top + 1 == MAX) {
+            System.out.println("Stack Oversized");
+            return false;
+        }
+        else{
+            a[++top]=x;
+        }
+        return true;
     } 
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+       if(a[top]==0)
+       {
+           System.out.println("Stack Underflow");
+           return -1;
+       }
+       else {
+           return(a[top--]);
+       }
     } 
   
     int peek() 
     { 
-        //Write your code here
+        if(top>=0){
+            return(a[top--]);
+        }
+        return -1;
     } 
 } 
   
