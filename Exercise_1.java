9e9d0b950cbf4f0d6b1183ddd8b547dc2c4a0b7a
nirlep-Kathiriya <91766563+nirlep-Kathiriya@users.noreply.github.com>
2022-06-15 15:16:30 -0500

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,74 @@
+// Time Complexity: O(1) for push,pop and peek operation
+// Space Complexity: O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :  n
+
+
 class Stack { 
     //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+	//Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        if(top==-1)
+			return true;
+		else
+			return false;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top=-1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        if(top == MAX-1)
+		{
+			System.out.println("stack overflow");
+			return false;
+		}
+		else
+		{
+			top++;
+			a[top]=x;
+			return true;
+		}
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+		if(top==-1)
+		{
+			System.out.println("stack underflow");
+			return 0;
+		}
+		else
+		{
+			int val = a[top];
+			top--;
+			return val;
+		}
     } 
   
     int peek() 
     { 
-        //Write your code here
+		if(top==-1)
+		{
+			System.out.println("stack underflow");
+			return 0;
+		}
+		else
+		{
+			int val = a[top];
+			return val;
+
+		}
     } 
 } 
   
