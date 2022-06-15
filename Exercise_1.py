a3bdcb90fa0678962828d5218a6b31127110b031
Lokesh vaddi <36542134+lokesh31333@users.noreply.github.com>
2022-06-15 15:51:15 -0700

    Completed PreCourse-1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,52 @@
-class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+'''
+Time complexity : 
+for push, pop is O(1) (Amortized because we use dynamic array to implement it)
+Space complexity : 
+as we are not using extra memory(although we are using dynamic array which grows as input) space complexity would be O(1) amortized
+'''
+
+class myStack:  
+  #python constructor to initialize the list 
+  def __init__(self):
+    self.stack = []
+  # this function checks if the stack is empty or not and returns the boolean value
+  def isEmpty(self):
+    if self.stack:
+      return False
+    else :
+      return True
+  # this function takes item as the input and appends top of the stack i.e end of the list
+  def push(self, item):
+    self.stack.append(item)
+
+# this function removes the top item from the stack
+  def pop(self):
+    if not self.isEmpty():
+      return self.stack.pop()
+    else :
+      return "Stack is empty canno't pop from it"
+
+# this fuction returns the top item from the stack
+  def peek(self):
+    if not self.isEmpty():
+      return self.stack[-1]
+    else :
+      return "stack is empty canno't peek into it"
+
+# this function returns the size of the stack
+  def size(self):
+    return len(self.stack)
+
+# this function returns the elements in the stack
+  def show(self):
+    return self.stack
 
 s = myStack()
 s.push('1')
 s.push('2')
+print(s.show())
 print(s.pop())
+print(s.peek())
+print(s.isEmpty())
+print(s.size())
 print(s.show())
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,7 +3,9 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+      self.data = data
+      self.next = next
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +19,64 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        temp = ListNode(data)
+
+        if self.head is None:
+          self.head = temp
+        else :
+          curr = self.head
+          while(curr.next is not None):
+            curr = curr.next
+          curr.next = temp
+    
+    def prin(self):
+      '''
+      Print's the elements in the Linked List (for test)
+      '''
+      curr = self.head
+      while(curr is not None):
+        print(curr.data)
+        curr = curr.next
+          
         
     def find(self, key):
-        """
-        Search for the first element with `data` matching
-        `key`. Return the element or `None` if not found.
-        Takes O(n) time.
-        """
+      """
+      Search for the first element with `data` matching
+      `key`. Return the element or `None` if not found.
+      Takes O(n) time.
+      """
+      curr = self.head
+      while(curr):
+        if(curr.data == key):
+          return key
+          break
+        curr = curr.next
+      return None
+
+  
+    def remove(self,key):
+      """
+      Remove the first occurrence of `key` in the list.
+      Takes O(n) time.
+      """
+      curr = self.head
+      if(curr.data is key):
+        self.head = self.head.next
+      else :
+        while(curr.next is not None):
+          if(curr.next.data is key):
+            curr.next = curr.next.next
+          if (curr.next is not None):
+            curr = curr.next
+
         
-    def remove(self, key):
-        """
-        Remove the first occurrence of `key` in the list.
-        Takes O(n) time.
-        """
+
+ll1 = SinglyLinkedList()
+ll1.append(10)
+ll1.append(20)
+ll1.append(30)
+ll1.append(40)
+ll1.append(50)
+print(ll1.find(10))
+ll1.remove(50)
+ll1.prin()
