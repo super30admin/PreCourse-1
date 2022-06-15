b3adcfd6e77ca9cf0c0ac9b918f30a30014374c1
Pranav Singh <46050869+pranavsinghps1@users.noreply.github.com>
2022-06-16 05:25:18 +0530

    Solutions

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,42 @@
+"""
+TC: All Functions are O(1)
+
+SC: All functions are O(1)
+
+"""
+
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+  def __init__(self):
+    self.stack=[]
+    self.size=10
+  def isEmpty(self):
+    return len(self.stack)==0
+  def push(self, item):
+    if len(self.stack)>=self.size:
+      print("Overflow")
+    else:
+      self.stack.append(item)
+
+    return self.stack
+  def pop(self):
+    last_ele=self.stack[-1]
+    if (len(self.stack))<=0:
+      print("Underflow")
+    else:
+      print("Removing:",last_ele)
+      self.stack=self.stack[:-1]
+    return self.stack
+  def peek(self):
+    return self.stack[-1]
+  def size(self):
+    print(len(self.stack))
+  def show(self):
+    return self.stack
 
 s = myStack()
 s.push('1')
 s.push('2')
+print(s.size)
 print(s.pop())
 print(s.show())
+s.size
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,30 +3,68 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+        self.data=data
+        self.next=None
 class SinglyLinkedList:
     def __init__(self):
+        self.head = None
         """
         Create a new singly-linked list.
         Takes O(1) time.
         """
-        self.head = None
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        newNode=ListNode(data)
+        if self.head is None:
+            self.head=newNode
+            return
+
+        last=self.head
+        while(last.next):
+            last=last.next
+        last.next=newNode
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        ptr = self.head
+        while(ptr):
+            if(ptr.data == key):
+                return key
+            ptr = ptr.next
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        ptr=self.head
+        if(ptr.data == key):
+            self.head = self.head.next
+        else :
+            while(ptr.next is not None):
+                if(ptr.next.data == key):
+                    ptr.next = ptr.next.next
+                if (ptr.next is not None):
+                    ptr = ptr.next
+
+ll= SinglyLinkedList()
+ll.append(5)
+ll.append(10)
+ll.append(25)
+ll.append(30)
+ll.remove(25)
+search_key = ll.find(25)
+print(f"\nThis is the element you are searching for {search_key}")
+search_key = ll.find(15)
+print(f"\nThis is the element you are searching for {search_key}")
+search_key = ll.find(5)
+print(f"\nThis is the element you are searching for {search_key}")
