ab2c63384960f25c21daf5a6fea37c76248b96f6
rsp2210 <rushikeshp2210@gmail.com>
2022-06-15 19:58:00 -0700

    Completed Sprint PreCourse-1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,41 @@
+# Time Complexity 
+    # isEmpty = o(1)  
+    # push = o(1) 
+    # pop = o(1) 
+    # peek = o(1)
+    # size = o(1)
+
+# Space Complexity = o(n) to store n elements
+# Any problem you faced while coding this : not sure about TC of show()
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+         self.arr = []
+
      def isEmpty(self):
-         
+         if self.arr:         # By default, an object is considered true unless its class defines either a __bool__() method that returns False or a __len__() method that returns zero, when called with the object. So here, if it's true, it's not empty.
+            return False
+         return True
+     
      def push(self, item):
-         
+         self.arr.append(item)    
+     
      def pop(self):
-        
-        
+         if self.isEmpty():
+               return "Can't pop! empty stack"
+         else:
+               return self.arr.pop() 
+     
      def peek(self):
-        
+         return self.arr[-1]
+     
      def size(self):
-         
+         return len(self.arr)
+     
      def show(self):
-         
+         return self.arr
 
 s = myStack()
 s.push('1')
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,9 +1,12 @@
+# Any problem you faced while coding this : Unable to return the removed node
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = None
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +20,51 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        new = ListNode(data)
+        if self.head:                           #checks if there's a head, if true then it traverses at the end and adds the new node. Else, makes the new node head 
+            while self.head.next:
+                self.head = self.head.next
+            self.head.next = new
+        else:
+            self.head = new        
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        tmp = self.head                        # to create an instance
+        while tmp:                       # traverses the list while there is a head.
+            if tmp.data == key:
+                return key
+            else:
+                tmp = tmp.next
+        return None            
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        tmp = self.head
+        if tmp.data == key:
+            self.head = self.head.next
+        elif tmp.next:
+            while tmp.next:                       
+                if tmp.next.data == key:
+                    tmp.next = tmp.next.next
+                    #return key
+                    break
+                tmp = tmp.next
+        else:
+            return "Not in the linkedList"  
+
+ll1 = SinglyLinkedList()
+ll1.append(1)
+ll1.append(2)
+ll1.append(4)
+ll1.append(8)
+print("Searching 4",ll1.find(4))
+print("Searching 3",ll1.find(3))
+print(ll1.remove(4))
+print(ll1.remove(3))
