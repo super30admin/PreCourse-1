f11e435e84d48f10076a2fc48d8fa891d9c69a94
Tushar <tusharpawar611>
2022-06-15 20:55:38 -0700

    PreCourse 1 solutions

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,17 +1,20 @@
+//Time complexity for push(), pop(), isEmpty() and peek() - O(1)
 class Stack { 
     //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+    //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
+        return top<0;
         //Write your code here 
     } 
 
     Stack() 
     { 
+        top = -1;
         //Initialize your constructor 
     } 
   
@@ -19,10 +22,28 @@ class Stack {
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top>=MAX-1){
+            System.out.println("You cannot PUSH,- stack Overflow-");
+            return false;
+        }
+        else{
+            a[++top]=x;
+            System.out.println("Successfully pushed "+x+" into the stack");
+            return true;
+        }
+
     } 
   
     int pop() 
     { 
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else{
+            int curr = a[--top];
+            return curr;
+        }
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
     } 
@@ -30,6 +51,15 @@ class Stack {
     int peek() 
     { 
         //Write your code here
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else {
+            int x = a[top];
+            return x;
+        }
+
     } 
 } 
   
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,70 +1,86 @@
-import java.io.*; 
-  
-// Java program to implement 
-// a Singly Linked List 
+
 public class LinkedList { 
   
-    Node head; // head of list 
+    StackNode root; 
   
-    // Linked list Node. 
-    // This inner class is made static 
-    // so that main() can access it 
-    static class Node { 
-  
+    static class StackNode { 
         int data; 
-        Node next; 
+        StackNode next; 
   
-        // Constructor 
-        Node(int d) 
+        StackNode(int data) 
         { 
-            //Write your code here 
+            //Constructor here 
+            this.data = data;
+
         } 
     } 
-  
-    // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
+    
+	
+    public boolean isEmpty() 
     { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
-
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
+        //Write your code here for the condition if stack is empty. 
+        if(root == null){
+            return true;
+        }
+        else return false;
     } 
   
-    // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
-    {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+    public void push(int data) 
+    { 
+        //Write code to push data to the stack. 
+        StackNode newNode = new StackNode(data);
+
+        if(isEmpty()){
+            root = newNode;
+        }
+        else{
+            StackNode temp = root;
+            root=newNode;
+            newNode.next = temp;
+        }
+        System.out.println("Successfully pushed "+data+" into the stack");
     } 
-   
-    // Driver code 
+  
+    public int pop() 
+    { 	
+	//If Stack Empty Return 0 and print "Stack Underflow"
+        //Write code to pop the topmost element of stack.
+	//Also return the popped element 
+        int res = Integer.MIN_VALUE;
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+        }
+        else{
+            res = root.data;
+            root = root.next;
+            return res;
+        }
+        return res;
+    } 
+  
+    public int peek() 
+    { 
+        //Write code to just return the topmost element without removing it.
+        if(!isEmpty()){
+            return root.data;
+        }
+        System.out.println("Stack Underflow");
+        return Integer.MIN_VALUE;
+    } 
+  
+	//Driver code
     public static void main(String[] args) 
     { 
-        /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
   
-        // 
-        // ******INSERTION****** 
-        // 
+        StackAsLinkedList sll = new StackAsLinkedList(); 
   
-        // Insert the values 
-        list = insert(list, 1); 
-        list = insert(list, 2); 
-        list = insert(list, 3); 
-        list = insert(list, 4); 
-        list = insert(list, 5); 
+        sll.push(10); 
+        sll.push(20); 
+        sll.push(30); 
   
-        // Print the LinkedList 
-        printList(list); 
+        System.out.println(sll.pop() + " popped from stack"); 
+  
+        System.out.println("Top element is " + sll.peek()); 
     } 
-}
+} 
+
