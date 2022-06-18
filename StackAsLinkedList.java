c9de08d8f809078f8c1f95e868a03fdb436bf0a0
viveksharma180 <sharma.viv@northeastern.edu>
2022-06-18 13:51:31 -0700

    Completed after Review. Thanks @jeelpatel18.
    Signed-off-by: viveksharma180 <sharma.viv@northeastern.edu>

--- StackAsLinkedList.java
+++ StackAsLinkedList.java
@@ -1,4 +1,4 @@
-// Time Complexity : O(n)
+// Time Complexity : O(1)
 // Space Complexity : O(N)
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this : N/A
@@ -34,7 +34,7 @@ class StackNode {
 }
 public class StackAsLinkedList {
 
-    StackNode root;
+    StackNode root = new StackNode();
 
     public boolean isEmpty() {
         //Write your code here for the condition if stack is empty.
@@ -47,37 +47,31 @@ public class StackAsLinkedList {
     public void push(int data) {
         //Write code to push data to the stack.
         StackNode newNode = new StackNode(data); // creating node with input data
-        StackNode currNode = root; //creating other node to work on, so we don't lose the head node.
-        if (root == null) { // if root is null then put the given value in head node
-            root = newNode;
-        } else {
-            while (currNode.next != null) { // to traverse to the last node and then insert node there
-                currNode = currNode.next;
-            }
-            newNode.prev = currNode; // connecting the last and new node.
-            currNode.next = newNode;
+        if (this.root == null){
+            root.data = data;
+        }else{
+            root.next = newNode;
+            newNode.prev = root;
+            root = root.next;
         }
     }
 
     public int pop() {
         //If Stack Empty Return 0 and print "Stack Underflow"
-        StackNode currNode = root; // for reference
-        while(currNode.next != null){ //traversing to the last node
-            currNode = currNode.next;
+        if(root == null){
+            System.out.println("Stack Underflow");
+            return 0;
         }
-        int result = currNode.data; //storing value in int for returning result
-        currNode.prev.next = null; // deleting the last node
-        return result;
+        int x = root.data;
+        root = root.prev;
+        root.next = null;
+        return x;
+
     }
 
     public int peek() {
         //Write code to just return the topmost element without removing it.
-        StackNode currNode = root;
-        while(currNode.next != null){
-            currNode = currNode.next; //traversing the nodes
-        }
-        int result = currNode.data;
-        return result; //returning the last value.
+        return root.data;
     }
 }
 
