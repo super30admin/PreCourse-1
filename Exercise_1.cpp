4a689479e4d67eb9af35adea761c17688fcd451f
ckhinvasara <khinvasa@usc.edu>
2022-06-18 19:02:55 -0700

    Added Precourse-1

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,54 +1,95 @@
-#include <bits/stdc++.h> 
-  
-using namespace std; 
-  
-#define MAX 1000 
-  
+// Time Complexity : O(1)
+// Space Complexity : O(1000) or O(MAX) depending on size of MAX 
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX 1000
+
 class Stack {
-  //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-    int top; 
-  
-public: 
-    int a[MAX]; // Maximum size of Stack 
-  
-    Stack() { //Constructor here } 
-    bool push(int x); 
-    int pop(); 
-    int peek(); 
-    bool isEmpty(); 
-}; 
-  
-bool Stack::push(int x) 
-{ 
+    int top;
+
+public:
+    int a[MAX]; // Maximum size of Stack
+
+    Stack() { //Constructor here
+      top = -1;
+    }
+    bool push(int x);
+    int pop();
+    int peek();
+    bool isEmpty();
+};
+
+bool Stack::push(int x)
+{
     //Your code here
     //Check Stack overflow as well
-} 
-  
-int Stack::pop() 
-{ 
+    if(top == MAX - 1)
+    {
+      cout << "Overflow condition hit";
+      return false;
+    }
+    // Add the data to the Stack
+    else
+    {
+      top++;
+      a[top] = x;
+      return true;
+    }
+}
+
+int Stack::pop()
+{
     //Your code here
-    //Check Stack Underflow as well 
-} 
-int Stack::peek() 
-{ 
+    int item;
+    //Check Stack Underflow as well
+    if (top == -1)
+    {
+      cout << "Underflow condition hit";
+      return -1;
+    }
+    //Pop data from the Stack
+    else
+    {
+      item = a[top];
+      top--;
+      return item;
+    }
+}
+int Stack::peek()
+{
     //Your code here
     //Check empty condition too
-} 
-  
-bool Stack::isEmpty() 
-{ 
-    //Your code here 
-} 
-  
-// Driver program to test above functions 
-int main() 
-{ 
-    class Stack s; 
-    s.push(10); 
-    s.push(20); 
-    s.push(30); 
-    cout << s.pop() << " Popped from stack\n"; 
-  
-    return 0; 
-} 
+    if(top == -1)
+    {
+      cout << "Stack is is empty";
+      return -1;
+    }
+    //Return the top most element
+    else
+    {
+      return a[top];
+    }
+}
+
+bool Stack::isEmpty()
+{
+    //Your code here
+    return (top < 0);
+}
+
+// Driver program to test above functions
+int main()
+{
+    class Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    cout << s.pop() << " Popped from stack\n";
+
+    return 0;
+}
+
--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,3 +1,8 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n) where n is size of LinkedList
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
 #include <bits/stdc++.h> 
 using namespace std; 
   
@@ -19,21 +24,51 @@ StackNode* newNode(int data)
 int isEmpty(StackNode* root) 
 { 
     //Your code here 
+    return root ==NULL;
+    
 } 
   
 void push(StackNode** root, int data) 
 { 
     //Your code here 
+    StackNode *temp = newNode(data);
+    temp->next = *root;
+    *root = temp;
 } 
   
 int pop(StackNode** root) 
 { 
     //Your code here 
+    if(isEmpty(*root))                 //check if the stack is empty. If yes you cant delete the element
+    {
+        return -1;
+    }
+    else
+    {
+        /* code */
+     StackNode *temp = *root;
+     *root = (*root)->next;
+     int item = temp->data;
+     free(temp);
+     return item;   
+       
+    }
+    
 } 
   
 int peek(StackNode* root) 
 { 
     //Your code here 
+    if(isEmpty(root))
+    {
+        return -1;
+    }
+    else
+    {
+        /* code */
+        return root->data;
+    }
+    
 } 
   
 int main() 
@@ -50,3 +85,6 @@ int main()
   
     return 0; 
 } 
+
+// Time Complexity 
+// Time complexity is O(1) for all the functions.
