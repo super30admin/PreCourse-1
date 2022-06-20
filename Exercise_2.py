f34b4c13cc5e135cdd27127faa50d25dab035332
aneesh2411 <aneeshkalisapudi@gmail.com>
2022-06-19 22:47:37 -0700

    Completed Pre Course 1

--- Exercise_2.py
+++ Exercise_2.py
@@ -7,9 +7,32 @@ class Node:
 class Stack:
     def __init__(self):
         
+        self.top = None
+        self.height = 0
+
+        
     def push(self, data):
+        new_node = Node(data)
+        if (self.height == 0):
+            self.top = new_node
+            self.top.next = None
+        else:
+            temp = self.top
+            self.top = new_node
+            self.top.next = temp
+        self.height += 1
+        return self.top.data
+            
         
     def pop(self):
+        if (self.height == 0):
+            return None
+        else:
+            temp = self.top
+            self.top = self.top.next
+            temp.next = None
+        self.height -= 1   
+        return temp.data
         
 a_stack = Stack()
 while True:
