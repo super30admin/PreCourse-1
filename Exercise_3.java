8be0e5434cc9725e8a3530a6a47cd3fc9d76ab9c
Pratyush Sharma <pratyush.sharma@sjsu.edu>
2022-06-19 20:32:01 -0700

    Precourse-1 completed

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,70 +1,77 @@
-import java.io.*; 
-  
-// Java program to implement 
-// a Singly Linked List 
-public class LinkedList { 
-  
+import java.io.*;
+
+// Insertion time complexity -> O(N)
+// Insertion space complexity -> O(1)
+// Print time complexity -> O(N)
+// Print space complexity -> O(1)
+// Problems while writing this code: None
+public class LinkedList {
+
     Node head; // head of list 
-  
+
     // Linked list Node. 
     // This inner class is made static 
     // so that main() can access it 
-    static class Node { 
-  
-        int data; 
-        Node next; 
-  
-        // Constructor 
-        Node(int d) 
-        { 
-            //Write your code here 
-        } 
-    } 
-  
-    // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
-    { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
+    static class Node {
 
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
-    } 
-  
-    // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
-    {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
-    } 
-   
+        int data;
+        Node next;
+
+        // Constructor 
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    // Method to insert a new node
+    public static LinkedList insert(LinkedList list, int data)
+    {
+        // Create a new node with given data
+        Node temp = new Node (data);
+        temp.next = null;
+        // If the Linked List is empty,
+        // then make the new node as head
+        if (list.head == null)
+            list.head = temp;
+        else {
+            Node last_node = list.head;
+            while (last_node != null)
+                last_node = last_node.next;
+
+            last_node.next = temp;
+        }
+        return list;
+    }
+
+    // Method to print the LinkedList.
+    public static void printList(LinkedList list)
+    {
+        Node temp = list.head;
+
+        System.out.println(temp.data + ", ");
+        temp = temp.next;
+    }
+
     // Driver code 
-    public static void main(String[] args) 
-    { 
+    public static void main(String[] args)
+    {
         /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
-  
+        LinkedList list = new LinkedList();
+
         // 
         // ******INSERTION****** 
         // 
-  
+
         // Insert the values 
-        list = insert(list, 1); 
-        list = insert(list, 2); 
-        list = insert(list, 3); 
-        list = insert(list, 4); 
-        list = insert(list, 5); 
-  
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+
         // Print the LinkedList 
-        printList(list); 
-    } 
+        printList(list);
+    }
 }
