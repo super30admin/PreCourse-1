cfca21dc9781813a70a783d3fead8d7a32096473
tejasp27 <tejaspatil790@gmail.com>
2022-06-20 02:45:17 -0400

    "Completed Precourse-1"

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,13 +1,18 @@
+//Time Complexity for every operation is O(1)
+//Space Complexity is O(n)
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
-  
     boolean isEmpty() 
     { 
-        //Write your code here 
+       if(top>0){
+        return false;
+       }else{
+        return true;
+       }
     } 
 
     Stack() 
@@ -19,17 +24,36 @@ class Stack {
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top<MAX){
+            a[top] = x;
+            top++;
+            return true;
+        } else{
+            return false;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top>0){
+            top=top-1;
+            return a[top];
+        }else{
+            System.out.println("Stack Underflow");
+            return 0;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(top>0){
+            return a[0];
+        }else{
+            return -1;
+        }
     } 
 } 
   
