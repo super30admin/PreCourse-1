a8c99fa57019bd0775cedfc7ed7001afd34e9093
Shreya Chaudhari <shreyamchaudhari@gmail.com>
2022-06-21 17:19:48 -0400

    Completed PreCourse-1 Ex1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,8 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : NA
+// Any problem you faced while coding this : No
+
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
