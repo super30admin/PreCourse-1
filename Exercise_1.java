c5e19754819ac0c5e54f8c57b3c0afcdeafe5311
Shreya Chaudhari <shreyamchaudhari@gmail.com>
2022-06-21 16:31:44 -0400

    Completed PreCourse1 Ex1

--- Exercise_1.java
+++ Exercise_1.java
@@ -8,28 +8,55 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        return top == -1;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top == (MAX-1)){
+            print("Stack Overflow");
+            return false;
+        }
+        else{
+            top++;
+            a[top] = x;
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top == -1){
+            print("Stack Underflow");
+            return 0;
+        }
+        else{
+            int res = a[top];
+            top--;
+            return res;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(a[top] == -1){
+            print("Stack Underflow")
+            return 0;
+        }
+        else{
+            return a[top];
+        }
     } 
 } 
   
