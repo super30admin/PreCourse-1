8f900157334b512b25aa5d574e3aea4eee62e758
Shreya Chaudhari <shreyamchaudhari@gmail.com>
2022-06-21 19:38:49 -0400

    Completed PreCourse-1 Ex3

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,3 +1,10 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : NA
+// Any problem you faced while coding this : No
+
+
+
 import java.io.*; 
   
 // Java program to implement 
@@ -18,6 +25,8 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            data = d; 
+            next = null;
         } 
     } 
   
@@ -25,26 +34,46 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
+         Node new_node = new Node(data);
+        new_node.next = null;
    
         // If the Linked List is empty, 
         // then make the new node as head 
+        if (list.head == null) {
+            list.head = new_node;
+        }
+        else {
+            // Else traverse till the last node
+            // and insert the new_node there
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+   
+            // Insert the new_node at last node
+            last.next = new_node;
+        }
         
-            // Else traverse till the last node 
-            // and insert the new_node there 
-
-            // Insert the new_node at last node 
+     
         // Return the list by head 
+        return list;
+
+
         
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
+        Node currNode = list.head;
         // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+        while (currNode != null) {
+            // Print the data at current node
+            System.out.print(currNode.data + " ");
+    
+            // Go to next node
+            currNode = currNode.next;
+        }
     } 
    
     // Driver code 
