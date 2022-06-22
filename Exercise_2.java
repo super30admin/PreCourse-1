8886e0ebe3ab8a267014bcf500a85ae1960c59fb
pinto <pinto@localhost>
2022-06-21 22:10:39 -0700

    Precourse-1 solution

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,7 @@
-public class StackAsLinkedList { 
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+
+public class Exercise_2 { 
   
     StackNode root; 
   
@@ -9,18 +12,40 @@ public class StackAsLinkedList {
         StackNode(int data) 
         { 
             //Constructor here 
-        } 
+        	this.data=data;        	        			
+        }
+
+		public StackNode() {
+			// TODO Auto-generated constructor stub
+			System.out.println("Constructor called!!!");
+		}
     } 
     
 	
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+        //Write your code here for the condition if stack is empty.
+    	if(root == null)
+    		return true;
+    	return false;
+    	
+    				
     } 
   
     public void push(int data) 
     { 
         //Write code to push data to the stack. 
+    	StackNode node = new StackNode();
+    	if(node == null)
+    		return;
+    	
+    	
+    	node.data = data;
+    	node.next = root;
+    	root = node;
+    
+    	
+    	
     } 
   
     public int pop() 
@@ -28,18 +53,33 @@ public class StackAsLinkedList {
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
+    	if(isEmpty())
+    	{
+    			return -1;
+    	}
+    	
+    	int top = peek();
+    	root = root.next;
+    	return top;
+    	
+    	
+    	
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+    	if(isEmpty())
+    		return -1;
+    	return root.data;
+    	
     } 
   
 	//Driver code
     public static void main(String[] args) 
     { 
   
-        StackAsLinkedList sll = new StackAsLinkedList(); 
+        Exercise_2 sll = new Exercise_2(); 
   
         sll.push(10); 
         sll.push(20); 
