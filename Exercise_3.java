fbae3a3d4d0aa822688189cc2b22b026bd09ae09
jayachandravvs <42566670+jayachandravvs@users.noreply.github.com>
2022-06-21 19:23:48 -0500

    Update Exercise_3.java

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,70 +1,126 @@
-import java.io.*; 
-  
-// Java program to implement 
-// a Singly Linked List 
-public class LinkedList { 
-  
-    Node head; // head of list 
-  
-    // Linked list Node. 
-    // This inner class is made static 
-    // so that main() can access it 
-    static class Node { 
-  
-        int data; 
-        Node next; 
-  
-        // Constructor 
-        Node(int d) 
-        { 
-            //Write your code here 
-        } 
-    } 
-  
-    // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
-    { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
+// Java program to Implement a stack
+// using singly linked list
+// import package
+import static java.lang.System.exit;
 
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
-    } 
-  
-    // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
-    {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
-    } 
-   
-    // Driver code 
-    public static void main(String[] args) 
-    { 
-        /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
-  
-        // 
-        // ******INSERTION****** 
-        // 
-  
-        // Insert the values 
-        list = insert(list, 1); 
-        list = insert(list, 2); 
-        list = insert(list, 3); 
-        list = insert(list, 4); 
-        list = insert(list, 5); 
-  
-        // Print the LinkedList 
-        printList(list); 
-    } 
+// Create Stack Using Linked list
+class StackUsingLinkedlist {
+
+	// A linked list node
+	private class Node {
+
+		int data; // integer data
+		Node link; // reference variable Node type
+	}
+	// create global top reference variable global
+	Node top;
+	// Constructor
+	StackUsingLinkedlist()
+	{
+		this.top = null;
+	}
+
+	// Utility function to add an element x in the stack
+	public void push(int x) // insert at the beginning
+	{
+		// create new node temp and allocate memory
+		Node temp = new Node();
+
+		// check if stack (heap) is full. Then inserting an
+		// element would lead to stack overflow
+		if (temp == null) {
+			System.out.print("\nHeap Overflow");
+			return;
+		}
+
+		// initialize data into temp data field
+		temp.data = x;
+
+		// put top reference into temp link
+		temp.link = top;
+
+		// update top reference
+		top = temp;
+	}
+
+	// Utility function to check if the stack is empty or not
+	public boolean isEmpty()
+	{
+		return top == null;
+	}
+
+	// Utility function to return top element in a stack
+	public int peek()
+	{
+		// check for empty stack
+		if (!isEmpty()) {
+			return top.data;
+		}
+		else {
+			System.out.println("Stack is empty");
+			return -1;
+		}
+	}
+
+	// Utility function to pop top element from the stack
+	public void pop() // remove at the beginning
+	{
+		// check for stack underflow
+		if (top == null) {
+			System.out.print("\nStack Underflow");
+			return;
+		}
+
+		// update the top pointer to point to the next node
+		top = (top).link;
+	}
+
+	public void display()
+	{
+		// check for stack underflow
+		if (top == null) {
+			System.out.printf("\nStack Underflow");
+			exit(1);
+		}
+		else {
+			Node temp = top;
+			while (temp != null) {
+
+				// print node data
+				System.out.printf("%d->", temp.data);
+
+				// assign temp link to temp
+				temp = temp.link;
+			}
+		}
+	}
+}
+// main class
+public class GFG {
+	public static void main(String[] args)
+	{
+		// create Object of Implementing class
+		StackUsingLinkedlist obj = new StackUsingLinkedlist();
+		// insert Stack value
+		obj.push(11);
+		obj.push(22);
+		obj.push(33);
+		obj.push(44);
+
+		// print Stack elements
+		obj.display();
+
+		// print Top element of Stack
+		System.out.printf("\nTop element is %d\n", obj.peek());
+
+		// Delete top element of Stack
+		obj.pop();
+		obj.pop();
+
+		// print Stack elements
+		obj.display();
+
+		// print Top element of Stack
+		System.out.printf("\nTop element is %d\n", obj.peek());
+	}
 }
