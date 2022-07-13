4b2db74c3861727cd88c143a392086298085bb66
Chandra Kiran Viswanath Balusu <cbalusu@gmu.edu>
2022-07-12 23:57:53 -0400

    Complete `Exercise1` Using Python - Added `isFull`, `__str__` functions for ease - Maximum Size Constraint added must be greater than 0 - `show` and `__str__` simply do the same

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,64 @@
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self, max_size: int):
+        self._top = -1
+        self._a = []
+        if max_size <= 0:
+            raise ValueError("Dude!!! Stack with -ve or 0 Size is useless, are you lost?")
+        self._size = max_size
 
-s = myStack()
-s.push('1')
-s.push('2')
-print(s.pop())
-print(s.show())
+    def __str__(self):
+        if self.isEmpty():
+            return "Stack Empty, Nothing to show"
+        return "Stack: " + str(self._a)
+
+    def isEmpty(self) -> bool:
+        if self._top == -1:
+            return True
+        return False
+
+    def isFull(self) -> bool:
+        if self._size == self._top + 1:
+            return True
+        return False
+
+    def push(self, item: str) -> (bool, str):
+        if self.isFull():
+            return False, f"Push Operation: Stack Overflow, item {item} is not pushed onto the stack"
+        self._a.append(item)
+        self._top += 1
+        return True, item
+
+    def pop(self) -> (bool, str):
+        if self.isEmpty():
+            return False, f"Pop Operation: Stack Underflow, Nothing Popped out of the stack"
+        self._top -= 1
+        return True, self._a.pop(self._top+1)
+
+    def peek(self) -> (bool, str):
+        if self.isEmpty():
+            return False, f"Peek Operation: Stack Empty"
+        return True, self._a[self._top]
+
+    def size(self) -> int:
+        if self.isEmpty():
+            return 0
+        return self._top+1
+
+    def show(self):
+        if self.isEmpty():
+            print("Stack Empty, Nothing to show")
+            return
+        print(self._a)
+
+
+if __name__ == "__main__":
+    s = myStack(3)
+    s.push('1')
+    s.push('2')
+    print(s.size())
+    print(s.push('3'))
+    print(s.push('4'))
+    print(s.size())
+    print(s.peek())
+    print(s.pop())
+    s.show()
