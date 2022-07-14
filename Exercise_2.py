ca99b2fc2bc789bed440ae66a52f2dd6c339375e
Charan Sundar Telaganeni <charansundartelaganeni@charansacstudio.myfiosgateway.com>
2022-07-14 19:50:47 -0400

    completed precourse exercise 2

--- Exercise_2.py
+++ Exercise_2.py
@@ -5,11 +5,36 @@ class Node:
        self.next = None
  
 class Stack:
-    def __init__(self):
+    def __init__(self):  
+
+        self.head = None
         
-    def push(self, data):
+    def push(self, data): 
+
+        #time complexity: O(1)
+        #space complexity: O(n) 
+
+        if self.head == None:
+            self.head=Node(data)
+             
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+
         
-    def pop(self):
+    def pop(self):  
+
+        #time complexity: O(1) 
+        #space complexity: O(1) 
+
+        if self.head is None:
+                return None             
+        else:
+            popped_node = self.head
+            self.head = self.head.next
+            return popped_node.data
+
         
 a_stack = Stack()
 while True:
