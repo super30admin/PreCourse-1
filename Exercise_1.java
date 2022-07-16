f4f61604b337a1507c66bb738b30120db0ad45d0
Yatindra Mahadik <mahadik.yatindra@gmail.com>
2022-07-15 22:30:10 -0400

    Completed Sprint Precourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,6 @@
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
@@ -7,29 +10,46 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        if(top == -1){
+            return true;
+        } else return false;
     } 
+
+    boolean isFull(){
+        if (top == MAX){
+            return true;
+        } else return false;
+    }
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        if(isFull()){
+            return false;
+        } else {
+            ++top;
+            a[top] = x;
+            return true;
+        }
     } 
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if(isEmpty()) {
+            return 0;
+        } else {
+            top--;
+            return a[top + 1];
+        }
     } 
   
     int peek() 
     { 
-        //Write your code here
+        return a[top];
     } 
 } 
   
