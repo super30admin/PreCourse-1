00ded5cc717659bb3de980933c92528f3e3645ca
sai-pavan-kalyan <90740957+sai-pavan-kalyan@users.noreply.github.com>
2022-07-20 22:09:59 -0700

    precourse1 completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,30 +1,58 @@
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
     def __init__(self):
-        self.stack = []
+        self.stack =[]
+        self.size = 0
     def isEmpty(self):
-        return len(self.stack) == 0     
-    def push(self, item):
-        self.stack.append(item)
-    def pop(self):
-        if(self.isEmpty()):
-            print("stack underflow")
-            return 0
-        return self.stack.pop()  
+        return(self.size == 0)
+    def push(self, data):
+        self.stack.append(data)
+        self.size += 1
     def peek(self):
-        if (self.isEmpty()):
-            return 0
-        return self.stack[len(self.stack) - 1]
-    def size(self):
-         return len(self.stack)
-    def show(self):     
-        while(self.isEmpty() != True):
-            print(self.stack.pop())
-
-s = myStack()
-s.push('1')
-s.push('2')
-print(s.pop())
-print(s.show())
-
+        if self.isEmpty():
+            return None
+        else:
+            return self.stack[-1]
+    def show(self):
+        return self.stack
+    def pop(self):
+        if self.isEmpty():
+            return None
+        else:
+            self.size -= 1
+            return self.stack.pop(-1)
+    
+            
+        
+a_stack = myStack()
+while True:
+    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    print('push <value>')
+    print('pop')
+    print('peek')
+    print('isEmpty')
+    print('show')
+    print('quit')
+    print('size')
+    do = input('What would you like to do? ').split()
+    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    operation = do[0].strip().lower()
+    if operation == 'push':
+        a_stack.push(int(do[1]))
+    elif operation == 'pop':
+        popped = a_stack.pop()
+        if popped is None:
+            print('Stack is empty.')
+        else:
+            print('Popped value: ', int(popped))
+    elif operation == 'peek':
+        print(a_stack.peek())
+    elif operation == 'show':
+        print(a_stack.show()) 
+    elif operation == 'isempty':
+        print(a_stack.isEmpty())        
+    elif operation == 'quit':
+        break
+    elif operation == 'size':
+        print(a_stack.size)
+    else:
+        print('please enter a valid input')    
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,15 +1,10 @@
 class ListNode:
-    """
-    A node in a singly-linked list.
-    """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = None
     
 class SinglyLinkedList:
     def __init__(self):
-        """
-        Create a new singly-linked list.
-        Takes O(1) time.
-        """
         self.head = None
 
     def append(self, data):
@@ -17,6 +12,13 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        if self.head == None:
+           self.head = ListNode(data)
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = ListNode(data)    
         
     def find(self, key):
         """
@@ -24,9 +26,59 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        if self.head == None:
+            return
+        else:
+            temp = self.head
+            while temp != None:
+                if temp.data == key:
+                    return key
+                else:
+                    temp = temp.next   
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head == None:
+            return
+        else:
+            if self.head.next is None:
+                if self.head.data == key:
+                    self.head = None
+                    print(key,'is removed')      
+            else:    
+                temp = self.head
+                prev = self.head
+                while temp != None:
+                    if temp.data == key:
+                        if prev == self.head:
+                            self.head = temp.next
+                        else:
+                            prev = temp.next
+                        print(key,'is removed')
+                        return
+                    else:
+                        prev = temp
+                        temp = temp.next
+                        
+
+mySingly = SinglyLinkedList()
+while True:
+    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    print('append <> value')
+    print('find <> value')
+    print('remove <> value')
+    print('quit')
+    do = input("What would you like to do?").split()
+    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    operation = do[0].strip().lower()
+    if operation == 'append':
+        mySingly.append(int(do[1]))
+    elif operation == 'find':
+        print(mySingly.find(int(do[1])))
+    elif operation == 'quit':
+        break
+    elif operation == 'remove':
+        mySingly.remove(int(do[1]))
+ 
