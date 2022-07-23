a39281e5cc7c3d4369b727a801c6cf0bb3713177
SIDDHI1239 <siddhi.chaturvedi93@gmail.com>
2022-07-22 17:25:43 -0700

    Adding solutions exercise 1 to 3

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,72 @@
+"""
+Time Complexity : O(1) for all operations except, show(), it takes O(n),
+n is the number of elements in the stack
+Space Complexity : O(n)
+Did this code successfully run on Leetcode : could not find it on leetcode
+Any problem you faced while coding this : No
+
+
+Your code here along with comments explaining your approach
+
+"""
+
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
+
+  def __init__(self):
+      """
+      create an empty stack
+      """
+      self.data = []
+    
+  def push(self, d):
+      """
+      Add an element to the  top of the stack
+      """
+      self.data.append(d)
+      
+  def pop_s(self):
+      """
+      pop the  element from the top of the stack
+      """
+      if self.is_empty():
+          raise IndexError('stack is empty')
+          
+      else:
+          return self.data.pop()
+      
+  def peek(self):
+      """
+      return the top element of the stack
+      """
+      if self.is_empty():
+          raise IndexError('stack is empty')
+      else:
+          return self.data[-1]
+      
+  def is_empty(self):
+      """
+      returns a boolean, checks if the stack is empty or not
+      """
+      return len(self.data) == 0
+
+  def size(self):
+      """
+      return length of the stack
+      """
+      return len(self.data)
+
+  def show(self):
+      """
+      display the elements of the stack
+      """
+      if self.is_empty():
+          raise IndexError('stack is empty')
+      
+      for i in range(len(self.data)):
+          print(self.data[i], end = " ")
+      print(" ")
+          
         
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
 
 s = myStack()
 s.push('1')
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,4 +1,13 @@
+"""
+Time Complexity : O(1) for all operations 
+Space Complexity : O(n)
+Did this code successfully run on Leetcode : could not find it on leetcode
+Any problem you faced while coding this : No
 
+
+Your code here along with comments explaining your approach
+
+"""
 class Node:
     def __init__(self, data):
        self.data = data
@@ -6,10 +15,37 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.head = None
+        self._size = 0
+
+    def is_empty(self):
+        """
+        returns a boolean, checks if the stack is empty or not
+        """
+        return self._size == 0
+    
     def push(self, data):
+        """
+        Add an element to the  top of the stack. Starting of the linked list.
+        """
+        node = Node(data)
+        node.next = self.head
+        self.head = node
+        self._size += 1
         
     def pop(self):
+        """
+        pop an element to the  top of the stack. Starting of the linked list.
+        """
+        if self.is_empty():
+            print('Stack is empty')
+            
+        return_data = self.head.data
+        self.head = self.head.next
+        self._size -= 1
+        
+        return return_data
+
         
 a_stack = Stack()
 while True:
@@ -30,3 +66,4 @@ while True:
             print('Popped value: ', int(popped))
     elif operation == 'quit':
         break
+         
