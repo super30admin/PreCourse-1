4f4794a7fa01c11c1b08322401bcb2aba4485e17
Rohan Acharya <rohachar@cisco.com>
2022-07-24 16:11:13 -0500

    exercise1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,69 @@
-class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+# Time Complexity : push -> O(1), pop -> O(1), peek -> O(1), is_empty -> O(1), size -> O(1), show -> O(n)
+# Space Complexity : 0(1) since max it can go is maxsize (1000)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : Minor bugs
 
-s = myStack()
+class my_stack:
+	
+	def __init__(self):
+    	# stack implmented as list in python with given size
+		self.maxsize=1000
+		self.stack=[]
+		self.error_message="Stack overflow"
+        
+	def is_empty(self):
+		# if list is empty
+		if not self.stack:
+			return True
+		return False
+		
+	def push(self, item):
+		# append list to push
+		if self.size() < self.maxsize:
+			self.stack.append(item)
+		else:
+			return self.error_message
+
+	def pop(self):
+		# pop from list to remove last item
+		if self.size():
+			return self.stack.pop()
+		else:
+			return self.error_message
+
+		
+		
+	def peek(self):
+		# return topmost element
+		if self.size():
+			return self.stack[-1]
+		else:
+			return self.error_message
+    
+	def size(self):
+		# return current len of stack
+		return len(self.stack)
+  
+	def show(self):
+		# print all elements in the stack
+		if not self.size():
+			return self.error_message
+		for i in range(self.size()):
+			print(self.stack[i], end=" ")
+
+
+      
+
+s = my_stack()
 s.push('1')
 s.push('2')
 print(s.pop())
-print(s.show())
+print(s.pop())
+print(s.pop())
+print(s.peek())
+print(s.size())
+print(s.is_empty())
+
+
+
+
