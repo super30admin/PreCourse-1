ef6f1c07f7d150ff4a7d107f182ac9830c6540c3
Anurag Yadav <anrg.ydv@gmail.com>
2022-08-08 23:54:01 +0530

    testing git commands

--- Exercise_1.java
+++ Exercise_1.java
@@ -4,7 +4,7 @@ class Stack {
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
-  
+  /*
     boolean isEmpty() 
     { 
         //Write your code here 
@@ -30,7 +30,7 @@ class Stack {
     int peek() 
     { 
         //Write your code here
-    } 
+    } */
 } 
   
 // Driver code 
@@ -38,9 +38,12 @@ class Main {
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
-        s.push(10); 
+        /*s.push(10); 
         s.push(20); 
         s.push(30); 
+        
         System.out.println(s.pop() + " Popped from stack"); 
+        */
+        System.out.println("llo World!");
     } 
 }
