ef0202eb6c200dbc5591642386e25d34c592d6f3
zubinladwa <ladwazubin@gmail.com>
2022-08-08 22:46:28 -0400

    Completed Precourse-1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,36 @@
+from ctypes import sizeof
+
+
 class myStack:
+  
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
+  def __init__(self):
+    self.stack =[]
          
-     def isEmpty(self):
+  def isEmpty(self):
+    return self.stack == []
+       
          
-     def push(self, item):
+  def push(self, item):
+    self.stack.append(item)
          
-     def pop(self):
+  def pop(self):
+    if (not self.isEmpty()):
+      lastElement = self.stack[-1]
+      del(self.stack[-1])
+      return lastElement
+    else:
+      return("Stack already empty")
+    
+  def peek(self):
+    return self.stack[-1]
         
-        
-     def peek(self):
-        
-     def size(self):
+  def size(self):
+    return sizeof(self.stack)
          
-     def show(self):
+  def show(self):
+    return self.stack
          
 
 s = myStack()
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = None
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,13 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        if self.head == None:
+            self.head=ListNode(data)
+              
+        else:
+            newnode = ListNode(data)
+            newnode.next = self.head
+            self.head = newnode
         
     def find(self, key):
         """
@@ -24,9 +33,24 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        if self.isempty():
+            return None
+              
+        else:
+            return self.head.data
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.isempty():
+            return None
+              
+        else:
+            # Removes the head node and makes 
+            #the preceding one the new head
+            poppednode = self.head
+            self.head = self.head.next
+            poppednode.next = None
+            return poppednode.data
