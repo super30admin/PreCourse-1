d32889b89e5b177c6a373351d8d18181a2beae1f
dhalyal <dhalyal@hawk.iit.edu>
2022-08-09 20:41:04 -0500

    Completed Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,7 +2,18 @@ class ListNode:
     """
     A node in a singly-linked list.
     """
-    def __init__(self, data=None, next=None):
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+    
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -10,13 +21,17 @@ class SinglyLinkedList:
         Create a new singly-linked list.
         Takes O(1) time.
         """
-        self.head = None
+        # When the list is first initialized it has no nodes, so the head is set to None
+        self.head = None 
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new_node = ListNode(data)
+        new_node.set_next(self.head)
+        self.head = new_node
         
     def find(self, key):
         """
@@ -24,9 +39,65 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        current = self.head
+        found = False
+        while current and found is False:
+            if current.get_data() == key:
+                found = True
+            else:
+                current = current.get_next()
+        if current is None:
+            return None
+        return key
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        current = self.head
+        previous = None
+        found = False
+        while current and found is False:
+            if current.get_data() == key:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+        if current is None:
+            raise ValueError("Key not found in list")
+        if previous is None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
+
+
+s = SinglyLinkedList()
+
+# Test Case 1 ->
+s.append(1)
+s.append(2)
+s.append(2)
+s.append(2)
+print(s.find(2))
+print(s.remove(2))
+print(s.find(1))
+print(s.find(2))
+print(s.remove(2))
+print(s.remove(2))
+print(s.find(2))
+print(s.find(2))
+s.append(2)
+print(s.find(2))
+
+# Output ->
+
+# 2
+# None
+# 1
+# 2
+# None
+# None
+# None
+# None
+# 2
