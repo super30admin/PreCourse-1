144826374d78b3f2d6a058144f360a1b1cabb78e
dhalyal <dhalyal@hawk.iit.edu>
2022-08-09 20:55:41 -0500

    Partial Complete - Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -6,10 +6,41 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head = None
+        
+    def isempty(self):
+        if self.head == None:
+            return True
+        else:
+            return False
         
     def push(self, data):
-        
+        if self.head == None:
+            self.head=Node(data)
+              
+        else:
+            newnode = Node(data)
+            newnode.next = self.head
+            self.head = newnode
+            
     def pop(self):
+        if self.isempty():
+            return None        
+        else:
+            poppednode = self.head
+            self.head = self.head.next
+            poppednode.next = None
+            return poppednode.data
+        
+    def display(self):
+        iternode = self.head
+        if self.isempty():
+            print("Stack Underflow")
+        else:
+            while(iternode != None): 
+                print(iternode.data,"->",end = " ")
+                iternode = iternode.next
+            return
         
 a_stack = Stack()
 while True:
@@ -17,6 +48,7 @@ while True:
     print('push <value>')
     print('pop')
     print('quit')
+    print('display')
     do = input('What would you like to do? ').split()
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
@@ -28,5 +60,10 @@ while True:
             print('Stack is empty.')
         else:
             print('Popped value: ', int(popped))
+    elif operation == 'display':
+        if a_stack.head == None:
+            print('Stack is Empty')
+        else:
+            print('Present Stack is: ', a_stack)
     elif operation == 'quit':
         break
