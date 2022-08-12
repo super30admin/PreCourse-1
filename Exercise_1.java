85866c1fcc5be991579fc1af95e59ad90b6a1d5d
Mahesh1011VS <110795884+Mahesh1011VS@users.noreply.github.com>
2022-08-12 11:25:57 -0400

    Update Exercise_1.java
    First exercise

--- Exercise_1.java
+++ Exercise_1.java
@@ -8,33 +8,66 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        if (top==-1){
+           return true; 
+        }
+        else{
+        return false;
+        }
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
+        // Check for stack Overflow
+        if (top == MAX-1) {
+            System.out.println("Stack overflow");
+            return false;
+        } else {
         //Write your code here
+            top = top + 1;
+            a[top] = x;
+            return true;
+
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
+        if (top ==-1){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else{
+        int temp = a[top];
         //Write your code here
+        a[top]=0;
+        top--;
+        return temp;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(top==-1){
+         //If empty return 0 and print " Stack Underflow"    
+        return 0;
+        }
+        else{
+            return a[top];
+        }
     } 
 } 
   
 // Driver code 
-class Main { 
+public class Main{ 
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
@@ -42,5 +75,7 @@ class Main {
         s.push(20); 
         s.push(30); 
         System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.peek() + " Is the element at the top of the stack"); 
+        
     } 
 }
