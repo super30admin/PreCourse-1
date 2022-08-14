3cf5b0299b63217de705d64890dd643bec6befa7
Shrutika Mahurkar <shrutika.vmahurkar@gmail.com>
2022-08-14 00:28:06 -0700

    exercise1 done

--- Exercise_1.java
+++ Exercise_1.java
@@ -7,29 +7,58 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        boolean empty;
+        if(a.length == 0){
+            empty=true;
+        }
+        else{
+            empty =false;
+        }
+        return empty;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        this.top = -1;
     } 
   
     boolean push(int x) 
-    { 
+    {
+        boolean success;
         //Check for stack Overflow
         //Write your code here
+        if(top<a.length){
+            a[top+1] = x;
+            top++;
+            success=true;
+        }
+        else{
+            System.out.println("Stack Overflow");
+            success=false;
+        }
+        return success;
     } 
   
     int pop() 
-    { 
+    {
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(isEmpty()==false){
+            top=top-1;
+        }
+        else{
+            System.out.println("Stack Underflow");
+        }
+        return a[top+1];
     } 
   
     int peek() 
-    { 
+    {
         //Write your code here
+        int peekedValue = a[top];
+        return peekedValue;
     } 
 } 
   
