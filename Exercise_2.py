15c6456a35c741b21ef522361b2898876373c36e
marmu001 <111223004+marmu001@users.noreply.github.com>
2022-08-14 23:55:40 +0530

    Update Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,15 +1,49 @@
+# Time Complexity : O(1)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode :
+# Any problem you faced while coding this : None
+
+
+# Your code here along with comments explaining your approach
+
 
 class Node:
     def __init__(self, data):
        self.data = data
        self.next = None
- 
+       
+class linkedlist:
+    def __init__(self):
+         self.head=None
+         
+    def __iter__(self):
+        cur = self.head
+        while cur:
+            yield cur
+            cur = cur.next
+
 class Stack:
     def __init__(self):
+        self.stack = linkedlist()
+    def isEmpty(self):
+        if self.stack.head is None:
+            return True
+        else:
+            return False
         
     def push(self, data):
+        node = Node(data)
+        node.next = self.stack.head
+        self.stack.head = node
         
     def pop(self):
+        if self.isEmpty():
+            return None
+        else:
+            value = self.stack.head.data
+            self.stack.head = self.stack.head.next
+            return value
+            
         
 a_stack = Stack()
 while True:
@@ -30,3 +64,4 @@ while True:
             print('Popped value: ', int(popped))
     elif operation == 'quit':
         break
+
