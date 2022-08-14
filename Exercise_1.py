5fb4619cbddc658daeb2f151df24ffae2ec39b29
armughan11 <52311490+armughan11@users.noreply.github.com>
2022-08-15 01:01:10 +0530

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,56 @@
+# Time Complexity : O(1)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode :
+# Any problem you faced while coding this : None
+
+
+# Your code here along with comments explaining your approach
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+        self.array =[]         
      def isEmpty(self):
+         if self.array == []:
+             return True
+         else:
+             return False
          
      def push(self, item):
+         self.array.append(item)
+         print("The item has been added")
          
      def pop(self):
+         if self.isEmpty():
+             return "Stack  is Empty"
+         else:
+             return self.array.pop()
+         
         
         
      def peek(self):
+         if self.isEmpty():
+             return "Stack  is Empty"
+         else:
+             return self.array[-1]
         
      def size(self):
+         return len(self.array)
          
      def show(self):
+         arr = self.array
+         if self.isEmpty():
+             print("Stack is empty")
+             return
+         for i in arr:
+             print(i, end= ' ')
          
 
 s = myStack()
 s.push('1')
 s.push('2')
-print(s.pop())
-print(s.show())
+s.push('3')
+
+s.pop()
+s.show()
