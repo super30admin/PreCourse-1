e903b89b9ad55a8659ba98e7e69a53b5af01eccc
Mandar Majalgaonkar <mandarjm@Mandars-MBP.lan>
2022-08-15 19:38:18 -0400

    Precourse-1 Completed

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,12 @@
+#!/Library/Frameworks/Python.framework/Versions/3.10/bin/python3
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        data=data
+        next=next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -10,6 +14,7 @@ class SinglyLinkedList:
         Create a new singly-linked list.
         Takes O(1) time.
         """
+        size=0
         self.head = None
 
     def append(self, data):
@@ -17,16 +22,38 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        newNode=ListNode(data)
+        if(self.size==0):
+            head= newNode
+            size=size+1
+            return
+        else:
+            temp=self.head
+            while(temp.next!=None):
+                temp=temp.next
+            temp.next=newNode
+            size=size+1
+            return
+    
     def find(self, key):
-        """
-        Search for the first element with `data` matching
-        `key`. Return the element or `None` if not found.
-        Takes O(n) time.
-        """
-        
+        temp=self.head
+        while(temp.next!=None):
+            if(temp.data==key):
+                return key
+            temp=temp.next
+        return None
+
     def remove(self, key):
-        """
-        Remove the first occurrence of `key` in the list.
-        Takes O(n) time.
-        """
+        temp=self.head
+        found=0
+        prev=temp
+        while(temp.next!=None):
+            if(temp.data==key):
+                found=1
+                break
+            prev=temp
+            temp=temp.next
+        
+        if(found==1):
+            prev.next=prev.next.next
+            size=size-1
