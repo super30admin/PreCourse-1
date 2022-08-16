d28f4d5f2a59d1c1bd8ef87400dcf12900593c74
Harnish Savadia <harnishsavadia@Harnishs-MacBook-Pro.local>
2022-08-16 17:00:10 -0400

    Completed Sprint PreCourse-1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,44 @@
+# Time Complexity : O(1) pushing and popping of elements 
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No problem faced
+
+
+# Your code here along with comments explaining your approach : treating an array 
+# as a stack and using last in first out approach
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
+  items = []
+
+  def __init__(self):
+    self.items = []
          
-     def isEmpty(self):
+  def isEmpty(self):
+    print(len(self.items) == 0)
          
-     def push(self, item):
+  def push(self, item):
+    self.items.append(item)
          
-     def pop(self):
+  def pop(self):
+    if len(self.items) == 0:
+      print('Empty list')
+    self.items.pop()
+    print('Last item popped from the list')
         
         
-     def peek(self):
+  def peek(self):
+    if len(self.items) == 0:
+      print('Stack is Empty')
+    print(self.items[len(self.items) - 1]) 
         
-     def size(self):
+  def size(self):
+    return len(self.items)
          
-     def show(self):
+  def show(self):
+    for i in self.items:
+      print(i)
          
 
 s = myStack()
--- Exercise_2.py
+++ Exercise_2.py
@@ -6,17 +6,35 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head = None
         
     def push(self, data):
+        if self.head == None:
+            self.head = Node(data)
+              
+        else:
+            newnode = Node(data)
+            newnode.next = self.head
+            self.head = newnode
         
     def pop(self):
+        if self.isempty():
+            return None
+              
+        else:
+            poppednode = self.head
+            self.head = self.head.next
+            poppednode.next = None
+            return poppednode.data
         
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    print('push <value>')
+    # print('push 10')
+    # a_stack.push(10)
     print('pop')
-    print('quit')
+    # a_stack.pop()
+    # print('quit')
     do = input('What would you like to do? ').split()
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
