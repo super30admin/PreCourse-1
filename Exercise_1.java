cf5030f3c7a900207b9a5163f554f84c6b0b01db
Zubin-glitch <zubintobias@gmail.com>
2022-08-23 17:39:03 -0400

    Completed Sprint Precourse-1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,41 +1,71 @@
 class Stack { 
     //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+    //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
     int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
+    int[] a; // Maximum size of Stack
+    int size;
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        return a == null || a.length == 0;
+
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        this.a = new int[MAX];
+        top=-1;
+        size = 0;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top<MAX){
+            top+=1;
+            a[top]=x;
+            return true;
+
+        }
+
+        else{
+            System.out.println("Stack overflow!");
+            return false;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
-    } 
+        if(top < 0) {
+            System.out.println("Stack underflow!");
+            return 0;
+        }
+        else{
+                int var = a[top];
+                System.out.println("Popped " + a[top]);
+                a[top] = 0;
+                top -= 1;
+                return var;
+        }
+    }
   
     int peek() 
     { 
         //Write your code here
+        System.out.println(a[top]);
+        return a[top];
     } 
 } 
   
 // Driver code 
 class Main { 
-    public static void main(String args[]) 
+    public static void main(String[] args)
     { 
         Stack s = new Stack(); 
         s.push(10); 
