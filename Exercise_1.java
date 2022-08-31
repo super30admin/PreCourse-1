348858a2c812c4b3e2e8348147eadcc373c5913c
N <49743648+nibfic99@users.noreply.github.com>
2022-08-31 18:51:00 -0400

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,66 @@
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
+ // Time Complexity  O(1)
+// Space Complexity  O(1)
+
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        return(top<0);
+        //returns true if top <0 and false if top>0
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top=-1;
+        //initializing top to
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
-    } 
+        if(top==MAX-1)
+        {
+        System.out.println("stack overflow");
+        return false;    
+        }
+        else{
+        top++; //incrementing the value of top
+        
+        a[top]=x;  //pushing a new element at new position
+        System.out.println("element pushed successfully");
+        return true;    
+    
+        }
+    }
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if (top==-1) //if top= -1 stack is empty
+        {
+            System.out.println("Stack Underflow");
+        }
+        else{
+        top--;
+        System.out.println( " top element popped from stack");     
+        }
+       return 0;
     } 
   
     int peek() 
     { 
-        //Write your code here
+        if(top==-1)
+        {
+            System.out.println("stack is empty");
+           
+        }
+        else
+        {
+          System.out.println(a[top] + "is the element present at top of the stack"); //printing top element in stack
+        }
     } 
 } 
   
@@ -41,6 +72,9 @@ class Main {
         s.push(10); 
         s.push(20); 
         s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        s.pop();
+        s.peek();
+      
+        
     } 
 }
