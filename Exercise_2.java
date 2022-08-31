b420bf25e894f6eef5a436d80575f450b22d6f01
Nimay Patel <60466954+Nimay-1709@users.noreply.github.com>
2022-08-31 16:02:31 -0700

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,45 +1,72 @@
-public class StackAsLinkedList { 
+// Time Complexity : Worst case : O(MAX) 
+// Space Complexity :Worst case : O(MAX)
+// Did this code successfully run on Leetcode : It runs successfully on Eclipse
+// Any problem you faced while coding this : Not much, other than checking for stack underflow
+
+public class Exercise_2 { 
   
     StackNode root; 
-  
+//private class defining single node
     static class StackNode { 
         int data; 
         StackNode next; 
-  
+     // Initialised constructor  
+
         StackNode(int data) 
         { 
             //Constructor here 
+        	this.data = data;
+        	this.next = null;
         } 
     } 
     
 	
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+        //checks for the condition if stack is empty. 
+    	return root == null;
     } 
   
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
+        //code to push data to the stack. 
+    	StackNode temp = new  StackNode(data);
+    	temp.next = root;
+    	root = temp;
     } 
   
     public int pop() 
     { 	
 	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+    	if (root == null) {
+            System.out.print("\nStack Underflow");
+            return  0;
+        }
+    	//code to pop the topmost element of stack.
+    	int popEle = root.data;
+        // update the top pointer to point to the next node
+        root = (root).next;
+      //return the popped element 
+        return popEle;
     } 
   
     public int peek() 
     { 
-        //Write code to just return the topmost element without removing it.
+        // code to just return the topmost element without removing it.
+    	if(!isEmpty())
+    		return root.data;
+    	else
+    	{
+    		System.out.println("Stack is empty");
+    		return -1;
+    	}
     } 
-  
-	//Driver code
+    
+//Driver code
     public static void main(String[] args) 
     { 
   
-        StackAsLinkedList sll = new StackAsLinkedList(); 
+        Exercise_2 sll = new Exercise_2(); 
   
         sll.push(10); 
         sll.push(20); 
