91f04ee1c85984c0c154d07237596fd5d18048c9
Shubham Ayachit <shubham.s.ayachit@gmail.com>
2022-08-31 17:13:53 -0400

    Completed PreCourse_1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,6 +1,8 @@
 class Stack { 
     //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+    //Kindly include Time and Space complexity at top of each file
+    // Time Complexity : O(1)
+    // Space Complexity : O(MAX) or O(n)
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
@@ -8,28 +10,60 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        //Approch: Check if the top is pointing to 0th position in array and return boolean based on that.
+        if(top == 0) {
+            return true;
+        } else {
+            return false;
+        }
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        //Initializing top to 0th position of array.
+        top = 0;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        //Check if top is not pointing to last position of array. 
+        //If not pointing to last position, assign value to that position and increment top.
+        if(top < MAX-1) {
+            a[top] = x;
+            top++;
+            return true;
+        } else {
+            return false;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        //Check if top is not pointing to 0th position, then get value of at position top-1 and decrease top.
+        if(top > 0) {
+            int popped = a[top-1];
+            top--;
+            return popped;
+        } else {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        //Check if top is pointing at 0th position, and then retrun value at top-1 position in array.
+        if(top > 0) {
+            return a[top-1];
+        }else {
+            return a[top];
+        }
     } 
 } 
   
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,10 @@
-import java.io.*; 
   
+//Complexity:
+//Time Complexity : O(n)
+//Space Complexity : O(n)
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList { 
   
     Node head; // head of list 
   
@@ -18,6 +20,8 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            this.data = d;
+            this.next = null;
         } 
     } 
   
@@ -25,15 +29,25 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
-   
+        Node n = new Node(data);
         // If the Linked List is empty, 
         // then make the new node as head 
-        
+        if(list.head == null) {
+            list.head = n;
+        } else {
             // Else traverse till the last node 
             // and insert the new_node there 
-
+            Node ne = list.head;
+            while(list.head.next != null) {
+                list.head = list.head.next;
+            }
+            list.head.next = n;
+            list.head = ne;
             // Insert the new_node at last node 
+        }
         // Return the list by head 
+        return list;
+        
         
     } 
   
@@ -41,7 +55,10 @@ public class LinkedList {
     public static void printList(LinkedList list) 
     {  
         // Traverse through the LinkedList 
-   
+        while(list.head != null) {
+            System.out.println(list.head.data);
+            list.head = list.head.next;
+        }
             // Print the data at current node 
        
             // Go to next node 
