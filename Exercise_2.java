3e521367eb5224e5a8cb69eabb3aa3f106b429d4
RASHI <44283896+rashidhir6@users.noreply.github.com>
2022-09-01 15:00:04 -0400

    Solved exercise 2

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,6 +1,15 @@
+/*
+Time Complexity: O(1) for all operations
+Space Complexity: O(n)
+Did this code successfully run on Leetcode : N/A
+Any problem you faced while coding this : No
+*/
+
 public class StackAsLinkedList { 
   
-    StackNode root; 
+    StackNode root;
+    StackNode first;
+   
   
     static class StackNode { 
         int data; 
@@ -9,6 +18,8 @@ public class StackAsLinkedList {
         StackNode(int data) 
         { 
             //Constructor here 
+            this.data = data;
+            this.next = null;
         } 
     } 
     
@@ -16,11 +27,23 @@ public class StackAsLinkedList {
     public boolean isEmpty() 
     { 
         //Write your code here for the condition if stack is empty. 
+        if(root==null)
+        return true;
+        return false;
     } 
   
     public void push(int data) 
     { 
         //Write code to push data to the stack. 
+        StackNode temp = new StackNode(node);
+        if(first==null){
+            first= temp;
+            root = temp;
+        }
+       else{
+        root.next = temp;
+        root = temp;
+       }
     } 
   
     public int pop() 
@@ -28,11 +51,22 @@ public class StackAsLinkedList {
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
+    StackNode temp = new StackNode(-1);
+    int num=root.data;
+    temp.next = first;
+    while(temp.next.next!=root){
+        temp=temp.next;
+    }
+    temp.next=null;
+    root=temp;
+    return num;
+      
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+        return root.data;
     } 
   
 	//Driver code
