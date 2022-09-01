d0e6161272c714c187d354e500915ea6441908ba
Darshan Shanthveer Patil <42darshu.dp@gmail.com>
2022-09-01 00:47:24 -0500

    Completed Sprint PreCourse-1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,37 @@
+# Time complexity - Push operation --> O(n)
+#                   Pop operation --> O(1)
+#                   peek operation --> O(1)
+#                   show operation --> O(1)
+# Total Time complexity --> O(n)
+# Space Complexity --> O(n) for the array of size n
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+      self.items = []
          
      def isEmpty(self):
+      self.items == []
+      return self.items
+
          
      def push(self, item):
+       if item not in self.items:
+        self.items.append(item)
          
      def pop(self):
-        
-        
+        return self.items.pop()
+
      def peek(self):
+      return self.items[0]
         
      def size(self):
-         
+         return len(self.items)
+
      def show(self):
+      return self.items
          
 
 s = myStack()
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,22 @@
+#time complexity --> O(1)
+#space complexity --> O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+    def getData(self):
+        return self.data
+    def setData(self,data):
+        self.data = data
+    def getNextNode(self):
+        return self.next
+    def setNextNode(self,node):
+        self.next = node
     
 class SinglyLinkedList:
     def __init__(self):
@@ -11,12 +25,18 @@ class SinglyLinkedList:
         Takes O(1) time.
         """
         self.head = None
+        self.size = 0
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        node = ListNode(data, self.head)
+        self.head = node
+        # incrementing the size of the linked list
+        self.size += 1
+        return True
         
     def find(self, key):
         """
@@ -24,9 +44,28 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        curr = self.head
+        while curr:
+            if curr.getData() == key:
+                return True
+            else:
+                curr = curr.getNextNode()
+        return False
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        prev = None
+        curr = self.head
+        while curr:
+            if curr.getData() == key:
+                if prev:
+                    prev.setNextNode(curr.getNextNode())
+                else:
+                    self.head = curr.getNextNode()
+                return True
+                
+            prev = curr
+            curr = curr.getNextNode()
