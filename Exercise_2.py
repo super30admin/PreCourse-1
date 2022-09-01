17ba4712c462a07853b900748ffac2e2e7f92b9e
Diganth P <jayanth.jayanth.prakash@gmail.com>
2022-09-01 12:15:36 -0500

    completed pre-course 1

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,24 +1,52 @@
+'''
+Time Complexity is O(n)
+Space Complexity is O(n)
+'''
 
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
+
 class Stack:
     def __init__(self):
-        
+        self.head = None
+
+    def isEmpty(self):
+        # To check if stack is empty
+        if self.head == None:
+            return True
+        else:
+            return False
+
     def push(self, data):
-        
+        if self.head == None:
+            self.head = Node(data)
+        else:
+            newnode = Node(data)
+            newnode.next = self.head
+            self.head = newnode
+
     def pop(self):
-        
+        if self.isEmpty():
+            return None
+        else:
+            # removes head node
+            PoppedNode = self.head
+            self.head = self.head.next
+            PoppedNode.next = None
+            return PoppedNode.data
+
+
 a_stack = Stack()
 while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
