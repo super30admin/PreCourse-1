258c362aaf53d5d3872a5cb33bed0ddcafb66bc6
Ruchika Sharma <sharma.ruc@northeastern.edu>
2022-09-04 17:22:16 -0400

    Completed PreCourse-1 . Please review

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,12 @@
 import java.io.*; 
   
+// time complexity is O(1)
+//	Space complexity is O(1) 
+import java.io.*; 
+  
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList { 
   
     Node head; // head of list 
   
@@ -18,6 +22,8 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+        	data=d;
+        	next= null;
         } 
     } 
   
@@ -25,15 +31,29 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
-   
+    	
+    	Node  new_node = new Node(data);
+    	
         // If the Linked List is empty, 
         // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
+        if(list.head==null) {
+        	list.head = new_node;
+        }
+        // Else traverse till the last node 
+        // and insert the new_node there 
 
-            // Insert the new_node at last node 
+        else {
+        	Node last = list.head;
+        	while(last.next!=null) {
+        		last = last.next;
+        	}
+        	
+        	 // Insert the new_node at last node 
+        	last.next=  new_node;
+        }
+              
         // Return the list by head 
+        return list;
         
     } 
   
@@ -41,11 +61,15 @@ public class LinkedList {
     public static void printList(LinkedList list) 
     {  
         // Traverse through the LinkedList 
-   
+    	Node currentNode= list.head;
+    	while(currentNode !=null) {
             // Print the data at current node 
-       
-            // Go to next node 
-    } 
+    		System.out.print(currentNode.data+" ");
+			// Go to next node 
+    		currentNode= currentNode.next;
+            
+    	}
+    }
    
     // Driver code 
     public static void main(String[] args) 
