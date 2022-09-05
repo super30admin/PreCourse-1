48cbe2d5ccc09416ff714c1ed6872e5d5a426359
Nandish Amin <nandish.amin@gmail.com>
2022-09-04 21:47:10 -0700

    Completed the Precourse-1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,32 @@
+# Time Complexity of all operations: O(1)
+# Space Complexity - O(n)
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
+  def __init__(self):
+    self.elements = list()
+
+  def isEmpty(self):
+    if len(self.elements)==0:
+      return True
+    else:
+      return False
+
+  def push(self, item):
+    self.elements.append(item)
+
+  def pop(self):
+    self.elements.pop()
         
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+  def peek(self):
+    return self.elements[-1]
+
+  def size(self):
+    return(len(self.elements))
+
+  def show(self):
+    return self.elements
 
 s = myStack()
 s.push('1')
