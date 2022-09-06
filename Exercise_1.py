176b99945ef46dfcdea8ac38dd045c8aab407f1b
Suriya Palanikumar <suriya.palanikumar@sjsu.edu>
2022-09-05 17:13:10 -0700

    Implemented code logic in Precourse-1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,50 @@
+'''
+Time Complexity
+    - isEmpty : O(1)
+    - push : O(1)
+    - pop : O(1)
+    - peek : O(1)
+    - size : O(1)
+    - show : O(N)
+
+Space Complexity: O(N)
+
+'''
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
+
+    def __init__(self):
+        self.stack = []
         
+    def isEmpty(self):
+        if(len(self.stack)==0):
+            return True
+        return False
         
-     def peek(self):
+    def push(self, item):
+        self.stack.insert(len(self.stack), item)
         
-     def size(self):
-         
-     def show(self):
+    def pop(self):
+        if(len(self.stack)==0):
+            return "Empty Stack"
+        top_ele = self.stack[len(self.stack)-1]
+        self.stack = self.stack[:-1]
+        return top_ele
+
+    def peek(self):
+        return self.stack[len(self.stack)]
+
+    def size(self):
+        return len(self.stack)
+        
+    def show(self):
+        # I assume show() returns the stack from top to bottom
+        rev_list = []
+        for i in range(len(self.stack)-1, -1, -1):
+            print(self.stack[i])
+            rev_list.append(self.stack[i])  
+        return rev_list
          
 
 s = myStack()
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,20 @@
+'''
+Time Complexity
+    - append : O(N)
+    - find : O(N)
+    - remove : O(N)
+
+
+Space Complexity: O(N)
+
+'''
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,16 +29,56 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        temp = ListNode(data= data)
+        if(self.head):
+            dummy = self.head
+            while(dummy):
+                if(not dummy.next):
+                    dummy.next = temp
+                    break
+                dummy = dummy.next
+        else:
+            self.head = temp
+            
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        dummy_node = self.head
+        while(dummy_node):
+            if(dummy_node.data == key):
+                return dummy_node
+                break
+            dummy_node = dummy_node.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        dummy_node = self.head
+        while(dummy_node.next):
+            if(dummy_node.next.data == key):
+                dummy_node.next = dummy_node.next.next
+                break
+            dummy_node = dummy_node.next
+        return self.head  
+
+    def showdata(self):
+        dummy_node = self.head
+        while(dummy_node):
+            print(dummy_node.data)
+            dummy_node = dummy_node.next
+
+s = SinglyLinkedList()
+s.append(1)
+s.showdata()
+s.append(2)
+s.showdata()
+s.remove(2)
+s.showdata()
+print(s.find(3))
+
