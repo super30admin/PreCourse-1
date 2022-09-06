d1fc88108a55c442fa92d326d2cbf270dc45d7b6
Prerana Rajeshvar Kharat <preranarajeshvarkharat@Preranas-MacBook-Pro.local>
2022-09-06 15:40:52 -0700

    Completed Sprint Precourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,7 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : did not know where to go on leetcode
+// Any problem you faced while coding this : No
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
@@ -8,28 +12,48 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        if(top==-1){
+            return true;
+        }
+        return false;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top=-1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top>=(MAX-1)){
+            System.out.println("Stack Overflow");
+            return false;
+        }else{
+            a[++top]=x;
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if (isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        }else{
+            int x= a[top--];
+            return x;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        return a[top];
     } 
 } 
   
