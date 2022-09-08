142e117e59347a87ed8f4de4dd194733bba764d4
Manan Tarun Shah (Student) <mshah64@sundevils.asu.edu>
2022-09-08 12:04:38 -0700

    Completed Precourse-1

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,24 +1,48 @@
-
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
 class Stack:
     def __init__(self):
-        
+        self.head = None
+
+    # Time Complexity: O(N)
+    # Space Complexity: O(1)
     def push(self, data):
-        
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = new_node
+
+    # Time Complexity: O(N)
+    # Space Complexity: Constant time
     def pop(self):
-        
+        if self.head is None:
+            return None
+        elif self.head.next is None:
+            tempdata = self.head.data
+            self.head = None
+            return tempdata
+        temp = self.head
+        while temp.next.next is not None:
+            temp = temp.next
+        popping = temp.next.data
+        temp.next = None
+        return popping
+
 a_stack = Stack()
 while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
--- Exercise_3.py
+++ Exercise_3.py
@@ -2,8 +2,12 @@ class ListNode:
     """
     A node in a singly-linked list.
     """
+
+    # Each node has data and a pointer to the next node
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = None
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -12,21 +16,69 @@ class SinglyLinkedList:
         """
         self.head = None
 
+    # Time Complexity: O(N)
+    # Space Complexity: O(1)
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        new_node = ListNode(data)
+        # If linked list is empty, the new node will be head.
+        if self.head is None:
+            self.head = new_node
+            return
+
+        # We need to traverse to the end of the linked list to append a node at the end.
+        temp = self.head
+        while temp.next is not None:
+            temp = temp.next
+        temp.next = new_node
+
+    # Time Complexity: O(N)
+    # Space Complexity: O(1)
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        # If linked list is empty, nothing to find.
+        if self.head is None:
+            return None
+
+        # Need to check till the end of the linked list for the key.
+        temp = self.head
+        while temp is not None:
+            if temp.data == key:
+                return temp
+            temp = temp.next
+        return None
+
+    # Time Complexity: O(N)
+    # Space Complexity: O(1)
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        # If linked list is empty, nothing to remove.
+        if self.head is None:
+            return
+
+        # If data of head is the key, then we need to remove this head and the next node becomes the
+        # new head.
+        if self.head.data == key:
+            self.head = self.head.next
+
+        # We traverse the entire list to find the key and return "Key not found" if we do not find the
+        # key even after checking all nodes.
+        else:
+            temp = self.head
+            while temp is not None:
+                if temp.next is None:
+                    return "Key not found"
+                if temp.next.data == key:
+                    temp.next = temp.next.next
+                    return
+                temp = temp.next
