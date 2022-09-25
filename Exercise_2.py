e0b097b52b13aadd2bd50462342e5479de62a9a9
Mitalee Minde <mitaleeminde@gmail.com>
2022-09-25 12:39:40 -0400

    commit all precourse req

--- Exercise_2.py
+++ Exercise_2.py
@@ -6,10 +6,35 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head = None #since 1st we initialize head to none
         
     def push(self, data):
-        
+        if self.head == None: #ie if the LL is blank then this we become the 1st elem
+            self.head=Node(data)
+            return
+
+        itr = self.head
+        while itr.next: #push to the end of list
+            itr= itr.next
+
+        itr.next = Node(data)
+    
     def pop(self):
+        if self.head is None: #ie if the LL is blank then this we become the 1st elem
+            return None
+
+        itr = self.head
+    
+        while itr.next:
+            if itr.next.next == None: #we have reached 2nd last element
+                pop = itr.next.data
+                itr.next = None
+                return pop
+            itr = itr.next
+        
+        pop=itr.data
+        self.head=None
+        return pop
         
 a_stack = Stack()
 while True:
