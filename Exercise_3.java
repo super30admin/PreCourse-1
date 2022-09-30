e5f5cbf5cc5e78d6b6ff2e597bff670b29109ef7
eshwar2807 <114554105+eshwar2807@users.noreply.github.com>
2022-09-29 21:54:53 -0700

    Update Exercise_3.java
    Time constraints:
    stack using array list=o(1)
    stack using linked list=o(1)
    Singly kinked list=o(n)

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,70 +1,49 @@
 import java.io.*; 
-  
-// Java program to implement 
-// a Singly Linked List 
+
 public class LinkedList { 
-  
-    Node head; // head of list 
-  
-    // Linked list Node. 
-    // This inner class is made static 
-    // so that main() can access it 
+   Node head;
+    
     static class Node { 
-  
         int data; 
         Node next; 
-  
-        // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+           data = d; 
+           next = null;
         } 
     } 
   
-    // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
-    { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
-
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
-    } 
-  
-    // Method to print the LinkedList. 
+    {
+        Node x = new Node(data);
+        if (list.head == null) {
+            list.head = x;
+        }
+        else {
+            Node y = list.head;
+            while (y.next != null) {
+                y = y.next;
+            }
+            y.next = x;
+        }
+        return list;
+    }
     public static void printList(LinkedList list) 
-    {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
-    } 
-   
-    // Driver code 
+    {
+        Node node = list.head;
+        while (node != null) {
+            System.out.print(node.data);
+            node = node.next;
+        }
+    }
     public static void main(String[] args) 
     { 
-        /* Start with the empty list. */
         LinkedList list = new LinkedList(); 
-  
-        // 
-        // ******INSERTION****** 
-        // 
-  
-        // Insert the values 
         list = insert(list, 1); 
         list = insert(list, 2); 
         list = insert(list, 3); 
         list = insert(list, 4); 
-        list = insert(list, 5); 
-  
-        // Print the LinkedList 
+        list = insert(list, 5);
         printList(list); 
     } 
 }
