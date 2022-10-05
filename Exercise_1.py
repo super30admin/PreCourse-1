d17a509358cb4dc329aa3bbcc3db79be159d3dea
Krishna Vardhan <36471981+krishnamandanapu@users.noreply.github.com>
2022-10-05 03:04:07 -0400

    Completed Tasks

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,39 @@
+# Time Complexity is O(1)
+# Space Complexity is O(n) --> no of elements we store in the stack
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self):
+        self.stack = []
+        self._size = 0
+
+    def isEmpty(self):
+        return True if self._size == 0 else False
+
+    def push(self, item):
+        self.stack.append(item)
+        self._size += 1
+
+    def pop(self):
+        if not self.isEmpty():
+            last = self.stack[-1]
+            del (self.stack[-1])
+            self._size -= 1
+            return last
+        else:
+            print("Stack is empty")
+
+    def peek(self):
+        return self.stack[-1]
+
+    def size(self):
+        return self._size
+
+    def show(self):
+        return self.stack
+
 
 s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
+print(s.size())
 print(s.show())
