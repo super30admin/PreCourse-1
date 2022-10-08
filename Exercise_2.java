7d96077d0144ef1ab714fec8294e2ee2cdaeff4d
Sumit <ss4366@njit.edu>
2022-10-08 06:32:33 -0400

    Exercise 1,2,3 done

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,45 +1,73 @@
-public class StackAsLinkedList { 
-  
-    StackNode root; 
-  
-    static class StackNode { 
+class StackNode { 
         int data; 
         StackNode next; 
+}
+
+class StackAsLinkedList { 
   
-        StackNode(int data) 
-        { 
-            //Constructor here 
-        } 
-    } 
+    private StackNode root; 
+
+    public StackAsLinkedList(){
+        this.root = null;
+    }
     
-	
+    
+    
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+        //Write your code here for the condition if stack is empty.
+        return root == null; 
     } 
   
     public void push(int data) 
     { 
+        StackNode n = new StackNode();
+        n.data = data;
+        n.next = root;
+        
+        root = n;
+        
+        
         //Write code to push data to the stack. 
     } 
   
     public int pop() 
     { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
+	    //If Stack Empty Return 0 and print "Stack Underflow"
+        if (isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+
+        StackNode n = root;
+
+        int temp = root.data;
+        root = root.next;
+        
+        return temp;
         //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+	    //Also return the popped element
+        
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
-    } 
+        return root.data;
+
+
+    }
+}  
+
   
 	//Driver code
-    public static void main(String[] args) 
+class Main
+{
+  public static void main(String[] args) 
     { 
   
         StackAsLinkedList sll = new StackAsLinkedList(); 
+        
   
         sll.push(10); 
         sll.push(20); 
@@ -49,4 +77,4 @@ public class StackAsLinkedList {
   
         System.out.println("Top element is " + sll.peek()); 
     } 
-} 
+}
--- Exercise_3.java
+++ Exercise_3.java
@@ -2,13 +2,20 @@ import java.io.*;
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+ class LinkedList { 
   
     Node head; // head of list 
   
     // Linked list Node. 
     // This inner class is made static 
     // so that main() can access it 
+    // public LinkedList(){
+    //     this.head = null;
+        
+    // }
+    
+    
+    
     static class Node { 
   
         int data; 
@@ -18,6 +25,8 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            data = d;
+            next = null;
         } 
     } 
   
@@ -25,13 +34,28 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
-   
+        Node n = new Node(data);
+        // n.data = data;
         // If the Linked List is empty, 
+        if (list.head == null){
+            list.head = n;
+        }
         // then make the new node as head 
-        
+        else {
+            Node last = list.head;
+            while (last.next != null){
+                last = last.next;
+            }
+
+            last.next = n;
+            
+            // n.next = list.head;
+            // list.head = n;
             // Else traverse till the last node 
             // and insert the new_node there 
+        }
 
+        return list;
             // Insert the new_node at last node 
         // Return the list by head 
         
@@ -41,13 +65,24 @@ public class LinkedList {
     public static void printList(LinkedList list) 
     {  
         // Traverse through the LinkedList 
-   
+        Node n;
+        n = list.head;
+        
+        while (n != null){
+            System.out.println(n.data + "\n");
+            n = n.next;        
+        }
             // Print the data at current node 
        
             // Go to next node 
     } 
+    
    
+   
+    
     // Driver code 
+    
+   
     public static void main(String[] args) 
     { 
         /* Start with the empty list. */
