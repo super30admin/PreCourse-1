f3664eaaedd4b6bbb784acf72ff4f72c7a5f6688
r3tro7 <72056013+r3tro7@users.noreply.github.com>
2022-10-22 17:19:27 -0700

    Pre-Course-1 Completed

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,38 @@
+# Time Complexity: O(1), as insertion and deletion will not need traversal of any other Nodes
+# Space Complexity: O(N)
+
+from tkinter import Variable
+from xml.dom.minicompat import NodeList
 
 class Node:
     def __init__(self, data):
        self.data = data
        self.next = None
- 
+
 class Stack:
+    # Initialize a top pointer to represent the element on top of the stack
     def __init__(self):
-        
+        self.top = None
+
+    # create a new node and point the next of the node to the previous node if the top is not None
+    # otherwise, create a new node and assign it to the top
     def push(self, data):
-        
+        if self.top:
+            self.next = self.top
+            self.top = Node(data)
+        else:
+            self.top = Node(data)
+
+    # to pop a node first save the top node into a Variable
+    # then move the top pointer to the previous node
     def pop(self):
-        
+        if self.top:
+            current_top = self.top.data
+            self.top = self.top.next
+            return current_top
+        else:
+            return None
+
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,9 +1,13 @@
+from platform import node
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -12,21 +16,52 @@ class SinglyLinkedList:
         """
         self.head = None
 
+    # if the head is none -> the list is empty and assign head to the element to be appended
+    # else -> continue to iterate until the last element is reached(next pointer points to null)
+    # at the last element intialize the new node to appended and the previous pointer to this node
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        current = self.head
+        if current:
+            while current.next:
+                current = current.next
+            current.next = ListNode(data)
+        else:
+            self.head = ListNode(data)
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        current = self.head
+        if current:
+            while current:
+                if current.data == key:
+                    return current.data
+                current = current.next
+        return None
+
+    # initialize a prev node to keep track of the last node
+    # if found and its the first element: move the head pointer to the next node
+    # if not the first element point the last node's next pointer to the next node
+    # if not found the change prev to the current node and the current to the next node
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        current = self.head
+        prev = None
+        while current:
+            if current.data == key:
+                if prev:
+                    prev.next = current.next
+                    return current.data
+                self.head = current.next
+            prev = current
+            current = current.next
