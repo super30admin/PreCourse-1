bf1d25b6594dd340386b57f3dd9282d66cdc844a
Sushant <sush1393@gmail.com>
2022-10-25 18:59:18 -0400

    Completed Precourse-1

---
+++ Exercise_1.js
@@ -0,0 +1,27 @@
+class Stack {
+  constructor() {
+    this.array = [];
+  }
+
+  peek() {
+    return this.array[this.array.length - 1];
+  }
+
+  push(value) {
+    return this.array.push(value);
+  }
+
+  pop() {
+    return this.array.pop();
+  }
+}
+
+const myStack = new Stack();
+myStack.peek();
+myStack.push('google');
+myStack.push('udemy');
+myStack.push('discord');
+myStack.peek();
+myStack.pop();
+myStack.pop();
+myStack.pop();
