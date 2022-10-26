a6c2f60c7a22fc80630187b4ba24c4efdc796420
zeel <zeeltrivedi@csu.fullerton.edu>
2022-10-26 11:37:27 -0700

    done with the three excercise

--- Exercise_3.java
+++ Exercise_3.java
@@ -2,13 +2,13 @@ import java.io.*;
   
 // Java program to implement 
 // a Singly Linked List 
+//time complexity : O(n)
+//space compelxity : O(n)
 public class LinkedList { 
   
     Node head; // head of list 
   
-    // Linked list Node. 
-    // This inner class is made static 
-    // so that main() can access it 
+    
     static class Node { 
   
         int data; 
@@ -17,34 +17,51 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            data = d;
+            next = null;
         } 
     } 
   
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
     { 
-        // Create a new node with given data 
+        Node new_node = new Node(data);
+         
    
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
-
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
+       
+        if (list.head == null) {
+            list.head = new_node;
+        }
+        else {
+           
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+   
+           
+            last.next = new_node;
+        }
+   
+       
+        return list;
     } 
   
-    // Method to print the LinkedList. 
+    
     public static void printList(LinkedList list) 
     {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+        Node currentNode = list.head;
+    
+        System.out.print("LinkedList: ");
+    
+        
+        while (currentNode != null) {
+           
+            System.out.print(currentNode.data + " ");
+    
+           
+            currentNode = currentNode.next;
+        }
     } 
    
     // Driver code 
