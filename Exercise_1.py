eb7296cec0bd52d0d1f3d1b1b2441368c9636f96
Daksh Patel <82846365+dakshpatel01@users.noreply.github.com>
2022-10-26 14:13:12 -0400

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,36 @@
+#time complexity is O(1) as insertion or deletation in a stack takes constant time 
+#space complexity is O(1) as no extra space is required for the operations
+# Did this code successfully run on Leetcode :Not available
+# Any problem you faced while coding this : got little confused for def show(self) but had the correct idea
+
+from inspect import stack
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+      self.stack = []
          
      def isEmpty(self):
+      isEmpty.self == []
          
      def push(self, item):
-         
-     def pop(self):
+         self.stack.append(item)
         
+     def pop(self):
+         return self.stack.pop()
         
      def peek(self):
-        
+         return self.stack[-1]
+      
      def size(self):
-         
+         return len(self.stack)
      def show(self):
-         
+         return self.stack
 
 s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
 print(s.show())
+
