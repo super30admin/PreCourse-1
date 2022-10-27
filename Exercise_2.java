d9dc92e1b67cd5013c93102633334c5a0e720eb6
YukeshRaghav <88180372+YukeshRaghav@users.noreply.github.com>
2022-10-26 23:35:20 -0400

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -9,6 +9,12 @@ public class StackAsLinkedList {
         StackNode(int data) 
         { 
             //Constructor here 
+		int data;
+        StackNode next;
+  
+        StackNode(int data) { this.data = data; }
+    }
+  
         } 
     } 
     
@@ -16,11 +22,31 @@ public class StackAsLinkedList {
     public boolean isEmpty() 
     { 
         //Write your code here for the condition if stack is empty. 
+	    {
+        if (root == null) {
+            return true;
+        }
+        else
+            return false;
+    }
     } 
   
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
+        //Write code to push data to the stack.
+	    {
+        StackNode newNode = new StackNode(data);
+  
+        if (root == null) {
+            root = newNode;
+        }
+        else {
+            StackNode temp = root;
+            root = newNode;
+            newNode.next = temp;
+        }
+        System.out.println(data + " pushed to stack");
+    }
     } 
   
     public int pop() 
@@ -28,11 +54,32 @@ public class StackAsLinkedList {
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
+	    {
+        int popped = Integer.MIN_VALUE;
+        if (root == null) {
+            System.out.println("Stack is Empty");
+        }
+        else {
+            popped = root.data;
+            root = root.next;
+        }
+        return popped;
+    }
+  
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+	     {
+        if (root == null) {
+            System.out.println("Stack is empty");
+            return Integer.MIN_VALUE;
+        }
+        else {
+            return root.data;
+        }
+    }
     } 
   
 	//Driver code
