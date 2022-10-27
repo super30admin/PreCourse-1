d15de266d6cb16b3386b1680fefceeaeff7fb7ca
ZeelSatasiya <zeel.satasiya@gmail.com>
2022-10-26 19:45:20 -0700

    Completed Precourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,10 @@
+//Time Complexity :O(1)
+//Space Complexity :O(1)
+//Did this code successfully run on Leetcode :
+//Any problem you faced while coding this : No
+
+
+//Your code here along with comments explaining your approach
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
@@ -8,28 +15,57 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+    	return (top == -1 ? true : false );
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+    	top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
+    	boolean insert = true;
+    	if(top >= MAX-1) {
+    		//checks condition: if top is MAX-1 then its full
+    		insert = false;
+    	}
         //Write your code here
+    	else {
+    		a[++top] = x;
+    	}
+    	return insert;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
+    	int value;
+    	if(!isEmpty()) {
+    		value = 0;
+    		System.out.println("Stack Underflow");
+    	}
         //Write your code here
+    	else {
+    		value = a[top--];
+    	}
+    	return value;
     } 
   
     int peek() 
     { 
         //Write your code here
+    	int value;
+    	if(!isEmpty()) {
+    		value = 0;
+    	}
+        //Write your code here
+    	else {
+    		value = a[top];
+    	}
+    	return value;
     } 
 } 
   
