9785815073589744b9e0604f593ba7174e90a14b
Dhruv Vaghela <dhruvvaghela@dhruvs-mbp.myfiosgateway.com>
2022-10-26 20:46:08 -0400

    Test commit

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,26 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+  def __init__(self):
+      self.list = []
+
+  def isEmpty(self):
+    return len(self.list) == 0
+
+  def push(self, item):
+    self.list.append(item)
+
+  def pop(self):
+    return self.list.pop()
+    
+  def peek(self):
+    return self.list[-1]
+
+  def size(self):
+    return len(self.list)
+      
+  def show(self):
+    for idx, item in enumerate(self.list[::-1]): print(f"{item} -> ", end="") if idx != (len(self.list)-1) else print(item)
 
 s = myStack()
 s.push('1')
--- Exercise_2.py
+++ Exercise_2.py
@@ -2,16 +2,38 @@
 class Node:
     def __init__(self, data):
        self.data = data
-       self.next = None
+       self.next = None        
  
 class Stack:
     def __init__(self):
-        
+        self.ll = None
+        self.count = 0
+    
+    def __repr__(self):
+        while self.ll:
+            print(self.ll.data)
+        pass
+
     def push(self, data):
-        
+        if self.ll is None:
+            self.ll = Node(data)
+            self.count += 1
+            return data
+        new_node = Node(data)
+        new_node.next = self.ll
+        self.ll = new_node
+        self.count += 1
+
     def pop(self):
-        
+        print(self.ll)
+        if self.ll is None:
+            return None
+        popped_value = self.ll.data
+        self.ll = self.ll.next
+        self.count -= 1
+
 a_stack = Stack()
+
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
@@ -22,6 +44,7 @@ while True:
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
+        print(a_stack.count)
     elif operation == 'pop':
         popped = a_stack.pop()
         if popped is None:
