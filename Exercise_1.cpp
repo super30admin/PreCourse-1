58ccd2db661c1ac9ef4a4d795c426ff18eb920a5
Yuvaraj Nagi <yuvaraj@Yuvarajs-MacBook-Air.local>
2022-10-26 20:26:39 -0400

    Completed Sprint Precourse1

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,54 +1,66 @@
-#include <bits/stdc++.h> 
-  
-using namespace std; 
-  
-#define MAX 1000 
-  
-class Stack {
-  //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-    int top; 
-  
-public: 
-    int a[MAX]; // Maximum size of Stack 
-  
-    Stack() { //Constructor here } 
-    bool push(int x); 
-    int pop(); 
-    int peek(); 
-    bool isEmpty(); 
-}; 
-  
-bool Stack::push(int x) 
-{ 
-    //Your code here
-    //Check Stack overflow as well
-} 
-  
-int Stack::pop() 
-{ 
-    //Your code here
-    //Check Stack Underflow as well 
-} 
-int Stack::peek() 
-{ 
-    //Your code here
-    //Check empty condition too
-} 
-  
-bool Stack::isEmpty() 
-{ 
-    //Your code here 
-} 
-  
-// Driver program to test above functions 
-int main() 
-{ 
-    class Stack s; 
-    s.push(10); 
-    s.push(20); 
-    s.push(30); 
-    cout << s.pop() << " Popped from stack\n"; 
-  
-    return 0; 
-} 
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+#define MAX 1000
+
+int top = -1;
+int arr[MAX];
+
+int push(int num)
+{
+    if (top <= MAX)
+    {
+        top++;
+        arr[top] = num;
+    }
+
+    else
+    {
+        cout << "Stack Overflow";
+    }
+
+    return 0;
+}
+
+int pop()
+{
+    if (top <= -1)
+    {
+        cout << "Cannot perform pop operation as stack is empty";
+    }
+
+    else
+    {
+
+        cout << "The popped element is: " << arr[top] << endl;
+        top--;
+    }
+    return 0;
+}
+
+void peek()
+{
+    for (int i = 0; i <= top; i++)
+    {
+        cout << arr[i] << "\n";
+    }
+}
+
+int main()
+{
+    // Stack obj1;
+
+    // obj1.push(5);
+    // obj1.push(1);
+    // obj1.push(2);
+    // obj1.push(4);
+    // obj1.peek();
+
+    // return 0;
+    push(5);
+    push(10);
+    push(7);
+    // pop();
+    peek();
+}
