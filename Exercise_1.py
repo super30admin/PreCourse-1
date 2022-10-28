2afcdd0cfff716df0c0f4a7325875eae7f773a42
VijayMaddineni17 <vmaddin1@asu.edu>
2022-10-27 23:47:04 -0700

    Precourse-1 has been completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,52 @@
+from inspect import stack
+
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+       self.stack=[]#Initialized empty list
+      
      def isEmpty(self):
+       if len(self.stack)==0:#if there are no elements then printing stack is empty
+         print('Stack is Empty')
+       else:
+         return self.stack #Otherwise returning stack
          
      def push(self, item):
+       self.stack.append(item) #Adding element using append function
+       #print(self.stack)
          
      def pop(self):
+       if len(self.stack)==0:
+         print('Stack is empty')  #If Stack is empty
+       return self.stack.pop()
         
         
      def peek(self):
+       if len(self.stack)==0:
+         print('Stack is empty')
+       else:
+         print(self.stack[len(self.stack)-1]) #Printed last element in the list
         
      def size(self):
+       print(len(self.stack)) #To check Currnet size of stack
          
      def show(self):
+       return self.stack #Printing current elements in the stack.
          
 
 s = myStack()
+
 s.push('1')
 s.push('2')
+s.size()
+s.peek()
 print(s.pop())
 print(s.show())
+s.isEmpty()
+
+#Time complexity for this code is O(1)
+#Space complexity is O(1)
+
+
