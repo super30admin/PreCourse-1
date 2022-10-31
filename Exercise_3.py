6b0f3cb1b375f4ee1817c07a50f6391a173c262d
Daksh Patel <82846365+dakshpatel01@users.noreply.github.com>
2022-10-31 15:34:15 -0400

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,14 @@
+# time complexity of singly linked list = O(n)
+# space complexity of singly linked list = O(n)
+
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = None
     
 class SinglyLinkedList:
     def __init__(self):
@@ -11,14 +17,32 @@ class SinglyLinkedList:
         Takes O(1) time.
         """
         self.head = None
+        
 
     def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            x = self.head
+            while x.next != None:
+                x = x.next
+            x.next = new_node    
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
         
     def find(self, key):
+         if self.head == None:
+            return None
+        else:
+           current = self.head
+           if current.data == key:
+                return current.data
+           else:
+                current = current.next
+        return None   
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
@@ -26,6 +50,11 @@ class SinglyLinkedList:
         """
         
     def remove(self, key):
+        if self.head == None:
+            return None
+        else:
+            current = self.head
+            current.key = current.next.key
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
