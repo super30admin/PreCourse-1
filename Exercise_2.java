85316decdbe4288526b5935b1abefcde8a8d21d2
Seelam Vinay Kumar Reddy <vinayseelam.98@gmail.com>
2022-11-16 20:53:21 -0800

    Precourse1 Submission

--- Exercise_2.java
+++ Exercise_2.java
@@ -5,22 +5,44 @@ public class StackAsLinkedList {
     static class StackNode { 
         int data; 
         StackNode next; 
+        StackNode top = null;
   
         StackNode(int data) 
         { 
             //Constructor here 
+
+            this.data = data;
+            this.next = null;
+            this.top = null;
         } 
+       
     } 
     
 	
     public boolean isEmpty() 
     { 
         //Write your code here for the condition if stack is empty. 
+        if(top == null){
+            return true;
+        }
+        else{
+            return false;
+        }
     } 
   
     public void push(int data) 
     { 
         //Write code to push data to the stack. 
+        StackNode new_node = new  StackNode(data);
+
+        if(top == null){
+            top = data;
+        }
+        else
+        {
+            new_node.next = top;
+            top = new_node;
+        }
     } 
   
     public int pop() 
@@ -28,11 +50,21 @@ public class StackAsLinkedList {
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
+
+    if(top == null){
+        System.out.println("Stack underflow");
+        return 0;
+    }
+    else{
+        top = top.next;
+    }
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+
+        return top.data;
     } 
   
 	//Driver code
@@ -50,3 +82,15 @@ public class StackAsLinkedList {
         System.out.println("Top element is " + sll.peek()); 
     } 
 } 
+
+// Time Complexity : 0(1)
+// we can push n number of elements in linked list bcz we dont have any size restriction like array. We can pop any element at begining
+// Space Complexity : 0(n)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+// Here I am initilising one linked list by taking top as head. If I want to push one element I will just push it at the begining. Then my top will be assigned to newNode.next and newnode will be assigned to top.
+// Top pop one element I will just assign top.next to top. In LinkedList there is no constant memory so there will be no overflow.
--- Exercise_3.java
+++ Exercise_3.java
@@ -4,7 +4,7 @@ import java.io.*;
 // a Singly Linked List 
 public class LinkedList { 
   
-    Node head; // head of list 
+    Node head = null; // head of list 
   
     // Linked list Node. 
     // This inner class is made static 
@@ -17,22 +17,42 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            //Write your code here
+            this.data = d;
+            this.next = null; 
         } 
     } 
+    
   
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
+        LinkedList temp = list;
+        Node new_node = new Node(data);
    
         // If the Linked List is empty, 
         // then make the new node as head 
-        
+        if(head == null){
+            head = new_node;
+        }
+        else{
             // Else traverse till the last node 
-            // and insert the new_node there 
 
+            while(temp != null){
+                temp = temp.next;
+            }
             // Insert the new_node at last node 
+
+            temp = new_node;
+
+
+        }
+        return head;
+        
+            
+
+            
         // Return the list by head 
         
     } 
@@ -41,10 +61,23 @@ public class LinkedList {
     public static void printList(LinkedList list) 
     {  
         // Traverse through the LinkedList 
+        Node temp = head;
+
+        if(list == null){
+            System.out.println("There is no LL");
+        }
+        else{
+            while(temp != null){
+                 // Print the data at current node 
+                System.out.println(temp.data);
+                // Go to next node 
+                temp = temp.next;
+            }
+        }
    
-            // Print the data at current node 
+           
        
-            // Go to next node 
+            
     } 
    
     // Driver code 
@@ -68,3 +101,16 @@ public class LinkedList {
         printList(list); 
     } 
 }
+
+// Time Complexity : O(n)
+// For Insertion It will take n time 
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+// If My head is empty then There is no Linked List. If My head is not null I need to traverse till my last node using temp node.
+// Initially assigning temp to head and traversing till my temp is eual to null. if my temp is null then I will assign the new node to temp.
+// To print node data need to traverse Linked List using temp node till my temp is equal to null.
