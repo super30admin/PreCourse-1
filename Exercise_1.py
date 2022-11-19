c0f1fb2f35ef8675ae1f01524e0ed5b29b8c9d22
Girish Chhabra <Girish.Chhabra@ibm.com>
2022-11-19 06:23:06 +0530

    PreCourse 1 completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,33 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
+# // Time Complexity : O(1)
+# // Space Complexity : O(1)
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : No
      def __init__(self):
+      self.st = []
          
      def isEmpty(self):
+      return self.st
          
      def push(self, item):
+      self.st.append(item)
          
      def pop(self):
+      val = self.st.pop()
+      return val
         
         
      def peek(self):
+      if self.st:
+        return self.st[-1]
         
      def size(self):
+      return len(self.st)
          
      def show(self):
+      return self.st
          
 
 s = myStack()
