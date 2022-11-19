8d0f97959d9a021c63c529e42bfad2b7f772b729
tsaipraveen99 <saipraveen.tatiparthi@sjsu.edu>
2022-11-19 15:16:39 -0800

    Please Review

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,47 @@
+"""Space Complexity : O(n)
+Time Complexity : O(1)"""
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
+  def __init__(self):
+    self.arr = []
+    self.top = None
          
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+  def isEmpty(self):
+    if len(self.arr)==0:
+      return True               #returns True if the stack is empty
+    else: 
+      return False              #returns False if the stack is not empty
 
+  def push(self, item):
+    self.arr = [item] + self.arr    #add the item to the first position and then appends the existing list
+    self.top = item                 #New item is set as the Top 
+         
+  def pop(self):
+    if self.top is None: 
+      return ("Stack is already empty")
+    else:
+    #First element is removed and taken into a temp var
+    #If the list is not empty, then the top is set, else it is set to None.
+      tempTop = self.arr.pop(0) 
+      if not self.isEmpty():
+        self.top = self.arr[0]
+      else:
+        self.top = None
+      return tempTop
+        
+  def peek(self):
+    return self.top
+        
+  def size(self):
+    return len(self.arr)
+         
+  def show(self):
+    return self.arr
+         
 s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
+print(s.pop())
 print(s.show())
+print(s.peek())
+print(s.isEmpty())
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,9 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        print("I am in the init function")
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,16 +20,62 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        print("I am in the append function")
+        if self.head is None: 
+            self.head = ListNode(data)
+            return
+        current = self.head
+        while current.next is not None:
+            current = current.next 
+        current.next = ListNode(data)
+
     def find(self, key):
+        print("I am in the find function")
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        if self.head is None:
+            return None
+        current = self.head
+        position = 0
+        while current is not None:
+            position+=1
+            if current.data == key:
+                return "The key is present at the {} position".format(position)
+            current = current.next
+        return None
         
     def remove(self, key):
+        print("I am in the remove function")
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        current = self.head
+        if current is None:
+            return
+        if current.data == key:
+            self.head = current.next 
+            del current
+            return
+        previous = None
+        while current.next is not None:
+            if current.data == key:
+                previous.next = current.next
+                del current
+                return
+            current = current.next
+            previous = current
+        return None
+
+linkedlistObject = SinglyLinkedList()
+linkedlistObject.append(2)
+linkedlistObject.append(2)
+print(linkedlistObject.find(2))
+linkedlistObject.remove(2)
+print(linkedlistObject.find(2))
+linkedlistObject.remove(2)
+print(linkedlistObject.find(2))
+
