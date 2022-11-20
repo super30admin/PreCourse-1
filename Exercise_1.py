05680cad791d87e21aaef955321d0701cfd73c00
Gagan Ujjini <gaganujjini@Gagans-MacBook-Air.local>
2022-11-20 01:09:56 -0600

    Completed Spring Precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,53 @@
+### Time complexity: ###
+# isEmpty(): O(1)
+# push(): O(1)
+# pop(): O(1)
+# peek(): O(1)
+# size(): O(1)
+# show(): O(n)
+
+### Space Complexity: ##
+# Since we use an array data structure to store every element or perform any operation
+# the time complexity will be O(n)
+
+# Did not try to run this code on leetcode as the questions are a litte different on leetcode
+
+# No issues faced while coding this exercise
+
+## Code along with comments explaining my approach
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+  def __init__(self):
+    self.stack = []   #creating an array
+
+  def isEmpty(self):
+    if len(self.stack) == 0:    #checking if length is zero
+      return True
+    else:
+      return False
+
+  def push(self, item):
+    self.stack.append(item)     #push the item on to stack using append()
+    print(f"Item {item} pushed to stack.")
+
+  def pop(self):
+    return self.stack.pop()     #pop element from stack using pop()
+
+  def peek(self):
+    return self.stack[-1]       #check element on top of stack 
+
+  def size(self):
+    return len(self.stack)      #return length of the stack
+
+  def show(self):
+    return self.stack           #show the entire stack
 
 s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
+print(s.show())
+print(s.isEmpty())
+s.push('3')
+print(s.peek())
+print(s.size())
 print(s.show())
