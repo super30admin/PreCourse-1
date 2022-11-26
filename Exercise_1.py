cbc26266d2226fd90609f1bc3f68142fd6bf98d1
MalavyaRaval <malavyaraval@gmail.com>
2022-11-26 08:41:31 +0530

    Completed precourse

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,7 +2,7 @@ class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+         self.list = []
      def isEmpty(self):
          
      def push(self, item):
@@ -21,4 +21,4 @@ s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
-print(s.show())
+print(s.show()
