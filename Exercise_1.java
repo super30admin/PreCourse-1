349093e0448e57b7d2e3ac75c3e4aac63be2ddf6
Prudhvi Manukonda <98933152+PMANU005@users.noreply.github.com>
2022-12-13 19:01:35 -0800

    Stack Using Array

--- Exercise_1.java
+++ Exercise_1.java
@@ -8,11 +8,14 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        boolean isTrue=top<0;
+        return isTrue;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top=-1;
     } 
   
     boolean push(int x) 
@@ -20,17 +23,30 @@ class Stack {
         //Check for stack Overflow
         //Write your code here
         //#sample testing
+        if(top>MAX-1){
+            return false;
+        }
+        //Write your code here
+        a[++top]=x;
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top<0){
+            return -1;
+        }
+        //Write your code here
+        int ans= a[top--];
+        return ans;
     } 
   
     int peek() 
     { 
         //Write your code here
+        return a[top];
     } 
 } 
   
@@ -45,3 +61,5 @@ class Main {
         System.out.println(s.pop() + " Popped from stack"); 
     } 
 }
+//TIME COMPLEXITY O(1) for add, peek,pop.
+//Space Complexity O(1) since input array is given
