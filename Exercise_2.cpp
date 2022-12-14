e95279ae437924178943a9879bb172ecff6dcc3d
KVLN Raju <kvlnraju@KVLNs-MacBook-Air.local>
2022-12-15 02:32:35 +0530

    Completed PreCourse 1: Exercises-1,2,3

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,3 +1,12 @@
+// Time complexity:
+// isEmpty(): O(1)
+// push(): O(1)
+// pop(): O(n)
+// peek(): O(1)
+// Space Complexity : O(n) where n is size of the elements in Structure
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Didn't understand wether we can create global Pointer for maintaining the tail or not.
+
 #include <bits/stdc++.h> 
 using namespace std; 
   
@@ -7,7 +16,9 @@ public:
     int data; 
     StackNode* next; 
 }; 
-  
+
+StackNode* tail=NULL; 
+
 StackNode* newNode(int data) 
 { 
     StackNode* stackNode = new StackNode(); 
@@ -18,22 +29,57 @@ StackNode* newNode(int data)
   
 int isEmpty(StackNode* root) 
 { 
-    //Your code here 
+    //Your code here
+    if(root==NULL){
+        return 1;
+    }
+    return 0; 
 } 
   
 void push(StackNode** root, int data) 
 { 
-    //Your code here 
+    //Your code here
+    if(*root==NULL){
+        *root = newNode(data);
+        tail = *root;
+    }
+    else{
+        tail->next = newNode(data);
+        tail = tail->next;
+    }
 } 
   
 int pop(StackNode** root) 
 { 
-    //Your code here 
+    //Your code here
+    if(*root == NULL){
+        return NULL;
+    }
+    if((*root)->next == NULL){
+        int val = tail->data;
+        *root = NULL;
+        tail = NULL;
+        return val;
+    }
+    StackNode* prev =  *root;
+    while(prev->next!=tail && prev!=NULL){
+        prev = prev->next;
+    }
+    int val = tail->data;
+    tail = prev;
+    tail->next = NULL;
+    return val;
 } 
   
 int peek(StackNode* root) 
 { 
-    //Your code here 
+    //Your code here
+    if(root==NULL){
+        return NULL;
+    } 
+    else{
+        return tail->data;
+    }
 } 
   
 int main() 
@@ -41,12 +87,14 @@ int main()
     StackNode* root = NULL; 
   
     push(&root, 10); 
-    push(&root, 20); 
-    push(&root, 30); 
+    push(&root, 70); 
+    push(&root, 60);
   
     cout << pop(&root) << " popped from stack\n"; 
   
-    cout << "Top element is " << peek(root) << endl; 
+    cout << "Top element is " << peek(root) << endl;
+
+    cout << "stack is empty " << isEmpty(root) << endl;
   
     return 0; 
 } 
--- Exercise_3.cpp
+++ Exercise_3.cpp
@@ -1,3 +1,12 @@
+// Time complexity:
+// PrintList(): O(n)
+// push(): O(1)
+// append(): O(n)
+// insertAfter(): O(p) where p is the position in which you want to keep the node
+// Space Complexity : O(n) where n is size of the elements in Linkedlist
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
 #include <bits/stdc++.h> 
 using namespace std; 
   
@@ -14,13 +23,17 @@ to the head of a list and an int, inserts
 a new node on the front of the list. */
 void push(Node** head_ref, int new_data)  
 {  
-    /* 1. allocate node */ 
+    /* 1. allocate node */
+    Node* newNode = new Node();
   
-    /* 2. put in the data */  
+    /* 2. put in the data */
+    newNode->data = new_data;  
   
-    /* 3. Make next of new node as head */ 
+    /* 3. Make next of new node as head */
+    newNode->next = *head_ref;  
   
     /* 4. move the head to point to the new node */
+    *head_ref = newNode;
 }  
   
 /* Given a node prev_node, insert a new node after the given  
@@ -28,34 +41,55 @@ prev_node */
 void insertAfter(Node* prev_node, int new_data)  
 {  
     /*1. check if the given prev_node is NULL */ 
-  
+    if(prev_node==NULL){
+        return;
+    }
+
     /* 2. allocate new node */ 
+    Node* newNode = new Node();
   
-    /* 3. put in the data */ 
+    /* 3. put in the data */
+    newNode->data = new_data;   
   
     /* 4. Make next of new node as next of prev_node */
+    newNode->next = prev_node->next;
   
-    /* 5. move the next of prev_node as new_node */ 
+    /* 5. move the next of prev_node as new_node */
+    prev_node->next = newNode;
+    return; 
 }  
   
 /* Given a reference (pointer to pointer) to the head  
 of a list and an int, appends a new node at the end */
 void append(Node** head_ref, int new_data)  
 {  
-    /* 1. allocate node */ 
+    /* 1. allocate node */
+    Node* newNode = new Node(); 
   
-    /* 2. put in the data */ 
+    /* 2. put in the data */
+    newNode->data = new_data;  
   
     /* 3. This new node is going to be  
     the last node, so make next of  
-    it as NULL*/  
+    it as NULL*/
+    newNode->next = NULL;  
   
     /* 4. If the Linked List is empty, 
     then make the new node as head */
+    if(*head_ref==NULL){
+        *head_ref = newNode;
+        return;
+    }
   
     /* 5. Else traverse till the last node */
+    Node* temp = *head_ref;
+    while(temp!=NULL && temp->next!=NULL){
+        temp = temp->next;
+    }
   
     /* 6. Change the next of last node */ 
+    temp->next = newNode;
+    return;
 }  
   
 // This function prints contents of 
@@ -63,6 +97,11 @@ void append(Node** head_ref, int new_data)
 void printList(Node *node)  
 {  
     //Your code here
+    while(node!=NULL){
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
 }  
   
 /* Driver code*/
