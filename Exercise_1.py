42a0c3bbd5b445ef42d318a8a539414a6eb4afdf
Helen <helen.zheng55@gmail.com>
2023-01-15 15:58:09 -0800

    Committing with updates in python.

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,47 @@
+# Time Complexity : O(N), N being the number of executions. 
+# Space Complexity : O(N), N being the number of executions. 
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : Formatting of new python
+# files was weird in VScode. 
+
+
+# Your code here along with comments explaining your approach
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self):
+        self.array = []
+
+    def isEmpty(self):
+        if len(self.array)==0:
+            return True
+        else:
+            return False
+
+    def push(self, item):
+        self.array.append(item)
+
+    #Removes the item at the end of array and returns item, if 
+    # there is an item available.
+    def pop(self):
+        if len(self.array)==0:
+            return ""
+        else:
+            return self.array.pop(-1)
+
+    #Returns the last item, if length of array is greater than 0.   
+    def peek(self):
+        if len(self.array)==0:
+            return ""
+        else:
+            return self.array[-1]
+
+    def size(self):
+        return len(self.array)
+
+    def show(self):
+        print(self.array)
 
 s = myStack()
 s.push('1')
