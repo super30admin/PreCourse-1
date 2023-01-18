597f5b1b862d19e1d62b960c0fad40bac2c3f371
poojaGit95 <pooh.nadagoudabgm@gmail.com>
2023-01-18 12:38:25 -0800

    completed precourse-1

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,5 @@
+# Note: Time and space complexity provided in the comment
+# section of each stack function implemented
 
 class Node:
     def __init__(self, data):
@@ -5,20 +7,62 @@ class Node:
        self.next = None
  
 class Stack:
+
+    # initializes the head of linked list
+    # Time Complexity - O(1)
+    # Space complexity - O(1)
     def __init__(self):
-        
+        self.head = None
+
+    # check if head of linked list exists or not. If empty then create new Node
+    # object and assign it to head of linked list. If not empty then traverse
+    # linked list till the end and append a new Node of data at the end.
+    # Time Complexity - O(n)
+    # Space complexity - O(n)
     def push(self, data):
-        
+        if not self.head:
+            self.head = Node(data)
+            return
+
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        cur.next = Node(data)
+
+
+    # check if head of linked list exists or not. If empty then return None. If only 1 element
+    # exists in linked list then assign head to None and return. If more than 1 elements exist
+    # then traverse linked list till last but one element and point its next pointer to None.
+    # Time Complexity - O(n)
+    # Space complexity - O(n)
     def pop(self):
-        
+        if not self.head:
+            return None
+
+        if not self.head.next:
+            temp = self.head.data
+            self.head = None
+            return temp
+
+        prev = None
+        cur = self.head
+        while cur.next:
+            prev = cur
+            cur = cur.next
+        prev.next = None
+        return cur.data
+
+
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
     print('pop')
     print('quit')
-    do = input('What would you like to do? ').split()
+
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    do = input('What would you like to do? ').split()
+
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,32 +1,96 @@
+# Note: Time and space complexity provided in the comment
+# section of each linked list function implemented
+
 class ListNode:
-    """
-    A node in a singly-linked list.
-    """
+    """ A node in a singly-linked list."""
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
+
+    # initialize the linked list with first head element
+    # Time Complexity - O(1)
+    # Space complexity - O(1)
     def __init__(self):
-        """
-        Create a new singly-linked list.
-        Takes O(1) time.
-        """
         self.head = None
 
+    # check if linked list is empty or not. If empty then create a new ListNode object and assign
+    # this as the head. If not empty then traverse the linked list till the end and append the
+    # data as new ListNode.
+    # Time Complexity - O(n)
+    # Space complexity - O(n)
     def append(self, data):
-        """
-        Insert a new element at the end of the list.
-        Takes O(n) time.
-        """
-        
+        if not self.head:
+            self.head = ListNode(data)
+            return
+
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        cur.next = ListNode(data)
+
+    # check if linked list is empty or not. If not empty then traverse through
+    # the linked list and compare data of each element with key. If found print msg
+    # and return, if not found print msg and return
+    # Time Complexity - O(n)
+    # Space complexity - O(n)
     def find(self, key):
-        """
-        Search for the first element with `data` matching
-        `key`. Return the element or `None` if not found.
-        Takes O(n) time.
-        """
-        
+        if not self.head:
+            print("Linked List empty")
+            return None
+
+        cur = self.head
+        while cur:
+            if cur.data == key:
+                print("key found")
+                return cur
+            cur = cur.next
+        print("key not found")
+        return None
+
+    # checking if linked list is empty or not. I not empty check if key matches with head,
+    # if yes remove the first element and make next element as head.
+    # Time Complexity - O(n)
+    # Space complexity - O(n)
     def remove(self, key):
-        """
-        Remove the first occurrence of `key` in the list.
-        Takes O(n) time.
-        """
+        if not self.head:
+            print("Linked List empty")
+            return None
+
+        if self.head.data==key:
+            temp = self.head.next
+            self.head.next = None
+            self.head = temp
+            print("key removed successfully")
+            return
+
+        cur = self.head
+        while cur.next:
+            if cur.next.data==key:
+                cur.next = cur.next.next
+                print("key removed successfully")
+                return
+            cur = cur.next
+        print("key not present")
+        return None
+
+
+    # displays the elements in linked list in order
+    # Time Complexity - O(n)
+    # Space complexity - O(n)
+    def show(self):
+        cur = self.head
+        while cur:
+            print(cur.data)
+            cur = cur.next
+
+
+s = SinglyLinkedList()
+s.append(5)
+s.append(6)
+s.show()
+(s.find(5))
+(s.remove(5))
+s.show()
+(s.find(6))
