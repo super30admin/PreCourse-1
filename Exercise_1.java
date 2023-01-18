0bb5df69bc0438f705c32b689bd29edd285b528a
Pradhatri Vemula <pradhatrivemula@Pradhatris-Air.lan>
2023-01-17 20:46:05 -0800

    Completed PreCourse_1

--- Exercise_1.java
+++ Exercise_1.java
@@ -8,28 +8,55 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        return (top < 0);  
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;  
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        
+        if (top == a.length-1 )   {
+        System.out.println("\n Overflow"); 
+        return false; 
+        }
+        else   
+        {  
+        top = top +1;   
+        a[top] = x;   
+        return true;
+        }   
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        int pop=0;
+        if (top == -1)  
+        {  
+            System.out.println("Underflow !!");  
+            return 0;  
+        }  
+        else   
+        {  
+            pop= a[top];
+            top --;   
+            return pop;  
+        }  
+        
     } 
   
     int peek() 
     { 
         //Write your code here
+        return a[top];
     } 
 } 
   
