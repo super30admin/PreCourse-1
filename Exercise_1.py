30c7c960bec3ead29bb45215f660c46a5d5ceeb3
NAVEEN US <88378808+usnaveen@users.noreply.github.com>
2023-01-21 16:48:35 +0530

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,36 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
+     def __init__(self,limit=100):
+        self.stack=[]
+        
      def isEmpty(self):
+        return len(self.stack) <=0
          
      def push(self, item):
+        if len(self.stack)>=self.limit:
+          print("Overflow")
+        else:
+          self.stack.append(item)
+          print("Stack after push: ",self.stack)
          
      def pop(self):
-        
+        if len(self.stack)<=0:
+          print("Underflow")
+        else:
+          return self.stack.pop()        
         
      def peek(self):
+        if len(self.stack)<=0:
+          print("Underflow")
+        else:
+          return self.stack[-1]
         
      def size(self):
+        return len(self.stack)
          
      def show(self):
+        return self.stack
          
 
 s = myStack()
