6f4260206529ccd71d325f113db2e9e8b3d6a932
NAVEEN US <88378808+usnaveen@users.noreply.github.com>
2023-01-21 17:26:34 +0530

    Update Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,15 +1,41 @@
+# Time complexity of push, pop : o(1)
+# Space complexity : o(1)
 
 class Node:
-    def __init__(self, data):
+    def __init__(self, data= None):
        self.data = data
        self.next = None
+    def setData(self,data):
+      self.data= data
+    def getData(self):
+      return self.data
+    def setNext(self,next=None):
+      self.next=next
+    def getNext(self):
+      if self.hasNext():
+        return self.next
+    def hasNext(self):
+      return self.next!=None 
  
 class Stack:
-    def __init__(self):
+    def __init__(self, data=None):
+      self.head=Node
+      if data: 
+        for data in data:
+          self.push(data)
         
     def push(self, data):
+      temp=Node()
+      temp.setData(data)
+      temp.setNext(self.head)
+      self.head=temp
         
     def pop(self):
+      if self.head is None:
+        print("Underflow")
+      temp = self.head.getData()
+      self.head = self.head.getNext()
+      return temp
         
 a_stack = Stack()
 while True:
