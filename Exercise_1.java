1164eae09afab335e7a1c16d666580a9acbd229a
rutujamandlikFT <123407038+rutujamandlikFT@users.noreply.github.com>
2023-01-24 17:13:58 -0500

    Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,9 @@
+// Time Complexity : O(1) for each operation
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : N/A
+// Any problem you faced while coding this : No
+
+
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
@@ -8,28 +14,46 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        return top < 0;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if (top > MAX - 1) {
+            System.out.println("stack Overflow");
+            return false;
+        }
+        else {
+            top = top + 1;
+            a[top] = x;
+            return true;
+        }
+        
     } 
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
+        // If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if ( top < 0 ) {
+            System.out.println( "Stack Underflow");
+            return 0;
+        } else {
+            return a[top--];
+        }
     } 
-  
-    int peek() 
+   int peek() 
     { 
         //Write your code here
+        return a[top]
     } 
 } 
   
