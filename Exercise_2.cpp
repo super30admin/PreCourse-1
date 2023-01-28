a70b569521d566fd27ffe34a6acc8572f3eccfbe
amitdoddamani123 <123243508+amitdoddamani123@users.noreply.github.com>
2023-01-28 13:37:59 +0000

    excersise2

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,52 +1,159 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-  
-// A structure to represent a stack 
-class StackNode { 
-public: 
-    int data; 
-    StackNode* next; 
-}; 
-  
-StackNode* newNode(int data) 
-{ 
-    StackNode* stackNode = new StackNode(); 
-    stackNode->data = data; 
-    stackNode->next = NULL; 
-    return stackNode; 
-} 
-  
-int isEmpty(StackNode* root) 
-{ 
-    //Your code here 
-} 
-  
-void push(StackNode** root, int data) 
-{ 
-    //Your code here 
-} 
-  
-int pop(StackNode** root) 
-{ 
-    //Your code here 
-} 
-  
-int peek(StackNode* root) 
-{ 
-    //Your code here 
-} 
-  
-int main() 
-{ 
-    StackNode* root = NULL; 
-  
-    push(&root, 10); 
-    push(&root, 20); 
-    push(&root, 30); 
-  
-    cout << pop(&root) << " popped from stack\n"; 
-  
-    cout << "Top element is " << peek(root) << endl; 
-  
-    return 0; 
-} 
+// Time Complexity : 0(1)
+// Space Complexity : 0(1)
+// Did this code successfully run on Leetcode :  
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// creating a linked list;
+class Node {
+public:
+	int data;
+	Node* link;
+	
+	// Constructor
+	Node(int n)
+	{
+		this->data = n;
+		this->link = NULL;
+	}
+};
+
+class Stack {
+	Node* top;
+
+public:
+	Stack() { top = NULL; }
+
+	void push(int data)
+	{
+
+		// Create new node temp and allocate memory in heap
+		Node* temp = new Node(data);
+
+		// Check if stack (heap) is full.
+		// Then inserting an element would
+		// lead to stack overflow
+		if (!temp) {
+			cout << "\nStack Overflow";
+			exit(1);
+		}
+
+		// Initialize data into temp data field
+		temp->data = data;
+
+		// Put top pointer reference into temp link
+		temp->link = top;
+
+		// Make temp as top of Stack
+		top = temp;
+	}
+
+	// Utility function to check if
+	// the stack is empty or not
+	bool isEmpty()
+	{
+		// If top is NULL it means that
+		// there are no elements are in stack
+		return top == NULL;
+	}
+
+	// Utility function to return top element in a stack
+	int peek()
+	{
+		// If stack is not empty , return the top element
+		if (!isEmpty())
+			return top->data;
+		else
+			exit(1);
+	}
+
+	// Function to remove
+	// a key from given queue q
+	void pop()
+	{
+		Node* temp;
+
+		// Check for stack underflow
+		if (top == NULL) {
+			cout << "\nStack Underflow" << endl;
+			exit(1);
+		}
+		else {
+
+			// Assign top to temp
+			temp = top;
+
+			// Assign second node to top
+			top = top->link;
+
+			// This will automatically destroy
+			// the link between first node and second node
+
+			// Release memory of top node
+			// i.e delete the node
+			free(temp);
+		}
+	}
+
+	// Function to print all the
+	// elements of the stack
+	void display()
+	{
+		Node* temp;
+
+		// Check for stack underflow
+		if (top == NULL) {
+			cout << "\nStack Underflow";
+			exit(1);
+		}
+		else {
+			temp = top;
+			while (temp != NULL) {
+
+				// Print node data
+				cout << temp->data;
+
+				// Assign temp link to temp
+				temp = temp->link;
+				if (temp != NULL)
+					cout << " -> ";
+			}
+		}
+	}
+};
+
+// Driven Program
+int main()
+{
+	// Creating a stack
+	Stack s;
+
+	// Push the elements of stack
+	s.push(11);
+	s.push(22);
+	s.push(33);
+	s.push(44);
+
+	// Display stack elements
+	s.display();
+
+	// Print top element of stack
+	cout << "\nTop element is " << s.peek() << endl;
+
+	// Delete top elements of stack
+	s.pop();
+	s.pop();
+
+	// Display stack elements
+	s.display();
+
+	// Print top element of stack
+	cout << "\nTop element is " << s.peek() << endl;
+
+	return 0;
+}
