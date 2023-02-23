0d147de0fe8e64a47ecc69eaf1949adc6e016600
Alekhya1311 <alekhyabairaboina1311@gmail.com>
2023-02-23 12:58:50 -0800

    completed precourse 1

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,16 +19,41 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        new_node = ListNode(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+            current_node.next = new_node
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        current_node = self.head
+        while current_node is not None:
+            if current_node.data == key:
+                return current_node
+            current_node = current_node.next
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        previous_node = None
+        current_node = self.head
+        while current_node is not None:
+            if current_node.data == key:
+                if previous_node is None:
+                    self.head = current_node.next
+                else:
+                    previous_node.next = current_node.next
+                return
+            previous_node = current_node
+            current_node = current_node.next
