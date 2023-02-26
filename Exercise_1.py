3f336026d53e84ab5b02121e854cb867dba4c86e
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2023-02-25 21:43:08 -0800

    python implementation of stack with array

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,49 @@
+
+
+# inplement stach using list:
+"""
+The time complexity:
+
+push(): O(1)
+pop(): O(1)
+is_empty(): O(1)
+top(): O(1)
+
+"""
+"""
+space complexity O(n), where n is the number of elements in the stack.
+"""
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
+    def __init__(self):
+        self.stack=[]
+
+    def isEmpty(self):
+        return len(self.stack)==0
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.isEmpty():
+            return self.stack.pop()
+        else:
+            return None       
+    
+    def peek(self):
+        if not self.isEmpty():
+            return self.stack[-1]
+        else:
+            return None   
+
+    def size(self):
+        return len(self.stack)
+            
+    def show(self):
+        return self.stack
         
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
 
 s = myStack()
 s.push('1')
