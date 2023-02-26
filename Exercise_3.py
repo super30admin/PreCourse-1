42e8de7ca54982457ff293b1abbe605564903619
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2023-02-25 21:55:44 -0800

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -17,6 +17,15 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            current_node = self.head
+            while current_node.next:
+                current_node = current_node.next
+            current_node.next = new_node
+
         
     def find(self, key):
         """
@@ -24,9 +33,26 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        current_node = self.head
+        while current_node:
+            if current_node.data == data:
+                return current_node
+            current_node = current_node.next
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if not self.head:
+            return
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        current_node = self.head
+        while current_node.next:
+            if current_node.next.data == data:
+                current_node.next = current_node.next.next
+                return
+            current_node = current_node.next
