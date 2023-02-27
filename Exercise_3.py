c3bc9d2e892108d5b9d7b598b05e53ee7f1e9818
guruvigneshdev <126303381+guruvigneshdev@users.noreply.github.com>
2023-02-26 19:32:07 -0600

    Completed exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data=data
+        self.next=None
     
 class SinglyLinkedList:
     def __init__(self):
@@ -10,13 +12,19 @@ class SinglyLinkedList:
         Create a new singly-linked list.
         Takes O(1) time.
         """
-        self.head = None
-
+        self.head = ListNode('nn')
+        self.ptr = self.head
+        self.size = 0
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        newNode = ListNode(data)
+        self.ptr.next = newNode
+        self.ptr = newNode
+        self.size += 1
+        print("inserted element",self.ptr.data)
         
     def find(self, key):
         """
@@ -24,9 +32,37 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        currentNode = self.head.next
+        while currentNode is not None:
+            if currentNode.data == key:
+                return currentNode
+            currentNode = currentNode.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+
+        prevNode = self.head
+        currentNode = self.head.next
+        while currentNode is not None:
+            if currentNode.data == key:
+                prevNode.next = currentNode.next
+                if currentNode == self.ptr:
+                    self.ptr = prevNode
+
+                del currentNode
+                self.size -= 1
+                return
+            prevNode = currentNode
+            currentNode = currentNode.next
+
+
+s = SinglyLinkedList()
+s.append(1)
+
+s.remove(1)
+
+
