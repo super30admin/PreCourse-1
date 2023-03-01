5e2a90ea658c3b1f2edf61ed8d8e722d71d09fb3
TrivediShaili <126596926+TrivediShaili@users.noreply.github.com>
2023-03-01 00:07:11 -0500

    Completed.Please review it

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,20 +1,44 @@
 
+# Time Complexity : O(1)
+# Space Complexity : O(n)
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
+       self.data = data     # initialize the data and store the date passed in the arguments
+       self.next = None     # Inintialize the next pointer and set it to the NONE
  
 class Stack:
     def __init__(self):
-        
+        self.head_pointer = None # head pointer pointing at none
+        self.stack_size = 0 # initializing the stakc size as 0
+
+
     def push(self, data):
+        new_node = Node(data) # create obj for the Node class
+        if self.head_pointer is None: # if the head pointer is pointing NONE
+            self.head_pointer = new_node # then update the pointer and now the head pointer will point to the new node
+        else:
+            new_node.next = self.head_pointer # if there is already items in stack point the header to the new node
+            self.head_pointer = new_node        # and also increment the stack size to 1
+            self.stack_size += 1
         
-    def pop(self):
-        
+    def pop(self):              # pop the value if stack is not NONE 
+        if(self.isEmpty()):     # if NONE set heaader to None and return
+            deleted_node=None
+            return None
+        else:
+            deleted_node=self.head_pointer.data # if NOT NONE pop out the item and set the header to next node
+            self.head_pointer=self.head_pointer.next 
+            self.stack_size -= 1    # also update the stack size
+        return deleted_node            # return the popped node
+    
+    def isEmpty(self):              # check if the stack is Empty or not.
+          return self.stack_size == 0 # returns TRUE if Empty.
+    
+
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    print('push <value>')
+    print('push')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
@@ -30,3 +54,4 @@ while True:
             print('Popped value: ', int(popped))
     elif operation == 'quit':
         break
+
