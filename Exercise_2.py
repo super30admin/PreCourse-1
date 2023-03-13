7d0a59442ba70c8bf979381749833ef0adea1558
rasika-1405 <rasika.sasturkar14@gmail.com>
2023-03-13 19:05:32 -0400

    PreCourse-1 problems completed

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,32 +1,65 @@
+"""
+Rasika Sasturkar
+Time complexity: O(n), total no. of elements in the linkedlist.
+Space complexity: O(n), total no. of elements in the linkedlist.
+"""
+
 
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
+
 class Stack:
     def __init__(self):
-        
+        self.head = None
+
     def push(self, data):
-        
-    def pop(self):
-        
-a_stack = Stack()
-while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    print('push <value>')
-    print('pop')
-    print('quit')
-    do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    operation = do[0].strip().lower()
-    if operation == 'push':
-        a_stack.push(int(do[1]))
-    elif operation == 'pop':
-        popped = a_stack.pop()
-        if popped is None:
-            print('Stack is empty.')
+        if self.head is None:
+            self.head = Node(data)
         else:
-            print('Popped value: ', int(popped))
-    elif operation == 'quit':
-        break
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = Node(data)
+
+    def pop(self):
+        if self.head:
+            curr = self.head
+            if curr.next is None:
+                self.head = None
+                return curr.data
+            prev = self.head
+            while curr.next:
+                prev = curr
+                curr = curr.next
+            prev.next = None
+            return curr.data
+        return
+
+
+def main():
+    a_stack = Stack()
+    while True:
+        # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+        print('push <value>')
+        print('pop')
+        print('quit')
+        do = input('What would you like to do? ').split()
+        # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+        operation = do[0].strip().lower()
+        if operation == 'push':
+            a_stack.push(int(do[1]))
+        elif operation == 'pop':
+            popped = a_stack.pop()
+            if popped is None:
+                print('Stack is empty.')
+            else:
+                print('Popped value: ', int(popped))
+        elif operation == 'quit':
+            break
+
+
+if __name__ == "__main__":
+    main()
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,9 +1,20 @@
+"""
+Rasika Sasturkar
+Time complexity: O(n), total no. of elements in the linkedlist.
+Space complexity: O(n), total no. of elements in the linkedlist.
+"""
+
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
+
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +28,43 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        if self.head is None:
+            self.head = ListNode(data)
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = ListNode(data)
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        if self.head:
+            curr = self.head
+            while curr:
+                if curr.data == key:
+                    return curr
+                curr = curr.next
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head:
+            curr = self.head
+            if curr.data == key:
+                self.head = curr.next
+                return
+            prev = self.head
+            while curr:
+                if curr.data == key:
+                    prev.next = curr.next
+                    return
+                prev = curr
+                curr = curr.next
+        return
