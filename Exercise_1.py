f48b9bcdbd1d2d21fbb2febf9b25ac1c99ba20c8
Dimple Mehra <60533553+mehra-dimple@users.noreply.github.com>
2023-03-25 15:59:24 -0700

    Precourse1-DimpleMehra

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,39 @@
+# // Time Complexity : O(1)
+# // Space Complexity : O(1)
+
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+          self.mystack = []
          
      def isEmpty(self):
-         
+          return len(self.mystack) == 0
+
+                  
      def push(self, item):
+          self.mystack.append(item)
          
      def pop(self):
-        
+          if not self.isEmpty():
+            return self.mystack.pop()
+
         
      def peek(self):
+          if not self.isEmpty():
+            return self.mystack[-1]
         
      def size(self):
+          return len(self.mystack)
          
      def show(self):
+        print(self.mystack)
+
          
 
 s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
-print(s.show())
+s.show()
