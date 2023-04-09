9e20e064b2111681c3858867f2100997d1fc8438
sharvanitamaskar <sharvanitamaskar@gmail.com>
2023-04-09 19:54:35 -0400

    completed precourse1

--- Exercise_3.py
+++ Exercise_3.py
@@ -4,7 +4,7 @@ class ListNode:
     """
     def __init__(self, data=None, next=None):
     
-class SinglyLinkedList:
+class LinkedList:
     def __init__(self):
         """
         Create a new singly-linked list.
@@ -12,11 +12,20 @@ class SinglyLinkedList:
         """
         self.head = None
 
+
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        if self.head is None:          
+            self.head = ListNode(data)
+        else:                           
+            temp_node = self.head
+            new_node = ListNode(data)
+            while temp_node.next is not None:
+                temp_node = temp_node.next
+            temp_node.next = new_node
         
     def find(self, key):
         """
@@ -24,9 +33,33 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        present_node = self.head
+
+        while present_node is not None:   
+            if present_node.data == key:
+                return present_node
+            present_node = present_node.next
+
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head.data == key:       
+            self.head = self.head.next
+            return
+
+        elif self.head is None:         
+            return
+
+        else:                          
+            present_node = self.head.next
+            previous_node = self.head
+            while previous_node.next is not None:
+                if present_node.data == key:
+                    previous_node.next = present_node.next
+                    return
+
+        return
