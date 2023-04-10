8e208247d07aa05ee272e0bd8db297d2b3811155
Poojashree NS <poojashree.ns@sjsu.edu>
2023-04-09 19:15:16 -0700

    Commit - 1 | Precourse Assignment

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,42 @@
+## Implement Stack using Array.
+
+# Stack is a linear and abstract data type that follows the principle of Last in First out
+# Operations: Push, Pop, Peek, isEmpty, Size
+# Time complexity for all the operation : O(1)
+# Space complexity : O(1)
+
+# Did this code successfully run on Leetcode : Unable to point question on leetcode. Ran locally and code looks fine
+
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    # Please read sample.java file before starting.
+    # Kindly include Time and Space complexity at top of each file.
+    def __init__(self):
+        self.stack = []
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if len(self.stack) == 0:
+            return None
+        return self.stack.pop()
+
+    def peek(self):
+        if len(self.stack) == 0:
+            print("Stack is empty")
+            return None
+        return self.stack[len(self.stack) - 1]
+
+    def size(self):
+        return len(self.stack)
+
+    def show(self):
+        for item in range(len(self.stack)):
+            print(self.stack[item])
+
 
 s = myStack()
 s.push('1')
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,15 @@
+## Implement Stack using Linked List.
+
+# Stack is a linear and abstract data type that follows the principle of Last in First out
+# Operations: Push, Pop and isEmpty
+# Time complexity for all the operations :
+            # To Insert at current location: O(1)
+            # To Delete at Current location : O(1)
+            # To Insert at the end : O(n)
+            # To Delete at the end : O(n)
+# Space complexity : O(n)
+
+# Did this code successfully run on Leetcode : Unable to point question on leetcode. Ran locally and code looks fine
 
 class Node:
     def __init__(self, data):
@@ -6,10 +18,31 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head = None
         
     def push(self, data):
-        
+        if self.head == None:
+            self.head = Node(data)
+        else:
+            newnode = Node(data)
+            newnode.next = self.head
+            self.head = newnode
+
+    def isEmpty(self):
+        if self.head is None:
+            return True
+        else:
+            return False
+
     def pop(self):
+        if self.isEmpty():
+            return None
+        else:
+            popnode = self.head
+            self.head = self.head.next
+            popnode.next = None
+            return popnode.data
+
         
 a_stack = Stack()
 while True:
@@ -18,6 +51,7 @@ while True:
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
+
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
     if operation == 'push':
