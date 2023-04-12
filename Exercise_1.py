91d90920d063adc7b40fc8c1da6250881b5dc528
Bharath Barakam <bharath.barakam@ultra.me>
2023-04-12 03:04:17 -0700

    Finished precourse 1 in python

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,30 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+  def __init__(self):
+      self.stack = [];
+
+  def isEmpty(self):
+      return     
+  def push(self, item):
+      self.stack.append(item)   
+
+  def pop(self):
+    if self.stack:
+      return self.stack.pop()
+    else:
+      return None
+
+  def peek(self):
+      if (not self.stack):
+        return None
+      else:
+        return self.stack[0]
+
+  def size(self):
+      return len(self.stack)
+  def show(self):
+      return self.stack
 
 s = myStack()
 s.push('1')
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,7 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = None
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +18,54 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        nodes = self.head
+
+        while nodes.next:
+            nodes = nodes.next
+        nodes.next = ListNode(data)
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        nodes = self.head
+        while nodes is not None:
+           if (nodes.data == key):
+            return nodes.data
+           nodes = nodes.next
+        return None  
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        headVal = self.head
+
+        if (headVal is not None):
+            if(headVal.data == key):
+                self.head.next = headVal.next
+                headVal = None
+                return 
+        
+        while headVal is not None:
+            if (headVal.data == key):
+                break
+
+            prev = headVal
+            headVal = headVal.next
+
+        if (headVal == None):
+            return
+        
+        prev.next = headVal.next
+        headVal = None
+
+newLinkedList = SinglyLinkedList()
+newLinkedList.head = ListNode(5)
+newLinkedList.append(6)
+newLinkedList.append(7)
+newLinkedList.append(8)
+newLinkedList.append(9)
+newLinkedList.find(5)
+newLinkedList.remove(5)
