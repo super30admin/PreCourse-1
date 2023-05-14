c6f4a50e084cbe7f07dfa89165ce178b930972d9
DheerajGedupudi <dheerajgedupudi@gmail.com>
2023-05-14 17:55:43 +0530

    Completed PreCourse-1

--- Sample.java
+++ Sample.java
@@ -1,7 +1,15 @@
 // Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+    //added to all the exercise files.
 
+// Space Complexity :
+    //added to all the exercise files.
+
+// Did this code successfully run on Leetcode :
+    //I used vscode.
+
+// Any problem you faced while coding this :
+    //1st exercise ran without any issues
+    //2nd and 3rd exercises would not run unless I change the name of the file to the class name.
 
 // Your code here along with comments explaining your approach
+    //Do I add this to exercise files?
