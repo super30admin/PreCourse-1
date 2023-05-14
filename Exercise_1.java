7cdbac914a6930e7150a549c5bacd7926fb37aa8
Sana Pathan <sanapathan28@gmail.com>
2023-05-14 16:40:01 -0700

    Precourse-1 completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,62 @@
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
+    // the tc for this implementation is O(1)
+    // the size of the stack is 1000, thus sc is 1000 which is O(n)
+    static final int MAX = 1000;
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        return (top < 0);
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if (top >= (MAX - 1)) {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        else {
+            a[++top] = x;
+            System.out.println(x + " pushed into stack");
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else {
+            return a[top--];
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        if (top < 0) {
+            System.out.println("Stack is Empty");
+            return 0;
+        }
+        else {
+            return a[top];
+        }
     } 
 } 
   
