89a5eb52c2b56d0ad573a72b0a4589b34d73b364
sanketauti8 <sanketauti8@gmail.com>
2023-05-17 12:50:53 -0700

    Precourse1 code
    Precourse1 code

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,65 @@
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
+  //Time complexity=O(1)  Space complexity=O(n)
+  
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
-  
+ 
     boolean isEmpty() 
     { 
         //Write your code here 
+        if(top==-1){
+            System.out.println("Stack is empty");
+            return true;
+        }
+        return false;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top=-1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
+        
+        if(top==a.length-1){
+            System.out.println("Stack overflow");
+            return false;
+        }
         //Write your code here
+        top=top+1;
+        a[top]=x;
+         System.out.println("element pushed");
+         return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        
         //Write your code here
+        
+        int temp=a[top];
+        a[top--]=0;
+        return temp;
+        
     } 
   
     int peek() 
     { 
         //Write your code here
+        int kk;
+        kk= a[top];
+        return kk;
     } 
 } 
   
@@ -42,5 +72,8 @@ class Main {
         s.push(20); 
         s.push(30); 
         System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.peek() + " peek value"); 
+
+        
     } 
 }
