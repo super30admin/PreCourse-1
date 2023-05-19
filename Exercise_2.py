9e0b693f46eefc09198e45a7e30090a7809308ae
Chinmay Nandkishor Mathakari <chinmay.mathakari@gmail.com>
2023-05-19 09:22:20 -0400

    Added Pre-course 1 Please review it

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,38 @@
+# time complexity O(1)
+ # space complexity O(1)
 
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
 class Stack:
     def __init__(self):
-        
+        self.top = None
+
     def push(self, data):
-        
+        new_node = Node(data)
+        new_node.next = self.top
+        self.top = new_node
+
     def pop(self):
-        
+        if self.top is None:
+            return None
+        else:
+            popped_node = self.top
+            self.top = self.top.next
+            popped_node.next = None
+            return popped_node.data
+
+    def peek(self):
+        if self.top is None:
+            return None
+        else:
+            return self.top.data
+
+    def is_empty(self):
+        return self.top is None
+
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
@@ -27,6 +49,6 @@ while True:
         if popped is None:
             print('Stack is empty.')
         else:
-            print('Popped value: ', int(popped))
+            print('Popped value: ',int(popped))
     elif operation == 'quit':
         break
