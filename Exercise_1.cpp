2ff7b3f5196b91344138ef424c8df21b14ce8b35
jackpraveenraj <jackpraveenraj.work@gmail.com>
2023-05-20 11:55:45 -0700

    finished assignment

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -7,40 +7,82 @@ using namespace std;
 class Stack {
   //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
-    int top; 
+    int top = 0; 
   
 public: 
     int a[MAX]; // Maximum size of Stack 
   
-    Stack() { //Constructor here } 
+    Stack() { /*Constructor here*/ } 
     bool push(int x); 
     int pop(); 
     int peek(); 
     bool isEmpty(); 
 }; 
   
+    // Push an element from the stack
+    // Time complexity: O(1)
+    // Space complexity: O(1)
+    // Did this code successfully run on Leetcode : Yes
+    // Any problem you faced while coding this : No
 bool Stack::push(int x) 
 { 
     //Your code here
     //Check Stack overflow as well
+    
+       if(top>=MAX-1)
+       return false;
+       else {
+           top++;
+           a[top]=x;
+           cout<<"Digit Added:"<<a[top]<<endl;
+       }
+       return true;
 } 
-  
+
+    // Pop an element from the stack
+    // Time complexity: O(1)
+    // Space complexity: O(1)
+    // Did this code successfully run on Leetcode : Yes
+    // Any problem you faced while coding this : No 
 int Stack::pop() 
 { 
     //Your code here
     //Check Stack Underflow as well 
+    
+       if(top<=-1)
+        {
+            cout<<"Stack Underflow"<<endl;
+            return -1;
+        }
+        else {
+        return a[top];
+        top--;
+        
+        }
 } 
+    // Return top element in stack
+    // Time complexity: O(1)
+    // Space complexity: O(1)
+    // Did this code successfully run on Leetcode : Yes
+    // Any problem you faced while coding this : No
 int Stack::peek() 
 { 
-    //Your code here
-    //Check empty condition too
+        return a[top];
 } 
-  
+
+    // return whether stack is empty or not
+    // Time complexity: O(1)
+    // Space complexity: O(1)
+    // Did this code successfully run on Leetcode : Yes
+    // Any problem you faced while coding this : No
 bool Stack::isEmpty() 
 { 
-    //Your code here 
+       if(top<=-1)
+        return true;
+        else
+        return false;
 } 
-  
+
 // Driver program to test above functions 
 int main() 
 { 
@@ -51,4 +93,4 @@ int main()
     cout << s.pop() << " Popped from stack\n"; 
   
     return 0; 
-} 
+}
--- Exercise_3.cpp
+++ Exercise_3.cpp
@@ -1,80 +1,114 @@
 #include <bits/stdc++.h> 
-using namespace std; 
-  
-// A linked list node (changes) 
-class Node  
-{  
-    public: 
-    int data;  
-    Node *next;  
-};  
-  
-/* Given a reference (pointer to pointer) 
-to the head of a list and an int, inserts 
-a new node on the front of the list. */
-void push(Node** head_ref, int new_data)  
-{  
-    /* 1. allocate node */ 
-  
-    /* 2. put in the data */  
-  
-    /* 3. Make next of new node as head */ 
-  
-    /* 4. move the head to point to the new node */
-}  
-  
-/* Given a node prev_node, insert a new node after the given  
-prev_node */
-void insertAfter(Node* prev_node, int new_data)  
-{  
-    /*1. check if the given prev_node is NULL */ 
-  
-    /* 2. allocate new node */ 
-  
-    /* 3. put in the data */ 
-  
-    /* 4. Make next of new node as next of prev_node */
-  
-    /* 5. move the next of prev_node as new_node */ 
-}  
-  
-/* Given a reference (pointer to pointer) to the head  
-of a list and an int, appends a new node at the end */
-void append(Node** head_ref, int new_data)  
-{  
-    /* 1. allocate node */ 
-  
-    /* 2. put in the data */ 
-  
-    /* 3. This new node is going to be  
-    the last node, so make next of  
-    it as NULL*/  
-  
-    /* 4. If the Linked List is empty, 
-    then make the new node as head */
-  
-    /* 5. Else traverse till the last node */
-  
-    /* 6. Change the next of last node */ 
-}  
-  
-// This function prints contents of 
-// linked list starting from head  
-void printList(Node *node)  
-{  
-    //Your code here
-}  
-  
-/* Driver code*/
-int main()  
-{  
-    Node* head = NULL;   
-    append(&head, 6);  
-    push(&head, 7);  
-    push(&head, 1);    
-    append(&head, 4);    
-    insertAfter(head->next, 8);  
-    cout<<"Created Linked list is: ";  
-    printList(head);  
-    return 0;  
-}  
+#include <iostream>
+using namespace std;
+
+// A linked list node
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
+// Given a reference (pointer to pointer) to the head of a list and an int, inserts a new node on the front of the list.
+// Time complexity: O(1)
+// Space complexity: O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+void push(Node** head_ref, int new_data)
+{
+    // 1. Allocate node
+    Node* new_node = new Node();
+
+    // 2. Put in the data
+    new_node->data = new_data;
+
+    // 3. Make next of new node as head
+    new_node->next = *head_ref;
+
+    // 4. Move the head to point to the new node
+    *head_ref = new_node;
+}
+
+// Given a node prev_node, insert a new node after the given prev_node.
+// Time complexity: O(1)
+// Space complexity: O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+void insertAfter(Node* prev_node, int new_data)
+{
+    // 1. Check if the given prev_node is NULL
+    if (prev_node == NULL) {
+        cout << "The given previous node cannot be NULL.";
+        return;
+    }
+
+    // 2. Allocate new node
+    Node* new_node = new Node();
+
+    // 3. Put in the data
+    new_node->data = new_data;
+
+    // 4. Make next of new node as next of prev_node
+    new_node->next = prev_node->next;
+
+    // 5. Move the next of prev_node as new_node
+    prev_node->next = new_node;
+}
+
+// Given a reference (pointer to pointer) to the head of a list and an int, appends a new node at the end.
+// Time complexity: O(N), where N is the number of nodes in the linked list.
+// Space complexity: O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+void append(Node** head_ref, int new_data)
+{
+    // 1. Allocate node
+    Node* new_node = new Node();
+
+    // 2. Put in the data
+    new_node->data = new_data;
+
+    // 3. This new node is going to be the last node, so make next of it as NULL
+    new_node->next = NULL;
+
+    // 4. If the Linked List is empty, then make the new node as head
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+        return;
+    }
+
+    // 5. Else traverse till the last node
+    Node* last = *head_ref;
+    while (last->next != NULL) {
+        last = last->next;
+    }
+
+    // 6. Change the next of last node
+    last->next = new_node;
+}
+
+// This function prints contents of linked list starting from head
+// Time complexity: O(N), where N is the number of nodes in the linked list.
+// Space complexity: O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+void printList(Node* node)
+{
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+}
+
+int main()
+{
+    Node* head = NULL;
+    append(&head, 6);
+    push(&head, 7);
+    push(&head, 1);
+    append(&head, 4);
+    insertAfter(head->next, 8);
+    cout << "Created Linked list is: ";
+    printList(head);
+    return 0;
+}
