c6e1a4bf63889527f1b0e4d0568d165c747cc588
Satish Solanki <satishsolanki1990@gmail.com>
2023-05-20 11:52:59 -0700

    completed Excercise-3

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,10 +1,15 @@
+/*              Time Complexity | Space Complexity
+ * insert():           O(N)     |   O(1) 
+ * print():            O(N)     |   O(1)
+ */
+
 import java.io.*; 
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList { 
   
-    Node head; // head of list 
+    Node head; // head of list
   
     // Linked list Node. 
     // This inner class is made static 
@@ -18,33 +23,46 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            data = d;
         } 
     } 
   
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
     { 
-        // Create a new node with given data 
+        // Create a new node with given data
+        Node temp = new Node(data); 
    
         // If the Linked List is empty, 
-        // then make the new node as head 
-        
+        // then make the new node as head
+        if (list.head == null) {
+            list.head = temp;
+        }
+        else {
             // Else traverse till the last node 
-            // and insert the new_node there 
-
             // Insert the new_node at last node 
+            Node ptr = list.head;
+            while (ptr.next != null) ptr = ptr.next;
+            ptr.next = temp;
+        }
+
         // Return the list by head 
-        
+        return list;
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
-        // Traverse through the LinkedList 
-   
+        // Traverse through the LinkedList
+        Node ptr = list.head;
+        if (ptr == null) return;
+        while(ptr.next != null) {
             // Print the data at current node 
-       
+            System.out.println(ptr.data);
             // Go to next node 
+            ptr = ptr.next;
+        } 
+       
     } 
    
     // Driver code 
