b226a51e138b99f7d4a22b690237adf9bbcf8591
Ajay kumar Gogineni <ajkumar@Ajays-MacBook-Pro.local>
2023-05-21 19:16:20 -0700

    PreCourse1Completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,57 @@
+# Time complexity: O(1) Since every operation deals with changing the index or changing value at the last location
+# Space Complexity: O(n) where n is the maximum number of elements that can be stored in the stack
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+          # Initialize an array of size 10 to store elements
+          # The index variable points to the last element in the stack
+          self.arr = [0]*10
+          self.index = -1
          
      def isEmpty(self):
+          if self.index < 0:
+               return True
+          return False
          
      def push(self, item):
+          # Change value of the last index in array and update the index variable
+          if self.index+1 < len(self.arr):
+            self.arr[self.index + 1] = item
+            self.index += 1
+          else:
+               print('Maximum size of stack reached')
          
      def pop(self):
+          # This operation should remove the most recently added element from the list.
+          # This can be done by just decrementing the index
+          if self.index == 0:
+               print('Empty Stack. Cannot pop elements')
+               return
+          self.index -= 1
+          return self.arr[self.index + 1]
         
         
      def peek(self):
+          # Take a look at the most recently added eleemnt to the stack
+          return self.arr[self.index]
         
      def size(self):
+          print(f'Size: {self.index + 1}')
+          return self.index + 1
          
      def show(self):
+          # Printing all elements in the stack
+          return self.arr[:self.index + 1]
          
 
 s = myStack()
+print(s.isEmpty())
 s.push('1')
 s.push('2')
+print(s.isEmpty())
+print(s.size())
+print(s.peek())
 print(s.pop())
 print(s.show())
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,9 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        # Initialize value of the current node and pointer to the next node 
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +20,18 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        # Create a newNode that will be appended to the end of the list. 
+        newNode = ListNode(data)
+        if self.head == None:
+            self.head = newNode
+        else:
+            
+            temp = self.head
+            while temp.next:
+                temp = temp.next
+            # Traverse until the last element in the list and append the newNode
+            temp.next = newNode
+        # print(self.head.data)
         
     def find(self, key):
         """
@@ -24,9 +39,70 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        if self.head == None: return None
+        temp = self.head
+        # Initiate the temporary pointer to head and check elements in the linked list, one element at a time
+        while temp:
+            if temp.data == key:
+                print('Element found')
+                return temp
+            temp = temp.next
+        print('Element Not found')
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        # Return None when the list is empty as there are no elements to remove from
+        if self.head == None: return None
+
+        # A special case when the head element matches the key. In that case, we update the head pointer.
+
+        if self.head.data == key:
+            self.head = self.head.next
+            print('Element removed')
+            return
+
+        # Keep track of two pointers: previus and current. If the data at current pointer matches the key, 
+        # set prev.next = cur.next. This will remove the target element from the linked list
+        # The case where head element matches the key is taken care of in the above code
+
+        prev = self.head
+        cur = self.head.next
+
+        while cur:
+            if cur.data == key:
+                prev.next = cur.next
+                print('Element removed')
+                return
+            cur = cur.next
+            prev = prev.next
+        print('Could not remove the element')
+
+#  Time Complexity : O(n) for all operations
+#  Space Complexity : O(n) for all operations
+#  Did this code successfully run on Leetcode : Code ran successfully on the test cases mentioned below.
+
+
+## Testing
+
+obj = SinglyLinkedList()
+
+obj.append(1)
+obj.append(2)
+obj.append(3)
+obj.append(4)
+obj.append(5)
+obj.append(6)
+
+obj.find(1)
+obj.find(3)
+
+
+obj.remove(10)
+
+obj.remove(2)
+
+obj.find(2)
