71fee97b63c7a21cb0c1e0b62d552479c63219a5
Varun Raj <varunraj@Varuns-MacBook-Air.local>
2023-05-24 16:56:22 -0700

    completed the sprint1 precourse1

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,32 +1,44 @@
-
 class Node:
+   
+
     def __init__(self, data):
        self.data = data
        self.next = None
- 
+
+        self.data = data
+        self.next = None
+
+
 class Stack:
     def __init__(self):
-        
+
+        self.head = None
+
     def push(self, data):
-        
+
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
     def pop(self):
-        
+
+        if self.head is None:
+            return None
+        popped_value = self.head.data
+        self.head = self.head.next
+        return popped_value
+
+
 a_stack = Stack()
 while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    
     print('push <value>')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
-    elif operation == 'pop':
-        popped = a_stack.pop()
-        if popped is None:
-            print('Stack is empty.')
-        else:
-            print('Popped value: ', int(popped))
-    elif operation == 'quit':
-        break
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,32 +1,72 @@
-class ListNode:
-    """
-    A node in a singly-linked list.
-    """
-    def __init__(self, data=None, next=None):
-    
+def __init__(self, data=None, next=None):
+
+        self.data = data
+        self.next = next
+
+
 class SinglyLinkedList:
     def __init__(self):
-        """
-        Create a new singly-linked list.
-        Takes O(1) time.
-        """
-        self.head = None
+       
 
-    def append(self, data):
-        """
-        Insert a new element at the end of the list.
-        Takes O(n) time.
-        """
-        
+        new_node = ListNode(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
     def find(self, key):
-        """
-        Search for the first element with `data` matching
-        `key`. Return the element or `None` if not found.
-        Takes O(n) time.
-        """
-        
+       
+
+        current = self.head
+        while current:
+            if current.data == key:
+                return current
+            current = current.next
+        return None
+
     def remove(self, key):
+        
+        if self.head is None:
+            return
+
+        if self.head.data == key:
+            self.head = self.head.next
+            return
+
+        current = self.head
+        prev = None
+        while current:
+            if current.data == key:
+                prev.next = current.next
+                return
+            prev = current
+            current = current.next
+
+    def display(self):
         """
-        Remove the first occurrence of `key` in the list.
+        Display the elements in the list.
         Takes O(n) time.
         """
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+
+
+# Example usage:
+my_list = SinglyLinkedList()
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.append(4)
+
+my_list.display()
+
+print(my_list.find(3))
+
+my_list.remove(2)
+my_list.display()
