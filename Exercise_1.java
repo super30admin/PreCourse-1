d78781ce86ad755f812c46367b562a19b6bb50f9
unknown <shashankbidwai6@gmail.com>
2023-05-27 20:13:02 -0400

    Done with assignment

--- Exercise_1.java
+++ Exercise_1.java
@@ -6,41 +6,61 @@ class Stack {
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
-    { 
+    {
+        if(a.length == 0){
+            return true;
+        }
+        return false;
         //Write your code here 
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top  = 0;
+
     } 
   
     boolean push(int x) 
-    { 
+    {
+        if(a.length == top){
+            return false;
+        }
+        a[++top] = x;
+        return true;
         //Check for stack Overflow
         //Write your code here
     } 
   
     int pop() 
-    { 
+    {
+        if(top == 0){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        int no = a[top];
+        top--;
+        return no;
+
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
     } 
   
     int peek() 
-    { 
+    {
+        return a[top];
         //Write your code here
     } 
-} 
-  
-// Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
 }
+// Driver code
+class Main {
+    public static void main(String args[])
+    {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+    }
+}
+  
