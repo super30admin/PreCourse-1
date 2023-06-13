6ec826251c0007fa8d29afe5b2158eaab9ee2997
adhaar jaiswal <adhaarjaiswal@adhaars-MacBook-Pro.local>
2023-06-12 22:17:17 -0700

    Precourse 1 solutions. Please review

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h> 
+#include <iostream> 
   
 using namespace std; 
   
@@ -11,35 +11,68 @@ class Stack {
   
 public: 
     int a[MAX]; // Maximum size of Stack 
+    int counter=-1;
   
     Stack() { //Constructor here } 
     bool push(int x); 
     int pop(); 
     int peek(); 
     bool isEmpty(); 
+
+    for(int i=0;i<MAX;i++){
+        a[i]=INT_MIN;
+    }
 }; 
   
 bool Stack::push(int x) 
 { 
+    
     //Your code here
     //Check Stack overflow as well
+    if(counter<MAX-1){
+        counter++;
+        a[counter]=x;
+       
+        return true;
+    
+    }
+    top = a[counter];
+    return false;
 } 
   
 int Stack::pop() 
 { 
     //Your code here
     //Check Stack Underflow as well 
+    int temp=INT_MIN;
+    if(counter>-1){
+ temp=a[counter];
+        a[counter]=INT_MIN;
+    counter--;
+    }
+
+    if(counter>=0){
+top = a[counter];
+    }
+    return temp;
 } 
 int Stack::peek() 
 { 
     //Your code here
     //Check empty condition too
+        return top;
+
 } 
   
 bool Stack::isEmpty() 
 { 
     //Your code here 
+    if(counter>-1){
+        return false;
+    }
+    return true;
 } 
+}
   
 // Driver program to test above functions 
 int main() 
--- Exercise_3.cpp
+++ Exercise_3.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h> 
+#include <iostream>
 using namespace std; 
   
 // A linked list node (changes) 
@@ -15,12 +15,20 @@ a new node on the front of the list. */
 void push(Node** head_ref, int new_data)  
 {  
     /* 1. allocate node */ 
-  
+     Node* new_node = new Node();
+
+       Node* temp = *(head_ref);
+
     /* 2. put in the data */  
+    new_node->data = new_data;
   
     /* 3. Make next of new node as head */ 
+    new_node->next = temp;
   
     /* 4. move the head to point to the new node */
+   temp = new_node;
+*(head_ref) = temp;
+ 
 }  
   
 /* Given a node prev_node, insert a new node after the given  
@@ -28,14 +36,18 @@ prev_node */
 void insertAfter(Node* prev_node, int new_data)  
 {  
     /*1. check if the given prev_node is NULL */ 
+   if(prev_node!=NULL){
   
     /* 2. allocate new node */ 
-  
+   Node* new_node = new Node();
     /* 3. put in the data */ 
-  
+      new_node->data = new_data;
+
     /* 4. Make next of new node as next of prev_node */
-  
+  new_node->next = prev_node->next;
     /* 5. move the next of prev_node as new_node */ 
+    prev_node->next=new_node;
+   }
 }  
   
 /* Given a reference (pointer to pointer) to the head  
@@ -43,32 +55,61 @@ of a list and an int, appends a new node at the end */
 void append(Node** head_ref, int new_data)  
 {  
     /* 1. allocate node */ 
+    Node* new_node = new Node();
+
+    Node* temp = *(head_ref);
+
   
     /* 2. put in the data */ 
+    new_node->data = new_data;
   
     /* 3. This new node is going to be  
     the last node, so make next of  
     it as NULL*/  
-  
+  new_node->next =  NULL;
     /* 4. If the Linked List is empty, 
     then make the new node as head */
+if(!temp){
+temp=new_node;
+*(head_ref)=temp;
+}
+else{
   
     /* 5. Else traverse till the last node */
+        while(temp && temp->next) {
+            temp=temp->next;
+        }
   
     /* 6. Change the next of last node */ 
+    temp->next = new_node;
+}
+
+
+
 }  
   
 // This function prints contents of 
 // linked list starting from head  
 void printList(Node *node)  
 {  
+   
     //Your code here
+     Node* temp = node;
+
+     while(temp) {
+cout << temp->data << " ";
+       
+            temp=temp->next;
+        
+        }
+        cout << endl;
+  
 }  
   
 /* Driver code*/
 int main()  
 {  
-    Node* head = NULL;   
+  Node* head = NULL;   
     append(&head, 6);  
     push(&head, 7);  
     push(&head, 1);    
