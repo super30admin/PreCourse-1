9ed73d495b7d8b1818e7383674f2baa333d3635b
ayushichourasia02 <ayushichourasia02@gmail.com>
2023-06-20 17:39:16 -0500

    Completed Exercise 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -7,32 +7,52 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
-    } 
+        //Write your code here
+        if(top == 0 ){
+            System.out.println("Yes Empty");
+            return true;
+        }
+        else{
+            System.out.println("Not Empty");
+            return false;
+        }
+    }
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
+    Stack() {
+        //Initialize your constructor
+        top = -1;
+    }
+
+    boolean push(int x) {
         //Check for stack Overflow
+        if (top >= (MAX - 1)){
+            System.out.println("Stack Overflow");
+            return false;
+        } else {
+            a[++top] = x;
+            return true;
+        }
         //Write your code here
-    } 
-  
-    int pop() 
-    { 
+    }
+
+    int pop() {
         //If empty return 0 and print " Stack Underflow"
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            int x = a[top--];
+            return x;
+        }
+
+    }
+
+    int peek() {
         //Write your code here
-    } 
-  
-    int peek() 
-    { 
-        //Write your code here
-    } 
-} 
-  
+        return a[top];
+    }
+}
+
 // Driver code 
 class Main { 
     public static void main(String args[]) 
@@ -40,7 +60,12 @@ class Main {
         Stack s = new Stack(); 
         s.push(10); 
         s.push(20); 
-        s.push(30); 
+        s.push(30);
+        s.isEmpty();
         System.out.println(s.pop() + " Popped from stack"); 
-    } 
+        System.out.println(s.peek() + " Peeked from stack");
+        s.push(40);
+        System.out.println(s.peek() + " Peeked from stack");
+
+    }
 }
