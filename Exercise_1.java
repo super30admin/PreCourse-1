9b66d032c11f4b9fd7eb069325d2dda7ea4f1c84
Bency2712 <bencythomas6645@gmail.com>
2023-06-21 16:24:36 -0400

    Completed Sprint PreCourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,46 +1,76 @@
-class Stack { 
-    //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+// Time Complexity : O(1) 
+// Space Complexity : O(size of the array)  // Did this code successfully run on Leetcode : NA
+// Any problem you faced while coding this:  No
+// Your code here along with comments explaining your approach: For pushing the data, first we check if the index of the array has space in it to add elements, if it has, then we add the element to the next index of the array. To pop, it checks if the array is not empty, if it is not then it returns the value at the last index.
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
-        //Check for stack Overflow
-        //Write your code here
-    } 
-  
-    int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
-    } 
-  
-    int peek() 
-    { 
-        //Write your code here
-    } 
-} 
-  
-// Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+
+class Stack {
+    // Please read sample.java file before starting.
+    // Kindly include Time and Space complexity at top of each file
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack
+
+    boolean isEmpty() {
+        // Write your code here
+        return (top < 0);
+    }
+
+    Stack() {
+        // Initialize your constructor
+        top = -1;
+
+    }
+
+    boolean push(int x) {
+        // Check for stack Overflow
+        // Write your code here
+        if (top >= MAX - 1) {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+
+        else {
+            a[++top] = x;
+            return true;
+        }
+
+    }
+
+    int pop() {
+        // If empty return 0 and print " Stack Underflow"
+        // Write your code here
+
+        if (top < 0) {
+            System.out.println("Stack Undefllow");
+            return 0;
+        }
+
+        else {
+
+            return a[top--];
+        }
+    }
+
+    int peek() {
+        // Write your code here
+        if (top < 0) {
+            return 0;
+        }
+
+        else {
+            return a[top];
+        }
+    }
+}
+
+// Driver code
+class Main {
+    public static void main(String args[]) {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+    }
 }
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,70 +1,108 @@
-import java.io.*; 
-  
-// Java program to implement 
-// a Singly Linked List 
-public class LinkedList { 
-  
-    Node head; // head of list 
-  
-    // Linked list Node. 
-    // This inner class is made static 
-    // so that main() can access it 
-    static class Node { 
-  
-        int data; 
-        Node next; 
-  
-        // Constructor 
-        Node(int d) 
-        { 
-            //Write your code here 
-        } 
-    } 
-  
-    // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
-    { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
+// Time Complexity : O(n) as it has to traverse through the complete linked list for inserting and printing
+// Space Complexity : O(n) 
+// Did this code successfully run on Leetcode : NA
+// Any problem you faced while coding this:  Had to go through study materials online for correcting the syntax 
+// Your code here along with comments explaining your approach: For insertion, a new node is added after the last node: For Inserting, first, we check if the LinkedList is empty and if it is, then we add the new node to the head of the list, or else we traverse till the last node and add it to the last node. For printing, we traverse through the complete linked list and print the data in the current node.
 
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
-    } 
-  
-    // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
-    {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
-    } 
-   
-    // Driver code 
-    public static void main(String[] args) 
-    { 
+import java.io.*;
+
+// Java program to implement
+// a Singly Linked List
+public class LinkedList {
+
+    Node head; // head of list
+
+    // Linked list Node.
+    // This inner class is made static
+    // so that main() can access it
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        // Constructor
+        Node(int d)
+        {
+            //Write your code here
+            this.data=d;
+            this.next=null;
+        }
+    }
+
+    // Method to insert a new node
+    public static LinkedList insert(LinkedList list, int data)
+    {
+
+        // Create a new node with given data
+        Node newNode=new Node(data);
+
+        // If the Linked List is empty,
+        // then make the new node as head
+        if(list.head==null)
+        {
+
+            list.head=newNode;
+            return list;
+
+        }
+
+        // Else traverse till the last node
+        // and insert the new_node there
+        else
+        {
+            Node lastNode=list.head;
+
+            while(lastNode.next!=null)
+            {
+                lastNode=lastNode.next;
+            }
+            // Insert the new_node at last node
+
+            lastNode.next=newNode;
+
+        }
+
+        // Return the list by head
+
+        return list;
+
+
+    }
+
+    // Method to print the LinkedList.
+    public static void printList(LinkedList list)
+    {
+        // Traverse through the LinkedList
+        Node currentNode=list.head;
+
+        while (currentNode!=null)
+        {
+            // Print the data at current node
+            System.out.println(currentNode.data);
+            // Go to next node
+            currentNode=currentNode.next;
+        }
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
         /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
-  
-        // 
-        // ******INSERTION****** 
-        // 
-  
-        // Insert the values 
-        list = insert(list, 1); 
-        list = insert(list, 2); 
-        list = insert(list, 3); 
-        list = insert(list, 4); 
-        list = insert(list, 5); 
-  
-        // Print the LinkedList 
-        printList(list); 
-    } 
+        LinkedList list = new LinkedList();
+
+        //
+        // ******INSERTION******
+        //
+
+        // Insert the values
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+
+        // Print the LinkedList
+        printList(list);
+    }
 }
