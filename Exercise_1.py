2e087f1226f04b502f5c7ebd0aa787f71a6b2f7f
yashatre98 <yashatre98@gmail.com>
2023-06-21 16:43:45 -0700

    Precourse commit 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,44 @@
+# Time Complexity : Push O(1) POP O(1)
+# Space Complexity :O(N)
+# Did this code successfully run on Leetcode : 
+
+# Any problem you faced while coding this : 
+
+# Your code here along with comments explaining your approach
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+          self.stack=[]
          
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
+     def isEmpty(self): # checks for empty stack
+         if (len(self.stack)):
+              return False
+         return True
+     
+     def push(self, item):  # pushes the item using append function
+         self.stack.append(item)
+
+     def pop(self): # pops the top element
+        if(not self.isEmpty()):
+            return self.stack.pop()
+        return "stack Empty"
         
+     def peek(self):  # returns the top element
+         if(not self.isEmpty()):
+             return self.stack[-1]
+         return "stack emplty"
         
-     def peek(self):
-        
-     def size(self):
+     def size(self):  # returns the size of list
+         if(not self.isEmpty()):
+             return len(self.stack)
+         return "stack empty"
          
-     def show(self):
+     def show(self):  # returns the list
+         if(not self.isEmpty()):
+             return self.stack
+         return "stack Empty"
          
 
 s = myStack()
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,10 @@
+# Time Complexity : Push = O(1) POP O(N)
+# Space Complexity : O(N)
+# Did this code successfully run on Leetcode :
+
+# Any problem you faced while coding this :
+
+# Your code here along with comments explaining your approach
 
 class Node:
     def __init__(self, data):
@@ -6,11 +13,36 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.head= Node(None)
+        self.first_node=True
+        self.curr=self.head
+        self.size=0
     def push(self, data):
+        if(self.first_node):
+            self.head.data=data
+            self.curr=self.head
+            self.size+=1
+            self.first_node=False
+        else:
+            new_node= Node(data)
+            self.curr.next=new_node
+            self.curr=new_node
+            self.size+=1
         
     def pop(self):
+        top_val= self.curr.data
+        temp=self.head
+        if(temp.next==None):
+            self.head=None
+            self.size-=1
+            return top_val
         
+        while(temp.next!=self.curr):
+            temp=temp.next
+        temp.next=None
+        self.curr=temp
+        self.size-=1
+        return top_val
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
