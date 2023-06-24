fc6b4bb3414503abb8d5a6c9dc4d9abb11522b67
somonnoy <sb7238@g.rit.edu>
2023-06-24 15:09:28 -0400

    Precourse1 Exercise 1 completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -7,29 +7,73 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
-    } 
+        //Write your code here
+        if(top == -1) // top == -1 indicates there is no element in the stack
+        {
+            return true;
+        }
+        return false;
+    }
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+
+        top++;
+
+        if(top <= MAX) // top <= MAX indicates there is space in the stack
+        {
+            a[top] = x;
+            return true;
+        }
+
+        else
+        {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+
+        if(top == -1)
+        {
+            System.out.println(" Stack Underflow");
+            return 0;
+        }
+
+        else
+        {
+            return a[top--];
+        }
+
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(top == -1)
+        {
+            System.out.println(" Stack Underflow");
+            return 0;
+        }
+
+        else
+        {
+            return a[top];
+        }
     } 
 } 
   
@@ -41,6 +85,9 @@ class Main {
         s.push(10); 
         s.push(20); 
         s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.pop() + " Popped from stack");
+        s.pop();
     } 
 }
