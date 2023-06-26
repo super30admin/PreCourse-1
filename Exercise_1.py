1d5cae5f3b9f533904fe07a9171d0c8b7d693fcc
iamshubham28 <shubham.ashtekar28@gmail.com>
2023-06-26 22:29:15 +0530

    Completed Sprint PreCourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,29 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
+  #time complexity= O(1)
+  #space complexity= O(1)
+
+    def __init__(self):
+      self.stack=[]
          
-     def isEmpty(self):
+    def isEmpty(self):
+      return(len(self.stack)==0)
          
-     def push(self, item):
+    def push(self, item):
+      self.stack.append(item)
          
-     def pop(self):
+    def pop(self):
+      return self.stack.pop()
         
+    def peek(self):
+      return self.stack[-1]
         
-     def peek(self):
-        
-     def size(self):
+    def size(self):
+      return len(self.stack)
          
-     def show(self):
+    def show(self):
+      return self.stack
          
 
 s = myStack()
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,12 @@
+#time complexity= O(n)
+#space complexity= O(n)
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +21,13 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        if self.head is None:
+            self.head = ListNode(data)
+        else:
+            currNode = self.head
+            while(currNode.next is not None):
+                currNode = currNode.next
+            currNode.next = ListNode(data)
         
     def find(self, key):
         """
@@ -24,9 +35,45 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        currNode = self.head
+        while(currNode is not None):
+            if(currNode.data == key):
+                return currNode
+            currNode = currNode.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if(self.head is None):
+            print("List is Empty")
+            return False
+        if(self.head.data == key):
+            self.head = self.head.next
+            print("The element removed is %s" %key)
+            return True
+        curr = self.head
+        while(curr.next is not None):
+            if(curr.next.data == key):
+                curr.next = curr.next.next
+                print("The element removed is %s" %key)
+                return True
+            curr = curr.next
+        print("The Key doesn't exist in the List")
+        return False
+    def __str__(self):
+        curr = self.head
+        out = []
+        while(curr is not None):
+            out.append(str(curr.data))
+            curr = curr.next
+        return " ".join(out)
+
+myList = SinglyLinkedList()
+myList.append(1)
+myList.append(2)
+print(myList)
+myList.remove(2)
+print(myList)
