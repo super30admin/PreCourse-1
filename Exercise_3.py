e1db5454f87252762b549b042d96c81b0749fa89
gunjan dayani <41314977+dgh007786@users.noreply.github.com>
2023-06-26 21:26:58 -0400

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,8 +2,12 @@ class ListNode:
     """
     A node in a singly-linked list.
     """
-    def __init__(self, data=None, next=None):
-    
+
+    def __init__(self, data, next):
+        self.data = data
+        self.next = None
+
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -15,18 +19,48 @@ class SinglyLinkedList:
     def append(self, data):
         """
         Insert a new element at the end of the list.
-        Takes O(n) time.
+        Takes O(1) time.
         """
-        
+        new_node = ListNode(data)
+        new_node.set_next(self.head)
+        self.head = new_node
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        current = self.head
+        found = False
+        while current and found is False:
+            if current.data == key:
+                found = True
+            else:
+                current = current.get_next()
+        if current is None:
+            raise ValueError("Data not in list")
+        return current
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        current = self.head
+        previous = None
+        found = False
+        while current and found is False:
+            if current.data == key:
+                found = True
+            else:
+                previous = current
+                current = current.next
+        if current is None:
+            raise ValueError("Data not in list")
+        if previous is None:
+            self.head = current.next
+        else:
+            previous.next = current.next
+
+
