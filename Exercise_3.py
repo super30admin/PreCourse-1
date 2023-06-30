ae1b7a18be37d9e254aae0880e02fd91a13f2298
Sudha Ramachandran <137917754+SudhaRamachandran@users.noreply.github.com>
2023-06-30 08:25:06 +0530

    completed pre-course 1 exercises

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,13 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        if self.head == None:
+            self.head = ListNode(data=data, next=None)
+        else:
+            new_node = ListNode(data=data, next=None)
+            while self.head.next != None:
+                self.head = self.head.next
+            self.head.next = new_node        
         
     def find(self, key):
         """
@@ -24,9 +33,29 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        if self.head == None:
+            return self.head
+        else:
+            node = self.head
+            while node and node.data != key:
+                node = node.next
+            return node
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head == None:
+            return self.head
+        else:
+            node = self.head
+            while node and node.data!= key:
+                node = node.next
+            next_node = node.next
+            if next_node != None:
+                node.data = next_node.data
+                node.next = next_node.next
+                next_node.next = None
+                del(next_node)
+            
