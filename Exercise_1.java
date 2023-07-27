c5c4be8bd7a976eea0fe4a2800bffda1260c9426
anoushkaerram <anoushkaerram@gmail.com>
2023-07-27 13:06:57 -0700

    Completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,68 @@
 class Stack { 
     //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+    //Kindly include Time and Space complexity at top of each file
+
+    //Time Complexity: O(1) for all the methods because they all perform constant-time operations.
+    //Space Complexity: O(N) where N is the max size of the stack.
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        return top == -1; 
+        //if this condition is true, then the stack is empty
+        //if its false, stack is not empty
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+       this.top = top;
+       //initializing top to -1 to indicate empty
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if (top >= MAX - 1){
+            System.out.print("Stack Overflow");
+            return false;
+        } else{
+            a[++top] = x;
+            return true;
+        }
+        //If top's value is greater than the MAX, theres stack overflow
+        //else, increment top and set value to x at that position
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(isEmpty()){
+            System.out.print("Stack Underflow");
+            return 0;
+        }else{
+            return a[top--];
+        }
+        //If the stack is empty, then prints Stack Underflow
+        //Else, pops off most recent value in array then decrements top
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(isEmpty()){
+            System.out.print("Stack Empty");
+            return 0;
+        }else{
+            return a[top];
+        }
+        //If stack is empty, then prints empty
+        //else, returns value at the index indicated by top's value
     } 
 } 
   
--- Exercise_3.java
+++ Exercise_3.java
@@ -2,6 +2,10 @@ import java.io.*;
   
 // Java program to implement 
 // a Singly Linked List 
+//Time Complexity: O(N) because for insert, we may need to traverse the entire list to find the last node,
+                // and for printList, we neeed to traverse through the entire list once to print
+//Space Complexity: O(1) for all the methods because we use a fixed amount of memory for storing the nodes
+    
 public class LinkedList { 
   
     Node head; // head of list 
@@ -18,6 +22,8 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            data = d;
+            next = null;
         } 
     } 
   
@@ -25,26 +31,42 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
+        Node newNode = new Node(data);
    
         // If the Linked List is empty, 
         // then make the new node as head 
-        
+        if(list.head == null){
+            list.head = newNode;
+        }else{
             // Else traverse till the last node 
             // and insert the new_node there 
+            Node end = list.head;
+            while(end.next != null){
+                end = end.next;
+            }
+        }
+     
 
-            // Insert the new_node at last node 
+        // Insert the new_node at last node 
+        end.next = newNode;
         // Return the list by head 
-        
+        return list;
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
+        Node curr = list.head;
+        System.out.print("LinkedList: ");
         // Traverse through the LinkedList 
-   
-            // Print the data at current node 
+        while(curr != null){
+            // Print the data at current node
+            System.out.print(curr.data + " ");
+            // Go to next node
+            curr = curr.next;
+        }
        
-            // Go to next node 
+ 
     } 
    
     // Driver code 
