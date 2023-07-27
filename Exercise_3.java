b0c765ab42464d93cdb0c5c76203d2e82e06a6c4
KrishnaChaithvika <krishna.chaithvika@gmail.com>
2023-07-27 15:00:40 -0700

    Completed PreCourse1 Assignments

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,10 @@
+// Did this code successfully run on Leetcode : Yes
+
 import java.io.*; 
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList { 
   
     Node head; // head of list 
   
@@ -18,33 +20,50 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            this.data = d;
         } 
     } 
-  
+
+    LinkedList() {
+        this.head = null;
+    }
+    // Time Complexity : O(N)
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
-   
         // If the Linked List is empty, 
         // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
-
-            // Insert the new_node at last node 
+        // Else traverse till the last node 
+        // and insert the new_node there 
+        // Insert the new_node at last node 
         // Return the list by head 
+
+        Node newNode = new Node(data);
+        if(list.head == null) {
+            list.head = newNode; 
+        } else {
+            Node lastNode = list.head;
+            while(lastNode.next != null) {
+                lastNode = lastNode.next;
+            }
+            lastNode.next = newNode;
+        }
+        return list;
         
     } 
-  
+    // Time Complexity : O(N)
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
         // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+        // Print the data at current node 
+        // Go to next node 
+            Node presentNode = list.head;
+            while(presentNode != null) {
+                System.out.println("Current node value: "+presentNode.data);
+                presentNode = presentNode.next;
+            }
     } 
    
     // Driver code 
