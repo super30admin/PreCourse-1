d4b9764441c9a5014f4939dd5eb3396a6d1e5ae6
ramyav2303 <ramyav@csu.fullerton.edu>
2023-07-29 10:09:09 -0700

    code updated

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,12 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 import java.io.*; 
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+public class Exercise_3 { 
   
     Node head; // head of list 
   
@@ -18,40 +22,60 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            data = d; 
+            next = null;
         } 
     } 
   
     // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
+    public static Exercise_3 insert(Exercise_3 list, int data) 
     { 
         // Create a new node with given data 
+        Node new_node = new Node(data);
    
         // If the Linked List is empty, 
         // then make the new node as head 
-        
+        if (list.head == null) { 
+            list.head = new_node; 
+        }       
             // Else traverse till the last node 
             // and insert the new_node there 
-
-            // Insert the new_node at last node 
+        else{
+            Node last = list.head; 
+            while (last.next != null) { 
+                last = last.next; 
+            } 
+            // Insert the new_node at last node
+            last.next = new_node;
+        }                 
         // Return the list by head 
+        return list;
         
     } 
   
     // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
+    public static void printList(Exercise_3 list) 
     {  
+        Node currNode = list.head; 
+     
+        System.out.print("LinkedList: "); 
+        
         // Traverse through the LinkedList 
+        while (currNode != null) { 
    
             // Print the data at current node 
+            System.out.print(currNode.data + " "); 
        
             // Go to next node 
+            currNode = currNode.next; 
+        }    
     } 
    
     // Driver code 
     public static void main(String[] args) 
     { 
         /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
+        Exercise_3 list = new Exercise_3(); 
   
         // 
         // ******INSERTION****** 
