7cb683e9263d1cc2385fb8544ff0fec79e214658
Sai Vinherkar <sai.vinherkar@angi.com>
2023-07-30 17:43:58 -0500

    Completed Sprint Precourse1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,5 +1,6 @@
 public class StackAsLinkedList { 
-  
+    // Time Complexity = O(n)
+  //Space Complexity =  O(n)
     StackNode root; 
   
     static class StackNode { 
@@ -8,23 +9,42 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
+            data = data;
+            next = null;
             //Constructor here 
         } 
+
+        StackAsLinkedList(){
+            root = null;
+        }
     } 
     
 	
     public boolean isEmpty() 
     { 
+        return (root == null);
         //Write your code here for the condition if stack is empty. 
     } 
   
     public void push(int data) 
     { 
+        StackNode temp = new StackNode();
+        temp.data = data;
+        temp.next = root;
+        root = temp;
         //Write code to push data to the stack. 
     } 
   
     public int pop() 
     { 	
+        if(root == null){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        int value = root.value;
+        root = root.next;
+        return value;
+
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
@@ -32,6 +52,11 @@ public class StackAsLinkedList {
   
     public int peek() 
     { 
+        if(!isEmpty()){
+            return top.data;
+        }
+            System.out.println("Stack is Empty");
+            return -1;
         //Write code to just return the topmost element without removing it.
     } 
   
