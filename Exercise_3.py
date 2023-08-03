949ffb344431820879c0438d6b4b9b0388bbefa0
Aayushi Vishnoi -X (aavishno - PERSISTENT SYSTEMS INC at Cisco) <aavishno@cisco.com>
2023-08-03 14:18:23 +0530

    Completed PreCouse1- Please review.

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = None
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,13 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        if self.head is None:
+            self.head = ListNode(data)
+        else:
+            while self.head.next != None:
+                self.head = self.head.next
+            self.head.next = ListNode(data)
+
         
     def find(self, key):
         """
@@ -24,9 +33,29 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        if self.head is None:
+            return None
+        else:
+            while self.head != None:
+                if self.head.data == key:
+                    return self.head.data
+        return None    
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head is None:
+            return None
+        currNode = self.head
+        prevNode = None
+        nextnode = None
+        while currNode is not None:
+            if currNode.data == key:
+                nextnode = currNode.next
+                prevNode.next = nextnode
+                prevNode = currNode
+            else:
+                prevNode = currNode
+            currNode = currNode.next
