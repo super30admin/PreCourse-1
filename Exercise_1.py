285d1e8ca0e755c3f7e8d1850bea8b9a3bfe7f9e
Krushika Gujarati <krushika2910@gmail.com>
2023-08-23 12:50:57 -0700

    completed sprint

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,7 +2,7 @@ class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+         self.i = 10 
      def isEmpty(self):
          
      def push(self, item):
