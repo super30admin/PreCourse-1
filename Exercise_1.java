766772e56a9e0b177bb2610d5f26bbfdae625c94
jaissica <jaissica.hora@gmail.com>
2023-08-26 16:01:16 -0400

    Exercise 1 in Java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,6 +1,7 @@
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
+  
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
@@ -8,28 +9,62 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        if (top ==-1)
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        top +=1;
+        if (top>=MAX)
+        {
+            return false;
+        } 
+        a[top] = x;
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if (top==-1)
+        {
+           System.out.println(" Stack Underflow");
+           return 0;
+        }
+        else 
+        {
+          int popped_item = a[top];
+          a[top]=0;
+          top-=1;
+          return popped_item;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        if (top ==-1)
+        {
+            return 0;
+        }
+        else {
+            return a[top];
+        }
     } 
 } 
   
@@ -42,5 +77,15 @@ class Main {
         s.push(20); 
         s.push(30); 
         System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.peek()+ " Top element of the stack");
+        System.out.println(s.isEmpty()+"  is Empty.");
+        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.isEmpty()+"  is Empty.");
+        s.push(10); 
+        s.push(20); 
+        s.push(30); 
+        System.out.println(s.peek()+ " Top element of the stack");
     } 
 }
