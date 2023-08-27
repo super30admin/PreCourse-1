b345a11eb3caa3b8763a6b959ee893ae3d5609f8
shivam130502 <73218130+shivam130502@users.noreply.github.com>
2023-08-27 08:43:29 -0700

    Update Exercise_1.java
    Shivam Dhir

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,6 @@
+// time  : O(n) .. n = number of operations
+// Space : O(MAX) .. MAX = size of array
+
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
@@ -8,28 +11,43 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        return top >= 0;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        top++;
+        if(top < MAX){
+            a[top] = x;
+            return true;
+        } else return false;
+        
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top >= 0)
+            return a[top--];
+        else {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        return a[top];
     } 
 } 
   
