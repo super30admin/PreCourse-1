ffbba0dcf560839a301b9d1e4027013f78c773dd
Jay Shah <jashaha@amazon.com>
2023-08-27 00:52:24 -0700

    Completed PreCourse-1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,46 +1,62 @@
-class Stack { 
-    //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+class Stack {
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        return top < 0; //returns True if the value of top is less than 0, otherwise returns false
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top = -1; //When new stack is created , top is set to -1
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        if(top < MAX - 1){
+            a[++top] = x;
+            return true;
+        }
+        System.out.println("Stack Overflow");
+        return false;
     } 
   
     int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+    {
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return a[top--];
     } 
   
     int peek() 
     { 
-        //Write your code here
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return a[top];
     } 
 } 
   
 // Driver code 
-class Main { 
+class Main {
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
         s.push(10); 
         s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack\n");
+        System.out.println("Top element is " + s.peek() +"\n");
+        System.out.println(s.pop() + " Popped from stack\n");
+        System.out.println(s.pop() + " Popped from stack\n");
+        System.out.println(s.pop() + " Popped from stack\n");
     } 
 }
