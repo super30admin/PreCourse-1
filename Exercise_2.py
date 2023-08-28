b3e38928390c47e75481ce233010128970f638a0
nrajaram99 <99rniharika10@gmail.com>
2023-08-27 19:26:11 -0500

    Completed Precourse-1

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,6 @@
+# Time Complexity: Push - O(1)
+# Time Complexity: Pop - O(1)
+# Space Complexity: O(N) where N is the number of elements in the list
 
 class Node:
     def __init__(self, data):
@@ -6,10 +9,24 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.head = None
+
     def push(self, data):
-        
+        new_node = Node(data)
+        if self.head == None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        print(self.head.data)
+
     def pop(self):
+        if self.head == None:
+            return None
+        else:
+            pop_node = self.head
+            self.head = self.head.next
+            return pop_node.data
         
 a_stack = Stack()
 while True:
