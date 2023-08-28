d6cfbc99e33455862478efadbb52c1f3148932bd
shridharbhardwaj <shridharbhardwaj@gmail.com>
2023-08-27 23:29:27 -0700

    Attempt-1 for Precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,23 +2,48 @@ class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+        self.items = []
          
      def isEmpty(self):
+        return len(self.items) == 0
          
      def push(self, item):
+        self.items.append(item)
          
      def pop(self):
+        if not self.isEmpty():
+           return self.items.pop()
+
+        else:
+           raise IndexError("remove from empty stack")
         
         
      def peek(self):
+        if not self.isEmpty():
+           return self.items[-1]
+        else:
+           raise IndexError("peek from empty stack")
         
      def size(self):
+        return len(self.items)
          
      def show(self):
+        if not self.isEmpty():
+           print("All the elements present in the stack:")
+           for item in reversed(self.items):
+              print(item)
+        else:
+           print("Stack is empty")
          
 
 s = myStack()
-s.push('1')
+s.push('1')                                            # push() will simply put the elements in the stack in LIFO
 s.push('2')
-print(s.pop())
-print(s.show())
+s.push('3')
+s.push('4')
+
+print(s.pop())                                         # pop() will remove the last element from the stack i.e.4
+print(s.peek())                                        # peek() will display the last element without removing it
+print(s.show())                                        # show() will display the final stacked elements, after any operations done already
+print("Is the stack empty?", s.isEmpty())              # isEmpty() will check the stack if it is empty or not, incase it is empty it will raise a exception
+print(s.size())                                        # size() will display the total elements' count
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,16 @@
+# Time Complexity :
+# Space Complexity :
+# Did this code successfully run on Leetcode : No, I'm getting problem in executing it
+# Any problem you faced while coding this : I'm stuck in this one!
+# Your code here along with comments explaining your approach :
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
-    def __init__(self, data=None, next=None):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,16 +25,51 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        current = self.head
+        while current:
+            if current.data == key:
+                return True
+            current = current.next
+        return False
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if not self.head:
+            return
+
+        if self.head.data == key:
+            self.head = self.head.next
+            return
+
+        current = self.head
+        while current.next:
+            if current.next.data == key:
+                current.next = current.next.next
+                return
+            current = current.next
+
+linked_list = SinglyLinkedList()
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
+print("Is 2 in the list?", linked_list.find(2))
+linked_list.remove(2)
+
