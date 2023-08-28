9f6213008a8eb2ae501b35e3eec9d51e9908905c
RagendhuRamesh <ragendhuram@gmai.com>
2023-08-28 07:09:37 +0530

    Completed Sprint Precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,22 +2,26 @@ class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+         self.stack=[]
      def isEmpty(self):
-         
+         return len(self.stack)==0
      def push(self, item):
-         
+         self.stack.append(item)
      def pop(self):
-        
-        
+         if not self.isEmpty():
+             return self.stack.pop()
+         else:
+             return "stack is empty"
      def peek(self):
-        
+         if not self.isEmpty():
+             return self.stack[-1]
+         else:
+             return "stackis empty"
      def size(self):
-         
+         return len(self.stack)
      def show(self):
-         
-
-s = myStack()
+         return self.stack
+s = myStack() 
 s.push('1')
 s.push('2')
 print(s.pop())
--- Exercise_2.py
+++ Exercise_2.py
@@ -6,12 +6,29 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.head=None
+
     def push(self, data):
-        
+        new_node = Node(data)
+        if self.head is None:
+            self.head= new_node
+        else:
+            new_node.next=self.head
+            self.head=new_node
+
     def pop(self):
-        
+        if self.is_empty():
+            return None
+        else:
+            popped=self.head.data
+            self.head=self.head.next
+            return popped
+
+    def is_empty(self):
+        return self.head is None
+    
 a_stack = Stack()
+
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
