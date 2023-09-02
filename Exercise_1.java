ada113e91d67fc16b4b94a3976a27d470d9dbb0d
GunjanHardasmalani <hardasmalani.g@northeastern.edu>
2023-09-02 13:39:55 -0700

    precourse 1 complete

--- Exercise_1.java
+++ Exercise_1.java
@@ -8,39 +8,70 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        if(top == -1){
+            return true;
+        }
+        return false;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top == MAX - 1){
+             System.out.println("Stack overflow");
+            return false;
+        }else{
+            top++;
+            a[top] = x;
+        }
+    return true;
+
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(isEmpty()){
+            System.out.println("Stack underflow");
+            return 0;
+        }else{
+            int element = a[top];
+            top--;
+            return element;
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(isEmpty()){
+            return 0;
+        }else{
+            return a[top];
+        }
     } 
 } 
   
 // Driver code 
-class Main { 
+class StackUsingArray { 
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
+        System.out.println("Is stack is empty " + s.isEmpty());
         s.push(10); 
+        System.out.println(s.peek() + " Peek element");
         s.push(20); 
-        s.push(30); 
+        s.push(30);
+         System.out.println("Is stack is empty " + s.isEmpty());
         System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.peek() + " Peek element");
     } 
 }
