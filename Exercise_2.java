d2effbb630367d9f25bf186accf8361c508f9a1d
Santosh Reddy <psreddy.031998@gmail.com>
2023-09-06 18:47:50 -0700

    Completed PreCourse-1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,6 @@
-public class StackAsLinkedList { 
+// Time complexity: O(1)
+// Space Complexity: O(1)
+class StackAsLinkedList {
   
     StackNode root; 
   
@@ -8,31 +10,54 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
-            //Constructor here 
+            //Constructor here
+            this.data = data;
+            next= null;
         } 
     } 
     
 	
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+        //Write your code here for the condition if stack is empty.
+        return (root==null);
     } 
   
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
+        //Write code to push data to the stack.
+        StackNode temp = root;
+        root = new StackNode(data);
+        root.next = temp;
     } 
   
     public int pop() 
     { 	
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+	//Also return the popped element
+        if(isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+
+        // code to pop the topmost element of stack.
+        else {
+            StackNode temp= root;
+            int topelement= root.data;
+            root = root.next;
+            // returning the popped element
+            return topelement;
+        }
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+        if(isEmpty())
+            return 0;
+        else
+            return root.data;
     } 
   
 	//Driver code
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,10 @@
 import java.io.*; 
   
 // Java program to implement 
-// a Singly Linked List 
-public class LinkedList { 
+// a Singly Linked List
+// Time complexity is O(1)
+// Space complexity is O(1)
+class LinkedList {
   
     Node head; // head of list 
   
@@ -15,36 +17,45 @@ public class LinkedList {
         Node next; 
   
         // Constructor 
-        Node(int d) 
+        Node(int data)
         { 
-            //Write your code here 
+            //Write your code here
+            this.data = data;
+            next = null;
+
         } 
     } 
   
     // Method to insert a new node 
     public static LinkedList insert(LinkedList list, int data) 
     { 
-        // Create a new node with given data 
+        // Create a new node with given data
+        Node  newnode = new Node(data);
    
         // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
-
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
+        // then make the new node as head
+        if(list.head == null){
+            list.head = newnode;
+        }
+        else {
+            Node last = list.head;
+            while(last.next!=null) {
+                last = last.next;
+            }
+            last.next=  newnode;
+        }
+        return list;
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
-    {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
+    {
+        Node currentNode= list.head;
+        while(currentNode !=null) {
+            System.out.print(currentNode.data+" ");
+            // Go to next node
+            currentNode= currentNode.next;
+        }
     } 
    
     // Driver code 
