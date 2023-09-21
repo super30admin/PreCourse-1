ec16843d07af3cd6d248d1bf6c161cadd772d5c1
NeuYash <pawar.ya@northeastern.edu>
2023-09-21 19:23:00 -0400

    Completed Precourse 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -2,34 +2,51 @@ class Stack {
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
-    int top; 
+    int top;
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
         //Write your code here 
+        return top<0;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        this.top=-1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        if (top >= MAX - 1) {
+            System.out.println("Stack Overflow");
+            return false;
+        } else {
+            a[++top] = x;
+            return true;
+        }
     } 
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            int x = a[top--];
+            return x;
+        }
     } 
   
     int peek() 
     { 
-        //Write your code here
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return 0;
+        } else {
+            int x = a[top];
+            return x;
+        }
     } 
 } 
   
