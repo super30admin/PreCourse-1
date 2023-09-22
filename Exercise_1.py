1e43df708f02508621655cd79f3dd4eac79aea3e
apekshakhilari <apeksha.khilari@tamr.com>
2023-09-22 16:23:04 -0700

    Pre Course-1 completed - please review

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,60 @@
+# Exercise_1 : Implement Stack using Array.
+
+# Time Complexity : O(1).
+# Space Complexity : O(1) for all operations except show since the space required to store the list = no of elements in the list
+# Did this code successfully run on Leetcode : could not find the question on Leetcode
+# Any problem you faced while coding this : Struggled with time and space complexity
+
+# Your code here along with comments explaining your approach
+
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    # Please read sample.java file before starting.
+    # Kindly include Time and Space complexity at top of each file
+    def __init__(self):
+        # initialize top of stack to -1. This means tos is not pointing to any index in the array, as the array is empty
+        self.tos = -1
+        self.arr = []
+
+    def isEmpty(self):
+        # if tos is -1, that means array is empty
+        if self.tos == -1:
+            return True
+        return False
+
+    def push(self, item):
+        # increment tos by 1 and then append the element. tos will always point to the last element in the array
+        self.tos += 1
+        self.arr.append(item)
+        print(self.show())
+
+    def pop(self):
+        # check if tos == -1, if yes array is empty. There is nothing to pop.
+        if self.isEmpty():
+            return "Empty Stack, nothing to pop"
+        # else: decrement tos and pop the last element from the array
+        self.tos -= 1
+        return self.arr.pop()
+
+    def peek(self):
+        # if array is not empty, return the value at which tos points. this is the last element of the array
+        if self.isEmpty():
+            return "Empty Stack, nothing to show"
+        return self.arr[self.tos]
+
+    def size(self):
+        # return length of the array
+        return len(self.arr)
+
+    def show(self):
+        # return the array
+        return self.arr
+
 
 s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
+s.push('5')
+print(s.peek())
 print(s.show())
+print(s.size())
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,9 +1,22 @@
+# Exercise_3 : Implement Singly Linked List.
+
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : N/A
+# Any problem you faced while coding this : thinking of edge cases
+
+# Your code here along with comments explaining your approach
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
+
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +30,75 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        # if head is none, means there is no linked list yet. So create a new node with passed data and point head to it
+        if self.head is None:
+            self.head = ListNode(data)
+        curr = self.head
+        # if head is not none, there is atleast one node already present.
+        # Until curr.next is not none, keep forwarding curr pointer.
+        # Append a new node whenever curr.next is None meaning its the end of the list
+        while curr.next is not None:
+            curr = curr.next
+        curr.next = ListNode(data)
+        return curr.next.data
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        # if head is none, means there is no linked list yet. So return none
+        if self.head is None:
+            return None
+        curr = self.head
+        # Forward curr pointer until you find that curr.data equals the asked key.
+        while curr.next is not None:
+            curr = curr.next
+            if curr.data == key:
+                return curr
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        # if head is none, means there is no linked list yet. So return none
+        if self.head is None:
+            return None
+        # if the element to be removed is at head itself, forward head to the next node
+        if self.head.data == key:
+            self.head = self.head.next
+            return
+        curr = self.head
+        # else - keep forwarding curr until you find that curr.next.data equals the key
+        while curr.next is not None:
+            if curr.next.data == key:
+                curr.next = curr.next.next
+                return
+            curr = curr.next
+        return None
+
+    # print linked list for understanding purpose
+    def printList(self):
+        if self.head is None:
+            print("Empty linked list")
+            return
+        curr = self.head
+        while curr.next is not None:
+            print(curr.next.data)
+            curr = curr.next
+
+
+l_list = SinglyLinkedList()
+
+l_list.append(5)
+l_list.append(2)
+l_list.append(3)
+l_list.printList()
+
+print('Found element: ', l_list.find(3).data)
+
+l_list.remove(2)
+l_list.printList()
