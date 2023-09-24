5a2524a0e55cab49e3a91f8b7faa5d5f8703e3c2
Aanchal <aanchalrohira972@gmail.com>
2023-09-24 17:35:30 -0400

    PrecourseWork-1 completed, please review

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,11 @@
-import java.io.*; 
+import java.io.*;
+import java.net.http.HttpHeaders; 
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+//Time Complexity: O(n) 
+//Space Complexity: O(n) 
+public class Exercise_3 { 
   
     Node head; // head of list 
   
@@ -18,11 +21,13 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            this.data   = d; 
+            this.next   = null; 
         } 
     } 
   
     // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
+    public static Exercise_3 insert(Exercise_3 list, int data) 
     { 
         // Create a new node with given data 
    
@@ -34,24 +39,49 @@ public class LinkedList {
 
             // Insert the new_node at last node 
         // Return the list by head 
+
+        Node new_node = new Node(data); 
+        
+        if(list.head == null) {
+            list.head   = new_node;
+            return list; 
+        }
+
+        Node current = list.head; 
+        while(current.next!= null) { 
+            current = current.next; 
+        }
+
+        current.next = new_node; 
+        return  list;
         
     } 
   
     // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
+    public static void printList(Exercise_3 list) 
     {  
         // Traverse through the LinkedList 
    
             // Print the data at current node 
        
             // Go to next node 
-    } 
+        if(list.head == null) { 
+            System.out.println("List is empty"); 
+        }
+        else
+        {
+        Node current = list.head; 
+        while(current.next!= null && current!=null) { 
+            System.out.print(" -> " + current.data); 
+            current = current.next; 
+        }}
+    }
    
     // Driver code 
     public static void main(String[] args) 
     { 
         /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
+        Exercise_3 list = new Exercise_3(); 
   
         // 
         // ******INSERTION****** 
