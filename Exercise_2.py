89b462ec2270dafd84b6a94fd62ec5e620f1ee4b
icecube-pixel <dornipadu.n@northeastern.edu>
2023-09-26 15:54:45 -0400

    Updated the code for the Precourse assignment

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,4 +1,7 @@
-
+# Time Complexity : O(1) for push, pop
+# Space Complexity : O(1) as list is used to store elements
+# Did this code successfully run on Leetcode : N/A
+# Any problem you faced while coding this : No problem faced
 class Node:
     def __init__(self, data):
        self.data = data
@@ -6,10 +9,27 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head = None
+        self.counter = 0
         
     def push(self, data):
+        new_node = Node(data)
+        if(self.counter == 0):
+            self.head = new_node
+        else:
+            self.head.next = new_node
+            self.head = self.head.next
+        self.counter+=1
         
     def pop(self):
+        if(self.counter == 0):
+            return None
+        temp = self.head
+        self.head = self.head.next
+        self.counter-=1
+        temp.next = None
+        return temp.data
+         
         
 a_stack = Stack()
 while True:
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,7 +3,9 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -11,12 +13,23 @@ class SinglyLinkedList:
         Takes O(1) time.
         """
         self.head = None
+        self.tail = None
+        self.size = 0
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new_node = ListNode(data=data)
+        if(self.head is None):
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = self.tail.next
+        self.size+=1
+
         
     def find(self, key):
         """
@@ -24,9 +37,35 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        temp = self.head
+        while temp:
+            if(key == temp.data):
+                return temp.data
+            temp = temp.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        temp = self.head
+        prev = None
+        while temp:
+            if(key == temp.data):
+                if prev:  # If there's a previous node
+                    prev.next = temp.next
+                    if temp.next is None:  # If the removed node was the tail
+                        self.tail = prev
+                    else:  # If the removed node is the head
+                        self.head = temp.next
+                        if self.head is None:  # If the list becomes empty
+                            self.tail = None
+                temp.next = None
+                self.size -= 1
+                return True
+
+            prev = temp
+            temp = temp.next
+        return False
+
