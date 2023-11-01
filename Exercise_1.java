bceb4903835bdf3b0abcd89f8785eb6426fbb8ab
ruutujaa <ghogare.r@northeastern.edu>
2023-11-01 00:23:23 -0400

    Done precourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -8,27 +8,35 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        return top == -1;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
+        a[++top] = x;
+        return true;
         //Check for stack Overflow
         //Write your code here
     } 
   
     int pop() 
     { 
+        int x = a[top--];
+        return x;
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
     } 
   
     int peek() 
     { 
+        int x = a[top];
+        return x;
         //Write your code here
     } 
 } 
--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,52 @@
+# - push: O(1) time, O(n) space (in the worst case)
+    # - pop: O(1) time, O(1) space
+    # - peek: O(1) time, O(1) space
+    # - size: O(1) time, O(1) space
+    # - show: O(n) time, O(1) space
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
+    def __init__(self):
+        self.items = []
          
-     def isEmpty(self):
+    def isEmpty(self):
+        if len(self.items) == 0:
+            print("stack is empty")
+        else:
+            print("Stack is not empty") 
          
-     def push(self, item):
-         
-     def pop(self):
+    def push(self, item):
+        self.items.append(item)
+          
+    def pop(self):
+        if len(self.items) > 0:
+            popped_elements = self.items.pop()
+            print("popped elements: ", popped_elements)
+        else:
+            print("stack is empty")
         
+    def peek(self):
+        if len(self.items) > 0:
+            top_element = self.items[-1]
+            print("topmost element of stack: ", top_element)
+        else:
+            print("stack is empty")
         
-     def peek(self):
-        
-     def size(self):
+    def size(self):
+        print("Size of stack:", len(self.items))
          
-     def show(self):
+    def show(self):
+        for elements in self.items:
+            print(elements)
          
 
 s = myStack()
 s.push('1')
 s.push('2')
-print(s.pop())
-print(s.show())
+s.push('3')
+s.push('4')
+# print(s.pop())
+# print(s.show())
+s.pop()
+s.peek()
+s.size()
+s.show()
+
