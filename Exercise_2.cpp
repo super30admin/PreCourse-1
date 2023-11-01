bdf0d4043e18b7d31fa9171ce6fcb13e50347505
shizuka1308 <kajol1308@gmail.com>
2023-11-01 12:31:42 -0400

    Done precourse1"

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,3 +1,10 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : This exact problem was not there on Leetcode
+// Any problem you faced while coding this : The <bits/stdc++.h> header is not standard and might not be available on all systems, including my macOS systems. So added all the libraries manually while running the code
+
+
+// Your code here along with comments explaining your approach
 #include <bits/stdc++.h> 
 using namespace std; 
   
@@ -19,21 +26,39 @@ StackNode* newNode(int data)
 int isEmpty(StackNode* root) 
 { 
     //Your code here 
+    return !root;
 } 
   
 void push(StackNode** root, int data) 
 { 
     //Your code here 
+    StackNode* stackNode = newNode(data);
+    stackNode->next = *root;
+    *root = stackNode;
 } 
   
 int pop(StackNode** root) 
 { 
     //Your code here 
+    if(isEmpty(*root)){
+        cout<<"Stack Underflow\n";
+        return -1;
+    }
+    StackNode* temp = *root;
+    *root =(*root)->next;
+    int popped = temp->data;
+    delete temp;
+    return popped;
 } 
   
 int peek(StackNode* root) 
 { 
     //Your code here 
+    if(isEmpty(root)){
+        cout<<"Stack is empty\n";
+        return -1;
+    }
+    return root->data;
 } 
   
 int main() 
--- Exercise_3.cpp
+++ Exercise_3.cpp
@@ -1,3 +1,14 @@
+// Time Complexity :
+// push: O(1)
+// insertAfter: O(1)
+// append: O(n)
+// printList: O(n)
+// Space Complexity for : O(1)
+// Did this code successfully run on Leetcode : This exact problem was not there on Leetcode
+// Any problem you faced while coding this : The <bits/stdc++.h> header is not standard and might not be available on all systems, including my macOS systems. So added all the libraries manually while running the code
+
+
+// Your code here along with comments explaining your approach
 #include <bits/stdc++.h> 
 using namespace std; 
   
@@ -15,12 +26,13 @@ a new node on the front of the list. */
 void push(Node** head_ref, int new_data)  
 {  
     /* 1. allocate node */ 
-  
+    Node* new_node = new Node();
     /* 2. put in the data */  
-  
+    new_node->data = new_data;
     /* 3. Make next of new node as head */ 
-  
+    new_node->next = *head_ref;
     /* 4. move the head to point to the new node */
+    *head_ref = new_node;
 }  
   
 /* Given a node prev_node, insert a new node after the given  
@@ -28,14 +40,22 @@ prev_node */
 void insertAfter(Node* prev_node, int new_data)  
 {  
     /*1. check if the given prev_node is NULL */ 
+    if(prev_node == NULL){
+        cout<<" Prev node can't be NULL";
+        return;
+    }
   
     /* 2. allocate new node */ 
+    Node* new_node = new Node();
   
     /* 3. put in the data */ 
-  
+    new_node->data = new_data;
+
     /* 4. Make next of new node as next of prev_node */
+    new_node->next = prev_node->next;
   
     /* 5. move the next of prev_node as new_node */ 
+    prev_node->next = new_node;
 }  
   
 /* Given a reference (pointer to pointer) to the head  
@@ -43,19 +63,32 @@ of a list and an int, appends a new node at the end */
 void append(Node** head_ref, int new_data)  
 {  
     /* 1. allocate node */ 
+    Node* new_node = new Node();
   
     /* 2. put in the data */ 
-  
+    new_node->data = new_data;
+
     /* 3. This new node is going to be  
     the last node, so make next of  
     it as NULL*/  
+    new_node->next = NULL;
   
     /* 4. If the Linked List is empty, 
     then make the new node as head */
+    if(*head_ref == NULL)
+    {
+        *head_ref = new_node;
+        return;
+    }
   
     /* 5. Else traverse till the last node */
+    Node* last = *head_ref;
+    while(last->next != NULL){
+        last = last->next;
+    }
   
     /* 6. Change the next of last node */ 
+    last->next = new_node;
 }  
   
 // This function prints contents of 
@@ -63,6 +96,11 @@ void append(Node** head_ref, int new_data)
 void printList(Node *node)  
 {  
     //Your code here
+    while(node != NULL){
+        cout<<node->data<<" ";
+        node = node->next;
+    }
+    cout<<endl;
 }  
   
 /* Driver code*/
