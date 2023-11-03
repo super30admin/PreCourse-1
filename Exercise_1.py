ca7f6a8c83c986caa8b1ebac477eded9ac44b41f
Sanchit Pathak <sanchitpathak17@gmail.com>
2023-11-02 22:35:11 -0500

    Submission of PreCourse-1 Exercises

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,36 @@
+# Pushing and Popping from the stack are efficient Big O(1) operations since one can do both operations in constant time by accessing top of the stack parameter.
+
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def __init__(self):
+        self.stack = []                 # Initialize an empty stack
+
+    def isEmpty(self):
+        return len(self.stack) == 0     # If Stack is empty, return True
+
+    def push(self, item):
+        self.stack.append(item)         # Push values to the top of the stack
+
+    def pop(self):
+        if len(self.stack) != 0:
+          self.stack.pop()              # Pop values from the stack in Last In First Out Manner
+          return self.stack
+        else:
+          return "Stack is empty"
+
+    def peek(self):
+        if len(self.stack) != 0:
+          return self.stack[-1]         # Returns last pushed value in the stack
+        else:
+          return "Stack is empty"
+
+    def size(self):
+        return len(self.stack)          # Returns number of values in the stack
+
+    def show(self):
+      if len(self.stack) != 0:
+        return self.stack               # List values in the stack
+      else:
+        return "Stack is empty"
 
 s = myStack()
 s.push('1')
