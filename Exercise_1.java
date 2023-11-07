d403a5a87f08018b85ead6b3a31ffd262e020c05
aditird <aditird98@gmail.com>
2023-11-06 19:42:52 -0700

    exercise_1

--- Exercise_1.java
+++ Exercise_1.java
@@ -7,28 +7,50 @@ class Stack {
   
     boolean isEmpty() 
     { 
+        return top<0;
         //Write your code here 
     } 
 
     Stack() 
     { 
+        top=-1;
         //Initialize your constructor 
     } 
   
     boolean push(int x) 
     { 
+        if(top > (MAX)){
+            System.out.println("Stack overflow");
+            return false;
+        }
+        else{
+             a[++top] = x;
+             return true;
+        }
+       
         //Check for stack Overflow
         //Write your code here
     } 
   
     int pop() 
     { 
+        if(top <-1){
+            System.out.println("Stack underflow");
+            return 0;
+        }
+        else{
+                int x = a[top--];
+                return x;
+        }
+    
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
     } 
   
     int peek() 
     { 
+       int x = a[top];
+        return x;
         //Write your code here
     } 
 } 
@@ -40,7 +62,7 @@ class Main {
         Stack s = new Stack(); 
         s.push(10); 
         s.push(20); 
-        s.push(30); 
+        s.push(30);
         System.out.println(s.pop() + " Popped from stack"); 
     } 
 }
