ac49c4c6f56849b3843d86669eeb4716ad348bc0
Rohith Kandula <rohithkandula97@gmail.com>
2023-11-08 15:33:11 -0800

    Done precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,30 @@
+# Time Complexity : O(1)
+# Space Complexity : O(1)
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
+  def __init__(self):
+    self.lis=[]
+
+  def isEmpty(self):
+    if len(self.lis)==0:
+      return True
          
-     def isEmpty(self):
+  def push(self, item):
+    self.lis.append(item)
          
-     def push(self, item):
-         
-     def pop(self):
+  def pop(self):
+    return self.lis.pop()    
         
+  def peek(self):
+    return self.lis[len(self.lis)-1]
         
-     def peek(self):
-        
-     def size(self):
+  def size(self):
+    return len(self.lis)
          
-     def show(self):
+  def show(self):
+    print(self.lis)
          
 
 s = myStack()
