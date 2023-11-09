fcdf16b09888bd2fbc222b2b92076f40215f6c14
Dimpleben Kanjibhai Patel <patel.dim@northeastern.edu>
2023-11-09 15:20:15 -0500

    Completed Precourse-1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,4 @@
-public class StackAsLinkedList { 
+class StackAsLinkedList {
   
     StackNode root; 
   
@@ -8,31 +8,48 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
-            //Constructor here 
+            //Constructor here
+            this.data = data;
         } 
     } 
     
 	
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+        //Write your code here for the condition if stack is empty.
+        return root == null;
     } 
   
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
+        //Write code to push data to the stack.
+        StackNode node = new StackNode(data);
+        node.next = root;
+        root = node;
     } 
   
     public int pop() 
     { 	
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+	//Also return the popped element
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        StackNode node = root;
+        root = root.next;
+        return node.data;
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return root.data;
     } 
   
 	//Driver code
@@ -47,6 +64,10 @@ public class StackAsLinkedList {
   
         System.out.println(sll.pop() + " popped from stack"); 
   
-        System.out.println("Top element is " + sll.peek()); 
+        System.out.println("Top element is " + sll.peek());
+        sll.pop();
+        sll.pop();
+        sll.pop();
+        sll.peek();
     } 
 } 
--- Exercise_3.java
+++ Exercise_3.java
@@ -2,9 +2,9 @@ import java.io.*;
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList {
   
-    Node head; // head of list 
+     Node head; // head of list
   
     // Linked list Node. 
     // This inner class is made static 
@@ -17,35 +17,50 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            //Write your code here
+            this.data = d;
         } 
     } 
   
     // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
+    public static LinkedList insert(LinkedList list, int data)
     { 
-        // Create a new node with given data 
+        // Create a new node with given data
+        Node node = new Node(data);
    
         // If the Linked List is empty, 
         // then make the new node as head 
-        
+        if(list.head == null) {
+            list.head = node;
+        }
+        else{
             // Else traverse till the last node 
             // and insert the new_node there 
+            Node curr = list.head;
+            while(curr.next != null) curr = curr.next;
 
-            // Insert the new_node at last node 
-        // Return the list by head 
+            // Insert the new_node at last node
+            curr.next = node;
+        }
+        // Return the list by head
+        return list;
         
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
-    {  
+    {
         // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
-    } 
+        Node curr = list.head;
+        while(curr != null){
+            // Print the data at current node
+            System.out.print(curr.data + "--->");
+            // Go to next node
+            curr = curr.next;
+        }
+        System.out.println("null");
+
+    }
    
     // Driver code 
     public static void main(String[] args) 
@@ -55,8 +70,8 @@ public class LinkedList {
   
         // 
         // ******INSERTION****** 
-        // 
-  
+        //
+
         // Insert the values 
         list = insert(list, 1); 
         list = insert(list, 2); 
