7dfce9e8ca591a194a68ab96adf4d3828ecf9fa0
rajathbrgowda <rajathbrgowda@gmail.com>
2023-11-11 19:45:40 -0500

    Done PreCourse-1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,36 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+  def __init__(self):
+    self.items = []
+      
+  def isEmpty(self):
+    return len(self.items) == 0
+      
+  def push(self, item):
+    self.items.append(item)
+    
+  def pop(self):
+    if self.isEmpty() == False:
+        return self.items.pop()
+    else:
+      print("Empty Stack")
+    
+  def peek(self):
+    if self.isEmpty() == False:
+        return self.items[-1]
+    else:
+      print("Empty Stack")
+    
+  def size(self):
+    return len(self.items)
+      
+  def show(self):
+    return self.items
 
 s = myStack()
 s.push('1')
+
 s.push('2')
 print(s.pop())
 print(s.show())
