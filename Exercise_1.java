a8edbf4038a9768388a05bf8351e692dab149437
Nidhi Dewan <nidhidewan@Nidhis-MacBook-Pro.local>
2023-12-06 13:41:36 -0800

    test

--- Exercise_1.java
+++ Exercise_1.java
@@ -2,7 +2,8 @@ class Stack {
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
-    int top; 
+    int top;
+    int test;
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
