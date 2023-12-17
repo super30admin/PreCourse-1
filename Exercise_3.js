07d25ced4ebd5fafa2332114d62f24525e247949
Monisha Sydam <112054217+Monisha3696@users.noreply.github.com>
2023-12-17 15:00:17 -0600

    Update Exercise_3.js

--- Exercise_3.js
+++ Exercise_3.js
@@ -4,46 +4,55 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
-     // Linked list Node.
-     static Node = class {
+
+    // Node class representing a node in the linked list
+    static Node = class {
         constructor(d) {
             this.data = d;
             this.next = null;
         }
     }
-​
-     // Method to insert a new node
-    function insert(list, data) {
-        // Create a new node with given data
-​
-        // If the Linked List is empty,
-        // then make the new node as head
-​
-        // Else traverse till the last node
-        // and insert the new_node there
-​
-        // Insert the new_node at last node
-        // Return the list by head
+
+    // Method to insert a new node at the end of the linked list
+    insert(data) {
+        // Create a new node with the given data
+        const newNode = new LinkedList.Node(data);
+
+        // If the linked list is empty, set the new node as the head
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            // Traverse to the last node and insert the new node there
+            let last = this.head;
+            while (last.next !== null) {
+                last = last.next;
+            }
+
+            // Insert the new node at the last node
+            last.next = newNode;
+        }
     }
-​
-     // Method to print the LinkedList.
-    function printList(list) {
-        // Traverse through the LinkedList
-​
-        // Print the data at current node
-​
-        // Go to next node
+
+    // Method to print the linked list
+    printList() {
+        let current = this.head;
+
+        console.log("Linked List:");
+        while (current !== null) {
+            console.log(current.data);
+            current = current.next;
+        }
     }
 }
-       // Driver code
-       /* Start with the empty list. */
-       let list = new LinkedList();
-​
-        // ******INSERTION******
-        // Insert the values
-        list.insert(list, 1);
-        list.insert(list, 2);
-        list.insert(list, 3);
-        list.insert(list, 4);
-        // Print the LinkedList
-        list.printList(list);
+
+// Driver code
+const list = new LinkedList();
+
+// Insert values into the list
+list.insert(1);
+list.insert(2);
+list.insert(3);
+list.insert(4);
+
+// Print the linked list
+list.printList();
