b9d2c708ea5afd200ab8b4b5335f7075fe6c45b3
Manjunath <manjunathshastryr@gmail.com>
2023-12-18 02:08:56 +0530

    Exercise 1 Completed.

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -12,43 +12,88 @@ class Stack {
 public: 
     int a[MAX]; // Maximum size of Stack 
   
-    Stack() { //Constructor here } 
+    Stack() {
+        top=-1;
+            }
     bool push(int x); 
     int pop(); 
     int peek(); 
     bool isEmpty(); 
 }; 
   
-bool Stack::push(int x) 
-{ 
-    //Your code here
-    //Check Stack overflow as well
-} 
-  
-int Stack::pop() 
-{ 
-    //Your code here
-    //Check Stack Underflow as well 
-} 
-int Stack::peek() 
-{ 
-    //Your code here
-    //Check empty condition too
-} 
-  
-bool Stack::isEmpty() 
-{ 
-    //Your code here 
-} 
+
+bool Stack::push(int x)
+{
+   if (top == MAX-1){
+       cout<<"CANNOT PERFORM PUSH "<<endl;
+       return false;
+   }
+   else {
+       a[++top]=x;
+       cout<<x<<" Pushed Successfully"<<endl;
+       return true;
+   }
+
+}
+
+int Stack::pop()
+{
+    if (isEmpty()){
+        cout<<"CANNOT PERFORM POP"<<endl;
+        return 0;
+    }
+    else{
+        cout<<a[top]<<" popped from Stack"<<endl;
+        top--;
+        return 0;
+    }
+
+}
+int Stack::peek()
+{
+
+    if (isEmpty()){
+        cout<<"CANNOT PEEK AN EMPTY STACK"<<endl;
+        return 0;
+    }
+    else {
+        cout<<"STACK PEEKING:- "<<endl;
+        for ( int i=0; i<=top; i++)
+        {
+            cout<<a[i]<<endl;
+        }
+    }
+    return 0;
+
+}
+
+bool Stack::isEmpty()
+{
+    if (top==-1)
+    {
+        cout<<"Stack is Empty!!!"<<endl;
+        return true;
+    }
+    else {
+        return false;}
+}
+
   
 // Driver program to test above functions 
 int main() 
 { 
-    class Stack s; 
+    class Stack s;
+
     s.push(10); 
     s.push(20); 
-    s.push(30); 
-    cout << s.pop() << " Popped from stack\n"; 
+    s.push(30);
+    s.peek();
+    s.pop();
+    s.pop();
+    s.pop();
+    s.pop();
+    s.isEmpty();
+    s.peek();
   
     return 0; 
 } 
