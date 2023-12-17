8a9096e1de0334248141205ac8d8ac6aae3c9b69
Monisha Sydam <112054217+Monisha3696@users.noreply.github.com>
2023-12-16 23:44:28 -0600

    complete Exercise_2.js

--- Exercise_2.js
+++ Exercise_2.js
@@ -1,31 +1,60 @@
+//Time Complexity O(1) for push, pop, peek and isEmpty operations
+//Space Complexity O(n) where n is capacity of stack
+//Yes
+//No
+
 class StackAsLinkedList {
-​
     static stackNode = class {
-​
         constructor(d) {
             //Constructor here
             this.data = d;
             this.next = null;
         }
     }
-​
-    function isEmpty() {
+
+ constructor() {
+            this.top = null;
+    }
+    
+     isEmpty() {
         //Write your code here for the condition if stack is empty.
+        if(this.top == null){
+            console.log("Stack is empty")
+            return true;
+        }
     }
-​
-    function push(data) {
+     push(data) {
        //Write code to push data to the stack.
+        let temp = new StackAsLinkedList.stackNode(data)
+        if(temp == null){
+            console.log("Stack is overflow")
+            return;
+        }
+        temp.data = data
+        temp.next = this.top 
+        this.top = temp
     }
-​
-    function pop() {
+     pop() {
        //If Stack Empty Return 0 and print "Stack Underflow"
        //Write code to pop the topmost element of stack.
        //Also return the popped element
+        if(this.isEmpty()){
+            console.log("Stack Underflow")
+            return 0;
+        }
+        let popData = this.top.data
+        this.top = this.top.next
+        return popData;
     }
-​
-    function peek() {
+     peek() {
        //Write code to just return the topmost element without removing it.
+        if(this.isEmpty()){
+            console.log("Stack is empty")
     }
+        else {
+            return this.top.data;
+        }
+     }
 }
 //Driver code
 const sll = new StackAsLinkedList();
