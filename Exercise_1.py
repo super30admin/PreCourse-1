0fd416613f4d1405db6759741273b42914ade8ff
singhguri <singhguri5598@gmail.com>
2023-12-17 22:52:15 -0500

    completed pr-1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,41 @@
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    # Please read sample.java file before starting.
+    # Kindly include Time and Space complexity at top of each file
+
+    MAX = 1000
+
+    def __init__(self):
+        self.st = []
+
+    def isEmpty(self):
+        return len(self.st) == 0
+
+    def push(self, item):
+        if self.size() == self.MAX:
+            print("Stack overflow")
+            return False
+
+        self.st.insert(0, item)
+        return self.st
+
+    def pop(self):
+        if len(self.st) == 0:
+            print("Stack Underflow")
+            return 0
+        return self.st.pop(0)
+
+    def peek(self):
+        return self.st[0]
+
+    def size(self):
+        return len(self.st)
+
+    def show(self):
+        return self.st
+
 
 s = myStack()
-s.push('1')
-s.push('2')
+s.push("1")
+s.push("2")
 print(s.pop())
 print(s.show())
--- Exercise_3.py
+++ Exercise_3.py
@@ -2,8 +2,12 @@ class ListNode:
     """
     A node in a singly-linked list.
     """
+
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +21,57 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        node = ListNode(data)
+
+        if self.head is None:
+            self.head = node
+            return
+
+        cur_node = self.head
+        while cur_node.next:
+            cur_node = cur_node.next
+        cur_node.next = node
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        cur_node = self.head
+
+        if cur_node is None:
+            return
+
+        while cur_node is not None:
+            if cur_node.data == key:
+                return cur_node
+            cur_node = cur_node.next
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        cur_node = self.head
+        prev_node = None
+
+        if cur_node is None:
+            return
+
+        while cur_node is not None:
+            if cur_node.data == key:
+                break
+            prev_node = cur_node
+            cur_node = cur_node.next
+
+        prev_node.next = cur_node.next
+
+    def show(self):
+        cur_node = self.head
+        while cur_node is not None:
+            if cur_node.next is None:
+                print(cur_node.data)
+            else:
+                print(cur_node.data, end="->")
+            cur_node = cur_node.next
