73f2cd50d0e6282f69e2df92dad6f2ef2160a6e7
raunak2095 <raunakkhithani@gmail.com>
2023-12-18 07:35:44 +0000

    Precourse1

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,54 +1,74 @@
-#include <bits/stdc++.h> 
-  
-using namespace std; 
-  
-#define MAX 1000 
-  
-class Stack {
-  //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-    int top; 
-  
-public: 
-    int a[MAX]; // Maximum size of Stack 
-  
-    Stack() { //Constructor here } 
-    bool push(int x); 
-    int pop(); 
-    int peek(); 
-    bool isEmpty(); 
-}; 
-  
-bool Stack::push(int x) 
-{ 
-    //Your code here
-    //Check Stack overflow as well
-} 
-  
-int Stack::pop() 
-{ 
-    //Your code here
-    //Check Stack Underflow as well 
-} 
-int Stack::peek() 
-{ 
-    //Your code here
-    //Check empty condition too
-} 
-  
-bool Stack::isEmpty() 
-{ 
-    //Your code here 
-} 
-  
-// Driver program to test above functions 
-int main() 
-{ 
-    class Stack s; 
-    s.push(10); 
-    s.push(20); 
-    s.push(30); 
-    cout << s.pop() << " Popped from stack\n"; 
-  
-    return 0; 
-} 
+// Time Complexity :
+// push(int x); pop(); peek(); isEmpty() = O(1)
+// Space Complexity :O(MAX)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this : did not find the exact same problem on leetcode
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+
+class Stack
+{
+    // Please read sample.java file before starting.
+    // Kindly include Time and Space complexity at top of each file
+    int top;
+    // top will keep track of the first element of stack
+    // before insert in stack top++ then insert
+public:
+    int a[MAX]; // Maximum size of Stack
+
+    Stack()
+    {
+        top = -1; // Constructor here
+    }
+    bool push(int x);
+    int pop();
+    int peek();
+    bool isEmpty();
+};
+
+bool Stack::push(int x)
+{
+    if (top == MAX - 1)
+    {
+        cout << " Stack Underflow" << endl;
+        return false;
+    }
+    a[++top] = x;
+    return true;
+}
+// will be returning 0 if stack empty
+int Stack::pop()
+{
+    if (top == -1)
+    {
+        cout << " Stack Underflow" << endl;
+        return 0;
+    }
+    return a[top--];
+}
+int Stack::peek()
+{
+    if (top == -1)
+    {
+        cout << " Stack Underflow" << endl;
+        return 0;
+    }
+    return a[top];
+}
+// will be returning 0 if stack empty
+bool Stack::isEmpty()
+{
+    return (top == -1);
+}
+
+// Driver program to test above functions
+int main()
+{
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    cout << s.pop() << " Popped from stack\n";
+    return 0;
+}
--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,3 +1,8 @@
+// Time Complexity :
+// push(int x); pop(); peek(); isEmpty() = O(1)
+// Space Complexity :O(n) where n is the number of elements in the stack
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this : did not find the exact same problem on leetcode
 #include <bits/stdc++.h> 
 using namespace std; 
   
@@ -18,23 +23,49 @@ StackNode* newNode(int data)
   
 int isEmpty(StackNode* root) 
 { 
-    //Your code here 
+    return (root==nullptr);
 } 
   
 void push(StackNode** root, int data) 
 { 
-    //Your code here 
+    StackNode* temp = newNode(data);
+    temp->next = *root;
+    *root = temp; 
 } 
   
 int pop(StackNode** root) 
 { 
-    //Your code here 
+    if(*root==nullptr)
+    {
+        cout << " Stack Underflow" << endl;
+        return 0;
+    }
+    StackNode* toDelete = *root;
+    int val = toDelete->data;
+    *root = toDelete->next;
+    delete toDelete;
+    return val;
 } 
   
 int peek(StackNode* root) 
 { 
-    //Your code here 
-} 
+    if(root==nullptr)
+    {
+        cout << " Stack Underflow" << endl;
+        return 0;
+    }
+    return root->data;
+}
+void eraseAll(StackNode** root)
+{
+    StackNode* current = *root;
+
+    while(current!=nullptr)
+    {
+        pop(&current);
+    }
+    *root = nullptr;
+}
   
 int main() 
 { 
@@ -47,6 +78,7 @@ int main()
     cout << pop(&root) << " popped from stack\n"; 
   
     cout << "Top element is " << peek(root) << endl; 
-  
+    //to release all the memory allocated
+    eraseAll(&root);
     return 0; 
 } 
