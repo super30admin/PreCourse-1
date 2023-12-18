3511e8d85fb7bdeba13cd5e17b4bf1f21fab4cab
15apk2000 <kumarpavan543@gmail.com>
2023-12-18 15:19:03 -0800

    Done Precourse 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,40 @@
+# Time Complexity: O(1) for all operations except show() which is O(n)
+# Space Complexity: O(n) where n is the number of elements in the stack
+
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+      self.stack = []
          
      def isEmpty(self):
-         
+
+      return len(self.stack)  == 0
+          
      def push(self, item):
+
+      self.stack.append(item)
          
      def pop(self):
-        
+
+      if not self.isEmpty():
+        return self.stack.pop()
+
+      return "Stack is Empty"        
         
      def peek(self):
+      if not self.isEmpty():
+        return self.stack[-1]
+
+      return "Stack is Empty"  
+       
         
      def size(self):
+      return len(self.stack)
          
      def show(self):
+      return self.stack
          
 
 s = myStack()
--- Exercise_2.py
+++ Exercise_2.py
@@ -3,13 +3,33 @@ class Node:
     def __init__(self, data):
        self.data = data
        self.next = None
+
+"""
+Time Complexity: O(1) for both push and pop operations.
+Space Complexity: O(1) for both push and pop operations.
+"""
  
 class Stack:
     def __init__(self):
-        
+        self.head = None
+
     def push(self, data):
+        new_node = Node(data)
+
+        new_node.next = self.head
+
+        self.head = new_node
+
         
     def pop(self):
+        if self.head is None:
+            return None
+        
+        pop_data = self.head.data
+
+        self.head = self.head.next
+
+        return pop_data
         
 a_stack = Stack()
 while True:
