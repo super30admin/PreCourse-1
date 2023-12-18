7900b14b4785d3293b98a354a357d039ad7270dc
SathwikReddyGK <sathwikreddy.gk@gmail.com>
2023-12-18 11:06:49 -0500

    testing gitpush for precourse 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,6 +2,7 @@ class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+          # Testing the pull request
          
      def isEmpty(self):
          
