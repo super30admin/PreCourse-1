6226a7870b73c372241e304020d0b5c21aa56a40
VRwtcall <WT.VR.Project@gmail.com>
2023-12-18 21:47:48 -0600

    pre-course_1 done

--- Exercise_2.py
+++ Exercise_2.py
@@ -6,10 +6,27 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.top = None
         
     def push(self, data):
+        #adding an item to the top of the stack so it gets added to the beginning of the Linked List
+        if self.top == None:
+            self.top = Node(data)
+        else:
+            new_node = Node(data)
+            new_node.next = self.top #making the new node the first node
+            self.top = new_node
         
     def pop(self):
+        #Removing and returning the top element from the stack
+        if self.top == None:
+            return None
+        else:
+            # remove the head node and make the preceding one the next node
+            pop_item = self.top
+            self.top = self.top.next
+            pop_item.next = None
+            return pop_item.data
         
 a_stack = Stack()
 while True:
