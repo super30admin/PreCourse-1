57ce34b6b6f01f0d833aea1000642f04c6f58d47
My Nguyen <my@vnguyen.net>
2023-12-18 17:21:27 -0800

    Exercise 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,31 @@
+# Time Complexity :
+# Space Complexity:
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+        self.stack = []
          
      def isEmpty(self):
-         
+        return len(self.stack) == 0
+
      def push(self, item):
+        self.stack.append(item)
          
      def pop(self):
-        
-        
+        if self.isEmpty():
+           return
+
+        return self.stack.pop()
+
      def peek(self):
+        return self.stack[len(self.stack) - 1]
         
      def size(self):
+        return len(self.stack)
          
      def show(self):
+        print(self.stack)
          
 
 s = myStack()
