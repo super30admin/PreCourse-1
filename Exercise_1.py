587ead7cbe364a1b0cca41d64e54ffede60d5240
Vd1299 <vishvesh.dumbre@gmail.com>
2023-12-19 15:01:38 -0800

    Completed Precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,50 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+  #TC = O(1)
+  #SC = O(n)
 
-s = myStack()
+    def __init__(self, total_size):
+      self.stack = []
+      self.total_size = total_size
+
+    def isEmpty(self):
+      return len(self.stack) == 0
+     
+    def isFull(self):
+      return len(self.stack) == self.total_size
+     
+    def push(self, item):
+      if self.isFull():
+        print("Stack Overflow")
+      else: 
+        self.stack.append(item)
+        print(item + " pushed to stack ")
+
+    def pop(self):
+      if self.isEmpty():
+        print("Stack underflow")
+        return 0
+      return self.stack.pop()
+     
+    def peek(self):
+        if self.isEmpty():
+          return 0
+        return self.stack[len(self.stack) - 1]
+        
+    def size(self):
+      return len(self.stack)
+     
+    def show(self):
+      return self.stack
+
+s = myStack(2)
 s.push('1')
 s.push('2')
+s.push('3')
+print(s.peek())
+print(s.size())
+print(s.pop())
+print(s.pop())
 print(s.pop())
 print(s.show())
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,13 @@
+#TC = O(n)
+#SC = O(n)
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +22,14 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        newNode = ListNode(data)
+        if not self.head:
+            self.head = newNode
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = newNode
         
     def find(self, key):
         """
@@ -24,9 +37,61 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        curr = self.head
+        while curr:
+            if curr.data == key:
+                return curr
+            curr = curr.next
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if not self.head:
+            return
+        if self.head.data == key:
+            self.head = self.head.next
+            return
+        curr = self.head
+        while curr.next:
+            if curr.next.data == key:
+                curr.next = curr.next.next
+                return
+            curr = curr.next
+
+if __name__ == "__main__":
+    linked_list = SinglyLinkedList()
+
+    linked_list.append(1)
+    linked_list.append(2)
+    linked_list.append(3)
+
+    print("Original Linked List:")
+    curr = linked_list.head
+    while curr:
+        print(curr.data, end=" ")
+        curr = curr.next
+    print()
+
+    #Find and print an element in the linked list
+    key_to_find = 3
+    found_node = linked_list.find(key_to_find)
+    if found_node:
+        print(f"Element {key_to_find} found in the linked list.")
+    else:
+        print(f"Element {key_to_find} not found in the linked list.")
+
+    #Remove an element from the linked list
+    key_to_remove = 2
+    linked_list.remove(key_to_remove)
+
+    #Print the updated linked list after removal
+    print("Linked List after removal:")
+    current = linked_list.head
+    while current:
+        print(current.data, end=" ")
+        current = current.next
+    print()
+
