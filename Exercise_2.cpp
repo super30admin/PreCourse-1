2f6461b1ae7c3125120d41b8455b42c61dfdace3
God7wyn <jamesgodwynj@gmail.com>
2023-12-20 04:30:23 -0500

    Completed Precourse 1

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,52 +1,94 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-  
-// A structure to represent a stack 
-class StackNode { 
-public: 
-    int data; 
-    StackNode* next; 
-}; 
-  
-StackNode* newNode(int data) 
-{ 
-    StackNode* stackNode = new StackNode(); 
-    stackNode->data = data; 
-    stackNode->next = NULL; 
-    return stackNode; 
-} 
-  
-int isEmpty(StackNode* root) 
-{ 
-    //Your code here 
-} 
-  
-void push(StackNode** root, int data) 
-{ 
-    //Your code here 
-} 
-  
-int pop(StackNode** root) 
-{ 
-    //Your code here 
-} 
-  
-int peek(StackNode* root) 
-{ 
-    //Your code here 
-} 
-  
-int main() 
-{ 
-    StackNode* root = NULL; 
-  
-    push(&root, 10); 
-    push(&root, 20); 
-    push(&root, 30); 
-  
-    cout << pop(&root) << " popped from stack\n"; 
-  
-    cout << "Top element is " << peek(root) << endl; 
-  
-    return 0; 
-} 
+/*
+----------------------------------------
+* Stack Implementation using Linked List
+----------------------------------------
+
+* Time Complexity:
+*  - Push : O(1)
+*  - Pop : O(1)
+*  - Peek : O(1)
+*  - isEmpty : O(1)
+
+* Space Complexity: O(n)
+
+Did this code successfully run on Leetcode : NA
+Any problem you faced while coding this : NO
+
+*/
+
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+// A structure to represent a stack
+class StackNode {
+public:
+    int data;
+    StackNode* next;
+};
+
+StackNode* newNode(int data) {
+    StackNode* stackNode = new StackNode();
+    stackNode->data = data;
+    stackNode->next = NULL;
+    return stackNode;
+}
+
+int isEmpty(StackNode* root) {
+    //Your code here
+    return root == nullptr;
+}
+
+int push(StackNode** root, int data) {
+    //Your code here
+    StackNode* node = newNode(data);
+    node->next = *root;
+    *root = node;
+    return (*root)->data;
+}
+
+int pop(StackNode** root) {
+    //Your code here
+    if (isEmpty(*root)) {
+        cout << "[Error: Stack is empty !!!] ---> ";
+        return INT_MIN;
+    }
+    StackNode* temp = *root;
+    *root = (*root)->next;
+    int popped = temp->data;
+    delete temp;
+    return popped;
+}
+
+int peek(StackNode* root) {
+    //Your code here
+    if(isEmpty(root)) {
+        cout << "[Error: Stack is empty !!!] ---> ";
+        return INT_MIN;
+    }
+    return root->data;
+}
+
+int main() {
+    StackNode* root = NULL;
+
+    // * Add elements to the stack
+    cout << "Push: " << push(&root, 10) << endl;
+    cout << "Push: " << push(&root, 20) << endl;
+    cout << "Push: " << push(&root, 30) << endl;
+
+    // * See the top element from stack
+    cout << "Peek " << peek(root) << endl;
+
+    // * Remove elements from the stack
+    cout << "Pop: " << pop(&root) << endl;
+    cout << "Pop: " << pop(&root) << endl;
+    cout << "Pop: " << pop(&root) << endl;
+
+    // ! When stack is empty
+    cout << "Pop: " << pop(&root) << endl;
+    cout << "Peek: " << peek(root) << endl;
+
+    return 0;
+}
--- Exercise_3.cpp
+++ Exercise_3.cpp
@@ -1,80 +1,115 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-  
-// A linked list node (changes) 
-class Node  
-{  
-    public: 
-    int data;  
-    Node *next;  
-};  
-  
-/* Given a reference (pointer to pointer) 
-to the head of a list and an int, inserts 
+/*
+------------------------------------
+* Singly Linked List Implementation
+------------------------------------
+
+* Time Complexity:
+*  - Push : O(1)
+*  - Insert After : O(1)
+*  - Append : O(n)
+*  - print List : O(n)
+
+* Space Complexity: O(n)
+
+Did this code successfully run on Leetcode : NA
+Any problem you faced while coding this : NO
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// A linked list node (changes)
+class Node
+{
+    public:
+    int data;
+    Node *next;
+};
+
+/* Given a reference (pointer to pointer)
+to the head of a list and an int, inserts
 a new node on the front of the list. */
-void push(Node** head_ref, int new_data)  
-{  
-    /* 1. allocate node */ 
-  
-    /* 2. put in the data */  
-  
-    /* 3. Make next of new node as head */ 
-  
+void push(Node** head_ref, int new_data)
+{
+    /* 1. allocate node */
+    Node* node = new Node;
+    /* 2. put in the data */
+    node->data = new_data;
+    /* 3. Make next of new node as head */
+    node->next = *head_ref;
     /* 4. move the head to point to the new node */
-}  
-  
-/* Given a node prev_node, insert a new node after the given  
+    *head_ref = node;
+}
+
+/* Given a node prev_node, insert a new node after the given
 prev_node */
-void insertAfter(Node* prev_node, int new_data)  
-{  
-    /*1. check if the given prev_node is NULL */ 
-  
-    /* 2. allocate new node */ 
-  
-    /* 3. put in the data */ 
-  
+void insertAfter(Node* prev_node, int new_data)
+{
+    /*1. check if the given prev_node is NULL */
+    if (prev_node == nullptr) return;
+    /* 2. allocate new node */
+    Node* node = new Node;
+    /* 3. put in the data */
+    node->data = new_data;
     /* 4. Make next of new node as next of prev_node */
-  
-    /* 5. move the next of prev_node as new_node */ 
-}  
-  
-/* Given a reference (pointer to pointer) to the head  
+    node->next = prev_node->next;
+    /* 5. move the next of prev_node as new_node */
+    prev_node->next = node;
+}
+
+/* Given a reference (pointer to pointer) to the head
 of a list and an int, appends a new node at the end */
-void append(Node** head_ref, int new_data)  
-{  
-    /* 1. allocate node */ 
-  
-    /* 2. put in the data */ 
-  
-    /* 3. This new node is going to be  
-    the last node, so make next of  
-    it as NULL*/  
-  
-    /* 4. If the Linked List is empty, 
+void append(Node** head_ref, int new_data)
+{
+    /* 1. allocate node */
+    Node* node = new Node;
+    /* 2. put in the data */
+    node->data = new_data;
+    /* 3. This new node is going to be
+    the last node, so make next of
+    it as NULL*/
+    node->next = nullptr;
+    /* 4. If the Linked List is empty,
     then make the new node as head */
-  
+    if (*head_ref == nullptr) {
+        *head_ref = node;
+        return;
+    }
     /* 5. Else traverse till the last node */
-  
-    /* 6. Change the next of last node */ 
-}  
-  
-// This function prints contents of 
-// linked list starting from head  
-void printList(Node *node)  
-{  
+    Node* last = *head_ref;
+    while (last->next) {
+        last = last->next;
+    }
+    /* 6. Change the next of last node */
+    last->next = node;
+}
+
+// This function prints contents of
+// linked list starting from head
+void printList(Node *node)
+{
     //Your code here
-}  
-  
+    cout << "[";
+    while (node) {
+        cout << node->data;
+        if (node->next != nullptr) cout << " -> ";
+        node = node->next;
+    }
+    cout << "]" << endl;
+}
+
 /* Driver code*/
-int main()  
-{  
-    Node* head = NULL;   
-    append(&head, 6);  
-    push(&head, 7);  
-    push(&head, 1);    
-    append(&head, 4);    
-    insertAfter(head->next, 8);  
-    cout<<"Created Linked list is: ";  
-    printList(head);  
-    return 0;  
-}  
+int main()
+{
+    Node* head = NULL;
+    append(&head, 6);
+    push(&head, 7);
+    push(&head, 1);
+    append(&head, 4);
+    insertAfter(head->next, 8);
+    cout<<"Created Linked list is: ";
+    printList(head);
+    return 0;
+}
