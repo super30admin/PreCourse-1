3a792606c9f5d25e8a774f97d5c9df94e48db1d2
vedantdalvi <dalvi.v@northeastern.edu>
2023-12-27 22:53:54 -0500

    precourse design

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,8 +2,12 @@ class ListNode:
     """
     A node in a singly-linked list.
     """
+
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +21,41 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        new_node = ListNode(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        current = self.head
+        while current:
+            if current.data == key:
+                return current
+            current = current.next
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        current = self.head
+        previous = None
+        while current:
+            if current.data == key:
+                if previous:
+                    previous.next = current.next
+                else:
+                    self.head = current.next
+                return
+            previous = current
+            current = current.next
