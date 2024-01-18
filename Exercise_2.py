d3e3a783c312173a64f8e5dde46e4d42dab4246f
Anish <anishsingh0106@gmail.com>
2024-01-18 11:49:06 -0500

    Precourse 1 complete in python

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,32 +1,54 @@
+# Time Complexity : all operations O(1)
+# Space Complexity : O(n) where n is the maximum size of the list at any point
+# Did this code successfully run on Leetcode : Not available on leetcode
+# Any problem you faced while coding this : No
+
+
+# Your code here along with comments explaining your approach
+
 
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
+
 class Stack:
     def __init__(self):
-        
+        self.head = None
+
     def push(self, data):
-        
+        newNode = Node(data)
+        # if head already exists
+        if self.head:
+            newNode.next = self.head
+        self.head = newNode
+
     def pop(self):
-        
+        # nothing to pop; head == None
+        if not self.head:
+            return
+        poppedValue = self.head.data
+        self.head = self.head.next
+        return poppedValue
+
+
 a_stack = Stack()
 while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    print('push <value>')
-    print('pop')
-    print('quit')
-    do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    print("push <value>")
+    print("pop")
+    print("quit")
+    do = input("What would you like to do? ").split()
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
-    if operation == 'push':
+    if operation == "push":
         a_stack.push(int(do[1]))
-    elif operation == 'pop':
+    elif operation == "pop":
         popped = a_stack.pop()
         if popped is None:
-            print('Stack is empty.')
+            print("Stack is empty.")
         else:
-            print('Popped value: ', int(popped))
-    elif operation == 'quit':
+            print("Popped value: ", int(popped))
+    elif operation == "quit":
         break
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,9 +1,22 @@
+# Time Complexity : O(n) for all operations
+# Space Complexity : O(n) where n is the maximum size of the list at any point
+# Did this code successfully run on Leetcode : Not available on leetcode
+# Any problem you faced while coding this : No
+
+
+# Your code here along with comments explaining your approach
+
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
+
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +30,43 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        newNode = ListNode(data)
+        # empty list
+        if not self.head:
+            self.head = newNode
+            return
+
+        temp = self.head
+        # appending in the end
+        while temp.next != None:
+            temp = temp.next
+        temp.next = newNode
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        temp = self.head
+        while temp != None:
+            if temp.data == key:
+                return temp
+            temp = temp.next
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head.data == key:
+            self.head = self.head.next
+            return
+        temp = self.head
+
+        while temp.next != None:
+            if temp.next.data == key:
+                temp.next = temp.next.next
+                return
+            temp = temp.next
