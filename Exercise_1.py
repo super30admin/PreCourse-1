bd595e326563dfc17f6e72e5f3fb88d7c643fec5
Akash Ajnadkar <akashajnadkar163@gmail.com>
2024-01-19 00:42:08 +0530

    Python problems solved

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,59 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
+
+  # Time Complexity
+  # push: O(1)
+  # pop: O(1)
+  # peek: O(1)
+  # size: O(1)
+  # show: O(n)
+
+  # Space Complexity
+  # Here we use a list to implement the stack so the space complexity will O(n).
+  # If we consider any additional space being utilized for the operations, then
+  # push: O(1)
+  # pop: O(1)
+  # peek: O(1)
+  # show: O(1)
+
+  def __init__(self):
+    # initiaize the stack as a list
+    self.stack = [] 
          
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
+  def isEmpty(self):
+    # check length of the list. If this length is greater than 0, we return false (stack is not empty) else we return True (stack is empty)
+    if len(self.stack) > 0:
+      return False
+    else:
+      return True
+
+
+  def push(self, item):
+    # Insert an element at top of stack, i.e. we insert the element at the end of the list.
+    # Python provides an inbuilt method that inserts an element at end of the list.
+    self.stack.append(item)
+
+  def pop(self):
+    # Here we take out the top element in the stack, i.e we remove the last element from the list and return it.
+    # First check if the stack is empty or not. Then proceed with the pop operation. Python provides an inbuilt method of pop to remove the last element from the list.
+    if not self.isEmpty():
+      return self.stack.pop()
+    else:
+      return '0'
         
-        
-     def peek(self):
-        
-     def size(self):
+  def peek(self):
+    # Return the top element on the stack. Using -1 we address the last element in a list .
+    if not self.isEmpty():
+      return self.stack[-1]
+    
+  def size(self):
+    #  Return the size of the stack.
+    return len(self.stack)
          
-     def show(self):
+  def show(self):
+    # Displays all the elements in a stack.
+    print(self.stack)
          
 
 s = myStack()
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,13 @@
+# Time Complexity
+
+# push: In the case of push operation using a LinkedList, we add a new node at the end of the list. Hence the Time Complexity will be O(n).
+# pop: In the case of pop operation, we delete the last node in LinkedList. We traverse the entire all the nodes and then delete the last node. Therefore the Time Complexity will be O(n).
+
+# Space Complexity
+
+# The space complexity of a LinkedList is O(n).
+
+
 
 class Node:
     def __init__(self, data):
@@ -6,10 +16,30 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.head = Node(0) # Create a Null Node as Head.
+    
     def push(self, data):
+        # Add an element at the top of the stack. Since this is a LinkedList implementation, we insert a node at the end of the LinkedList.
+        ptr = self.head
+        while ptr.next!=None: # Continue till we reach the end of the LinkedList
+            ptr = ptr.next # Move the pointer to the next node
+        newNode = Node(data) # Create a new Node with data.
+        newNode.next = None # Last node of a LinkedList point to NULL
+        ptr.next = newNode # The current last node points to the new last Node 
         
     def pop(self):
+        # Take out the last top element from the stack. In the case of LinkedList implementation, we delete the last node of the LinkedList.
+        if self.head.next is None: #Check if the LinkedList is empty
+            return None
+        else:
+            ptr = self.head
+            while ptr.next.next != None: # Continue till we reach the second last node
+                ptr = ptr.next
+            ptr2 = ptr.next # Create a pointer that points to the last node
+            ptr.next = None # Make second last node point to Null to break link to last node
+            ptr2_dat = ptr2.data
+            del ptr2 # Delete the last node
+            return ptr2_dat # return the data of the last node
         
 a_stack = Stack()
 while True:
