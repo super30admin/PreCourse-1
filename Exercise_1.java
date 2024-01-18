a9448681d1aba83ccc55998cb8e1db451a4c2f16
iarshtejay <arshtejay@gmail.com>
2024-01-17 20:36:11 -0400

    feat: Excercise 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,43 @@
 class Stack { 
-    //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+    // Time Complexity: O(1) for all operations
+    // Space Complexity: O(MAX)
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        return top==-1;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top=-1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        if(top==MAX-1)    return false;
+        a[++top]=x;
+        return true;
     } 
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return a[top--];
     } 
   
     int peek() 
-    { 
-        //Write your code here
+    {
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+            return -1;
+        }
+        return a[top];
     } 
 } 
   
@@ -42,5 +50,5 @@ class Main {
         s.push(20); 
         s.push(30); 
         System.out.println(s.pop() + " Popped from stack"); 
-    } 
+    }
 }
