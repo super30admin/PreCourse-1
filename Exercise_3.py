b3135c1b9d96dabb94385396834f944b94b67547
charan_reddy <charanreddymailbox@gmail.com>
2024-01-23 10:40:04 -0800

    Precourse 1 submissions

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,15 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new_node = ListNode(data, None)
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        current.next = new_node
+
         
     def find(self, key):
         """
@@ -24,9 +35,23 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        curr_node = self.head
+        while curr_node is not None:
+            if curr_node.data == key:
+                return curr_node
+            curr_node = curr_node.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head is None: return None
+        if self.head.data == key: self.head = self.head.next 
+        curr_node = self.head
+        while curr_node is not None and curr_node.next.data != key:
+            curr_node = curr_node.next
+        if curr_node.next is not None:
+            curr_node.next = curr_node.next.next
+
