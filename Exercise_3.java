f9347a06f31a831d76865a3217e479b46a2faf51
Mythreye Pasula <mythreyepasula@Mythreyes-Air.lan>
2024-01-24 10:08:18 -0500

    Done Precourse1

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,3 +1,6 @@
+//space complexity - O(n), time complexity - O(n)
+//for insertion operation : if linkedlist is empty it creates a new node and assigns the paused value to it. If not we have to traverse till the end of list and attach new node at the end.
+// for printing - traverse the linkedlist until end and print each node's data while iterating.
 import java.io.*; 
   
 // Java program to implement 
@@ -17,6 +20,8 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
+            this.data = d;
+            this.next = null;
             //Write your code here 
         } 
     } 
@@ -25,26 +30,45 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
+        Node newNode = new Node(data);
+
    
         // If the Linked List is empty, 
         // then make the new node as head 
+        if(list.head == null){
+            list.head = newNode;
+        }
         
             // Else traverse till the last node 
             // and insert the new_node there 
 
             // Insert the new_node at last node 
         // Return the list by head 
+        else{
+            Node last = list.head;
+            while(last.next!= null) {
+                last = last.next;
+            }
+            last.next = newNode;
+        }
+        return list;
         
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
+        Node current = list.head;
         // Traverse through the LinkedList 
    
             // Print the data at current node 
        
             // Go to next node 
+        while(current != null){
+            System.out.println(current.data +" ");
+            current = current.next;
+        }
+        System.out.println();
     } 
    
     // Driver code 
