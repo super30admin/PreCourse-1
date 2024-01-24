bdf7f80f1712a296bd7322bf2a8ed7ae1bc1d09d
Sriram Akella <Sriram.Akella@gmail.com>
2024-01-24 15:32:58 -0700

    PreCouse 1 Compplete - Sriram

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,32 +1,55 @@
+# Time Complexity : O(1)
+# Space Complexity : O(n)
+"""
+push, pop have a time complexity of O(1)
+space complexity should be O(n)
+"""
+
+# Did this code successfully run on Leetcode :
+# Any problem you faced while coding this : none
+
 
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
+
 class Stack:
     def __init__(self):
-        
+        self.top = None
+
     def push(self, data):
-        
+        new_node = Node(data)
+        new_node.next = self.top
+        self.top = new_node
+
     def pop(self):
-        
+        if self.top == None:
+            return None
+        else:
+            node_popped = self.top
+            self.top = self.top.next
+            node_popped.next = None
+            return node_popped.data
+
+
 a_stack = Stack()
 while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    print('push <value>')
-    print('pop')
-    print('quit')
-    do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    print("push <value>")
+    print("pop")
+    print("quit")
+    do = input("What would you like to do? ").split()
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
-    if operation == 'push':
+    if operation == "push":
         a_stack.push(int(do[1]))
-    elif operation == 'pop':
+    elif operation == "pop":
         popped = a_stack.pop()
         if popped is None:
-            print('Stack is empty.')
+            print("Stack is empty.")
         else:
-            print('Popped value: ', int(popped))
-    elif operation == 'quit':
+            print("Popped value: ", int(popped))
+    elif operation == "quit":
         break
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,32 +1,65 @@
 class ListNode:
-    """
-    A node in a singly-linked list.
-    """
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
+
 class SinglyLinkedList:
     def __init__(self):
-        """
-        Create a new singly-linked list.
-        Takes O(1) time.
-        """
         self.head = None
+        self.tail = None
+        self.count = 0
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
+        This method will help uso have O(1) time complexity for appending
         """
-        
+        new_node = ListNode(data)
+        if self.count == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.count += 1
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        if self.count == 0:
+            return
+        else:
+            test_node = self.head
+            countdown = self.count
+            while countdown > 0:
+                if test_node.data == key:
+                    return test_node
+                else:
+                    test_node = test_node.next
+                    countdown -= 1
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.count == 0:
+            return
+        else:
+            current_node = self.head
+            if current_node.data == key:
+                self.head = current_node.next
+                current_node.next = None
+                return
+            prev_node = None
+            while current_node.data != key:
+                prev_node = current_node
+                current_node = current_node.next
+            prev_node.next = current_node.next
+            if current_node == self.tail:
+                self.tail = prev_node
