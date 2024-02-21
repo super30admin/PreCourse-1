e6de90ea512bd2334099793b8447629390349454
ParvathiRPai <parvathirpai@outlook.com>
2024-02-21 13:09:54 -0800

    precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,46 @@
 class myStack:
-  #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
+  # Time and space complexity is O(N)
+  def __init__(self):
+    self.list = []
+    self.maxsize = 1000
+
+  def isEmpty(self):
+    if self.list == []:
+      return True
+    else:
+      return False
+  
+  def isFull(self):
+    if len(self.list) == self.maxsize:
+      return True
+    else:
+      return False
+    
+  def push(self, item):
+    if self.isFull() == False:
+      self.list.append(item)
+    else:
+     return "The stack is full"
+
+  def pop(self):
+    if self.isEmpty():
+      return "The stack is empty"
+    else:
+      return self.list.pop()
+
+  def peek(self):
+    if self.isEmpty():
+      return "There are no elements in the list"
+    else:
+      return self.list[len(self.list) -1]
+
+  def size(self):
+    return len(self.list)
+
+  def show(self):
+    for i in self.list:
+      print(i)
          
 
 s = myStack()
