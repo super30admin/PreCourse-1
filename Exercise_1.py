484fdbc873d2abd5a809dc08138e6693f7e63c60
Sneha Sorde <snehassorde@gmail.com>
2024-02-21 09:31:47 -0600

    Done PreCourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,38 @@
+# Time Complexity : O(1)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+# Your code here along with comments explaining your approach
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+      self.data = []
+
+     #Checking if stack is Empty or not, by checking the length of stack    
      def isEmpty(self):
-         
+        return len(self.data) == 0
+     
+      # adding element, by using append operation    
      def push(self, item):
-         
+        self.data.append(item)
+
+      # remove element, by using pop operation  
      def pop(self):
+        if self.isEmpty():
+           return
+        return self.data.pop()
         
-        
+      #find top element  
      def peek(self):
-        
+        return self.data[-1]
+
+      #size   
      def size(self):
-         
+        return len(self.data)
+
+      #show element   
      def show(self):
+        return self.data
          
 
 s = myStack()
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,15 +1,39 @@
+# Time Complexity : O(1)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode :
+# Any problem you faced while coding this : Taking time for implementation 
 
+# Your code here along with comments explaining your approach
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, next):
        self.data = data
        self.next = None
  
 class Stack:
     def __init__(self):
-        
+        self.head = None
+        self.size = 0
+
+    #add element, this method adds a new element to the top of stack   
+    #self.head, a new node is created with the provided data and next pointer of the new node is set to current
+    #head of stack. Then, head of stack is updated to point to the newly created node, adding new element to top of stack
     def push(self, data):
-        
+        self.head = Node(data, self.head)
+        self.size = self.size + 1
+    
+    #checking if stack is empty or not
+    def isEmpty(self):
+        return self.size == 0
+
+    #remove element and return top element 
+    #to remove the element, 'head' of stack is updated to point to the next node in the stack  
     def pop(self):
+        if self.isEmpty():
+            return 
+        value = self.head.data
+        self.head = self.head.next
+        self.size = self.size - 1
+        return value
         
 a_stack = Stack()
 while True:
