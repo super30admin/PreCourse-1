2d05cd2d63e64a86ed8ae865a5d5ac3a1fd18daf
[Rutvi Patel] <[rp925@njit.edu]>
2024-02-21 22:27:43 -0500

    Done Precourse 1

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,13 @@
+// If the list is empty, insertion takes O(1) time.
+// If the list is not empty, insertion takes O(n) time.
+// Printing the linked list requires traversing through all the nodes once, taking O(n) time.
+// The space complexity of a linked list is O(n). 
+
 import java.io.*; 
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList { 
   
     Node head; // head of list 
   
@@ -18,6 +23,8 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            data = d;
+            next = null;
         } 
     } 
   
@@ -25,26 +32,40 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
+        Node new_node = new Node(data);
    
         // If the Linked List is empty, 
         // then make the new node as head 
-        
+        if (list.head == null) {
+            list.head = new_node;
+        } else {
             // Else traverse till the last node 
             // and insert the new_node there 
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            } 
+
 
             // Insert the new_node at last node 
+            last.next = new_node;
+        }
         // Return the list by head 
-        
+        return list; 
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
         // Traverse through the LinkedList 
-   
+        Node currNode = list.head;
+        while (currNode != null) {
             // Print the data at current node 
+            System.out.print(currNode.data + " "); 
        
             // Go to next node 
+            currNode = currNode.next;
+        }
     } 
    
     // Driver code 
