56b3099239f30be40ab3ebfb18725330b21430dc
Mohan <113561544+Mohanmanjunatha@users.noreply.github.com>
2024-02-23 15:04:10 -0500

    final

--- Exercise_1.java
+++ Exercise_1.java
@@ -7,36 +7,66 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        if top==-1;
-        return true
-        else 
-        return false;
+        if (top<0)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
         //Write your code here 
     } 
 
     Stack() 
     { 
+        top=-1;
         //Initialize your constructor 
     } 
   
     boolean push(int x) 
     { 
 
-        if 
+        if(top>=MAX-1){
+            System.out.println("stack overfloe");
+            return false;
+        }
+        else{
+            top=top+1;
+            a[top]=x;
+            System.out.println(x+ "pushed to stack");
+            return true;
+        }
         //Check for stack Overflow
         //Write your code here
     } 
   
     int pop() 
     { 
-        if top==1;
+        if (top==-1){
+            return false;
 
+        }
+        else{
+           int x=a[top];
+           top=top-1;
+           
+        return x;
+
+
+        }
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
     } 
   
     int peek() 
     { 
+        if(top<0){
+            Ststem.out.println("Stack Underflow");
+            return 0;
+        }
+        else{
+            return a[top];
+        }
         //Write your code here
     } 
 } 
