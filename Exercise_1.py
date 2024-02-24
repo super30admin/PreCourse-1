f4d71524bf961fafbed40c948fd6a854f8b7fdef
Vraj Patel <vcp37@scarletmail.rutgers.edu>
2024-02-24 14:28:51 -0500

    Precouse 1 done in python

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,33 @@
+# Time Complexity : O(1)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode :
+# Any problem you faced while coding this :
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
+    def __init__(self):
+      self.stack = []
          
-     def isEmpty(self):
+    def isEmpty(self):
+      return len(self.stack) == 0
          
-     def push(self, item):
+    def push(self, item):
+      self.stack.append(item)
          
-     def pop(self):
+    def pop(self):
+      self.stack.pop()
         
+    def peek(self):
+      if self.stack:
+        return self.stack[-1]
+      else:
+        return None
         
-     def peek(self):
-        
-     def size(self):
+    def size(self):
+      return len(self.stack)
          
-     def show(self):
+    def show(self):
+      return self.stack
          
 
 s = myStack()
@@ -22,3 +35,6 @@ s.push('1')
 s.push('2')
 print(s.pop())
 print(s.show())
+print(s.isEmpty())
+print(s.peek())
+print(s.size())
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = None
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,18 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new_node = ListNode(data)
+        
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        last = self.head
+        while(last.next):
+            last = last.next
+            
+        last.next = new_node
+            
         
     def find(self, key):
         """
@@ -24,9 +38,35 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        curr = self.head
+        
+        while curr != None:
+            if curr.data == key:
+                return curr
+            curr = curr.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        curr = self.head
+        
+        if curr is not None:
+            if curr.data == key:
+                self.head = curr.next
+                curr = None
+                return
+        
+        while curr is not None:
+            if curr.data == key:
+                break
+            prev = curr
+            curr = curr.next
+            
+        if curr == None:
+            return
+        
+        prev.next = curr.next
+        curr = None
