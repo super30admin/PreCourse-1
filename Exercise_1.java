c156a2d4b8a019c2c2d71b39c2040a4b272e32c9
vsls0224 <vsls0224@gmail.com>
2024-02-25 17:28:37 -0800

    Precourse-1 completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -4,32 +4,48 @@ class Stack {
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
-  
+    int start;
+    int end;
     boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+    {
+        if (end == 0)
+            return true;
+        else
+            return false;
+    }
 
     Stack() 
-    { 
-        //Initialize your constructor 
+    {
+        //Initialize your constructor
+        start = 0;
+        end = 0;
+
     } 
   
     boolean push(int x) 
-    { 
+    {
         //Check for stack Overflow
-        //Write your code here
+        if (end == MAX) {
+            return false;
+        }
+        a[end] = x;
+        end++;
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return a[--end];
     } 
   
     int peek() 
-    { 
-        //Write your code here
+    {
+        return a[end-1];
     } 
 } 
   
@@ -41,6 +57,14 @@ class Main {
         s.push(10); 
         s.push(20); 
         s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack");
+        s.push(40);
+        s.push(50);
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.peek() + " Peek from stack");
+        System.out.println(s.pop() + " Popped from stack");
+        s.push(60);
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.peek() + " Peek from stack");
     } 
 }
