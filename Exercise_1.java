d51ef4f32115161dbcd970de18d4fd2912a961d1
VishalAnandamani <vishalbangalore26@gmail.com>
2024-02-27 13:43:54 -0700

    Precourse Excercise_1 completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,9 +1,11 @@
 class Stack { 
-	// Time Complexity : 
-	// Space Complexity :
-	// Did this code successfully run on Leetcode :
+	// Time Complexity :   All methods O(1)
+	// Space Complexity : Individual Methods does not take up any space, however, array used to maintain the stack has O(1000)
+	//                    At any given time there exists the array of size 1000.   
+	// Did this code successfully run on LeetCode : No
 	// Any problem you faced while coding this :
-
+		// using index -1 to determine if the stack is empty or not seemed a little "Hack-y".
+		// wanted to use null instead, since I'm new to Java, not sure how to use integer type with null. 
 
 	// Your code here along with comments explaining your approach
     static final int MAX = 1000; 
@@ -12,13 +14,13 @@ class Stack {
   
     boolean isEmpty() 
     { 
-    	return top == null;
+    	return top == -1; // Use -1 to indicate that the stack is empty since 
     } 
 
     Stack() 
     { 
         //Initialize your constructor
-    	top = null;
+    	top = -1; // Initialize -1 to indicate that the stack is empty. 
     } 
   
     boolean push(int x) 
@@ -29,40 +31,34 @@ class Stack {
     		return false;
     	}
     	else {
-        	if(top == null) top = -1;
-        	top++;
-    		a[top] = x;
+        	// Top Maintains the current top, we will need to push to the next index
+            // works even for empty stacks since the empty stacks are denoted by -1.
+    		a[++top] = x;
     		return true;
     	}
     } 
   
     int pop() 
     {
-    	if(top == null) {
+        //If empty return 0 and print " Stack Underflow"
+    	if(top == -1) {
     		System.out.println("Stack is Empty: Element cannot be poped");
     		return 0;
     	}
     	else {
-    		int res = a[top];
-    		if(top == 0) top = null;
-    		else top--;
-    		return res;
-    		
+    		return a[top--];
     	}
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
     } 
   
     int peek() 
     { 
-    	if(top == null) {
+    	if(top == -1) {
     		System.out.println("Stack is Empty");
     		return 0;
     	}
     	else {
     		return a[top];
     	}
-        //Write your code here
     } 
 } 
   
