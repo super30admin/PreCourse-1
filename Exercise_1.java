ca6046c0f24d51a4382517d04d3c6a8ebec7330f
VishalAnandamani <vishalbangalore26@gmail.com>
2024-02-27 13:41:44 -0700

    Precourse Excercise_1 completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,34 +1,67 @@
 class Stack { 
-    //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+	// Time Complexity : 
+	// Space Complexity :
+	// Did this code successfully run on Leetcode :
+	// Any problem you faced while coding this :
+
+
+	// Your code here along with comments explaining your approach
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+    	return top == null;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+    	top = null;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
-        //Write your code here
+    	if(top == MAX) {
+    		System.out.println("Size Exceeded: Element cannot be pushed");
+    		return false;
+    	}
+    	else {
+        	if(top == null) top = -1;
+        	top++;
+    		a[top] = x;
+    		return true;
+    	}
     } 
   
     int pop() 
-    { 
+    {
+    	if(top == null) {
+    		System.out.println("Stack is Empty: Element cannot be poped");
+    		return 0;
+    	}
+    	else {
+    		int res = a[top];
+    		if(top == 0) top = null;
+    		else top--;
+    		return res;
+    		
+    	}
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
     } 
   
     int peek() 
     { 
+    	if(top == null) {
+    		System.out.println("Stack is Empty");
+    		return 0;
+    	}
+    	else {
+    		return a[top];
+    	}
         //Write your code here
     } 
 } 
