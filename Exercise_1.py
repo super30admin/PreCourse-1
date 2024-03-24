66ea848088d76c80dccdc475d3c240bb16dd27a7
Sharwari <sharwarigothe5@gmail.com>
2024-03-23 21:29:09 -0400

    Commiting Precourse 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,19 +2,52 @@ class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+        # Time Complexity: O(1)
+        # Space Complexity: O(1)
+          self.stack = []
          
      def isEmpty(self):
+        # Time Complexity: O(1)
+        # Space Complexity: O(1)
+        
+        if len(self.stack) == 0:
+          return True
+        else:
+          return False
          
      def push(self, item):
+        # Time Complexity: O(1)
+        # Space Complexity: O(1)
+        
+        self.stack.append(item)
          
      def pop(self):
-        
+        # Time Complexity: O(1)
+        # Space Complexity: O(1)
+          
+        item = self.stack.pop()
+        return item
         
      def peek(self):
+        # Time Complexity: O(1)
+        # Space Complexity: O(1)
+
+        item = self.stack[-1]
+        return item
         
      def size(self):
+        # Time Complexity: O(1)
+        # Space Complexity: O(1)
+
+        s = len(self.stack)
+        return s
          
      def show(self):
+        # Time Complexity: O(N)
+        # Space Complexity: O(N)
+
+        s = self.stack
+        return s
          
 
 s = myStack()
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,10 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        l1 = self.head
+        while(l1.next != None):
+            l1 = l1.next
+        l1.next = ListNode(data)
         
     def find(self, key):
         """
@@ -24,9 +30,31 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        l1 = self.head
+        while(l1.next) != None:
+            if l1.data == key:
+                return l1
+            else:
+                l1 = l1.next
+        return None
+
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head.next == None:
+            return ('No Key found')
+
+        l1 = self.head
+        r1 = self.head.next
+        while (r1):
+            if r1.data == key:
+                l1.next = r1.next
+                r1.next = None
+                return self.head.next
+            else:
+                r1 = r1.next
+                l1 = l1.next
+        return ('No key found')
